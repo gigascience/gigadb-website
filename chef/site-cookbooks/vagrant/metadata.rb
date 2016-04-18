a69467bcada6c2b574b8289ba8308f12e8fe5a0d
pli888 <peter@gigasciencejournal.com>
2016-04-18 17:31:22 +0800

    Add metadata.rb file for the vagrant cookbook

---
+++ chef/site-cookbooks/vagrant/metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Cogini"
+maintainer_email "phu@cogini.com"
+license          "All rights reserved"
+description      "Installs/Configures vagrant"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+name "vagrant"
+
+depends 'gigadb'
+depends 'nginx'
