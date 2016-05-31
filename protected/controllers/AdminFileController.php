8ba3766ab90a758e8434959408a47f483dcfe956
xiaosizhe <xiaosizheme@gmail.com>
2016-05-31 16:08:34 +0800

    fixes admin page update file(sample id) bug #issue18

--- protected/controllers/AdminFileController.php
+++ protected/controllers/AdminFileController.php
@@ -428,7 +428,8 @@ class AdminFileController extends Controller
                     } else {
                         $fs = $fs[0];
                     }
-                    $fs->sample_id = $_POST['File']['sample_name'];
+                    $sample = Sample::model()->findbyAttributes(array('name'=>$_POST['File']['sample_name']));
+                    $fs->sample_id = $sample->id;
                     $fs->file_id = $model->id;
                     if( $fs->sample_id !='None'&& $fs->sample_id !="" )
                     {
