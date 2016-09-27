939de6766ad40d8356b8420e1ce39a48377c6811
xiaosizhe <xiaosizheme@gmail.com>
2016-09-27 15:26:20 +0800

    fixes the conflict chef

--- protected/models/File.php
+++ protected/models/File.php
@@ -422,21 +422,48 @@ class File extends MyActiveRecord
 
      public function getallsample($id){
         
-      $sql="select sample.* from sample,file_sample,file where sample.id=file_sample.sample_id and file_sample.file_id=file.id and file.id=$id";
+       $sql="select sample.* from sample,file_sample,file where sample.id=file_sample.sample_id and file_sample.file_id=file.id and file.id=$id";
       $samples= Sample::model()->findAllBySql($sql);
+      $num = count($samples);
+      $short="";
       $ret = "";
       $first = true;
-      foreach ($samples as $sample) {
-
+      $second = true;
+      $flag=0;
+        foreach ($samples as $sample) {
+        if($flag >= 1)
+        {
+            break;
+        }
         if ($first === true) {
             $first = false;
+        } else {
+            $short .= ', ';
+        }
+        $short .= $sample->name;
+        $flag++;
+    }
+      foreach ($samples as $sample) {
+
+        if ($second === true) {
+            $second = false;
         } else {
             $ret .= ', ';
         }
         $ret .= $sample->name;
     }
+    if($num>3)
+    {
+        return <<<HTML
+		<span class="js-short-$this->id">$short</span>
+        		<span class="js-long-$this->id" style="display: none;">$ret</span>
+                <a href='#' class='js-desc' data='$this->id'>+</a>
+HTML;
+    }else
 
-    return $ret;
+    return <<<HTML
+        		<span class="js-long-$this->id">$ret</span>
+HTML;
     }
     
     public function getNameHtml() {
