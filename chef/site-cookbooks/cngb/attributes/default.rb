ea4c554b749ca2dc140fd09172bb11d8df8bbd44
pli888 <peter@gigasciencejournal.com>
2016-11-15 12:00:02 +0800

    Create cngb cookbook
    This cookbook is for CNGB to install GigaDB in their data center.

---
+++ chef/site-cookbooks/cngb/attributes/default.rb
@@ -0,0 +1,7 @@
+default[:gigadb][:db][:backup_folder] = '/vagrant/db_backups/'
+
+default[:iptables][:iptables_sysconfig][:IPTABLES_SAVE_ON_STOP] = 'yes'
+
+default[:ssh][:enable_password] = 'no'
+
+
