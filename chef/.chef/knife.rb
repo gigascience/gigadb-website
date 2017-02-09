5da24c6938af07e453723bc75b839c2becce72bd
pli888 <peter@gigasciencejournal.com>
2017-02-09 17:31:38 +0800

    Use vim as the editor

--- chef/.chef/knife.rb
+++ chef/.chef/knife.rb
@@ -3,8 +3,9 @@ node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
 data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
+encrypted_data_bag_secret "encrypted_data_bag_secret"
 
 knife[:berkshelf_path] = "cookbooks"
 Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
 
+knife[:editor] = "vim"
