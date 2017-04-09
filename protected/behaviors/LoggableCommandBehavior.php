2a8649acbfcd6d7d354a7a609e3d85b6636493dd
Rija Ménagé <gitlab@rijam.ml1.net>
2017-04-09 14:42:04 +0800

    Added timestamp in the log using Hong Kong time (for #67 and #66)

--- protected/behaviors/LoggableCommandBehavior.php
+++ protected/behaviors/LoggableCommandBehavior.php
@@ -10,6 +10,7 @@ Class LoggableCommandBehavior extends CBehavior
         set_error_handler( array($this, "error") );
         register_shutdown_function( array($this, "handle_fatal_error") );
 
+
         parent::init();
 
     }
@@ -18,15 +19,19 @@ Class LoggableCommandBehavior extends CBehavior
         //Yii::log($message, "info","application.commands." . $command) ;
 
         $job_id = isset($this->owner->current_job) ? "(" . $this->owner->current_job['id'] . ")" : false ;
+        date_default_timezone_set('Asia/Hong_Kong');
+        $time_index = date(DATE_ATOM);
 
-        echo "[". get_class($this->owner) ."] * $job_id $message " . PHP_EOL ;
+        echo "[". $time_index ."]" . "[". get_class($this->owner) ."] * $job_id $message " . PHP_EOL ;
     }
 
     public function error($num, $str, $file, $line, $context = null) {
 
         $job_id = isset($this->owner->current_job) ? "(" . $this->owner->current_job['id'] . ")" : false ;
+        date_default_timezone_set('Asia/Hong_Kong');
+        $time_index = date(DATE_ATOM);
 
-        echo "[". get_class($this->owner) ."] * $job_id A PHP Error $num occured in " . $file  ."[" . $line . "]: $str". PHP_EOL;
+        echo "[". $time_index ."]" . "[". get_class($this->owner) ."] * $job_id A PHP Error $num occured in " . $file  ."[" . $line . "]: $str". PHP_EOL;
     }
 
     public function handle_fatal_error() {
