f512590b9a05bbbd3eafb3437f84eb0e29f121a9
pli888 <peter@gigasciencejournal.com>
2017-11-15 13:52:48 +0800

    Use Postgresql version 9.1
    Was previously using 8.4 which is the native version of Postgres
    that Centos 6.7 uses. Version 9.1 is enables by adding the
    postgresql.org centos 9.1 repo. Need to exclude Centos Postgres
    packages into Chef build.

--- chef/site-cookbooks/vagrant/recipes/redhat.rb
+++ chef/site-cookbooks/vagrant/recipes/redhat.rb
@@ -14,3 +14,13 @@ directory assets_dir do
     recursive true
     mode '0777'
 end
+
+# To do: exclude Centos 6 postgres packages
+bash 'Add postgres package repositories' do
+		user 'root'
+		cwd '/tmp'
+    code <<-EOH
+        curl -O https://download.postgresql.org/pub/repos/yum/9.1/redhat/rhel-6-x86_64/pgdg-centos91-9.1-6.noarch.rpm
+        rpm -ivh pgdg*
+    EOH
+end
