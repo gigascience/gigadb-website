6aa2c430a6943c0a3919a1edaf806a40bb0ac4eb
Rija Menage <rija+git@cinecinetique.com>
2018-01-06 19:10:42 +0800

    Affiliate login (#64): login with ORCID
    Implemented a revoke endpoint to allow a user of the current session to revoke gigadb authorisation for his/her oauth provider directly from gigadb web site. Also useful for clearing state in automated tests. Only work for ORCID for now.
    The revocation is idempotent and for unsupported provider it justs logout the user.
    
    All implemented test scenarios (with the @ok tags) are now passing.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -44,6 +44,37 @@ class UserIdentity extends CUserIdentity {
     {
         return $this->_id;
     }
+
+    public static function revoke_token() {  # revoke affilate login token for Google and ORCID
+        $provider = null;
+        $token = null;
+
+        if(isset($_SESSION['affiliate_login'])) {
+            $provider = isset($_SESSION['affiliate_login']['provider']) ? $_SESSION['affiliate_login']['provider'] : null ;
+            $token = isset($_SESSION['affiliate_login']['token']) ? $_SESSION['affiliate_login']['token'] : null ;
+        }
+
+        if ("Orcid" == $provider) {
+            $service_url = 'https://sandbox.orcid.org/oauth/revoke';  #TODO: make orcid coming form config
+            $curl = curl_init($service_url);
+            $curl_post_data = array(
+                "client_id" => Yii::app()->getModules()['opauth']['opauthParams']["Strategy"]["Orcid"]["client_id"],
+                "client_secret" => Yii::app()->getModules()['opauth']['opauthParams']["Strategy"]["Orcid"]["client_secret"],
+                "token" => $token,
+            );
+            $headers = array(
+                'Accept: application/json',
+            );
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($curl_post_data) );
+            $http_response = curl_exec($curl);
+            $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE); 
+            curl_close($curl);
+
+        }
+    }
     
 
 }
