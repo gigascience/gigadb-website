eb7840134d3875a0c9fec59081928c4c8921cc44
Rija Menage <rija+git@cinecinetique.com>
2018-01-08 00:01:58 +0800

    Affiliate Login (#64): speed up the tests
    12mn -> 10mn -> 9m36

---
+++ sql/reset.sql
@@ -0,0 +1,5 @@
+SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname='gigadb';
+drop database gigadb ;
+create database gigadb owner gigadb;
+\connect gigadb;
+\i /vagrant/sql/gigadb_testdata.sql
