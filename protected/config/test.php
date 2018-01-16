49732ba51f7a31c4f4c150f7d0b6621db4026a4a
Rija Menage <rija+git@cinecinetique.com>
2018-01-16 15:34:47 +0800

    Author-names(#81): unit tests setup correction
    I didn't realised the test.php config for Yii was also pulling main.php and therefore overriding the $dbConfig variable causing the unit tests to use the mai database. Corrected it by loadding the test database config into a distinct variable.

--- protected/config/test.php
+++ protected/config/test.php
@@ -1,7 +1,7 @@
 <?php
 
 // load database config
-$dbConfig = json_decode(file_get_contents(dirname(__FILE__).'/db_test.json'), true);
+$testdb = json_decode(file_get_contents(dirname(__FILE__).'/db_test.json'), true);
 
 // enable multibyte unicode aware string functions. Only needed for PHP < 5.6. Requires php-mbstring module.
 ini_set('mbstring.internal_encoding','UTF-8');
@@ -15,7 +15,7 @@ return CMap::mergeArray(
 			),
 			 // uncomment the following to provide test database connection
 			'db'=>array(
-				'connectionString'=>"pgsql:dbname={$dbConfig['database']};host={$dbConfig['host']}",
+				'connectionString'=>"pgsql:dbname={$testdb['database']};host={$testdb['host']}",
 			),
 
 		),
