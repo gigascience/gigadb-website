811e7a3a0e13365f7ede696984d0f862f49902cc
Peter Li <peterli@Peters-Mac-Pro.local>
2018-01-31 10:29:35 +0800

    Remove docker directory
    This folder is not required. Running the gigadb-website as a
    Docker application relied on yii2-laradock which is used as a
    git submodule.

--- docker/docker-chef-solo/run-chef.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Make sure we're not confused by old, incompletely-shutdown httpd
-# context after restarting the container.  httpd won't start correctly
-# if it thinks it is already running.
-#rm -rf /run/httpd/*
-
-exec /usr/bin/chef-solo -l info -c /vagrant/docker/docker-chef-solo/solo.rb -j /vagrant/dokcer/docker-chef-solo/node.json
