a0d995eb8474797e1feafbc0926ad0c9c8bfc7cc
pli888 <peter@gigasciencejournal.com>
2018-02-28 16:47:56 +0800

    Upgrade git version
    The vagrant build for gigadb-website is broken due to Yii not
    being able to be downloaded from GitHub. The problem seems
    to be caused by git command line tool being too old in Centos 6.7.
    When git is upgraded to version 2.x, Yii is downloadable from
    GitHub.

--- chef/site-cookbooks/vagrant/recipes/redhat.rb
+++ chef/site-cookbooks/vagrant/recipes/redhat.rb
@@ -7,6 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
+bash 'upgrade_git_version' do
+	user 'root'
+	cwd '/tmp'
+	code <<-EOH
+	# Install WANDisco repo package
+	sudo yum install -y http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
+	# Install latest version of Git 2.x
+	sudo yum install -y git
+	# Fix SSL problem
+	sudo yum update -y nss curl libcurl
+	EOH
+end
+
 assets_dir = "/vagrant/assets"
 
 directory assets_dir do
