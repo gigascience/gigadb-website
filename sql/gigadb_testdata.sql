bb9f4b9fd25136d74a983d68455b20e188d5f4ef
xiaosizhe <xiaosizheme@gmail.com>
2018-08-17 17:00:12 +0800

    update the database sql file: add the curation_log table and update the dataset table

--- sql/gigadb_testdata.sql
+++ sql/gigadb_testdata.sql
@@ -286,6 +286,8 @@ CREATE TABLE dataset (
     id integer NOT NULL,
     submitter_id integer NOT NULL,
     image_id integer,
+    curator_id integer,
+    manuscript_id character varying(50),
     identifier character varying(32) NOT NULL,
     title character varying(300) NOT NULL,
     description text DEFAULT ''::text NOT NULL,
@@ -1856,6 +1858,24 @@ CREATE TABLE sample_rel (
 ALTER TABLE public.sample_rel OWNER TO gigadb;
 
 --
+-- Name: curation_log; Type: TABLE; Schema: public; Owner: gigadb; Tablespace: 
+--
+
+CREATE TABLE curation_log (
+    id integer NOT NULL,
+    dataset_id integer NOT NULL,
+    CREATION_DATE date, 
+    CREATED_BY varchar(20),
+    LAST_MODIFIED_DATE date, 
+    LAST_MODIFIED_BY varchar(20),
+    ACTION varchar(100), 
+    COMMENTS varchar(200)
+);
+
+
+ALTER TABLE public.curation_log OWNER TO gigadb;
+
+--
 -- Name: sample_rel_id_seq; Type: SEQUENCE; Schema: public; Owner: gigadb
 --
 
@@ -1874,6 +1894,26 @@ ALTER TABLE public.sample_rel_id_seq OWNER TO gigadb;
 --
 
 ALTER SEQUENCE sample_rel_id_seq OWNED BY sample_rel.id;
+
+--
+-- Name: curation_log_id_seq; Type: SEQUENCE; Schema: public; Owner: gigadb
+--
+
+CREATE SEQUENCE curation_log_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.curation_log_id_seq OWNER TO gigadb;
+
+--
+-- Name: sample_rel_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: gigadb
+--
+
+ALTER SEQUENCE curation_log_id_seq OWNED BY curation_log.id;
 
 
 --
@@ -3089,6 +3129,13 @@ ALTER TABLE ONLY dataset_session
 ALTER TABLE ONLY dataset_type
     ADD CONSTRAINT dataset_type_pkey PRIMARY KEY (id);
 
+--
+-- Name: curation_log_pkey; Type: CONSTRAINT; Schema: public; Owner: gigadb; Tablespace: 
+--
+
+ALTER TABLE ONLY curation_log
+    ADD CONSTRAINT curation_log_pkey PRIMARY KEY (id);
+
 
 --
 -- Name: email_unique; Type: CONSTRAINT; Schema: public; Owner: gigadb; Tablespace: 
@@ -3439,6 +3486,19 @@ CREATE UNIQUE INDEX identifier_idx ON dataset USING btree (identifier);
 ALTER TABLE ONLY "AuthAssignment"
     ADD CONSTRAINT "AuthAssignment_itemname_fkey" FOREIGN KEY (itemname) REFERENCES "AuthItem"(name) ON UPDATE CASCADE ON DELETE CASCADE;
 
+--
+-- Name: curation_log_fkey; Type: FK CONSTRAINT; Schema: public; Owner: gigadb
+--
+
+ALTER TABLE ONLY "curation_log"
+    ADD CONSTRAINT "curation_log_dataset_id_fkey" FOREIGN KEY (dataset_id) REFERENCES "dataset"(id) ON UPDATE NO ACTION ON DELETE CASCADE;
+
+--
+-- Name: dataset_fkey; Type: FK CONSTRAINT; Schema: public; Owner: gigadb
+--
+
+ALTER TABLE ONLY "dataset"
+    ADD CONSTRAINT "dataset_curator_id_fkey" FOREIGN KEY (curator_id) REFERENCES "gigadb_user"(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 --
 -- Name: alternative_identifiers_extdb_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: gigadb
