7be9e1511288a431a69257c4d6cb0940106c8daa
Rija Menage <rija+git@cinecinetique.com>
2018-10-11 21:11:45 +0800

    Fix autoload issue with functional test supporting classes
    The supporting classes don't use namespace
    but I've erroneously associated namespaces to their path in composer.json
    Which prevent the autoloader to correctly find them
    (which I've workaround temporarily by manually require_once them in PHPUnit's bootstrap.php)

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -7,11 +7,5 @@ $composer=dirname(__FILE__)."/../../vendor/autoload.php";
 
 require_once($yiit);
 require_once($composer);
-require_once(dirname(__FILE__)."/support/BrowserSignInSteps.php");
-require_once(dirname(__FILE__)."/support/BrowserPageSteps.php");
-require_once(dirname(__FILE__)."/support/BrowserFormSteps.php");
-require_once(dirname(__FILE__)."/support/BrowserFindSteps.php");
-require_once(dirname(__FILE__)."/support/FunctionalTesting.php");
-require_once(dirname(__FILE__)."/support/CommonDataProviders.php");
 Yii::$enableIncludePath = false;
 Yii::createWebApplication($config);
