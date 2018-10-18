8af880e4602d1b415e4d39d02b71dfd03233d01a
Rija Menage <rija+git@cinecinetique.com>
2018-10-18 15:42:21 +0800

    Tweak autoloading for performance and correctness

--- ops/scripts/webapp_setup.sh
+++ ops/scripts/webapp_setup.sh
@@ -2,7 +2,21 @@
 
 set -e -u -x
 
-composer install
+source "./.env"
+
+if [ $COMPOSE_FILE == "ops/deployment/docker-compose.yml:ops/deployment/docker-compose.production.yml" ];then
+	echo "* Production mode *"
+	composer install -a
+elif [ $COMPOSE_FILE == "ops/deployment/docker-compose.yml:ops/deployment/docker-compose.ci.yml" ];then
+	echo "* CI mode *"
+	composer install -a
+elif [ $COMPOSE_FILE == "ops/deployment/docker-compose.yml:ops/deployment/docker-compose.prof.yml" ];then
+	echo "* Profiling mode *"
+	composer install -a
+else
+	echo "* Development mode *"
+	composer install -o
+fi
 
 ./protected/yiic lesscompiler
 
