707f072ab9380bd6afa5620e0f577f394eb4c1c8
Rija Menage <rija+git@cinecinetique.com>
2018-11-01 17:21:14 +0800

    Add certbot to the web container and configure ssl with LetsEncrypt

--- ops/scripts/webapp_setup.sh
+++ ops/scripts/webapp_setup.sh
@@ -5,11 +5,12 @@ set -e -u
 source "./.env"
 
 if [ $GIGADB_ENV == "dev" ];then
-	echo "* Development mode *"
 	composer install -o
-else
-	echo "* Production mode *"
+	cp ops/configuration/nginx-conf/le.dev.ini /etc/letsencrypt/cli.ini
+elif [ $GIGADB_ENV == "CI" ];then
 	composer install -a
+elif [ $GIGADB_ENV == "staging" ];then
+	cp ops/configuration/nginx-conf/le.staging.ini /etc/letsencrypt/cli.ini
 fi
 
 ./protected/yiic lesscompiler
