028b739a7b89950766ef1eadbd34c494df81b4a6
Rija Menage <rija+git@cinecinetique.com>
2018-11-14 18:52:12 +0800

    Run webapp_setup on deploy staging job but not on the deployment target
    the css/site.css needs to be baked in the container

--- ops/scripts/webapp_setup.sh
+++ ops/scripts/webapp_setup.sh
@@ -8,6 +8,8 @@ if [ $GIGADB_ENV == "dev" ];then
 	composer install -o
 elif [ $GIGADB_ENV == "CI" ];then
 	composer install -a
+elif [ $GIGADB_ENV == "staging" ];then
+	composer install -a --no-dev
 fi
 
 ./protected/yiic lesscompiler
