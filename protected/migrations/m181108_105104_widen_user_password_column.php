1f6e541475d60de160b485bd313e783ef63ff510
Rija Menage <rija+git@cinecinetique.com>
2018-11-28 17:29:07 +0800

    Add explanation to the password column migration and allow reverting

--- protected/migrations/m181108_105104_widen_user_password_column.php
+++ protected/migrations/m181108_105104_widen_user_password_column.php
@@ -2,15 +2,24 @@
 
 class m181108_105104_widen_user_password_column extends CDbMigration
 {
+	/**
+	 * the password column need to be bigger to accomdate the new algorithm for password hashes
+	 *
+	 */
 	public function up()
 	{
 		$this->alterColumn("gigadb_user","password","varchar(128)");
 	}
 
+	/**
+	 * because column size is trivial and relatively inconsequential if it's bigger than necessary
+	 * and because new password hashes are bigger than the previous size
+	 * the down() migration will keep the size unchanged to make potential reverting of migrations smoother
+	 * and not throw exceptions unenecessarily
+	 */
 	public function down()
 	{
-		echo "m181108_105104_widen_user_password_column does not support migration down.\n";
-		return false;
+		$this->alterColumn("gigadb_user","password","varchar(128)");
 	}
 
 	/*
