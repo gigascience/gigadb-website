f79fc4bf0d4b7dddc73f995ddb62a2b2132740bb
Rija Menage <rija+git@cinecinetique.com>
2019-04-19 21:51:12 +0200

    Enable filewatcher hook to update database table with file metadata(wip)

---
+++ db_schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE file (
+    id serial PRIMARY KEY,
+    doi_suffix integer NOT NULL,
+    name character varying(100) NOT NULL,
+    size bigint NOT NULL,
+    status int DEFAULT 1, -- 1: upload, 2: private, 3: public
+    location character varying(200),
+    description text DEFAULT ''::text,
+    initial_md5 text DEFAULT ''::text,
+    format text DEFAULT 'Unknown'::text,
+    data_type text DEFAULT 'Unknown'::text,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_file_modtime BEFORE UPDATE ON file
+FOR EACH ROW EXECUTE PROCEDURE  update_modified_column();
