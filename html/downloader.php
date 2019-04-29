d7c0ce2823924d0f5513385751fc452fc3d4efa0
Rija Menage <rija+git@cinecinetique.com>
2019-04-29 21:17:49 +0800

    Add php-fpm app server for dynamic prototype scripts in php (wip)
    after fixing the issue with configuring nginx and php-fpm containers,
    we now have working dynamic Uppy-based uploader (uploader.php) and mockup (downloader.php)
    prototype as well as a dashboard (index.php) that link to them.

--- html/downloader.php
+++ html/downloader.php
@@ -1,21 +1,15 @@
 <?php
-
-	require 'lib/db.php';
-
-    $appconfig = parse_ini_file("/var/appconfig.ini");
-    $web_endpoint = $appconfig["web_endpoint"];
-
 	$thisurl = parse_url($_SERVER['REQUEST_URI']);
 	parse_str($thisurl["query"], $params);
 	// echo $params["d"];
 
 	/**
 	 * the file class
-	 * id | dataset_id | name | size | status | location | description | initial_md5 | format | data_type | created_at | updated_at
+	 * id | doi_suffix | name | size | status | location | description | initial_md5 | format | data_type | created_at | updated_at
 	 */
 	class File {
 	    public $id;
-	    public $dataset_id;
+	    public $doi_suffix;
 	    public $name;
 	    public $size;
 	    public $status;
@@ -27,6 +21,17 @@
 	    public $created_at;
 	    public $updated_at;
 	}
+	/**
+	 * Connect to the database
+	 *
+	 * @return object return a database connection handle
+	 */
+	function connectDB(): object
+	{
+		$dbh = new PDO('pgsql:host=tus-uppy-proto_database_1;dbname=proto', 'proto', 'proto');
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //PHP warnings for SQL errors
+		return $dbh ;
+	}
 
 	/**
 	 * Return the list of file and metadata for a given dataset
@@ -35,11 +40,11 @@
 	 * @param int $dataset DOI suffix
 	 * @return array list of files
 	 */
-	function getFileTable(object $dbh, int $dataset_doi): array
+	function getFileTable(object $dbh, int $dataset): array
 	{
-		$sql = "select * from file where dataset_id = (select id from dataset where identifier = ?)  and status = 'uploading'";
+		$sql = "select * from file where doi_suffix= ? and status = 'uploading'";
 		$st = $dbh->prepare($sql);
-		$st->bindParam(1, $dataset_doi);
+		$st->bindParam(1, $dataset);
 		$st->execute();
 		return $st->fetchAll(PDO::FETCH_CLASS, "File");
 	}
@@ -49,108 +54,44 @@
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
-    <meta charset="utf-8">
-    <title>Prototype for File Upload Wizard (Mockup)</title>
+	<meta charset="utf-8">
+	<title>Prototype for File Upload Wizard (Mockup)</title>
 </head>
-
 <body>
-    <headers>
-        <h1>Mockup for dataset
-            <?= $params["d"]?>
-        </h1>
-    </headers>
-    <nav><a href="<?= $web_endpoint ?>">[Go back to Dashboard]</a></nav>
-    <main role="main">
-        <section>
-            <article>
-                <img style="float:left;padding:0.5em" src="https://picsum.photos/id/1002/300/200">
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
-                eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
-                montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo,
-                fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-                imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-                Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate
-                eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-                enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-                viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam<p>
+	<h1>Mockup for dataset <?= $params["d"]?> </h1>
 
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
-                eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
-                montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo,
-                fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-                imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-                Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate
-                eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-                enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-                viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam<p>
-            </article>
-        </section>
-        <section>
-            <h2>Files</h2>
-            <table border="1">
-                <tr>
-                    <th>id</th>
-                    <th>dataset_id</th>
-                    <th>name</th>
-                    <th>size</th>
-                    <!-- <th>status</th> -->
-                    <th>location</th>
-                    <!-- <th>description</th> -->
-                    <!-- <th>initial_md5</th> -->
-                    <!-- <th>format</th> -->
-                    <!-- <th>data_type</th> -->
-                    <th>created_at</th>
-                    <th>updated_at</th>
-                </tr>
-                <? foreach ($table as $file) { ?>
-                <tr>
-                    <td>
-                        <?= $file->id ?>
-                    </td>
-                    <td>
-                        <?= $file->dataset_id ?>
-                    </td>
-                    <td>
-                        <?= $file->name ?>
-                    </td>
-                    <td>
-                        <?= $file->size ?>
-                    </td>
-<!--                     <td>
-                        <?= $file->status ?>
-                    </td> -->
-                    <td><a href="<?= $file->location ?>">
-                            <?= $file->location ?></a></td>
-<!--                     <td>
-                        <?= $file->description ?>
-                    </td>
-                    <td>
-                        <?= $file->initial_md5 ?>
-                    </td>
-                    <td>
-                        <?= $file->format ?>
-                    </td>
-                    <td>
-                        <?= $file->data_type ?>
-                    </td> -->
-                    <td>
-                        <?= $file->created_at ?>
-                    </td>
-                    <td>
-                        <?= $file->updated_at ?>
-                    </td>
-                </tr>
-                <? } ?>
-            </table>
-        </section>
-    </main>
-    <footer>
-        <em>(c) 2019, Prototype of mockup page for GigaDB</em>
-    </footer>
+	<table border="1">
+		<tr>
+    		<th>id</th>
+    		<th>doi_suffix</th>
+    		<th>name</th>
+    		<th>size</th>
+    		<th>status</th>
+    		<th>location</th>
+    		<th>description</th>
+    		<th>initial_md5</th>
+    		<th>format</th>
+    		<th>data_type</th>
+    		<th>created_at</th>
+    		<th>updated_at</th>
+        </tr>
+        <? foreach ($table as $file) { ?>
+        	<tr>
+	        	<td><?= $file->id ?></td>
+	        	<td><?= $file->doi_suffix ?></td>
+	        	<td><?= $file->name ?></td>
+	        	<td><?= $file->size ?></td>
+	        	<td><?= $file->status ?></td>
+	        	<td><a href="<?= $file->location ?>"><?= $file->location ?></a></td>
+	        	<td><?= $file->description ?></td>
+	        	<td><?= $file->initial_md5 ?></td>
+	        	<td><?= $file->format ?></td>
+	        	<td><?= $file->data_type ?></td>
+	        	<td><?= $file->created_at ?></td>
+	        	<td><?= $file->updated_at ?></td>
+	        </tr>
+        <? } ?>
+	</table>
 </body>
-
 </html>
