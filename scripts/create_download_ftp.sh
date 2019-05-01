5a92aa7c07eeb15214d067d4c2e5e198a5198c04
Rija Menage <rija+git@cinecinetique.com>
2019-05-01 15:10:38 +0800

    Add php and bash scripts to create passwords, folders and ftp accounts

---
+++ scripts/create_download_ftp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "pure-pw useradd d-$1 -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u downloader -d /home/downloader/$1  < /var/private/$1/download_password.txt"
+sudo docker exec tus-uppy-proto_ftpd_1 bash -c "pure-pw useradd d-$1 -f /etc/pure-ftpd/passwd/pureftpd.passwd -m -u downloader -d /home/downloader/$1  < /var/private/$1/download_password.txt"
