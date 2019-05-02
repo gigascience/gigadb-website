4176be4ad6fa0921a26f47c0379a7bd1a4ddd002
Rija Menage <rija+git@cinecinetique.com>
2019-05-02 18:40:28 +0800

    Make the table of file upload wizard accounts in dashboard dynamic

--- db_schema.sql
+++ db_schema.sql
@@ -23,3 +23,35 @@ $$ language 'plpgsql';
 
 CREATE TRIGGER update_file_modtime BEFORE UPDATE ON file
 FOR EACH ROW EXECUTE PROCEDURE  update_modified_column();
+
+CREATE TABLE account (
+    id serial PRIMARY KEY,
+    doi_suffix integer NOT NULL,
+    ulogin character varying(100),
+    utoken character varying(128),
+    dlogin character varying(100),
+    dtoken character varying(128),
+    space_used bigint,
+    status character varying(100),
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    retired_at timestamp
+);
+
+CREATE TRIGGER update_account_modtime BEFORE UPDATE ON account
+FOR EACH ROW EXECUTE PROCEDURE  update_modified_column();
+
+
+CREATE OR REPLACE FUNCTION update_retired_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.retired_at = now();
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_account_retiredtime BEFORE UPDATE ON account
+FOR EACH ROW
+WHEN (OLD.status <> NEW.status AND NEW.status = 'retired')
+EXECUTE PROCEDURE  update_retired_column();
+
