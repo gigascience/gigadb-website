f5b02b82889c81ebc82f0dd716efc939325c8144
Rija Menage <rija+git@cinecinetique.com>
2019-05-07 19:31:55 +0800

    Use Docker API for inter-container comm., fix watcher race condition

--- scripts/remove_download_ftp.sh
+++ scripts/remove_download_ftp.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-echo "pure-pw userdel d-$1 -f /etc/pure-ftpd/passwd/pureftpd.passwd -m"
-sudo docker exec tus-uppy-proto_ftpd_1 bash -c "pure-pw userdel d-$1 -f /etc/pure-ftpd/passwd/pureftpd.passwd -m"
+set -e
+
+# determine the name of the container that run pure-ftpd
+FTPD_CONTAINER=$(sudo /usr/bin/curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" http:/v1.37/containers/json | jq -r '.[] | select(.Labels."com.docker.compose.service" == "ftpd") | .Names | .[0]')
+
+# create a resource on Docker API for the command to run on the ftpd container
+BODY="{\"AttachStdin\": false, \"AttachStdout\": true, \"AttachStderr\": true, \"Tty\": false, \"Cmd\": [\"bash\",\"-c\",\"/usr/bin/pure-pw userdel d-$1 -f /etc/pure-ftpd/passwd/pureftpd.passwd -m\"] }"
+echo $BODY > /tmp/payload_remove_download_$1.json
+EXEC_ID=$(sudo /usr/bin/curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -d @/tmp/payload_remove_download_$1.json http:/v1.37/containers${FTPD_CONTAINER}/exec | jq -r ".Id")
+
+# Run the command to create an ftp account on the ftpd container
+sudo /usr/bin/curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -d @/tmp/payload_remove_download_$1.json http:/v1.37/exec/${EXEC_ID}/start
