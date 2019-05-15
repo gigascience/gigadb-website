720bb337fd82d580088990d24ca0289a5ec98843
Rija Menage <rija+git@cinecinetique.com>
2019-05-16 00:37:40 +0800

    Make the prototype deploy, build and start on AWS (wip)
    Terraform for instantiating infrastructure on AWS.
    Ansible for setting up Docker, deploying the files and start the application.
    
    All the containers are built using in-project Dockerfiles
    (except for Nginx and PostgreSQL which are off-the-shelf official images)
    
    ports and domains not yet correct.

---
+++ infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  description = "AWS Access key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
+
+variable "aws_vpc_id" {
+  description = "AWS VPC id"
+}
+
+variable "deployment_target" {
+  description = "environment to build"
+}
