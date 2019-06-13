c7983fcb271e145bb47bde1c6a19db93cb1e497c
Rija Menage <rija+git@cinecinetique.com>
2019-06-13 21:54:17 +0200

    Add creation of pipeline directories and file with generated ftp token

--- fuw/app/backend/models/FiledropAccount.php
+++ fuw/app/backend/models/FiledropAccount.php
@@ -59,4 +59,51 @@ class FiledropAccount extends \yii\db\ActiveRecord
             'retired_at' => 'Retired At',
         ];
     }
+
+    /**
+     * Create directories required for file upload pipeline
+     *
+     * @param string $doi dataset identifier for which to create directory
+     */
+    function createDirectories(string $doi): void
+    {
+        mkdir("/var/incoming/ftp/$doi", 0770);
+        chmod("/var/incoming/ftp/$doi", 0770); //to bypass umask
+        mkdir("/var/repo/$doi", 0755);
+        mkdir("/var/private/$doi", 0750);
+    }
+
+    /**
+     * Create a randomly generated string token and write to file
+     *
+     * @param string $doi dataset identifier
+     * @param string $fileName file name where to write token
+     * @return bool whether the operation is successful or not
+     *
+     */
+    public function makeToken(string $doi, string $fileName): bool
+    {
+        $token = $this->generateRandomString(16);
+        return file_put_contents("/var/private/$doi/".$fileName, $token) ? true : false ;
+    }
+
+    /**
+     * Generate a random string
+     *
+     * @param int $size size of the string
+     * @return string generated string
+     *
+     */
+    private function generateRandomString(int $size): string
+    {
+        $range = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $input_length = strlen($range);
+        $random_string = '';
+        for($i = 0; $i < $size; $i++) {
+            $random_character = $range[mt_rand(0, $input_length - 1)];
+            $random_string .= $random_character;
+        }
+
+        return $random_string;
+    }
 }
