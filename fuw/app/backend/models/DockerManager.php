f6347f3ebf89e8aa9a435d21e68d07d4d1815bc1
Rija Menage <rija+git@cinecinetique.com>
2019-06-18 22:07:55 +0200

    Move Docker related functions to its own class

---
+++ fuw/app/backend/models/DockerManager.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace backend\models;
+
+use Docker\Docker;
+use Docker\API\Model\ContainerSummaryItem;
+use Docker\API\Model\{ContainersIdExecPostBody,
+                      ExecIdStartPostBody,
+                    };
+
+class DockerManager
+{
+	/**
+     * @var array list of containers that should not be accessed programmatically
+     */
+    const FORBIDDEN = ["/console_1/","/db_1/"];
+
+	/**
+     * @var object $docker static class variable to hold connection to Docker
+     */
+	private static $docker;
+
+	/**
+     * Retrieve a matching container using Docker API
+     *
+     * @param string regex pattern for the container to match
+     * @return null|\Docker\API\Model\ContainerSummaryItem container details
+     */
+    public function getContainer(string $containerPattern): ?\Docker\API\Model\ContainerSummaryItem
+    {
+        if( in_array($containerPattern, self::FORBIDDEN) ) {
+            return null;
+        }
+
+        $docker = Docker::create();
+        $containers = $docker->containerList();
+        foreach ($containers as $container) {
+            if ( preg_match($containerPattern,implode("",$container->getNames())) ) {
+                return $container;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Factory for making instance of *PostBody objet for Docker PHP API
+     *
+     * @param string $postBodyType type of postBody to make
+     * @param array $commandArray array forming a command to execute on the container
+     * @return null|\Docker\API\Model\ContainersIdExecPostBody|\Docker\API\Model\ExecIdStartPostBody
+     */
+    public function makePostBodyFor(string $postBodyType, array $commandArray = null): ?object
+    {
+        if ("execConfig" == $postBodyType) {
+            $execConfig = new ContainersIdExecPostBody();
+            $execConfig->setAttachStdout(true);
+            $execConfig->setAttachStderr(true);
+            $execConfig->setCmd($commandArray);
+            return $execConfig;
+        }
+        elseif ("execStartConfig" == $postBodyType) {
+            $execStartConfig = new ExecIdStartPostBody();
+            $execStartConfig->setDetach(false);
+            $execStartConfig->setTty(false);
+            return $execStartConfig;
+        }
+        return null;
+    }
+}
+?>
