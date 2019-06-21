58f806ec1c91d189fe78bfb5f59b248ed3906988
Rija Menage <rija+git@cinecinetique.com>
2019-06-21 23:21:32 +0200

    Get DockerManager load function return Docker stream,ftpd to use host FS
    ftpd using host file system for storing account data will facilitate test automation and backup eventually.
    
    Add functional test for creating ftp account
    
    and make generateRandomString a static method so it can be use in the functional tests to generate random doi.
    This way, functional test won't clash with developer's data.

--- fuw/app/backend/tests/functional/FiledropAccountCest.php
+++ fuw/app/backend/tests/functional/FiledropAccountCest.php
@@ -1,74 +1,17 @@
 <?php namespace backend\tests\functional;
 use backend\tests\FunctionalTester;
-use common\fixtures\UserFixture;
 
 use backend\models\FiledropAccount;
 use backend\models\DockerManager;
 
-use Yii;
-
 class FiledropAccountCest
 {
-	/**
-     * Load fixtures before db transaction begin
-     * Called in _before()
-     * @see \Codeception\Module\Yii2::_before()
-     * @see \Codeception\Module\Yii2::loadFixtures()
-     * @return array
-     */
-    public function _fixtures()
-    {
-        return [
-            'user' => [
-                'class' => UserFixture::className(),
-                'dataFile' => codecept_data_dir() . 'login_data.php'
-            ]
-        ];
-    }
-
     public function _before(FunctionalTester $I)
     {
     	// make sure the ftpd container is reset
     }
 
-	/**
-     * functional test that directory are created, tokens generated and set to model class
-     * @param FunctionalTester $I
-     */
-    public function prepareAccountSetFields(FunctionalTester $I)
-    {
-    	$filedrop = new FiledropAccount();
-    	$dockerManager = new DockerManager();
-    	$doi = FiledropAccount::generateRandomString(6);
-
-    	$I->assertTrue($filedrop->prepareAccountSetFields($doi));
-
-        // clean up directories created by the test
-        $filedrop->removeDirectories("$doi");
-    }
-
-
     /**
-     * functional test that ftp accounts are created on the ftpd container
-     * @param FunctionalTester $I
-     */
-    public function checkingAnFTPAccount(FunctionalTester $I)
-    {
-        $filedrop = new FiledropAccount();
-        $dockerManager = new DockerManager();
-        $doi = FiledropAccount::generateRandomString(6);
-        $ftpServer = "ftpd";
-
-
-        $filedrop->setDOI($doi);
-        $filedrop->setDockerManager($dockerManager);
-
-        $response = $filedrop->checkFTPAccount( $dockerManager, $doi );
-        $I->assertNotTrue($response);
-
-    }
-    /**
-     * functional test that ftp accounts are created on the ftpd container
      * @param FunctionalTester $I
      */
     public function createFTPAccounts(FunctionalTester $I)
@@ -92,119 +35,28 @@ class FiledropAccountCest
 		$downloadLogin = "downloader-$doi";
 		$downloadToken = rtrim(file("/var/private/$doi/downloader_token.txt")[0]);
 
+    	// in case no accounts
+    	touch("/etc/pure-ftpd/passwd/pureftpd.passwd");
+
     	// create accounts
     	$status = $filedrop->createFTPAccount( $dockerManager, $doi );
     	$I->assertTrue($status);
 
-        // check success (TODO)
-        // $status = $filedrop->checkFTPAccount( $dockerManager, $doi );
-        // $I->assertTrue($status, "account created for $doi, returned $status");
+    	// we should be able to connect. problem 2 is there is delay in filesytem sync
+    	// Problem 1 is ftpd cannot be connected by ftp (because of PUBLICHOST value)
+  //   	clearstatcache();
+  //   	$password_file = file("/etc/pure-ftpd/passwd/pureftpd.passwd");
+  //   	clearstatcache();
+  //   	$password_file = file("/etc/pure-ftpd/passwd/pureftpd.passwd");
+  //    	clearstatcache();
+  //   	$password_file = file("/etc/pure-ftpd/passwd/pureftpd.passwd");
+  //    	clearstatcache();
+  //   	$password_file = file("/etc/pure-ftpd/passwd/pureftpd.passwd");
+  //    	clearstatcache();
+  //   	$password_file = file("/etc/pure-ftpd/passwd/pureftpd.passwd");
+  //    	clearstatcache();
+  //   	$password_file = file("/etc/pure-ftpd/passwd/pureftpd.passwd");
+		// $I->assertRegExp("/$downloadLogin/", $password_file[count($password_file) - 1], "size: ".count($password_file));
 
-
-        // clean up directories created by the test
-        $filedrop->removeDirectories("$doi");
-        $filedrop->removeFTPAccount( $dockerManager, $doi );
-
-    }
-
-    /**
-     * functional test that accounts are created and save in database
-     *
-     * @todo validate directories, token and ftp account exist before saving
-     * @param FunctionalTester $I
-     */
-    public function createAccountsDatabaseRecord(FunctionalTester $I)
-    {
-
-    	$filedrop = new FiledropAccount();
-    	$dockerManager = new DockerManager();
-    	$doi = FiledropAccount::generateRandomString(6);
-    	$ftpServer = "ftpd";
-
-    	$uploadLogin = "uploader-$doi";
-
-    	$filedrop->setDOI($doi);
-    	$filedrop->setDockerManager($dockerManager);
-    	$filedrop->status = FiledropAccount::STATUS_ACTIVE;
-    	$filedrop->save();
-
-    	$accounts = FiledropAccount::find()
-    		->where(['doi' => $doi])
-    		->all();
-    	$I->assertCount(1, $accounts);
-    	$I->assertEquals($uploadLogin, $accounts[0]->upload_login);
-
-        // clean up directories created by the test
-        $filedrop->removeDirectories("$doi");
-        $filedrop->removeFTPAccount( $dockerManager, $doi );
-    }
-
-    /**
-     * functional test http post to create account
-     *
-     * @param FunctionalTester $I
-     */
-    public function sendRestHttpPostToCreateAccount(FunctionalTester $I)
-    {
-
-    	// what's in the token:
-  		// {
-		//   "sub":"API Access request from client",
-		//   "iss": "www.gigadb.org",
-		//   "aud": "fuw.gigadb.org",
-		//   "email": "sfriesen@jenkins.info",
-		//   "name": "John Smith",
-		//   "admin_status": "true",
-		//   "role": "create",
-		//   "iat" : "1561730823",
-		//   "nbf" : "1561730823",
-		//   "exp" : "2729513220"
-		// }
-    	$doi = FiledropAccount::generateRandomString(6);
-    	$I->amBearerAuthenticated("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBUEkgQWNjZXNzIHJlcXVlc3QgZnJvbSBjbGllbnQiLCJpc3MiOiJ3d3cuZ2lnYWRiLm9yZyIsImF1ZCI6ImZ1dy5naWdhZGIub3JnIiwiZW1haWwiOiJzZnJpZXNlbkBqZW5raW5zLmluZm8iLCJuYW1lIjoiSm9obiBTbWl0aCIsImFkbWluX3N0YXR1cyI6InRydWUiLCJyb2xlIjoiY3JlYXRlIiwiaWF0IjoiMTU2MTczMDgyMyIsIm5iZiI6IjE1NjE3MzA4MjMiLCJleHAiOiIyNzI5NTEzMjIwIn0.uTZpDB1eCGt3c_23wLaVxpFUw_WFH2Jep_vpzky2o18");
-    	$I->sendPOST("/filedrop-accounts",['doi' =>"$doi"]);
-    	$I->seeResponseCodeIs(201);
-    	$I->seeResponseContainsJson(array('doi' => "$doi"));
-    	$I->seeResponseContainsJson(array('upload_login' => "uploader-$doi"));
-    	$I->seeResponseContainsJson(array('download_login' => "downloader-$doi"));
-    	$I->seeResponseContainsJson(array('status' => FiledropAccount::STATUS_ACTIVE));
-    	$I->seeResponseJsonMatchesJsonPath('$.upload_token');
-    	$I->seeResponseJsonMatchesJsonPath('$.download_token');
-
-        // clean up directories created by the test
-        $filedrop = new FiledropAccount();
-        $dockerManager = new DockerManager();
-        $filedrop->setDOI($doi);
-        $filedrop->setDockerManager($dockerManager);
-        $filedrop->removeDirectories("$doi");
-        $filedrop->removeFTPAccount( $dockerManager, $doi );
-    }
-
-    /**
-     * functional test http delete to delete account
-     *
-     * @param FunctionalTester $I
-     */
-    public function sendRestHttpDeleteToDeleteAccount(FunctionalTester $I)
-    {
-    	$doi = FiledropAccount::generateRandomString(6);
-
-    	$filedrop = new FiledropAccount();
-    	$dockerManager = new DockerManager();
-
-    	$filedrop->setDOI($doi);
-    	$filedrop->setDockerManager($dockerManager);
-    	$filedrop->status = FiledropAccount::STATUS_ACTIVE;
-    	$filedrop->save();
-        $I->assertEquals(FiledropAccount::STATUS_ACTIVE, $filedrop->status);
-    	$I->amBearerAuthenticated("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBUEkgQWNjZXNzIHJlcXVlc3QgZnJvbSBjbGllbnQiLCJpc3MiOiJ3d3cuZ2lnYWRiLm9yZyIsImF1ZCI6ImZ1dy5naWdhZGIub3JnIiwiZW1haWwiOiJzZnJpZXNlbkBqZW5raW5zLmluZm8iLCJuYW1lIjoiSm9obiBTbWl0aCIsImFkbWluX3N0YXR1cyI6InRydWUiLCJyb2xlIjoiY3JlYXRlIiwiaWF0IjoiMTU2MTczMDgyMyIsIm5iZiI6IjE1NjE3MzA4MjMiLCJleHAiOiIyNzI5NTEzMjIwIn0.uTZpDB1eCGt3c_23wLaVxpFUw_WFH2Jep_vpzky2o18");
-    	$I->sendDELETE("/filedrop-accounts/" . $filedrop->id);
-    	$I->seeResponseCodeIs(204);
-        $filedrop = FiledropAccount::findOne(["doi" => $doi]);
-    	$I->assertEquals(FiledropAccount::STATUS_TERMINATED, $filedrop->status, "account set to 'terminated'");
-
-        $I->assertNotTrue(file_exists("/var/incoming/ftp/$doi"), "incoming directory removed");
-        $I->assertNotTrue(file_exists("/var/repo/$doi"), "repository removed");
-        $I->assertNotTrue(file_exists("/var/private/$doi"), "private directory removed");
     }
 }
