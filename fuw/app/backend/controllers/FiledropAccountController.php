6f94dda5c7e3c8e7fb1cb805c1f89f17eb3f1b98
Rija Menage <rija+git@cinecinetique.com>
2019-06-27 15:40:22 +0200

    Add REST controller for creating filedrop account

--- fuw/app/backend/controllers/FiledropAccountController.php
+++ fuw/app/backend/controllers/FiledropAccountController.php
@@ -7,21 +7,10 @@ use yii\rest\ActiveController;
 /**
  * REST controller for FiledropAccount
  *
- * POST /filedrop-accounts/100001 -> /filedrop-accounts/create/100001
- * DELETE /filedrop-accounts/100001 -> /filedrop-accounts/delete/100001
- * GET /filedrop-accounts/100001 -> /filedrop-accounts/view/100001
- * GET /filedrop-accounts/ -> /filedrop-accounts/
  * @uses \backend\models\FiledropAccount
  *
  **/
 class FiledropAccountController extends ActiveController
 {
     public $modelClass = 'backend\models\FiledropAccount';
-
-    public function actions()
-    {
-        $actions = parent::actions();
-        $actions['create']['class'] = 'backend\controllers\FiledropAccount\CreateAction';
-        return $actions;
-    }
 }
