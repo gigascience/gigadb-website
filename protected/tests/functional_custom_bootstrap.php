0a139e134cd135cbee11c7f359f724cbd424d070
Rija Menage <rija+git@cinecinetique.com>
2019-07-24 19:13:40 +0200

    Clean up database backup and restore in functional tests

--- protected/tests/functional_custom_bootstrap.php
+++ protected/tests/functional_custom_bootstrap.php
@@ -11,16 +11,29 @@ Yii::$enableIncludePath = false;
 Yii::createWebApplication($config);
 
 // Before hooks for our functional tests
-print_r("Backing up current database...".PHP_EOL);
-exec("pg_dump gigadb -U gigadb -h database -F custom  -f /var/www/sql/before-run.pgdmp 2>&1",$output);
-print_r("Loading test database... ".PHP_EOL);
-exec("pg_restore -h database -U gigadb -d gigadb --clean --no-owner -v /var/www/sql/gigadb_testdata.pgdmp || true 2>&1",$output);
 print_r("Loading environment variables... ".PHP_EOL);
 $dotenv = Dotenv\Dotenv::create('/var/www', '.secrets');
 $dotenv->load();
+print_r("Loading database config...".PHP_EOL);
+$dbconf = json_decode(file_get_contents(dirname(__FILE__).'/../config/db.json'), true);
+$db_host = $dbconf["host"];
+$db_name = $dbconf["database"];
+$db_user = $dbconf["user"];
+$db_password = $dbconf["password"];
+print_r("database config (host, db name, user): $db_host, $db_name, $db_user.".PHP_EOL);
+print_r("Backing up current database...".PHP_EOL);
+exec("pg_dump $db_name -U $db_user -h $db_host -F custom  -f /var/www/sql/before-run.pgdmp 2>&1",$output);
+print_r("Loading test database... ".PHP_EOL);
+GigadbWebsiteContext::call_pg_terminate_backend($db_name);
+GigadbWebsiteContext::recreateDB($db_name);
+exec("pg_restore -h $db_host -U $db_user -d $db_name --clean --no-owner -v /var/www/sql/gigadb_testdata.pgdmp || true 2>&1",$output);
+GigadbWebsiteContext::containerRestart();
 
 // After hooks for our functional tests
 register_shutdown_function(function(){
    	print_r("Restoring current database...".PHP_EOL);
-    exec("pg_restore -h database  -U gigadb -d gigadb --clean --no-owner -v /var/www/sql/before-run.pgdmp 2>&1",$output);
+   	GigadbWebsiteContext::call_pg_terminate_backend($GLOBALS['db_name']);
+   	GigadbWebsiteContext::recreateDB($GLOBALS['db_name']);
+    exec("pg_restore -h $db_host  -U $db_user -d $db_name --clean --no-owner -v /var/www/sql/before-run.pgdmp 2>&1",$output);
+   	GigadbWebsiteContext::containerRestart();
 });
