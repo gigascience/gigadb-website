d1c6e061a625a0a25dad85103ad8897db2c5852b
Rija Menage <rija+git@cinecinetique.com>
2019-08-15 21:42:51 +0200

    Add REST API endpoint for terminating a filedrop account
    termination imply removing dirctoring, delete ftp accounts and changing status in database.
    
    use the DELETE http method for the endpoint and override the default delete action with a custom DeleteAction class
    that implements the above.

--- fuw/app/backend/actions/FiledropAccountController/DeleteAction.php
+++ fuw/app/backend/actions/FiledropAccountController/DeleteAction.php
@@ -6,14 +6,6 @@ use Yii;
 use yii\web\ServerErrorHttpException;
 use backend\models\FiledropAccount;
 
-/**
- * A custom RestController action for deleting a FiledropAccount model
- *
- * Instead of deleting the model, it marks the model as FiledropAccount::STATUS_TERMINATED
- *
- * @author Rija Menage <rija+git@cinecinetique.com>
- * @license GPL-3.0
-*/
 class DeleteAction extends \yii\rest\DeleteAction
 {
 	/**
@@ -34,16 +26,16 @@ class DeleteAction extends \yii\rest\DeleteAction
             call_user_func($this->checkAccess, $account->id, $account);
         }
 
-        $account->status = FiledropAccount::STATUS_TERMINATED;
+        $account->status = "terminated";
 
         if ( ! $account->validate() ) {
-        	throw new ServerErrorHttpException('validation failed:'.implode("\n", $account->getErrorSummary(true)));
+        	throw new ServerErrorHttpException('Failure during termination of account');
         }
         else if ( ! $account->save(false) ) {
-            throw new ServerErrorHttpException("Failed to terminate account for dataset ".$account->doi);
+            throw new ServerErrorHttpException('Failed to update terminated account');
         }
         else {
-        	Yii::info("Terminated Filedrop Account for dataset ".$account->doi);
+        	Yii::info("Terminated Filedrop Account $id");
         }
 
         Yii::$app->getResponse()->setStatusCode(204);
