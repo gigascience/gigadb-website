af60d718a8479e27bd3f2811aedd6cf1a3c77c45
Rija Menage <rija+git@cinecinetique.com>
2019-08-22 21:24:59 +0200

    Change prototype to use new FUW DB schema and to use sub-directory

--- html/lib/db.php
+++ html/lib/db.php
@@ -1,19 +1,19 @@
 <?php
-	/**
-	 * Connect to the database
-	 *
-	 * @return object return a database connection handle
-	 */
-	function connectDB(): object
-	{
-		$appconfig = parse_ini_file("/var/appconfig.ini");
+/**
+ * Connect to the database
+ *
+ * @return object return a database connection handle
+ */
+function connectDB(): object
+{
+    $appconfig = parse_ini_file("/var/appconfig.ini");
 
-		$db_user = $appconfig["db_user"];
-		$db_password = $appconfig["db_password"];
-		$db_source = $appconfig["db_source"];
+    $db_user = $appconfig["db_user"];
+    $db_password = $appconfig["db_password"];
+    $db_source = $appconfig["db_source"];
 
-		$dbh = new PDO("pgsql:host=database;dbname=$db_source", "$db_user", "$db_password");
-		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //PHP warnings for SQL errors
-		return $dbh ;
-	}
+    $dbh = new PDO("pgsql:host=$db_host;dbname=$db_source", "$db_user", "$db_password");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //PHP warnings for SQL errors
+    return $dbh ;
+}
 ?>
