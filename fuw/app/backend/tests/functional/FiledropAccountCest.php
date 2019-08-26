4c4004b9c237e6e10e27336351c1acfb90e59482
Rija Menage <rija+git@cinecinetique.com>
2019-08-26 20:22:42 +0200

    Fix some issues with inter-container communication with ftp container

--- fuw/app/backend/tests/functional/FiledropAccountCest.php
+++ fuw/app/backend/tests/functional/FiledropAccountCest.php
@@ -44,6 +44,26 @@ class FiledropAccountCest
     	$I->assertTrue($filedrop->prepareAccountSetFields($doi));
     }
 
+
+    /**
+     * functional test that ftp accounts are created on the ftpd container
+     * @param FunctionalTester $I
+     */
+    public function checkingAnFTPAccount(FunctionalTester $I)
+    {
+        $filedrop = new FiledropAccount();
+        $dockerManager = new DockerManager();
+        $doi = FiledropAccount::generateRandomString(6);
+        $ftpServer = "ftpd";
+
+
+        $filedrop->setDOI($doi);
+        $filedrop->setDockerManager($dockerManager);
+
+        $response = $filedrop->checkFTPAccount( $dockerManager, $doi );
+        $I->assertNotTrue($response);
+
+    }
     /**
      * functional test that ftp accounts are created on the ftpd container
      * @param FunctionalTester $I
@@ -75,6 +95,10 @@ class FiledropAccountCest
     	// create accounts
     	$status = $filedrop->createFTPAccount( $dockerManager, $doi );
     	$I->assertTrue($status);
+
+        // check success (TODO)
+        // $status = $filedrop->checkFTPAccount( $dockerManager, $doi );
+        // $I->assertTrue($status, "account created for $doi, returned $status");
 
     }
 
