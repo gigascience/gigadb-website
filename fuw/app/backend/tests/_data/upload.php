e9f96434718176ff17a778b4e0975c7d859e3fc6
Rija Menage <rija+git@cinecinetique.com>
2019-08-27 18:00:43 +0200

    Mark uploads as archived when Filedrop Account is terminated

---
+++ fuw/app/backend/tests/_data/upload.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    [
+        'doi' => '200000',
+        'name' => '083.pdf',
+        'size' => '122703',
+        'status' => 'uploading',
+        'location' => 'ftp://downloader-200000:35khksdf@localhost:9021/083.pdf',
+        'extension' => 'pdf',
+        'created_at' => '2019-08-22 13:02:12',
+        'updated_at' => '2019-08-22 13:02:12',
+    ],
+    [
+        'doi' => '200001',
+        'name' => '084.pdf',
+        'size' => '122703',
+        'status' => 'uploading',
+        'location' => 'ftp://downloader-200001:27h34tn@localhost:9021/084.pdf',
+        'extension' => 'pdf',
+        'created_at' => '2019-08-22 13:02:12',
+        'updated_at' => '2019-08-22 13:02:12',
+    ],
+    [
+        'doi' => '200001',
+        'name' => '085.pdf',
+        'size' => '122703',
+        'status' => 'uploading',
+        'location' => 'ftp://downloader-200001:27h34tn@localhost:9021/085.pdf',
+        'extension' => 'pdf',
+        'created_at' => '2019-08-22 13:02:12',
+        'updated_at' => '2019-08-22 13:02:12',
+    ],
+];
