c9a76515a3102a6b96293d5cbb4111b254e37b0c
Rija Menage <rija+git@cinecinetique.com>
2019-08-31 12:06:21 +0200

    Get demo user insert from prototype setup to migrations to fix CI deploy

---
+++ fuw/app/console/migrations/m190831_092512_insert_demo_user.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190831_092512_insert_demo_user
+ */
+class m190831_092512_insert_demo_user extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert('{{%user}}', ["username" => "prototype", "email" => "sfriesen@jenkins.info", "auth_key" => "dsfasdfasdfdsa", "password_hash" => "dsafadsgads","password_reset_token" => "oqwetad" , "status" => 10, "created_at" =>  $this->dateTime(), "updated_at" =>  $this->dateTime()]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->delete('{{%user}}',"username = 'prototype'");
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190831_092512_insert_demo_user cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
