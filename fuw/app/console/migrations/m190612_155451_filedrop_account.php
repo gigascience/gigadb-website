f762ae18ba2086dd463dd0e551c773de7fce95df
Rija Menage <rija+git@cinecinetique.com>
2019-09-01 22:25:18 +0200

    Refactor filedrop account DB schema to be more consistent and robust

--- fuw/app/console/migrations/m190612_155451_filedrop_account.php
+++ fuw/app/console/migrations/m190612_155451_filedrop_account.php
@@ -12,17 +12,17 @@ class m190612_155451_filedrop_account extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('filedrop_account', array(
-            'id' => 'serial PRIMARY KEY',
-            'doi' => 'character varying(100) NOT NULL',
-            'upload_login' => 'character varying(100)',
-            'upload_token' => 'character varying(128)',
-            'download_login' => 'character varying(100)',
-            'download_token' => 'character varying(128)',
-            'status' => 'character varying(100)',
-            'created_at' => 'timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL',
-            'updated_at' => 'timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL',
-            'retired_at' => 'timestamp',
+        $this->createTable('{{%filedrop_account}}', array(
+            'id' => $this->primaryKey(),
+            'doi' => $this->string(100)->notNull(),
+            'upload_login' => $this->string(100),
+            'upload_token' => $this->string(128),
+            'download_login' => $this->string(100),
+            'download_token' => $this->string(128),
+            'status' => $this->integer(),
+            'created_at' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'updated_at' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
+            'terminated_at' => $this->timestamp(),
         ));
 
         $this->execute("CREATE OR REPLACE FUNCTION update_modified_column()
@@ -36,18 +36,18 @@ $$ language 'plpgsql'");
        $this->execute("CREATE TRIGGER update_account_modtime BEFORE UPDATE ON filedrop_account
 FOR EACH ROW EXECUTE PROCEDURE  update_modified_column()");
 
-       $this->execute("CREATE OR REPLACE FUNCTION update_retired_column()
+       $this->execute("CREATE OR REPLACE FUNCTION update_terminated_column()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.retired_at = now();
+    NEW.terminated_at = now();
     RETURN NEW;
 END;
 $$ language 'plpgsql'");
 
-        $this->execute("CREATE TRIGGER update_account_retiredtime BEFORE UPDATE ON filedrop_account
+        $this->execute("CREATE TRIGGER update_account_terminatedtime BEFORE UPDATE ON filedrop_account
 FOR EACH ROW
-WHEN (OLD.status <> NEW.status AND NEW.status = 'retired')
-EXECUTE PROCEDURE  update_retired_column()");
+WHEN (OLD.status <> NEW.status AND NEW.status = 0)
+EXECUTE PROCEDURE  update_terminated_column()");
 
     }
 
