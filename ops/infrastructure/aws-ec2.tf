26e84bd0c608208200cd0bd7044d3c0ee928fee6
Rija Menage <rija+git@cinecinetique.com>
2019-09-03 13:25:33 +0200

    Configure File Upload Wizard for deployment to staging

--- ops/infrastructure/aws-ec2.tf
+++ ops/infrastructure/aws-ec2.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "ap-southeast-1"
+	access_key = "${var.aws_access_key}"
+ 	secret_key = "${var.aws_secret_key}"
+	region     = "ap-southeast-1"
 }
 
 resource "aws_security_group" "docker_host_sg" {
   name        = "docker_host_sg"
-  description = "Allow connection to docker host"
+  description = "Allow connection to docker host for ${var.deployment_target}"
   vpc_id      = "${var.aws_vpc_id}"
 
   ingress {
@@ -37,53 +37,38 @@ resource "aws_security_group" "docker_host_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 30000
-    to_port     = 30009
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 9021
-    to_port     = 9021
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+	from_port   = 0
+	to_port     = 0
+	protocol    = "-1"
+	cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 
-resource "aws_instance" "prototype_dockerhost" {
+resource "aws_instance" "staging_dockerhost" {
   ami = "ami-8e0205f2"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.docker_host_sg.id}"]
   key_name = "aws-centos7-keys"
 
   tags = {
-    Name = "ec2-as1-file-upload-wizard"
+    Name = "ec2-as1-${var.deployment_target}"
   }
 
   root_block_device = {
     delete_on_termination = "true"
-    volume_size = "25"
   }
 }
 
-data "aws_eip" "prototype_eip" {
+data "aws_eip" "docker_host_eip" {
   filter {
     name   = "tag:Name"
-    values = ["eip-prototype-file-upload-wizard"]
+    values = ["eip-${var.deployment_target}"]
   }
 }
 
-resource "aws_eip_association" "prototype_eip_association" {
-  instance_id   = "${aws_instance.prototype_dockerhost.id}"
-  allocation_id = "${data.aws_eip.prototype_eip.id}"
+resource "aws_eip_association" "docker_host_eip_assoc" {
+  instance_id   = "${aws_instance.staging_docker_host.id}"
+  allocation_id = "${data.aws_eip.docker_host_eip.id}"
 }
