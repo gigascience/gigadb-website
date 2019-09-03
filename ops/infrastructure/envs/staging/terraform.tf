8e0df3b95d95fe29248b45ae7313ae80ec2bf054
Rija Menage <rija+git@cinecinetique.com>
2019-09-03 15:01:47 +0200

    Refactor Terraform setup to support multiple env. and avoid duplication

---
+++ ops/infrastructure/envs/staging/terraform.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "ap-southeast-1"
+}
+
+
+module "ec2" {
+  source = "../../modules/aws-instance"
+
+  vpc_id = "${var.aws_vpc_id}"
+  deployment_target = "${var.deployment_target}"
+  eip_tag_name = "eip-${var.deployment_target}"
+}
