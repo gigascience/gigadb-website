7301f192d62ef6bf4f544d76f2a1c811c0b90620
Rija Menage <rija+git@cinecinetique.com>
2019-09-18 17:33:46 +0200

    Fixed Mixed content error about to twitter widgets insecurely loaded

--- protected/views/layouts/datasetpage.php
+++ protected/views/layouts/datasetpage.php
@@ -131,7 +131,7 @@
     <!-- Le javascript
      ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-     <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+     <script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>
      <!-- <script src="/js/jquery.js"></script>-->
      <!-- <script src="/js/google-code-prettify/prettify.js"></script>-->
      <!-- <script src="/js/bootstrap-transition.js"></script>
