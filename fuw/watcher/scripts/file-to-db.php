08fa83a224f7c3b9193362d17aaf26d33b035a40
Rija Menage <rija+git@cinecinetique.com>
2019-09-18 16:08:03 +0200

    Fix ftp urls in mockup to use appropriate hostname on each environment

--- fuw/watcher/scripts/file-to-db.php
+++ fuw/watcher/scripts/file-to-db.php
@@ -4,22 +4,22 @@
 
 const FLAG_PATH = "/var/tmp/processing_flag" ;
 const DATA_TYPES = array(
-					"txt" => "Read Me",
-					"sh" => "Script",
-					"jpg" => "Photography",
-					"png" => "Infographics",
-					"zip" => "Mixed Archive",
-					"pdf" => "Instructions",
-				);
+    "txt" => "Read Me",
+    "sh" => "Script",
+    "jpg" => "Photography",
+    "png" => "Infographics",
+    "zip" => "Mixed Archive",
+    "pdf" => "Instructions",
+);
 
 const FILE_FORMATS = array(
-					"txt" => "TEXT",
-					"doc" => "TEXT",
-					"md" => "TEXT",
-					"text" => "TEXT",
-					"readme" => "TEXT",
-					"fa" => "FASTA",
-					"pdf" => "PDF",
+    "txt" => "TEXT",
+    "doc" => "TEXT",
+    "md" => "TEXT",
+    "text" => "TEXT",
+    "readme" => "TEXT",
+    "fa" => "FASTA",
+    "pdf" => "PDF",
 );
 
 /**
@@ -31,11 +31,11 @@ const FILE_FORMATS = array(
 function getApproximateDataTypeFromFile(string $file_name): string
 {
 
-	$ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-	if (true == in_array($ext, array_keys(DATA_TYPES)) ) {
-		return DATA_TYPES[$ext];
-	}
-	return "Unknown Data Type" ;
+    $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+    if (true == in_array($ext, array_keys(DATA_TYPES)) ) {
+        return DATA_TYPES[$ext];
+    }
+    return "Unknown Data Type" ;
 }
 
 /**
@@ -47,11 +47,11 @@ function getApproximateDataTypeFromFile(string $file_name): string
 function getFileFormatFromFile(string $file_name): string
 {
 
-	$ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-	if (true == in_array($ext, array_keys(FILE_FORMATS)) ) {
-		return FILE_FORMATS[$ext];
-	}
-	return "Unknown File Format" ;
+    $ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+    if (true == in_array($ext, array_keys(FILE_FORMATS)) ) {
+        return FILE_FORMATS[$ext];
+    }
+    return "Unknown File Format" ;
 }
 
 /**
@@ -63,17 +63,17 @@ function getFileFormatFromFile(string $file_name): string
  */
 function generateFTPLink(string $file_name, string $dataset): string
 {
-	$handle = fopen("/var/access/$dataset/downloader_token.txt", "r");
-	$line = fgets($handle) ;
-	if (true == $line) {
-		$downloader_token = chop($line);
-	}
-	fclose($handle);
+    $handle = fopen("/var/access/$dataset/downloader_token.txt", "r");
+    $line = fgets($handle) ;
+    if (true == $line) {
+        $downloader_token = chop($line);
+    }
+    fclose($handle);
 
-	$appconfig = parse_ini_file("/config/db.ini");
-	$ftpd_endpoint = $appconfig["ftpd_endpoint"] ?? "localhost";
-	$ftp_link = "ftp://downloader-$dataset:$downloader_token@$ftpd_endpoint:9021/$file_name";
-	return $ftp_link;
+    $appconfig = parse_ini_file("/config/watcherconfig.ini");
+    $ftpd_endpoint = $appconfig["ftpd_endpoint"] ?? "localhost";
+    $ftp_link = "ftp://downloader-$dataset:$downloader_token@$ftpd_endpoint:9021/$file_name";
+    return $ftp_link;
 }
 /**
  * Get list of dataset directories from filesystem
@@ -83,24 +83,24 @@ function generateFTPLink(string $file_name, string $dataset): string
  */
 function getDatasetDirectories(string $download_path): array
 {
-	$datasets = [] ;
-	$handle = opendir($download_path);
-	while (($file = readdir($handle)) !== false) {
-		if ($file === '.' || $file === '..') {
-			echo "skipping $file because dot file".PHP_EOL;
-			continue;
-		}
-		if ( true != is_dir("$download_path/$file") ) {
-			echo "skipping $file because not a directory".PHP_EOL;
-			continue;
-		}
-		if (1 != preg_match("/\d+/",$file) ) {
-			echo "skipping $file because not matching digits".PHP_EOL;
-			continue;
-		}
-		array_push($datasets, $file);
-	}
-	return $datasets;
+    $datasets = [] ;
+    $handle = opendir($download_path);
+    while (($file = readdir($handle)) !== false) {
+        if ($file === '.' || $file === '..') {
+            echo "skipping $file because dot file".PHP_EOL;
+            continue;
+        }
+        if ( true != is_dir("$download_path/$file") ) {
+            echo "skipping $file because not a directory".PHP_EOL;
+            continue;
+        }
+        if (1 != preg_match("/\d+/",$file) ) {
+            echo "skipping $file because not matching digits".PHP_EOL;
+            continue;
+        }
+        array_push($datasets, $file);
+    }
+    return $datasets;
 }
 
 /**
@@ -111,9 +111,9 @@ function getDatasetDirectories(string $download_path): array
  */
 function getFiles(string $path): array
 {
-	clearstatcache();
-	$scanned_directory = array_diff(scandir($path), array('..', '.', '.DS_Store'));
-	return $scanned_directory;
+    clearstatcache();
+    $scanned_directory = array_diff(scandir($path), array('..', '.', '.DS_Store'));
+    return $scanned_directory;
 }
 
 /**
@@ -121,7 +121,7 @@ function getFiles(string $path): array
  */
 function touchFlag()
 {
-	touch(FLAG_PATH);
+    touch(FLAG_PATH);
 }
 
 /**
@@ -132,7 +132,7 @@ function touchFlag()
  */
 function getFlag(string $doi_suffix): string
 {
-	return FLAG_PATH."/".$doi_suffix;
+    return FLAG_PATH."/".$doi_suffix;
 }
 
 /**
@@ -143,7 +143,7 @@ function getFlag(string $doi_suffix): string
  */
 function setFlag(string $doi_suffix): bool
 {
-	return touch(FLAG_PATH."/".$doi_suffix);
+    return touch(FLAG_PATH."/".$doi_suffix);
 }
 
 /**
@@ -159,23 +159,23 @@ function setFlag(string $doi_suffix): bool
  */
 function isDirectoryNewerThan(string $dirToCheck, string $referenceFile): bool
 {
-	clearstatcache();
-	if (false == file_exists($referenceFile)) {
-		return true;
-	}
-	if (false == file_exists($dirToCheck)) {
-		return false;
-	}
+    clearstatcache();
+    if (false == file_exists($referenceFile)) {
+        return true;
+    }
+    if (false == file_exists($dirToCheck)) {
+        return false;
+    }
 
-	$dirTime = filemtime("$dirToCheck/.");
-	$refTime = filemtime($referenceFile);
-	echo "    is_newer:".PHP_EOL;
-	echo "      dir mtime: ".$dirTime.PHP_EOL;
-	echo "      flag mtime: ".$refTime.PHP_EOL;
-	if ($dirTime >= $refTime) {
-		return true;
-	}
-	return false;
+    $dirTime = filemtime("$dirToCheck/.");
+    $refTime = filemtime($referenceFile);
+    echo "    is_newer:".PHP_EOL;
+    echo "      dir mtime: ".$dirTime.PHP_EOL;
+    echo "      flag mtime: ".$refTime.PHP_EOL;
+    if ($dirTime >= $refTime) {
+        return true;
+    }
+    return false;
 }
 /**
  * Verify whether the directory is newer than the file flag
@@ -185,19 +185,19 @@ function isDirectoryNewerThan(string $dirToCheck, string $referenceFile): bool
  */
 function is_newer(string $directory_path): bool
 {
-	clearstatcache();
-	if (false == file_exists(FLAG_PATH)) {
-		return true;
-	}
-	$dir_stats = stat($directory_path);
-	$flag_stats = stat(FLAG_PATH);
-	echo "  is_newer:".PHP_EOL;
-	echo "    dir mtime: ".$dir_stats[9].PHP_EOL;
-	echo "    flag mtime: ".$flag_stats[9].PHP_EOL;
-	if ($dir_stats[9] >= $flag_stats[9]) {
-		return true;
-	}
-	return false;
+    clearstatcache();
+    if (false == file_exists(FLAG_PATH)) {
+        return true;
+    }
+    $dir_stats = stat($directory_path);
+    $flag_stats = stat(FLAG_PATH);
+    echo "  is_newer:".PHP_EOL;
+    echo "    dir mtime: ".$dir_stats[9].PHP_EOL;
+    echo "    flag mtime: ".$flag_stats[9].PHP_EOL;
+    if ($dir_stats[9] >= $flag_stats[9]) {
+        return true;
+    }
+    return false;
 }
 
 /**
@@ -209,23 +209,23 @@ function is_newer(string $directory_path): bool
  */
 function fileMetadata(string $file_name, string $dataset): array
 {
-	$file_path = "/home/downloader/$dataset/$file_name";
-	$file_stats = stat($file_path);
-	$metadata = array(
-					"file_name" => $file_name,
-					"data_type" => null,
-					"format" => null,
-					"size" => $file_stats[7],
-					"link" => null,
-					"md5" => null,
-					"extension" => pathinfo($file_path, PATHINFO_EXTENSION) ?? "",
-					"description" => $file_name
-				);
+    $file_path = "/home/downloader/$dataset/$file_name";
+    $file_stats = stat($file_path);
+    $metadata = array(
+        "file_name" => $file_name,
+        "data_type" => null,
+        "format" => null,
+        "size" => $file_stats[7],
+        "link" => null,
+        "md5" => null,
+        "extension" => pathinfo($file_path, PATHINFO_EXTENSION) ?? "",
+        "description" => $file_name
+    );
 
-	$metadata["format"] = getFileFormatFromFile($file_name);
-	$metadata["data_type"] = getApproximateDataTypeFromFile($file_name);
-	$metadata["link"] = generateFTPLink($file_name,$dataset);
-	return $metadata;
+    $metadata["format"] = getFileFormatFromFile($file_name);
+    $metadata["data_type"] = getApproximateDataTypeFromFile($file_name);
+    $metadata["link"] = generateFTPLink($file_name,$dataset);
+    return $metadata;
 }
 
 
@@ -236,15 +236,15 @@ function fileMetadata(string $file_name, string $dataset): array
  */
 function connectDB(): object
 {
-$appconfig = parse_ini_file("/config/db.ini");
+    $appconfig = parse_ini_file("/config/watcherconfig.ini");
 
-		$db_user = $appconfig["db_user"];
-		$db_password = $appconfig["db_password"];
-		$db_dsn = $appconfig["db_dsn"];
+    $db_user = $appconfig["db_user"];
+    $db_password = $appconfig["db_password"];
+    $db_dsn = $appconfig["db_dsn"];
 
-		$dbh = new PDO("$db_dsn", "$db_user", "$db_password");
-		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //PHP warnings for SQL errors
-		return $dbh ;
+    $dbh = new PDO("$db_dsn", "$db_user", "$db_password");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); //PHP warnings for SQL errors
+    return $dbh ;
 }
 
 /**
@@ -258,36 +258,36 @@ $appconfig = parse_ini_file("/config/db.ini");
  */
 function updateFileTable(object $dbh, string $dataset_doi, array $uploadedFilesMetadata): int
 {
-	$result = 0;
+    $result = 0;
 
-	$delete = "delete from upload where doi= ? and status = 'uploading'";
-	$insert = "insert into upload(doi,name,size,status,location,description, extension) values(:d , :n , :z , 'uploading', :l, :s, :e)";
+    $delete = "delete from upload where doi= ? and status = 'uploading'";
+    $insert = "insert into upload(doi,name,size,status,location,description, extension) values(:d , :n , :z , 'uploading', :l, :s, :e)";
 
-	$delete_statement = $dbh->prepare($delete);
-	$delete_statement->bindParam(1, $dataset_doi);
-	$delete_statement->execute();
+    $delete_statement = $dbh->prepare($delete);
+    $delete_statement->bindParam(1, $dataset_doi);
+    $delete_statement->execute();
 
-	$insert_statement = $dbh->prepare($insert);
-	$insert_statement->bindParam(':d', $dataset_doi);
-	$insert_statement->bindParam(':n', $name);
-	$insert_statement->bindParam(':z', $size);
-	$insert_statement->bindParam(':l', $location);
-	// $insert_statement->bindParam(':f', $format);
-	// $insert_statement->bindParam(':t', $data_type);
-	$insert_statement->bindParam(':e', $extension);
-	$insert_statement->bindParam(':s', $summary);
-	foreach ($uploadedFilesMetadata as $file) {
-		$name = $file["file_name"] ;
-		$size = $file["size"] ;
-		$location = $file["link"] ;
-		// $format = $file["format"] ;
-		// $data_type = $file["data_type"] ;
-		$extension = $file["extension"] ;
-		$summary = $file["description"] ;
-		$result += $insert_statement->execute();
-	}
+    $insert_statement = $dbh->prepare($insert);
+    $insert_statement->bindParam(':d', $dataset_doi);
+    $insert_statement->bindParam(':n', $name);
+    $insert_statement->bindParam(':z', $size);
+    $insert_statement->bindParam(':l', $location);
+    // $insert_statement->bindParam(':f', $format);
+    // $insert_statement->bindParam(':t', $data_type);
+    $insert_statement->bindParam(':e', $extension);
+    $insert_statement->bindParam(':s', $summary);
+    foreach ($uploadedFilesMetadata as $file) {
+        $name = $file["file_name"] ;
+        $size = $file["size"] ;
+        $location = $file["link"] ;
+        // $format = $file["format"] ;
+        // $data_type = $file["data_type"] ;
+        $extension = $file["extension"] ;
+        $summary = $file["description"] ;
+        $result += $insert_statement->execute();
+    }
 
-	return $result;
+    return $result;
 }
 
 clearstatcache();
@@ -295,20 +295,20 @@ $dbh = connectDB();
 echo "Scanning file system...".PHP_EOL;
 
 foreach (getDatasetDirectories("/home/downloader/") as $dataset_dir) {
-	echo "  * Found dataset directory $dataset_dir".PHP_EOL;
-	if ( isDirectoryNewerThan( "/home/downloader/$dataset_dir", getFlag($dataset_dir) ) ) {
+    echo "  * Found dataset directory $dataset_dir".PHP_EOL;
+    if ( isDirectoryNewerThan( "/home/downloader/$dataset_dir", getFlag($dataset_dir) ) ) {
 
-		$files = [];
-		foreach ( getFiles("/home/downloader/$dataset_dir") as $file ) {
-			echo "    Gathering metadata for $file".PHP_EOL;
-			array_push( $files, 	fileMetadata($file, $dataset_dir) );
-		}
-		echo "    Updating File table".PHP_EOL;
-		$nbRecords = updateFileTable($dbh, $dataset_dir, $files);
-		echo "    Number of records changed for files in $dataset_dir: $nbRecords".PHP_EOL;
+        $files = [];
+        foreach ( getFiles("/home/downloader/$dataset_dir") as $file ) {
+            echo "    Gathering metadata for $file".PHP_EOL;
+            array_push( $files, 	fileMetadata($file, $dataset_dir) );
+        }
+        echo "    Updating File table".PHP_EOL;
+        $nbRecords = updateFileTable($dbh, $dataset_dir, $files);
+        echo "    Number of records changed for files in $dataset_dir: $nbRecords".PHP_EOL;
 
-		setFlag($dataset_dir);
-	}
+        setFlag($dataset_dir);
+    }
 }
 
 
