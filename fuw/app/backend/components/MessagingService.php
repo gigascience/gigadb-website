d28bf5c7a15c3a15343bed3a1c4610e8f1f82281
Rija Menage <rija+git@cinecinetique.com>
2019-10-31 15:46:15 +0100

    Add a Messaging Service to File Upload Wizard
    Goal: This service's sendEmailMessage function will be called
    by filedrop-accounts controller's update action when accompanied with email related query strings.
    
    The service is implemented as a Yii2 Component in the backend (admin) webapp,
    it uses yii2-swiftmailer included in the Yii2 framework for sending mail.
    yii2-swiftmailer is configured in Yii2 config files (main-local.php.dist, test-local.php.dist).
    
    In the future the service would be place to handle other kind of messaging.

---
+++ fuw/app/backend/components/MessagingService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace backend\components;
+
+/**
+ * Service for handling message sending
+ *
+ * @author Rija Menage <rija+git@cinecinetique.com>
+ * @license GPL-3.0
+ */
+class MessagingService extends \yii\base\Component
+{
+
+	/** @var yii\mail\MailerInterface $mailer class to send email */
+	protected $mailer;
+
+	public function __construct(/*yii\mail\MailerInterface*/ $mailer, $config = [])
+	{
+		$this->mailer = $mailer;
+
+		parent::__construct($config);
+	}
+
+	/**
+	 * send email message
+	 *
+	 * @param string $from sender email
+	 * @param string $to recipient email
+	 * @param string $subject email's subject
+	 * @param string $content content to send
+	 */
+	public function sendEmailMessage(string $from, string $to, string $subject, string $content)
+	{
+		$this->mailer->compose()
+		    ->setFrom($from)
+		    ->setTo($to)
+		    ->setSubject($subject)
+		    ->setTextBody($content)
+		    ->send();
+	}
+
+}
+?>
---
+++ fuw/app/backend/tests/unit/MessagingServiceTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace backend\tests;
+
+use \yii\mail\Mailer;
+use \yii\mail\Message;
+use backend\components\MessagingService;
+
+
+class MessagingServiceTest extends \Codeception\Test\Unit
+{
+
+    /**
+     * test MessagingService can send email
+     */
+    public function testSendEmail()
+    {
+
+        $from = "admin@gigadb.org";
+        $to = "user@gigadb.org";
+        $subject = "Uploading instructions";
+        $content = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo";
+
+        $mockMailer = $this->getMockBuilder(Mailer::class)
+                    ->setMethods(['compose'])
+                    // ->disableOriginalConstructor()
+                    ->getMock();
+
+        $mockMessage = $this->getMockBuilder(Message::class)
+                    ->setMethods(['setFrom','setTo','setSubject','setTextBody','send'])
+                    // ->disableOriginalConstructor()
+                    ->getMock();
+
+
+        $mockMailer->expects($this->once())
+                    ->method('compose')
+                    ->willReturn($mockMessage);
+
+        $mockMessage->expects($this->once())
+                    ->method('setFrom')
+                    ->with($from)
+                    ->willReturn($mockMessage);
+
+        $mockMessage->expects($this->once())
+                    ->method('setTo')
+                    ->with($to)
+                    ->willReturn($mockMessage);
+
+        $mockMessage->expects($this->once())
+                    ->method('setSubject')
+                    ->with($subject)
+                    ->willReturn($mockMessage);
+
+
+        $mockMessage->expects($this->once())
+                    ->method('setTextBody')
+                    ->with($content)
+                    ->willReturn($mockMessage);
+
+        $mockMessage->expects($this->once())
+                    ->method('send');
+
+        $msgSrv = new MessagingService($mockMailer);
+        $msgSrv->sendEmailMessage($from, $to, $subject, $content);
+    }
+
+
+}
