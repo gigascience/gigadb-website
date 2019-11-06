49f619961c2c1c19518f4126cc362dc72be20c35
Rija Menage <rija+git@cinecinetique.com>
2019-11-06 17:45:23 +0800

    Update FiledropService's createAccount to return new object's properties
    The File Upload Wizard API endpoint for creating a filedrop account
    returns a json structure with the account properties.
    
    FiledropService can retrieve the json response and pass on to the caller
    (the GigaDB dataset admin page that need the id of the created account to save the email instructions in it)

--- fuw/app/backend/tests/functional/FiledropAccountCest.php
+++ fuw/app/backend/tests/functional/FiledropAccountCest.php
@@ -233,6 +233,8 @@ class FiledropAccountCest
                                             "subject" => $subject,
                                             "send" => 1 ]);
         $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(array('id' => $filedrop->id));
 
         $updated = FiledropAccount::find()->where(['id' => $filedrop->id])->one();
         $I->assertEquals($doi,$updated->doi);
