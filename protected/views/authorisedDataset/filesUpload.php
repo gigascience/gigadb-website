2d257bfa23f989b90c94075ef83b66b9a090b355
Rija Menage <rija+git@cinecinetique.com>
2019-11-20 18:29:27 +0800

    Add JS uploader from prototype to GigaDB in a Vue.js template
    So added all the new JS dependencies in a new Yii layout.
    Acceptance tests scenarios implemented further accordingly.
    
    As AdminDatasetController.php is for admin operations on dataset,
    and DatasetController.php for the operations by the public at large
    
    I've created AuthorisedDatasetController for operations by logged in users
    who need authorisation for performing actions (like uploading dataset files).
    
    (Eventually, the existing update/delete dataset operations on user profile
    will be moved in that controller.)
    
    As described above, the uploader will be actually  new GigaDB endpoints,
    which will connect to File Upload Wizard (FUW) REST API as needed.
    
    The FUW will have a fuw-admin-api container service for filedrop account management operations
    and a fuw-public-api container service (whose nginx config is fuw-frontend.dev.http.conf) for supporter uploader operations described above.

--- protected/views/authorisedDataset/filesUpload.php
+++ protected/views/authorisedDataset/filesUpload.php
@@ -1,62 +1,66 @@
 <?php ?>
+
 <div id="gigadb-dataset-uploader">
-    <h1>{{ message}} for dataset {{ doi }}</h1>
-    <form id="dataset-metadata-form">
-        <input id="dataset" type="hidden" v-bind:value="doi">
-    </form>
-    <div id="drag-drop-area"></div>
-    <script>
-    document.addEventListener("DOMContentLoaded", function(event) {
 
-        var app = new Vue({
-            el: '#gigadb-dataset-uploader',
-            data: {
-                doi: <?php  echo "'".$identifier."'"; ?>,
-                message: 'GigaDB File Uploader',
-                seen: true,
-                todos: [
-                    { text: 'Buy jacket' },
-                    { text: 'Call Dad' },
-                    { text: 'Feed the cat' }
-                ]
-            },
-            methods: {
-                reverseMessage: function() {
-                    this.message = this.message.split('').reverse().join('')
-                }
-            }
-        });
+<h1>{{ message}} for dataset {{ doi }}</h1>
 
-        var uppy = Uppy.Core()
-            .use(Uppy.Dashboard, {
-                inline: true,
-                target: '#drag-drop-area',
-                hideAfterFinish: true,
-                showProgressDetails: true,
-                hideUploadButton: false,
-                hideRetryButton: false,
-                hidePauseResumeButton: false,
-                hideCancelButton: false,
-                proudlyDisplayPoweredByUppy: false,
-                metaFields: [
-                    { id: 'name', name: 'Name', placeholder: 'file name' },
-                    { id: 'description', name: 'Description', placeholder: 'describe what the file is about' }
-                ],
-                locale: {}
-            })
-            .use(Uppy.Form, {
-                target: "#dataset-metadata-form",
-                getMetaFromForm: true,
-                addResultToForm: false,
-                triggerUploadOnSubmit: false,
-                submitOnSuccess: false
-            })
-            // .use(Uppy.Tus, {endpoint: 'https://master.tus.io/files/'});
-            .use(Uppy.Tus, { endpoint: '/files/' });
+<form id="dataset-metadata-form">
+	<input id="dataset" type="hidden" v-bind:value="doi">
+</form>
+	<div id="drag-drop-area"></div>
 
-        uppy.on('complete', function(result) {
-            console.log('Upload complete! We’ve uploaded these files:', result.successful);
-        });
+<script>
+document.addEventListener("DOMContentLoaded", function(event) {
 
-    });
-    </script>
+	var app = new Vue({
+		el:'#gigadb-dataset-uploader',
+	  	data: {
+		  	doi: <?php  echo "'".$identifier."'"; ?>,
+		  	message: 'GigaDB File Uploader',
+		  	seen: true,
+		    todos: [
+		      { text: 'Buy jacket'},
+		      { text: 'Call Dad'},
+		      { text: 'Feed the cat'}
+		    ]
+		 },
+	  	methods: {
+		  	reverseMessage: function () {
+		  		this.message = this.message.split('').reverse().join('')
+		  	}
+		}
+	});
+
+	var uppy = Uppy.Core()
+					.use(Uppy.Dashboard, {
+						inline: true,
+						target: '#drag-drop-area',
+						hideAfterFinish: true,
+						showProgressDetails: true,
+						hideUploadButton: false,
+						hideRetryButton: false,
+						hidePauseResumeButton: false,
+						hideCancelButton: false,
+						proudlyDisplayPoweredByUppy: false,
+						metaFields: [
+						    { id: 'name', name: 'Name', placeholder: 'file name' },
+						    { id: 'description', name: 'Description', placeholder: 'describe what the file is about' }
+						  ],
+						locale: {}
+					})
+					.use(Uppy.Form, {
+						  target: "#dataset-metadata-form",
+						  getMetaFromForm: true,
+						  addResultToForm: false,
+						  triggerUploadOnSubmit: false,
+						  submitOnSuccess: false
+						})
+					// .use(Uppy.Tus, {endpoint: 'https://master.tus.io/files/'});
+					.use(Uppy.Tus, {endpoint: '/files/'});
+
+		uppy.on('complete', function (result) {
+			console.log('Upload complete! We’ve uploaded these files:', result.successful);
+		});
+
+});
+</script>
