ab2f86bb87e97b8e0d6f781ab0029ba900317e20
Rija Menage <rija+git@cinecinetique.com>
2019-12-24 11:27:03 +0800

    Widen comments column in curation_log to accomodate upload instructions

---
+++ protected/migrations/m191224_025216_widen_curation_log_comments_column.php
@@ -0,0 +1,25 @@
+<?php
+
+class m191224_025216_widen_curation_log_comments_column extends CDbMigration
+{
+	public function up()
+	{
+		$this->alterColumn('curation_log','comments','varchar(2000)');
+	}
+
+	public function down()
+	{
+		$this->alterColumn('curation_log','comments','varchar(1000');
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
