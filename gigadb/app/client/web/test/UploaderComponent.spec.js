8090f9eb704778dc0b4bdbed10bfe9f50feaafab
Rija Menage <rija+git@cinecinetique.com>
2020-01-09 15:47:07 +0800

    Fix inconsistencies and brittleness in the JS unit tests
    Because we are testing the behaviour of the Uppy component after its host
    mounted and connected to the DOM, we need to ensure our test wrappper for
    the host component  has "attachToDocument" set to true,
    and all our expectations need to be wrapped in Vue.nextTick() to ensure the
    test wrapper has completely loaded before we probe the rendered structure.
    Also removed duplication in the test wrapper factory.

--- gigadb/app/client/web/test/UploaderComponent.spec.js
+++ gigadb/app/client/web/test/UploaderComponent.spec.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import { shallowMount, mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import UploaderComponent from '../src/components/UploaderComponent.vue'
 
-const factory = function(values = {}) {
+const factory = function(options = {}, values = {}) {
     return shallowMount(UploaderComponent, {
-        attachToDocument: true,
+        ...options,
         data() {
             return {
                 ...values
@@ -15,23 +15,28 @@ const factory = function(values = {}) {
 
 describe('Uploader component', function() {
     it('should load Uppy Dashboard', function() {
-        const renderedComponent = factory()
-        expect(renderedComponent.find('.uppy-Dashboard-dropFilesTitle').text()).toContain('Drop files here, paste or')
+        const renderedComponent = factory({
+                attachToDocument: true,
+                propsData: {
+                    identifier: '000000',
+                    endpoint: '/foobar/',
+                },
+            }
+        )
+        Vue.nextTick().then(function () {
+            expect(renderedComponent.find('.uppy-Dashboard-dropFilesTitle').text()).toContain('Drop files here, paste or')
+        })
     })
 
     it('should set value of the dataset hidden text field from props', function() {
-        const renderedComponent = mount(UploaderComponent, {
-            // attachToDocument: true,
-            propsData: {
-                identifier: '000000',
-                endpoint: '/foobar/',
-            },
-            data: function() {
-                return {
-                    uppy: '',
-                }
+        const renderedComponent = factory({
+                attachToDocument: true,
+                propsData: {
+                    identifier: '000000',
+                    endpoint: '/foobar/',
+                },
             }
-        })
+        )
         // console.log(renderedComponent.find('#dataset').element.attributes)
         expect(renderedComponent.exists()).toBe(true)
         Vue.nextTick().then(function () {
@@ -40,18 +45,14 @@ describe('Uploader component', function() {
     })
 
     it('should set TUS endpoint from props', function() {
-        const renderedComponent = shallowMount(UploaderComponent, {
-            // attachToDocument: true,
-            propsData: {
-                identifier: '000000',
-                endpoint: '/foobar/',
-            },
-            data: function() {
-                return {
-                    uppy: '',
-                }
+        const renderedComponent = factory({
+                attachToDocument: true,
+                propsData: {
+                    identifier: '000000',
+                    endpoint: '/foobar/',
+                },
             }
-        })
+        )
         // console.log(renderedComponent.vm.uppy.getPlugin('Tus').opts['endpoint'])
         expect(renderedComponent.vm.uppy.getPlugin('Tus').opts['endpoint']).toEqual('/foobar/')
     })
