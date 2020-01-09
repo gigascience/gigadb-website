157c40d842295a4e4706e57c76e8abcf8d84fe75
Rija Menage <rija+git@cinecinetique.com>
2020-01-09 19:07:49 +0800

    Replace direct use of Uppy by wrapping it in a Vue component
    This is so because we cannot have the page being a proper Vue application's in-document template,
    and separately instantiate the Uppy library in its own <script> tag.
    Additionally we need to handle in our Vue application events emitted by the Uppy library.
    So, instead we've wrapped the Uppy library in the UploaderComponent Vue component with events pass-through
    and use the associated custom element in the authorisedDatasetController.php filesUpload action's view page
    (filesUpload.php).

--- gigadb/app/client/web/src/index.js
+++ gigadb/app/client/web/src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import DatasetInfoComponent from './components/DatasetInfoComponent.vue'
+import UploaderComponent from './components/UploaderComponent.vue'
 
 Vue.config.productionTip = false
 
@@ -7,5 +8,6 @@ new Vue({
 	el: '#gigadb-fuw',
 	components: {
 		'dataset-info': DatasetInfoComponent,
+		'uploader': UploaderComponent,
 	}
 })
--- protected/views/authorisedDataset/filesUpload.php
+++ protected/views/authorisedDataset/filesUpload.php
@@ -1,8 +1,8 @@
 <?php ?>
 <div class="content">
-    <div class="container">
-        <section class="page-title-section">
-            <div id="gigadb-fuw">
+    <div id="gigadb-fuw">
+        <div class="container">
+            <section class="page-title-section">
                 <div class="page-title">
                     <ol class="breadcrumb pull-right">
                         <li><a href="/">Home</a></li>
@@ -10,49 +10,11 @@
                     </ol>
                     <dataset-info identifier="<?= $identifier ?>" />
                 </div>
-            </div>
-        </section>
-        <section>
-            <form id="dataset-metadata-form">
-                <input id="dataset" type="hidden" v-bind:value="doi">
-            </form>
-            <div id="drag-drop-area"></div>
-            <script>
-            document.addEventListener("DOMContentLoaded", function(event) {
-
-                var uppy = Uppy.Core()
-                    .use(Uppy.Dashboard, {
-                        inline: true,
-                        target: '#drag-drop-area',
-                        hideAfterFinish: true,
-                        showProgressDetails: true,
-                        hideUploadButton: false,
-                        hideRetryButton: false,
-                        hidePauseResumeButton: false,
-                        hideCancelButton: false,
-                        proudlyDisplayPoweredByUppy: false,
-                        metaFields: [
-                            { id: 'name', name: 'Name', placeholder: 'file name' },
-                            { id: 'description', name: 'Description', placeholder: 'describe what the file is about' }
-                        ],
-                        locale: {}
-                    })
-                    .use(Uppy.Form, {
-                        target: "#dataset-metadata-form",
-                        getMetaFromForm: true,
-                        addResultToForm: false,
-                        triggerUploadOnSubmit: false,
-                        submitOnSuccess: false
-                    })
-                    // .use(Uppy.Tus, {endpoint: 'https://master.tus.io/files/'});
-                    .use(Uppy.Tus, { endpoint: '/files/' });
-
-                uppy.on('complete', function(result) {
-                    console.log('Upload complete! We’ve uploaded these files:', result.successful);
-                });
-
-            });
-            </script>
-        </section>
+            </section>
+            <section>
+                <uploader identifier="<?= $identifier ?>"
+                            endpoint="/files/" />
+            </section>
+        </div>
     </div>
 </div>
