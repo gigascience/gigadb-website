dc9fd7e7fa613e90ac8a9974201e9c9a4a8576f4
Rija Menage <rija+git@cinecinetique.com>
2020-01-09 15:59:33 +0800

    Remove duplications in JS unit tests
    use Jasmine BeforeEach() to initialise the test wrapper for the component
    under test.

--- gigadb/app/client/web/test/UploaderComponent.spec.js
+++ gigadb/app/client/web/test/UploaderComponent.spec.js
@@ -14,8 +14,10 @@ const factory = function(options = {}, values = {}) {
 }
 
 describe('Uploader component', function() {
-    it('should load Uppy Dashboard', function() {
-        const renderedComponent = factory({
+    let renderedComponent = null
+
+    beforeEach(function () {
+        renderedComponent = factory({
                 attachToDocument: true,
                 propsData: {
                     identifier: '000000',
@@ -23,37 +25,25 @@ describe('Uploader component', function() {
                 },
             }
         )
+    })
+
+    it('should load Uppy Dashboard', function() {
         Vue.nextTick().then(function () {
             expect(renderedComponent.find('.uppy-Dashboard-dropFilesTitle').text()).toContain('Drop files here, paste or')
         })
     })
 
     it('should set value of the dataset hidden text field from props', function() {
-        const renderedComponent = factory({
-                attachToDocument: true,
-                propsData: {
-                    identifier: '000000',
-                    endpoint: '/foobar/',
-                },
-            }
-        )
         // console.log(renderedComponent.find('#dataset').element.attributes)
-        expect(renderedComponent.exists()).toBe(true)
         Vue.nextTick().then(function () {
             expect(renderedComponent.find('#dataset').attributes('value')).toBe("000000")
         })
     })
 
     it('should set TUS endpoint from props', function() {
-        const renderedComponent = factory({
-                attachToDocument: true,
-                propsData: {
-                    identifier: '000000',
-                    endpoint: '/foobar/',
-                },
-            }
-        )
         // console.log(renderedComponent.vm.uppy.getPlugin('Tus').opts['endpoint'])
+        // no need to use Vue.nextTick() here as we are testing instance's variable
+        // not the rendered content
         expect(renderedComponent.vm.uppy.getPlugin('Tus').opts['endpoint']).toEqual('/foobar/')
     })
 
