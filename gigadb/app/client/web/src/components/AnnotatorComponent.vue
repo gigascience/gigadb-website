744ccdaf75d238ab5c3fea52e226785469d3e7f4
Rija Menage <rija+git@cinecinetique.com>
2020-01-17 16:55:48 +0800

    Implement the populating of metadata form with list of uploaded files
    The Annotator Vue component makes an async request to the File Upload Wizard endpoint that
    list all uploaded files for a particular DOI and then populate the HTML table in its template.
    
    Some basics unit tests there and passing.

--- gigadb/app/client/web/src/components/AnnotatorComponent.vue
+++ gigadb/app/client/web/src/components/AnnotatorComponent.vue
@@ -12,10 +12,10 @@
         </thead>
         <tbody>
             <tr v-for="upload in uploads">
-            	<td>{{ upload.FileName }}</td>
-            	<td>{{ upload.DataType }}</td>
-            	<td>{{ upload.Format }}</td>
-            	<td>{{ upload.Size }}</td>
+            	<td>{{ upload.name }}</td>
+            	<td>{{ upload.datatype }}</td>
+            	<td>{{ upload.extension }}</td>
+            	<td>{{ upload.size }}</td>
             	<td></td>
             	<td></td>
             </tr>
@@ -29,25 +29,59 @@
 import axios from "axios"
 
 export default {
-	props: ['identifier'],
+	props: ['identifier','token'],
 	data: function() {
 		return {
 			uploads: null,
 			loading: true,
-	      	errored: false
+	      	errored: false,
+	      	apiUrl: 'http://gigadb.gigasciencejournal.com:9170',
+	      	endpoint: '/fuw/api/v1/public/upload/',
+	      	webclient: null,
+		}
+	},
+	methods: {
+		fetchRemoteData: function () {
+
+			console.log('Fetching remote data on endpoint '+this.endpoint)
+			this.webclient
+		      .get(this.endpoint, { params: {'filter[doi]': this.identifier}} )
+		      .then(response => {
+		        this.uploads = response
+		        console.log('Response:'+response)
+		      })
+		      .catch(error => {
+				if (error.response) {
+				  // The request was made and the server responded with a status code
+				  // that falls out of the range of 2xx
+				  console.log(error.response.data)
+				  console.log(error.response.status)
+				  console.log(error.response.headers)
+				} else if (error.request) {
+				  // The request was made but no response was received
+				  // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+				  // http.ClientRequest in node.js
+				  console.log(error.request)
+				} else {
+				  // Something happened in setting up the request that triggered an Error
+				  console.log('Error', error.message)
+				}
+				console.log(error.config)
+		        this.errored = true
+		      })
+		      .finally(() => this.loading = false)
 		}
 	},
 	mounted: function() {
-		axios
-	      .get('http://gigadb.gigasciencejournal:9170/fuw/api/v1/public/upload?doi='+this.identifier)
-	      .then(response => {
-	        this.uploads = response.uploads
-	      })
-	      .catch(error => {
-	        console.log(error)
-	        this.errored = true
-	      })
-	      .finally(() => this.loading = false)
+		const vm = this
+		this.$nextTick(function () {
+			vm.webclient = axios.create({
+			  baseURL: vm.apiUrl,
+			  timeout: 1000,
+			  headers: {'Authorization': 'Bearer '+vm.token}
+			})
+			vm.fetchRemoteData()
+		})
 	}
 }
 </script>
--- gigadb/app/client/web/test/AnnotatorComponent.spec.js
+++ gigadb/app/client/web/test/AnnotatorComponent.spec.js
@@ -5,7 +5,6 @@ import AnnotatorComponent from '../src/components/AnnotatorComponent.vue'
 import { eventBus } from '../src/index.js'
 
 import axios from 'axios'
-import moxios from 'moxios'
 
 const factory = function(options = {}, values = {}) {
     return shallowMount(AnnotatorComponent, {
@@ -20,38 +19,57 @@ const factory = function(options = {}, values = {}) {
 
 describe('Annotator component', function() {
 
-    beforeEach(function() {
-        moxios.install()
-    })
-    afterEach(function() {
-        moxios.uninstall()
-    })
 
+    it('should make a call to the FUW api with identifier parameter', function() {
+    	// first get a random string to be our test DOI
+    	let array = new Uint32Array(1)
+		let doi = window.crypto.getRandomValues(array).toString()
+		// add a spy to our http client and let the real method be called
+        let instance = axios.create()
+        spyOn(instance, 'get')
+        // instantiate our component
+        const renderedComponent = factory({
+			propsData: {
+				identifier: doi,
+			}
+        }, {
+        	webclient: instance,
+        })
+        Vue.nextTick().then(function() {
+	        expect(instance.get).toHaveBeenCalledWith('http://gigadb.gigasciencejournal.com:9170/fuw/api/v1/public/upload/', { params: {'filter[doi]': doi}});
+	    })
+    })
     it('should show rows matching the number of uploaded files', function() {
-        spyOn(axios, 'get').and.callThrough()
         const renderedComponent = factory({
 			propsData: {
 				identifier: '000000',
 			}
+        }, {
+        	apiUrl: 'http://json-server:3000'
         })
-        expect(axios.get).toHaveBeenCalledWith('http://gigadb.gigasciencejournal:9170/fuw/api/v1/public/upload?doi=000000');
-        moxios.wait(function() {
-            let request = moxios.requests.mostRecent()
-            request.respondWith({
-                status: 200,
-                response: {
-                    uploads: [
-                        { id: 1, doi: '000000', FileName: 'TheProof.csv', DataType: 'Text', Format: 'TEXT', Size: '112KiB' },
-                        { id: 2, doi: '000000', FileName: 'TheProof2.csv', DataType: 'Text', Format: 'TEXT', Size: '112KiB' },
-                    ]
-                }
-            }).then(function() {
-                expect(renderedComponent.findAll('tbody tr').length).toBe(2)
-                done()
-            })
+        Vue.nextTick().then(function (){
+	        renderedComponent.vm.webclient.done(function() {
+
+		        expect(renderedComponent.findAll('tbody tr').length).toBe(2)
+	        })
         })
     })
-    //TODO: should show file names of all upload files
+    it('should show file names of all upload files', function() {
+        const renderedComponent = factory({
+			propsData: {
+				identifier: '000000',
+			}
+        }, {
+        	apiUrl: 'http://json-server:3000'
+        })
+        Vue.nextTick().then(function (){
+	        renderedComponent.vm.webclient.done(function() {
+
+		        expect(renderedComponent.find('tbody')).toContain('TheProof.csv')
+		        expect(renderedComponent.find('tbody')).toContain('TheProof2.csv')
+	        })
+        })
+    })
     //TODO: should show data type of all upload files
     //TODO: should show format of all upload files
     //TODO: should show size of all upload files
