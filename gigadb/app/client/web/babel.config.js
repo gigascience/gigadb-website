5773a84b78252e1ccd06900dfd865909d7f3d990
Rija Menage <rija+git@cinecinetique.com>
2020-01-18 19:58:14 +0800

    Fix a crash when running Javascript unit tests
    The tests were crashing with error:
    
    ReferenceError: regeneratorRuntime is not defined
    
    Found the best fix here:
    https://stackoverflow.com/a/53712824/6518111

--- gigadb/app/client/web/babel.config.js
+++ gigadb/app/client/web/babel.config.js
@@ -10,7 +10,7 @@ module.exports = function (api) {
     presets,
     "env": {
             "test": {
-                "plugins": ["istanbul"]
+                "plugins": ["istanbul","@babel/plugin-transform-runtime"]
             }
         }
   };
