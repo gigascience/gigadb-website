49e95c5401a9195c98dee1b13dd41d4be3500a2a
Rija Menage <rija+git@cinecinetique.com>
2020-01-20 11:41:07 +0800

    Simplify scope of Annotator component in File Upload Wizard
    Instead of dealing with the complexity of async http client and its testing,
    the Annotator Vue component for the metadata form assume the list of files
    will be passed as parameters (props).

--- gigadb/app/client/web/test/AnnotatorComponent.spec.js
+++ gigadb/app/client/web/test/AnnotatorComponent.spec.js
@@ -3,8 +3,7 @@ import { shallowMount } from '@vue/test-utils'
 import AnnotatorComponent from '../src/components/AnnotatorComponent.vue'
 
 import { eventBus } from '../src/index.js'
-
-import axios from 'axios'
+import testdata from './helper/db.json'
 
 const factory = function(options = {}, values = {}) {
     return shallowMount(AnnotatorComponent, {
@@ -20,39 +19,17 @@ const factory = function(options = {}, values = {}) {
 describe('Annotator component', function() {
 
 
-    it('should make a call to the FUW api with identifier parameter', function() {
-    	// first get a random string to be our test DOI
-    	let array = new Uint32Array(1)
-		let doi = window.crypto.getRandomValues(array).toString()
-		// add a spy to our http client and let the real method be called
-        let instance = axios.create()
-        spyOn(instance, 'get')
-        // instantiate our component
-        const renderedComponent = factory({
-			propsData: {
-				identifier: doi,
-			}
-        }, {
-        	webclient: instance,
-        })
-        Vue.nextTick().then(function() {
-	        expect(instance.get).toHaveBeenCalledWith('http://gigadb.gigasciencejournal.com:9170/fuw/api/v1/public/upload/', { params: {'filter[doi]': doi}});
-	    })
-    })
-    it('should show rows matching the number of uploaded files', function() {
+    it('should show rows matching the number of uploaded files', function(done) {
         const renderedComponent = factory({
 			propsData: {
 				identifier: '000000',
+				uploads: testdata.uploads
 			}
-        }, {
-        	apiUrl: 'http://json-server:3000'
         })
-        Vue.nextTick().then(function (){
-	        renderedComponent.vm.webclient.done(function() {
-
-		        expect(renderedComponent.findAll('tbody tr').length).toBe(2)
-	        })
-        })
+       	renderedComponent.vm.$nextTick(function () {
+	       	expect(renderedComponent.findAll('tbody tr').length).toBe(2)
+	       	done()
+       	})
     })
     it('should show file names of all upload files', function() {
         const renderedComponent = factory({
@@ -64,9 +41,10 @@ describe('Annotator component', function() {
         })
         Vue.nextTick().then(function (){
 	        renderedComponent.vm.webclient.done(function() {
-
+		        expect(renderedComponent.find('th')).toContain('File Name')
 		        expect(renderedComponent.find('tbody')).toContain('TheProof.csv')
-		        expect(renderedComponent.find('tbody')).toContain('TheProof2.csv')
+		        expect(renderedComponent.find('tbody')).toContain('TheProof2.jpg')
+		        expect(renderedComponent.find('tbody')).not.toContain('foobar.doc')
 	        })
         })
     })
