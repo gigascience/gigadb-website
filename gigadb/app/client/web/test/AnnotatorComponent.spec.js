5b503e17c559d796d20f599814c203272fc9fa43
Rija Menage <rija@gigasciencejournal.com>
2020-02-04 19:51:19 +0100

    Allow Annotator component to notify when all uploads' fields are ready
    Knowing when the user has filled info for all uploads, by emitting an event
    will allow controlling the display or not of the button to move to the next stage
    
    This done by capturing events from changes to the form fields and keeping a tally of completed uploads.
    
    in the test, when instantiating a component, make sure we pass a copy of the list of uploads
    (using ``JSON.parse(JSON.stringify( uploads ))``) otherwise the original list will be
    modified and break isolation between each tests, causing unexpected values in the list.

--- gigadb/app/client/web/test/AnnotatorComponent.spec.js
+++ gigadb/app/client/web/test/AnnotatorComponent.spec.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
-import { shallowMount } from '@vue/test-utils'
+import { enableAutoDestroy, shallowMount } from '@vue/test-utils'
 import AnnotatorComponent from '../src/components/AnnotatorComponent.vue'
 
 import { eventBus } from '../src/index.js'
 import testdata from './helper/db.json'
 
+const { uploads } = testdata
 const factory = function(options = {}, values = {}) {
     return shallowMount(AnnotatorComponent, {
         ...options,
@@ -16,70 +17,119 @@ const factory = function(options = {}, values = {}) {
     })
 }
 
+// enableAutoDestroy(afterEach)
+
 describe('Annotator component', function() {
-	let renderedComponent
+
 	beforeEach(function () {
-		renderedComponent = factory({
+		this.renderedComponent = factory({
+            attachToDocument: true,
 			propsData: {
 				identifier: '000000',
-				uploads: testdata.uploads
+				uploads: JSON.parse(JSON.stringify( uploads )) //we need a copy, not reference
 			}
         })
 	})
+
+    afterEach(function () {
+        eventBus.$off()
+    })
+
     it('should show rows matching the number of uploaded files', function() {
-       	renderedComponent.vm.$nextTick(function () {
-	       	expect(renderedComponent.findAll('tbody tr').length).toBe(2)
+        const wrapper = this.renderedComponent
+       	this.renderedComponent.vm.$nextTick(function () {
+	       	expect(wrapper.findAll('tbody tr').length).toBe(2)
        	})
     })
     it('should show file names of all upload files', function() {
-        renderedComponent.vm.$nextTick(function () {
-	        expect(renderedComponent.html()).toContain('File Name')
-	        expect(renderedComponent.html()).toContain('TheProof.csv')
-	        expect(renderedComponent.html()).toContain('TheProof2.jpg')
+        const wrapper = this.renderedComponent
+        wrapper.vm.$nextTick(function () {
+	        expect(wrapper.html()).toContain('File Name')
+	        expect(wrapper.html()).toContain('TheProof.csv')
+	        expect(wrapper.html()).toContain('TheProof2.jpg')
         })
     })
     it('should show data type of all upload files', function() {
-        renderedComponent.vm.$nextTick(function () {
-	        expect(renderedComponent.html()).toContain('Data Type')
-	        expect(renderedComponent.html()).toContain('Text')
-	        expect(renderedComponent.html()).toContain('Image')
+        const wrapper = this.renderedComponent
+        wrapper.vm.$nextTick(function () {
+	        expect(wrapper.html()).toContain('Data Type')
+	        expect(wrapper.html()).toContain('Text')
+	        expect(wrapper.html()).toContain('Image')
         })
     })
    it('should show format of all upload files', function() {
-        renderedComponent.vm.$nextTick(function () {
-	        expect(renderedComponent.html()).toContain('Format')
-	        expect(renderedComponent.html()).toContain('TEXT')
-	        expect(renderedComponent.html()).toContain('JPEG')
+        const wrapper = this.renderedComponent
+        wrapper.vm.$nextTick(function () {
+	        expect(wrapper.html()).toContain('Format')
+	        expect(wrapper.html()).toContain('TEXT')
+	        expect(wrapper.html()).toContain('JPEG')
         })
     })
    it('should show size of all upload files', function() {
-        renderedComponent.vm.$nextTick(function () {
-	        expect(renderedComponent.html()).toContain('Size')
-	        expect(renderedComponent.html()).toContain('1120000')
-	        expect(renderedComponent.html()).toContain('1170000')
+        const wrapper = this.renderedComponent
+        wrapper.vm.$nextTick(function () {
+	        expect(wrapper.html()).toContain('Size')
+	        expect(wrapper.html()).toContain('1120000')
+	        expect(wrapper.html()).toContain('1170000')
         })
     })
     it('should set uploads from json in props', function() {
-        expect(renderedComponent.vm.uploadedFiles).toEqual(testdata.uploads)
+        expect(this.renderedComponent.vm.uploadedFiles).toEqual(testdata.uploads)
     })
 
     it('should update the metadata for the first upload', function () {
         // Update two fields on the first of the two uploaded files
-        const selectField = renderedComponent.find('select[id="upload-1-datatype"]')
+        const selectField = this.renderedComponent.find('select[id="upload-1-datatype"]')
         selectField.setValue('Rich Text')
-        
-        const inputField = renderedComponent.find('input[id="upload-1-description"]')
+
+        const inputField = this.renderedComponent.find('input[id="upload-1-description"]')
         inputField.setValue('Some description here')
 
-        renderedComponent.vm.$nextTick(function () {
-            expect(renderedComponent.vm.uploadedFiles[0].datatype).toBe('Rich Text')
-            expect(renderedComponent.vm.uploadedFiles[0].description).toBe('Some description here')
+        const wrapper = this.renderedComponent
+        wrapper.vm.$nextTick(function () {
+            expect(wrapper.vm.uploadedFiles[0].datatype).toBe('Rich Text')
+            expect(wrapper.vm.uploadedFiles[0].description).toBe('Some description here')
             // the other fields remained unchanged
-            expect(renderedComponent.vm.uploadedFiles[0].doi).toBe('000000')
-            expect(renderedComponent.vm.uploadedFiles[0].extension).toBe('TEXT')
+            expect(wrapper.vm.uploadedFiles[0].doi).toBe('000000')
+            expect(wrapper.vm.uploadedFiles[0].extension).toBe('TEXT')
         })
 
 
     })
-    //TODO: it should emit an event when all fields for all uploads are filled in
+
+
+    it('should emit a not-ready event when not all fields for all uploads are filled in', function () {
+        let $emitted = false
+        eventBus.$on('metadata-ready-status', function(status) {
+            $emitted = status
+        })
+        // do stuff here (update fields on both files)
+        this.renderedComponent.find('select[id="upload-1-datatype"]').setValue('Rich Text')
+
+        this.renderedComponent.find('input[id="upload-1-description"]').setValue('Some description here')
+
+        this.renderedComponent.find('select[id="upload-2-datatype"]').setValue('Image')
+
+        expect($emitted).toBeFalse()
+        expect(this.renderedComponent.vm.isMetadataComplete()).toBeFalse()
+    })
+
+    it('should emit a ready event when all fields for all uploads are filled in', function () {
+        let $emitted = false
+        eventBus.$on('metadata-ready-status', function(status) {
+            $emitted = status //event bus would catch our component's 'complete' event
+        })
+        // do stuff here (update fields on both files)
+        this.renderedComponent.find('select[id="upload-1-datatype"]').setValue('Rich Text')
+
+        this.renderedComponent.find('input[id="upload-1-description"]').setValue('Some description here')
+
+        this.renderedComponent.find('select[id="upload-2-datatype"]').setValue('Image')
+
+        this.renderedComponent.find('input[id="upload-2-description"]').setValue('Further details about the thing')
+
+        // as all fields of both files updated, expect the event to have been emitted
+        expect($emitted).toBeTrue()
+        expect(this.renderedComponent.vm.isMetadataComplete()).toBeTrue()
+    })
 })
