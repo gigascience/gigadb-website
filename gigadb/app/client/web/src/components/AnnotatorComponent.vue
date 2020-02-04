fdba68df775bd8978a620e7a16f751879ce752ea
Rija Menage <rija@gigasciencejournal.com>
2020-02-04 19:51:19 +0100

    Allow Annotator component to notify when all uploads' fields are ready
    Knowing when the user has filled info for all uploads, by emitting an event
    will allow controlling the display or not of the button to move to the next stage
    
    This done by capturing events from changes to the form fields and keeping a tally of completed uploads.
    
    in the test, when instantiating a component, make sure we pass a copy of the list of uploads
    (using ``JSON.parse(JSON.stringify( uploads ))``) otherwise the original list will be
    modified and break isolation between each tests, causing unexpected values in the list.

--- gigadb/app/client/web/src/components/AnnotatorComponent.vue
+++ gigadb/app/client/web/src/components/AnnotatorComponent.vue
@@ -16,7 +16,7 @@
                     <td>{{ upload.name }}</td>
                     <td>
                         <div class="form-group">
-                            <select v-model="upload.datatype" name="datatype" v-bind:id="'upload-'+(index+1)+'-datatype'">
+                            <select v-model="upload.datatype" name="datatype" v-bind:id="'upload-'+(index+1)+'-datatype'" v-on:change="fieldHasChanged(index, $event)">
                                 <option v-for="datatype in dataTypes">{{datatype}}</option>
                             </select>
                         </div>
@@ -26,7 +26,7 @@
                     <td>
                         <div class="form-group required">
                             <label class='control-label'>
-                                <input v-model="upload.description" type="text" name="description" v-bind:id="'upload-'+(index+1)+'-description'" required>
+                                <input v-model="upload.description" type="text" name="description" v-bind:id="'upload-'+(index+1)+'-description'" v-on:input="fieldHasChanged(index, $event)" required>
                             </label>
                         </div>
                     </td>
@@ -43,20 +43,51 @@
 }
 </style>
 <script>
-import axios from "axios"
+import {eventBus} from '../index.js'
 
 export default {
-    props: ['identifier', 'token', 'uploads'],
-    data: function() {
-        return {
-            uploadedFiles: this.uploads || [],
-            dataTypes: [
-                "Text",
-                "Image",
-                "Rich Text",
-                "Genome Sequence",
-            ],
+        props: ['identifier', 'token', 'uploads'],
+        data: function() {
+            return {
+                uploadedFiles: this.uploads || [],
+                metaComplete: [],
+                dataTypes: [
+                    "Text",
+                    "Image",
+                    "Rich Text",
+                    "Genome Sequence",
+                ],
+            }
+        },
+        methods: {
+            fieldHasChanged(uploadIndex, event) {
+                if (this.uploadedFiles[uploadIndex].datatype != undefined && this.uploadedFiles[uploadIndex].datatype.length > 0 && this.uploadedFiles[uploadIndex].description != undefined && this.uploadedFiles[uploadIndex].description.length > 0) {
+                    this.metaComplete[uploadIndex] = true
+                }
+                else {
+                    this.metaComplete = this.metaComplete.filter( 
+                        (x,i) => i !== uploadIndex 
+                    )
+                    eventBus.$emit('metadata-ready-status', false)
+                }
+
+                if ( this.isMetadataComplete() ) {
+                    eventBus.$emit('metadata-ready-status', true)
+                }
+                else {
+                    eventBus.$emit('metadata-ready-status', false)
+                }
+            },
+            isMetadataComplete() {
+                return this.metaComplete.length === this.uploadedFiles.length
+            }
+        },
+        beforeDestroy: function () {
+            console.log("before destroy")
+            delete this.uploadedfiles
+        },
+        destroyed: function () {
+            console.log("after destroy")
         }
-    },
-}
+    }
 </script>
