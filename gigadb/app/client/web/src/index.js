2d2e6610a397cdf8391882b406783fb00bc26568
Rija Menage <rija@gigasciencejournal.com>
2020-02-10 17:00:19 +0100

    Import and setup ElementUI and vue-data-tables for file attributes
    We going to use vue-data-tables to build the UI for adding files attributes
    to the uploaded files in the metadata form, the annotating stage of file upload.

--- gigadb/app/client/web/src/index.js
+++ gigadb/app/client/web/src/index.js
@@ -1,13 +1,27 @@
 import Vue from 'vue'
+Vue.config.productionTip = false
+
+/* start configuration of vue-data-tables */
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// set language to EN
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+locale.use(lang)
+
+import { DataTables } from 'vue-data-tables'
+Vue.use(DataTables)
+/* end configuration of vue-data-tables */
+
+export const eventBus = new Vue()
+
 import DatasetInfoComponent from './components/DatasetInfoComponent.vue'
 import UploaderComponent from './components/UploaderComponent.vue'
 import PagerComponent from './components/PagerComponent.vue'
 import AnnotatorComponent from './components/AnnotatorComponent.vue'
-
-Vue.config.productionTip = false
-
-export const eventBus = new Vue()
-
 new Vue({
 	el: '#gigadb-fuw',
 	components: {
