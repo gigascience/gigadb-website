e77552b97dca312ca0eadceb84922f2cd3793765
Rija Menage <rija@gigasciencejournal.com>
2020-02-13 19:55:41 +0100

    Implement the form logic for entering file attributes for uploaded files
    The collapsible panel on the metadata form that shows a new attribute form
    now has the logic to collect the fields values and append it to
    the list of attributes below the form.
    
    unit tests was tricky, took some time to figure that we need to use ``mount`` instead of ``shallowMount``
    so that the nested ElementUI's ``el-table`` component can be rendered in the test
    and have expectation run against it.

--- gigadb/app/client/web/src/components/SpecifierComponent.vue
+++ gigadb/app/client/web/src/components/SpecifierComponent.vue
@@ -1,28 +1,51 @@
 <template>
     <div>
-        <label class='control-label'>
-        	Name: 
-            <input v-model="attribute.name" type="text" name="Attribute[name]" id="new-attr-name-field" />
-        </label>
-        <label class='control-label'>
-        	Value: 
-            <input v-model="attribute.value" type="text" name="Attribute[value]" id="new-attr-value-field" />
-        </label>
-        <label class='control-label'>
-        	Unit: 
-            <input v-model="attribute.unit" type="text" name="Attribute[unit]" id="new-attr-unit-field" />
-        </label>
-        <button class="btn btn-success btn-small" type="submit">Add</button>
+        <form name="new-attribute-form">
+            <label class='control-label'>
+                Name:
+                <input v-model="name" type="text" id="new-attr-name-field" />
+            </label>
+            <label class='control-label'>
+                Value:
+                <input v-model="value" type="text" id="new-attr-value-field" />
+            </label>
+            <label class='control-label'>
+                Unit:
+                <input v-model="unit" type="text" id="new-attr-unit-field" />
+            </label>
+            <button v-on:click="addNewAttribute" class="btn btn-success btn-small" id="add-new-attribute">Add</button>
+        </form>
+        <el-table :data="attributes" height="250" style="width: 90%">
+            <el-table-column prop="name" label="Name">
+            </el-table-column>
+            <el-table-column prop="value" label="Value">
+            </el-table-column>
+            <el-table-column prop="unit" label="Unit">
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 <style>
 </style>
 <script>
 export default {
-	data: function () {
-		return {
-			attribute: [],
-		}
-	}
+    props: [ 'fileAttributes' ],
+    data: function() {
+        return {
+            name: '',
+            value: '',
+            unit: '',
+            attributes: this.fileAttributes || [],
+        }
+    },
+    methods: {
+        addNewAttribute(event) {
+            event.preventDefault()
+            this.attributes.push({ name: this.name, value: this.value, unit: this.unit })
+            this.name = ''
+            this.value = ''
+            this.unit = ''
+        }
+    }
 }
 </script>
