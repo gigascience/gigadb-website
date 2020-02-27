21327387f4762ee2a5fa51eeb0029bb3b57b81e0
Rija Menage <rija@gigasciencejournal.com>
2020-02-27 22:26:04 +0100

    Add help text, example link, file input for bulk upload of files
    The example spreadsheet will show the format to use for adding attributes (fixed number of attributes)
    
    Also installed the php library for manipulating spreadsheets.
    
    started adding form element to handle upload of the spreadsheet.

--- gigadb/app/client/web/src/components/AnnotatorComponent.vue
+++ gigadb/app/client/web/src/components/AnnotatorComponent.vue
@@ -39,6 +39,11 @@
                 </tr>
             </tbody>
         </table>
+        <aside>
+            <p>If you have many files you may wish to prepare the information in a spreadsheet and upload it to this form using the file input below. Note the columns should have a header row. Please check out <a href="/files/examples/bulk-data-upload-example.csv">this example spreadsheet</a> for header names.</p>
+            <label for="bulkmetadata">Upload file metadata:</label>
+            <input type="file" id="bulkmetadata" name="bulkmetadata" accept=".csv, .tsv">
+        </aside>
         <div v-if="uploadedFiles.length > 0">
             <el-drawer v-bind:title="'Add attributes to file: '+uploadedFiles[drawerIndex].name" v-bind:visible.sync="drawer" v-bind:with-header="true" ref="drawer">
                 <span>
