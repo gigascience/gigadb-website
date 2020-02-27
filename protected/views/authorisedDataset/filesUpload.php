ef8c97cedb136b220da529b9a7aa37602bcc9601
Rija Menage <rija@gigasciencejournal.com>
2020-02-27 21:59:46 +0100

    Change endpoint for TUSd file upload server to avoid conflict
    at the moment the TUSd service for http upload of iles is accessible by File upload wizard
    at ``/files/``. But that web server path is already used by GigaDB for serving static files,
    so change the path to Tusd to something else (``/fileserver/``)

--- protected/views/authorisedDataset/filesUpload.php
+++ protected/views/authorisedDataset/filesUpload.php
@@ -1,28 +1,24 @@
-<?php 
-?>
+<?php ?>
 <div class="content">
     <div id="gigadb-fuw">
         <article class="container">
-            <?php echo CHtml::beginForm(); ?>
-                <header class="page-title-section">
-                    <div class="page-title">
-                        <ol class="breadcrumb pull-right">
-                            <li><a href="/">Home</a></li>
-                            <li class="active">File Upload Wizard</li>
-                        </ol>
-                        <dataset-info identifier="<?php echo $identifier; ?>" />
-                    </div>
-                </header>
-                <section>
-                    <annotator identifier="<?php echo $identifier; ?>" 
-                                v-bind:uploads='<?php echo json_encode($uploads); ?>' 
-                                v-bind:attributes='<?php echo json_encode($attributes); ?>' 
-                    />
-                </section>
-                <footer>
-                    <pager identifier="<?php echo $identifier; ?>" />
-                </footer>
-            <?php echo CHtml::endForm(); ?>
+            <header class="page-title-section">
+                <div class="page-title">
+                    <ol class="breadcrumb pull-right">
+                        <li><a href="/">Home</a></li>
+                        <li class="active">File Upload Wizard</li>
+                    </ol>
+                    <dataset-info identifier="<?= $identifier ?>" />
+                </div>
+            </header>
+            <section>
+                <uploader identifier="<?= $identifier ?>"
+                            endpoint="<?php echo $tusd_path ?>"
+                />
+            </section>
+            <footer>
+                <pager identifier="<?= $identifier ?>"/>
+            </footer>
         </article>
     </div>
 </div>
