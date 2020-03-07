b24c546033e51f9668d4bd8c29b4c6983e77e9e5
Rija Menage <rija@gigasciencejournal.com>
2020-03-07 14:04:51 +0100

    More debug to figure out the database settings actually being used on CI

--- fuw/app/common/tests/_support/Helper/DbExtendedWithHooks.php
+++ fuw/app/common/tests/_support/Helper/DbExtendedWithHooks.php
@@ -21,7 +21,9 @@ class DbExtendedWithHooks extends \Codeception\Module\Db
 
     public function _before(\Codeception\TestInterface $test)
     {
+        parent::_before($test);
         echo var_dump( $this->_getDbh() );
+        echo var_dump( $this->getDatabases() );
     }
 
     /**
