0bf0d42cb60eb59e06c5967d60af8f3b8d91004d
Rija Menage <rija@gigasciencejournal.com>
2020-03-13 19:43:59 +0100

    Add command to generate JSON list of GigaDB's supported file types
    This is so that the Vue.js Javascript component and File Upload Wizard
    have access to the same list when meta-data are added to uploaded files
    and no reconciliation conflict arise when the uploaded files are publicly published
    in GigaDB database and FTP.
    
    We use JSON format instead of PHP array because the AnnotatorComponent.vue Vue.js component
    needs to access this list as well.

---
+++ protected/tests/unit/FileTypeDAOTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Test for Business object that interacts with the FileType ActiveRecord model.
+ *
+ * @author Rija Menage <rija+git@cinecinetique.com>
+ * @license GPL-3.0
+ */
+class FileTypeDAOTest extends CTestCase
+{
+
+	protected $fixtures=array(
+        'file_type'=>'FileType',
+    );
+	/**
+	 * function to export the list of file types as JSON
+	 * @return string a JSON string representing the list of file types
+	 */
+	public function testToJSON()
+	{
+		$systemUnderTest = new FileTypeDAO();
+		$filetypes = $systemUnderTest->toJSON();
+		$this->assertNotNull($filetypes);
+		$this->assertEquals(3, count(json_decode($filetypes,true)));
+		$this->assertEquals(1,json_decode($filetypes,true)["Text"]);
+		$this->assertEquals(2,json_decode($filetypes,true)["Sequence assembly"]);
+		$this->assertEquals(3,json_decode($filetypes,true)["Annotation"]);
+	}
+}
+
+
+?>
