efcabf8262dae1487dce5e4482e734b7ae9d0828
Rija Menage <rija@gigasciencejournal.com>
2020-03-14 19:54:16 +0100

    Add command to generate JSON list of GigaDB's supported file formats
    This is so that the Vue.js Javascript component and File Upload Wizard
    have access to the same list when meta-data are added to uploaded files
    and no reconciliation conflict arise when the uploaded files are publicly published
    in GigaDB database and FTP.
    
    We use JSON format instead of PHP array because the AnnotatorComponent.vue Vue.js component
    needs to access this list as well.

--- protected/tests/fixtures/file_format.php
+++ protected/tests/fixtures/file_format.php
@@ -6,6 +6,11 @@ return array(
 		'name'=>'TEXT',
 		'description'=>' (.doc, .readme, .text, .txt) - a text file',
 	),
+	array(
+		'id' => 2,
+		'name'=>'GFF',
+		'description'=>' The General Feature Format (GFF) is used for describing genes and other features of DNA, RNA and protein sequences',
+	),
 
 );
 
---
+++ protected/tests/unit/FileFormatDAOTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Test for Business object that interacts with the FileFormat ActiveRecord model.
+ *
+ * @author Rija Menage <rija+git@cinecinetique.com>
+ * @license GPL-3.0
+ */
+class FileFormatDAOTest extends CDbTestCase
+{
+
+	protected $fixtures=array(
+        'file_format'=>'FileFormat',
+    );
+	/**
+	 * function to export the list of file types as JSON
+	 * @return string a JSON string representing the list of file types
+	 */
+	public function testToJSON()
+	{
+		$systemUnderTest = new FileFormatDAO();
+		$fileformats = $systemUnderTest->toJSON();
+		$this->assertNotNull($fileformats);
+		$this->assertEquals(2, count(json_decode($fileformats,true)));
+		$this->assertEquals(1,json_decode($fileformats,true)["TEXT"]);
+		$this->assertEquals(2,json_decode($fileformats,true)["GFF"]);
+
+	}
+}
+
+
+?>
