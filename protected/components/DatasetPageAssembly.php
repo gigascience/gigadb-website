9b51408eb07734131521a9c62fc771b3cce8c32a
Rija Menage <rija@gigasciencejournal.com>
2020-03-25 19:52:34 +0100

    Refactor Dataset controller to extract common code needed for mockup
    The mockup page and the vanilla dataset page need similar components,
    so I extracted the data components that make up a page from DatasetController.php
    into a DatasetPageAsembly.php class.
    
    To make its use in controllers more flexible and readable, it makes use of a fluent interface pattern:
    builder/factory function (``assemble()``) AND setters that return the class itself.

---
+++ protected/components/DatasetPageAssembly.php
@@ -0,0 +1,300 @@
+<?php
+
+/**
+ * Component for assembling parts of dataset page information architecture for use in controllers
+ * 
+ * we use a fluent interface to improve readability in the way it is used
+ *
+ * @property DatasetSubmitterInterface $_submitter
+ * @property DatasetAccessionsInterface $_accessions
+ * @property DatasetMainSectionInterface $_mainSection
+ * @property DatasetConnectionsInterface $_connections
+ * @property DatasetExternalLinksInterface $_externalLinks
+ * @property DatasetFilesInterface $_files
+ * @property DatasetSamplesInterface $_samplesProvider
+ * @property Dataset $_dataset
+ * @property SearchForm $_searchForm
+ *
+ * @uses yii::app()
+ *
+ * @author Rija Menage <rija+git@cinecinetique.com>
+ * @license GPL-3.0
+ */
+
+class DatasetPageAssembly extends yii\base\Component
+{
+	private $_app;
+	private $_cacheDependency;
+	private $_submitter;
+	private $_accessions;
+	private $_mainSection;
+	private $_connections;
+	private $_externalLinks;
+	private $_files;
+	private $_samples;
+	private $_dataset;
+	private $_searchForm;
+
+
+	public function __construct(Dataset $dataset, CApplication $app, $config = []) {
+		$this->_dataset = $dataset;
+		$this->_app = $app;
+		$this->_cacheDependency = new CDbCacheDependency();
+	}
+
+	/**
+	 * factory function to create an instance of DatasetPageAssembly
+	 *
+	 * @param Dataset $d Dataset instance to pass to the instanciated Page assembly
+	 */
+	public static function assemble(Dataset $d, CApplication $app): DatasetPageAssembly
+	{
+		return new DatasetPageAssembly($d, $app);
+	}
+
+	/**
+	 * Getter
+	 * @return Dataset
+	 */
+	public function getDataset(): Dataset
+	{
+		return $this->_dataset;
+	}
+
+	/**
+	 * Create a dataset submitter dataset component to be use in a dataset page
+	 *
+	 * @return DatasetPageAssembly
+	 */
+	public function setDatasetSubmitter(): DatasetPageAssembly
+	{
+		$this->_submitter = $datasetSubmitter = new AuthorisedDatasetSubmitter(
+                                $this->_app->user,
+                                new CachedDatasetSubmitter(
+                                    $this->_app->cache,
+                                    $this->_cacheDependency,
+                                    new StoredDatasetSubmitter(
+                                        $this->_dataset->id,
+                                        $this->_app->db
+                                    )
+                                )
+                      	);
+		return $this;
+	}
+
+	/**
+	 * Getter
+	 * @return DatasetSubmitterInterface
+	 */
+	public function getDatasetSubmitter(): DatasetSubmitterInterface
+	{
+		return $this->_submitter;
+	}
+
+	/**
+	 * Create an accessions dataset component to be use in a dataset page
+	 *
+	 * @return DatasetPageAssembly
+	 */
+	public function setDatasetAccessions(): DatasetPageAssembly
+	{
+		$this->_accessions = new FormattedDatasetAccessions(
+                                new AuthorisedDatasetAccessions(
+                                    $this->_app->user,
+                                    new CachedDatasetAccessions(
+                                        $this->_app->cache,
+                                        $this->_cacheDependency,
+                                        new StoredDatasetAccessions(
+                                            $this->_dataset->id,
+                                            $this->_app->db
+                                        )
+                                    )
+                                ),
+                                'target="_blank"'
+        );
+		return $this;
+	}
+
+	/**
+	 * Getter
+	 * @return DatasetAccessionsInterface
+	 */
+	public function getDatasetAccessions(): DatasetAccessionsInterface
+	{
+		return $this->_accessions;
+	}
+
+	/**
+	 * Create an accessions dataset component to be use in a dataset page
+	 *
+	 * TODO: we may need a AuthorisedDatasetMainSection.php later maybe?
+	 *
+	 * @return DatasetPageAssembly
+	 */
+	public function setDatasetMainSection(): DatasetPageAssembly
+	{
+		$this->_mainSection = new FormattedDatasetMainSection(
+                        new CachedDatasetMainSection (
+                            $this->_app->cache,
+                            $this->_cacheDependency,
+                            new StoredDatasetMainSection(
+                                $this->_dataset->id,
+                                $this->_app->db
+                            )
+                    )
+                );
+		return $this;
+	}
+
+	/**
+	 * Getter
+	 * @return DatasetMainSectionInterface
+	 */
+	public function getDatasetMainSection(): DatasetMainSectionInterface
+	{
+		return $this->_mainSection;
+	}
+
+	/**
+	 * Create a connections dataset component to be use in a dataset page
+	 *
+	 * @return DatasetPageAssembly
+	 */
+	public function setDatasetConnections(): DatasetPageAssembly
+	{
+		$this->_connections = new FormattedDatasetConnections(
+                            $this->_app->getController(),
+                        new CachedDatasetConnections (
+                            $this->_app->getCache(),
+                            $this->_cacheDependency,
+                            new StoredDatasetConnections(
+                                $this->_dataset->id,
+                                $this->_app->getDb(),
+                                new \GuzzleHttp\Client()
+                            )
+                    )
+                );
+		return $this;
+	}
+
+	/**
+	 * Getter
+	 * @return DatasetConnectionsInterface
+	 */
+	public function getDatasetConnections(): DatasetConnectionsInterface
+	{
+		return $this->_connections;
+	}
+
+	/**
+	 * Create an ExternalLinks dataset component to be use in a dataset page
+	 *
+	 * @return DatasetPageAssembly
+	 */
+	public function setDatasetExternalLinks(): DatasetPageAssembly
+	{
+		$this->_externalLinks = new FormattedDatasetExternalLinks(
+                            new CachedDatasetExternalLinks(
+                                $this->_app->cache,
+                                 $this->_cacheDependency,
+                                new StoredDatasetExternalLinks(
+                                    $this->_dataset->id,
+                                    $this->_app->db
+                                )
+                            )
+                        );
+		return $this;
+	}
+
+	/**
+	 * Getter
+	 * @return DatasetExternalLinksInterface
+	 */
+	public function getDatasetExternalLinks(): DatasetExternalLinksInterface
+	{
+		return $this->_externalLinks;
+	}
+
+	/**
+	 * Create an files dataset component to be use in a dataset page
+	 *
+	 * @param int $pageSize item per page settings
+	 * @return DatasetPageAssembly
+	 */
+	public function setDatasetFiles(int $pageSize): DatasetPageAssembly
+	{
+		$this->_files = new FormattedDatasetFiles(
+							$pageSize,
+                            new CachedDatasetFiles(
+                                $this->_app->cache,
+                                 $this->_cacheDependency,
+                                new StoredDatasetFiles(
+                                    $this->_dataset->id,
+                                    $this->_app->db
+                                )
+                            )
+                        );
+		return $this;
+	}
+
+	/**
+	 * Getter
+	 * @return DatasetFilesInterface
+	 */
+	public function getDatasetFiles(): DatasetFilesInterface
+	{
+		return $this->_files;
+	}
+
+	/**
+	 * Create a Samples dataset component to be use in a dataset page
+	 *
+	 * @param int $pageSize item per page settings
+	 * @return DatasetPageAssembly
+	 */
+	public function setDatasetSamples(int $pageSize): DatasetPageAssembly
+	{
+		$this->_samples = new FormattedDatasetSamples(
+							$pageSize,
+                            new CachedDatasetSamples(
+                                $this->_app->cache,
+                                 $this->_cacheDependency,
+                                new StoredDatasetSamples(
+                                    $this->_dataset->id,
+                                    $this->_app->db
+                                )
+                            )
+                        );
+		return $this;
+	}
+
+	/**
+	 * Getter
+	 * @return DatasetSamplesInterface
+	 */
+	public function getDatasetSamples(): DatasetSamplesInterface
+	{
+		return $this->_samples;
+	}
+
+	/**
+	 * Create a Search Form component to be use in a dataset page
+	 *
+	 * @return DatasetPageAssembly
+	 */
+	public function setSearchForm(): DatasetPageAssembly
+	{
+		$this->_searchForm = new SearchForm();
+		return $this;
+	}
+
+	/**
+	 * Getter
+	 * @return SearchForm
+	 */
+	public function getSearchForm(): SearchForm
+	{
+		return $this->_searchForm;
+	}
+
+}
