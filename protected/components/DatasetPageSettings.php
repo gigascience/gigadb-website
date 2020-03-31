47556fc8de37f9f29480c623373caf01a2b2170a
Rija Menage <rija@gigasciencejournal.com>
2020-03-31 14:11:40 +0200

    Refactor out files/samples settings and cookie handling on dataset view
    As part of enabling the dataset view to work as mockup page, we continue removing the bits of logic
    scattered in DatasetController's actionView. This time taking the code that configure columns and pagination
    for files and samples tables into the dedicated DatasetPageSettings.php class.

--- protected/components/DatasetPageSettings.php
+++ protected/components/DatasetPageSettings.php
@@ -24,6 +24,8 @@ class DatasetPageSettings extends yii\base\BaseObject
 	/** @var string $_pageType mode: update or view */
 	private $_pageType;
 
+	/** @var string $_fileSettings */
+	private $_fileSettings;
 
 	public function __construct(?Dataset $model, DatasetDAO $dao = null, $config = [])
 	{
@@ -54,5 +56,90 @@ class DatasetPageSettings extends yii\base\BaseObject
 	{
 		return $this->_pageType;
 	}
+
+	/**
+	 * Return list of columns and page size that control display of files
+	 *
+	 * @param CMap $cookies collection of HTTP cookies (CCookieCollection is subclass of CMap)
+	 * @return array columns and pageSize settings as associative array
+	 */
+	public function getFileSettings(CMap $cookies = null): array
+	{
+		// default values
+		$fileSettings = [
+			"setting" => self::VIEW_DEFAULT_FILE_COLUMNS,
+			"page" => 10,
+		];
+
+		if( isset($cookies['file_setting']) ){
+			$fileSettings = json_decode( $cookies['file_setting']->value , true);
+		}
+
+		return [ "columns" => $fileSettings['setting'], "pageSize" => $fileSettings['page'] ];
+	}
+
+	/**
+	 * Store new columns and page setting in a cookie for files setting
+	 *
+	 * @param array $columns list of columns for the file table
+	 * @param int $pageSize size of a page of the file table
+	 * @param CMap $cookies collection of HTTP cookies (CCookieCollection is subclass of CMap)
+	 * @return array columns and pageSize settings as associative array
+	 */
+	public function setFileSettings(array $columns, int $pageSize, CMap $cookies): array
+	{
+		if( isset($cookies['file_setting']) ){
+			$cookies['file_setting']->value = json_encode([ "setting" => $columns, "page" => $pageSize]);
+		}
+		else {
+				$cookie = new CHttpCookie('file_setting', json_encode(array('setting'=> $columns, 'page'=>$pageSize)));
+		        $cookie->expire = time() + (60*60*24*30);
+		        $cookies['file_setting'] = $cookie;
+		}
+		return [ "columns" => $columns, "pageSize" => $pageSize ];
+	}
+
+	/**
+	 * Return list of columns and page size that control display of samples
+	 *
+	 * @param CMap $cookies collection of HTTP cookies (CCookieCollection is subclass of CMap)
+	 * @return array columns and pageSize settings as associative array
+	 */
+	public function getSampleSettings(CMap $cookies = null): array
+	{
+		// default values
+		$sampleSettings = [
+			"columns" => array('name', 'taxonomic_id', 'genbank_name', 'scientific_name', 'common_name', 'attribute'),
+			"page" => 10,
+		];
+
+		if( isset($cookies['sample_setting']) ){
+			$sampleSettings = json_decode( $cookies['sample_setting']->value , true);
+		}
+
+		return [ "columns" => $sampleSettings['columns'], "pageSize" => $sampleSettings['page'] ];
+	}
+
+	/**
+	 * Store new columns and page setting in a cookie for samples setting
+	 *
+	 * @param array $columns list of columns for the file table
+	 * @param int $pageSize size of a page of the file table
+	 * @param CMap $cookies collection of HTTP cookies (CCookieCollection is subclass of CMap)
+	 * @return array columns and pageSize settings as associative array
+	 */
+	public function setSampleSettings(array $columns, int $pageSize, CMap $cookies): array
+	{
+		if( isset($cookies['sample_setting']) ){
+			$cookies['sample_setting']->value = json_encode([ "columns" => $columns, "page" => $pageSize]);
+		}
+		else {
+				$cookie = new CHttpCookie('sample_setting', json_encode(array('columns'=> $columns, 'page'=>$pageSize)));
+		        $cookie->expire = time() + (60*60*24*30);
+		        $cookies['sample_setting'] = $cookie;
+		}
+		return [ "columns" => $columns, "pageSize" => $pageSize ];
+	}
+
 }
 ?>
