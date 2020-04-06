4d23078af11870173960035e2504a4fd69aa4ed8
Rija Menage <rija@gigasciencejournal.com>
2020-04-06 16:01:28 +0200

    Show file attributes associated with file uploads on dataset mockup page
    The main change was done in ``ResourcedDatasetFiles.php`` to collect attributes (by querying the FUW API using an instance of ```FileUploadService.php``) into the the same key already used in the structure found in ``StoredDatasetFiles.php``.
    
    By virtue of keeping the same structure and implementing the ``DatasetFilesInterface.php`` interface,
    ``ResourcedDatasetFiles.php`` can be used interchangeably within the S.O.L.I.D Russian doll pattern of StoredDatasetFiles|ResourcedDatasetFiels < CachedDatasetFiles < FormattedDatasetFiles,
    thus allowing the mockup page and the regular Dataset view to share most of their code.
    
    In order for the acceptance scenario to play the steps proving the functionality I had to change the display default for the files tables
    so that the files attributes are visible by default (see ``DatasetPageSettings.php`` and its test file).
    
    The steps implementations for the new acceptance scenario (in ``file-upload-6-reviewer-access-drop-box-files.feature``)
    are in ``ReviewerSteps.php``.

--- protected/tests/unit/ResourcedDatasetFilesTest.php
+++ protected/tests/unit/ResourcedDatasetFilesTest.php
@@ -166,6 +166,126 @@ public function testResourcedReturnsDatasetFilesWithResults()
 		// -> the datatype and extension need to be converted into database IDs
 	}
 
+	public function testResourcedReturnsDatasetFilesWithAttributes()
+	{
+		$dataset_id = 1;
+		$doi = 100243;
+
+		$uploadedFiles = [
+		    [
+		        'id' => 1,
+		        'doi' => '010010',
+		        'name' => 'FieldDataMethods.docx',
+		        'size' => 2352636,
+		        'status' => 0,
+		        'location' => 'ftp://some.location/FieldDataMethods.docx',
+		        'initial_md5' => 't5GU9NwpuGYSfb7FEZMAxqtuz2PkEvv',
+		        'description' => 'methods for field data',
+		        'datatype' => 'Text',
+		        'extension' => 'DOCX',
+		        'sample_id' => null,
+		        'updated_at' => (new DateTime())->format('U'),
+		    ],
+		    [
+		        'id' => 2,
+		        'doi' => '010010',
+		        'name' => 'Measurements.csv',
+		        'size' => 3252654,
+		        'status' => 0,
+		        'location' => 'ftp://some.location/Measurements.csv',
+		        'initial_md5' => 'X5GU9NwpuGYSfb7FEZMAxqtuz2PkEvv',
+		        'description' => 'measurements',
+		        'datatype' => 'Text',
+		        'extension' => 'CSV',
+		        'sample_id' => null,
+		        'updated_at' => (new DateTime())->format('U'),
+		    ],
+		    [
+		        'id' => 3,
+		        'doi' => '010020',
+		        'name' => 'SomeImage.jpg',
+		        'size' => 3252654,
+		        'status' => 0,
+		        'location' => 'ftp://some.location/SomeImage.jpg',
+		        'initial_md5' => 'Y5GU9NwpuGYSfb7FEZMAxqtuz2PkEvv',
+		        'description' => 'An image',
+		        'datatype' => 'Annotation',
+		        'extension' => 'JPG',
+		        'sample_id' => null,
+		        'updated_at' => (new DateTime())->format('U'),
+		    ],
+		];
+
+		$allAttributes = [
+			1 => [
+					[
+			                'name' => "Attribute A",
+			                'value' => "42",
+			                'unit' => "Metre",
+			                'upload_id' => 1,
+			        ],
+		          	[
+			                'name' => "Attribute B",
+			                'value' => "34",
+			                'unit' => "Litre",
+			                'upload_id' => 1,
+			        ]
+			    ],
+          2 => [],
+          3 =>	[
+	          		[
+                          'name' => "Attribute A",
+                          'value' => "54",
+                          'unit' => "Metre",
+                          'upload_id' => 3,
+                    ],
+                ]
+		];
+		$fuwClient = $this->createMock(FileUploadService::class);
+		$daoUnderTest = new ResourcedDatasetFiles(
+								$dataset_id,
+								$this->getFixtureManager()->getDbConnection(),
+								$fuwClient
+							);
+
+		$fuwClient->expects($this->once())
+			->method("getUploads")
+			->with($doi)
+			->willReturn($uploadedFiles);
+
+		$fuwClient->expects($this->exactly(3))
+			->method("getAttributes")
+			->withConsecutive(
+				[ 1 ],
+				[ 2 ],
+				[ 3 ]
+			)
+			->will($this->onConsecutiveCalls(
+				$allAttributes[1],
+				$allAttributes[2],
+				$allAttributes[3]
+			));
+
+
+		$datasetFiles = $daoUnderTest->getDatasetFiles();
+		$this->assertEquals(count($uploadedFiles), count($datasetFiles) ) ;
+		$this->assertEquals(null,$datasetFiles[0]["id"]);
+		$this->assertEquals(1,$datasetFiles[0]["dataset_id"]);
+		$this->assertEquals($uploadedFiles[0]["name"],$datasetFiles[0]["name"]);
+		$this->assertEquals($uploadedFiles[0]["location"],$datasetFiles[0]["location"]);
+		$this->assertEquals("docx",$datasetFiles[0]["extension"]);
+		$this->assertEquals($uploadedFiles[0]["size"],$datasetFiles[0]["size"]);
+		$this->assertEquals($uploadedFiles[0]["description"],$datasetFiles[0]["description"]);
+		$this->assertEquals("DOCX",$datasetFiles[0]["format"]);
+		$this->assertEquals("Text",$datasetFiles[0]["type"]);
+		$this->assertNotNull($datasetFiles[0]["date_stamp"]);
+		// var_dump($datasetFiles[0]["file_attributes"]);
+		$this->assertEquals(["Attribute A" => "42 Metre"],$datasetFiles[0]["file_attributes"][0]);
+		$this->assertEquals(["Attribute B" => "34 Litre"],$datasetFiles[0]["file_attributes"][1]);
+		$this->assertEquals([],$datasetFiles[1]["file_attributes"]);
+		$this->assertEquals(["Attribute A" => "54 Metre"],$datasetFiles[2]["file_attributes"][0]);
+	}
+
 }
 
 ?>
