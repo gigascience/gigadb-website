ce23986f22a2452aa0cdaeffb314867a7c18aa2e
Rija Menage <rija@gigasciencejournal.com>
2020-06-02 18:59:01 +0200

    Replace magic numbers by constants from appropriate classes
    The FiledropService.php and FileUplodService.php use magic numbers
    (raw integers that can't be understood from reading the contextual code)
    in order to retrieve the respective File Upload Wizard resources FiledropAccount.php and Upload.php
    that have the appropriate status.
    
    So instead we use the meaningful constants from those two model classes.
    
    In order to do that, we first need the GigaDB main app to access the model classes of File Upload Wizard.
    We do so by:
    * we need to make sure in docker-compose.yml the application and test container services have bind mounted the path of model classes
    * adding a PSR-4 block to the composer.json.dist template so that it knows the file path to find the namespaced classes

--- protected/components/FiledropService.php
+++ protected/components/FiledropService.php
@@ -26,6 +26,8 @@ use GuzzleHttp\Middleware;
 use Ramsey\Uuid\Uuid;
 use yii\validators\EmailValidator;
 
+use \backend\models\FiledropAccount;
+
 class FiledropService extends yii\base\Component
 {
 	const MOCKUP_POSSIBLE_VALIDITY  = [1,3,6];
@@ -248,7 +250,7 @@ class FiledropService extends yii\base\Component
 								    'headers' => [
 								        'Authorization' => "Bearer ".$this->token,
 								    ],
-								    'query' => [ 'filter[doi]' => $doi,  'filter[status]' => 1 ],
+								    'query' => [ 'filter[doi]' => $doi,  'filter[status]' => FiledropAccount::STATUS_ACTIVE ],
 								    'connect_timeout' => 5,
 								]);
 			if (200 === $response->getStatusCode() ) {
@@ -308,9 +310,9 @@ class FiledropService extends yii\base\Component
 	 * @param string $reviewerEmail the email of the reviewer the url is made for
 	 * @param int $monthsOfValidity How many month (1,3, or 6) the url is to be valid
 	 *
-	 * @return ?string UUID used as unique url fragment to the mockup url
+	 * @return ?array UUID used as unique url fragment to the mockup url, and userId of corresponding new user record
 	 */
-	public function makeMockupUrl(TokenService $tokenMaker, string $reviewerEmail, int $monthsOfValidity): ?string
+	public function makeMockupUrl(TokenService $tokenMaker, string $reviewerEmail, int $monthsOfValidity): ?array
 	{
 		if(!in_array($monthsOfValidity, self::MOCKUP_POSSIBLE_VALIDITY)) {
 			Yii::log("Invalid value for months of validity: $monthsOfValidity","error");
@@ -352,7 +354,10 @@ class FiledropService extends yii\base\Component
 			if (201 === $response->getStatusCode() ) {
 				//convert returned json to a PHP array so we can work with it
 				$responseData = json_decode($response->getBody(), true);
-				return $responseData['url_fragment'];
+				// add reviewer to fuw users so they can authenticate using JWT token when they annotate files
+				$userData = $this->tokenSrv->createUser(
+					$this->token, $this->webClient,$reviewerEmail."_".$this->identifier, $reviewerEmail);
+				return [ $responseData['url_fragment'], $userData['id'] ] ;
 			}
 		}
 		catch(RequestException $e) {
