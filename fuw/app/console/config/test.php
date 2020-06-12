ba029e9d371b89dbca82e5897f4060a3d9083b71
Rija Menage <rija@gigasciencejournal.com>
2020-06-12 16:06:36 +0200

    Use Flysystem in console command that processes ftp uploads
    Flysystem allow us to abstract the filesystem so we can change the type of filestorage
    without changing the code. This is important because we are still figuring out which cloud platform
    gigadb and FUW will be deployed to.
    
    The subsystem that move files to public ftp already uses Flysystem. This commit is for the FtpController.php.
    Next will do the same for the console command to process wizard file upload through TUS.
    
    The main changes are to replace absolute file path with path relative to the Flysystem's configured root path
    (which differs whether were are on test environment or not, hence the change to test.php)
    in code that deal with file operations.
    
    I also needed to homogenize the bind mounted path in all container services that need to execute the console commands
    (hence the change to docker-compose.yml to update the watcher container service bind mount paths and to watcher config files and scripts)

--- fuw/app/console/config/test.php
+++ fuw/app/console/config/test.php
@@ -1,4 +1,63 @@
 <?php
-return [
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
 
+return [
+    'id' => 'app-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log','queue','updateGigaDBqueue'],
+    'controllerNamespace' => 'console\controllers',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+        '@gigadb-data' => '/var',
+        '@uploads' => 'repo',
+        '@publicftp'   => 'ftp/public',        
+    ],
+    'controllerMap' => [
+        'fixture' => [
+            'class' => 'yii\console\controllers\FixtureController',
+            'namespace' => 'common\fixtures',
+          ],
+        'migrate' => [
+            'class' => \yii\console\controllers\MigrateController::class,
+            'migrationNamespaces' => [
+                'zhuravljov\yii\queue\monitor\migrations',
+            ],
+        ],          
+
+    ],
+    'components' => [
+        'fs' => [
+            'class' => 'creocoder\flysystem\LocalFilesystem',
+            'path' => '/app/console/tests/_output',
+        ],     
+        'queue' => [
+            'class' => \yii\queue\beanstalk\Queue::class,
+            'as log' => \yii\queue\LogBehavior::class,         
+            'host' => 'beanstalkd',
+            'port' => 11300,
+            'tube' => 'moveFilesQueue',
+        ],
+         'updateGigaDBqueue' => [
+            'class' => \yii\queue\beanstalk\Queue::class,
+            'as log' => \yii\queue\LogBehavior::class,           
+            'host' => 'beanstalkd',
+            'port' => 11300,
+            'tube' => 'updateGigaDB',
+        ], 
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
 ];
--- fuw/app/console/tests/functional/FtpCest.php
+++ fuw/app/console/tests/functional/FtpCest.php
@@ -14,6 +14,22 @@ class FtpCest
 {
     public function _before(FunctionalTester $I)
     {
+        Yii::$app->fs->write("300001/seq1.fa","foobar");
+        Yii::$app->fs->write("300001/seq2.fa","foobar");
+    }
+
+    public function _after(FunctionalTester $I)
+    {
+        if(Yii::$app->fs->has("300001/seq1.fa"))
+            Yii::$app->fs->delete("300001/seq1.fa");
+        if(Yii::$app->fs->has("300001/seq2.fa"))
+            Yii::$app->fs->delete("300001/seq2.fa");
+
+        if(Yii::$app->fs->has("file_repo/300001/seq1.fa"))
+            Yii::$app->fs->delete("file_repo/300001/seq1.fa");
+
+        if(Yii::$app->fs->has("file_repo/300001/seq2.fa"))
+            Yii::$app->fs->delete("file_repo/300001/seq2.fa");                 
     }
 
     // tests
@@ -21,16 +37,13 @@ class FtpCest
     {
     	$doi = "300001";
 
-        $I->copyDir(codecept_data_dir()."ftp",codecept_output_dir());
-        $I->copyDir(codecept_data_dir()."testrepo",codecept_output_dir());
-
     	$accountId = $I->haveInDatabase('filedrop_account',	[
 	        'doi' => $doi,
 	        'status' => FiledropAccount::STATUS_ACTIVE,
 	    ]);
 
     	$outcome = Yii::$app->createControllerByID('ftp')->run('process-upload',[
-    		"dataset_dir" => codecept_output_dir()."/$doi", 
+    		"dataset_dir" => codecept_output_dir()."/$doi",
             "file_repo" => codecept_output_dir()."/file_repo", 
     		"datafeed_path" => "/app/console/tests/_data",
     		"token_path" => "/app/console/tests/_data",
@@ -60,7 +73,7 @@ class FtpCest
     	$controller = Yii::$app->createControllerByID('ftp');
     	$I->assertEquals("/var/www/files/data/", $controller->datafeed_path);
     	$I->assertEquals("/var/private", $controller->token_path);
-        $I->assertEquals("/home/downloader",$controller->file_repo);
+        $I->assertEquals("/var/repo",$controller->file_repo);
     	$I->assertNull($controller->dataset_dir);
 
     }
