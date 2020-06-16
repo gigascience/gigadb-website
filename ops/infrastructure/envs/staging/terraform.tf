239ce9301207ff779b3308b22ff10d71a3b39475
Rija Menage <rija@gigasciencejournal.com>
2020-06-16 19:45:53 +0200

    Update configuration for terraform and ansible to support new version
    newer version of terraform and ansible have introduced changes that breaks the current configuration.
    
    remove terraform-inventory binary from codebase as it can be installed separately and prevent keeping out-of-date versions.
    In its place we have a bash script called terraform-inventory.sh that will call the separately installed binary.
    For terraform-inventory to function and prevent the error
    
    ```
    Error reading tfstate file: 0.12 format error: <nil>; pre-0.12 format error
    ```
    
    the terraform environment variable TF_STATE needs to be set as a relative path to the .tfstate file
    
    Updated the deployment documentation to mention the relevant changes

--- ops/infrastructure/envs/staging/terraform.tf
+++ ops/infrastructure/envs/staging/terraform.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region     = "ap-southeast-1"
 }
 
@@ -8,7 +8,7 @@ provider "aws" {
 module "ec2" {
   source = "../../modules/aws-instance"
 
-  vpc_id = "${var.aws_vpc_id}"
+  vpc_id = var.aws_vpc_id
   deployment_target = "${var.deployment_target}"
   eip_tag_name = "eip-${var.deployment_target}"
 }
