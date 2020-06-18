cfeffffb4c8ab96b925afea4b269d6d60a05e36b
Ken <kencho.gigascience@gmail.com>
2020-06-18 09:44:26 +0800

    Update image path

--- protected/views/site/faq.php
+++ protected/views/site/faq.php
@@ -736,7 +736,7 @@ We will assess the specific case and remove/reduce the amount of metadata availa
                                     <div class="panel-body">
                                         <p>The submission of data to GigaDB is integrated with the submission of a manuscript to journals GigaScience, the general workflow followed is outlined <a href="/site/guide" target="_blank">here</a>. During the process you may track the progress in "<a href="/site/login#submitted" target="_blank">your datasets</a>" on your personal profile page in GigaDB. Each of your datasets will have one of the statuses listed below.
                                         </p>
-                                        <img src="../images//status_list.png">
+                                        <img src="../images//status_flow.png">
                                         <p>Green steps are those carried out by Giga-staff, yellow are performed by Authors, Red indicates the end of the line for that dataset.
                                         </p>
                                     </div>
