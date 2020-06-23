9f304ab79e724131c1bc92ca2d3ea78557f1095e
Rija Menage <rija@gigasciencejournal.com>
2020-06-23 16:00:41 +0200

    Remove mention of gii module in backend config
    not needed in staging/production, but codebase still mention it here, hopefully only here
    dev dependencies are not built on staging/production, so call to gii causes an error there.
    the deleted block was already in an if block, but want to rule out any possibily while I debug the issue:
    ```
    $ docker-compose --tlsverify -H=$REMOTE_DOCKER_HOST -f ops/deployment/docker-compose.staging.yml exec -T console /app/yii migrate --interactive=0
    Exception 'yii\base\InvalidConfigException' with message 'Failed to instantiate component or class "yii\gii\Module".'
    in /app/vendor/yiisoft/yii2/di/Container.php:449
    ```

--- fuw/app/backend/config/main.php
+++ fuw/app/backend/config/main.php
@@ -107,20 +107,4 @@ $config =  [
     'params' => $params,
 ];
 
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        'allowedIPs' => ['*'],
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
-}
-
 return $config;
