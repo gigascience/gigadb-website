7c42d9f9cc93ac8ce198c60e0b788c7a93e37995
Ken <kencho.gigascience@gmail.com>
2020-06-26 12:04:56 +0800

    Update the back link

--- protected/views/attribute/update.php
+++ protected/views/attribute/update.php
@@ -1,4 +1,4 @@
-<a href="/funder/admin">Back</a>
+<a href="/attribute/admin">Back</a>
 <h1>Update Attribute <?php echo $model->id; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
