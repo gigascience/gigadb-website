1e977fb3881ce12065ad6f71c18f091df82450df
Ken <kencho.gigascience@gmail.com>
2020-06-26 12:19:43 +0800

    Fix: Heading for attribute update page
    The heading for the attribute update page has been corrected to
    display Update Attribute instead of Update Funder by updating
    `protected/views/attribute/update.php`.
    
    The Back link for this page has also been fixed to go back to the
    admin page for attributes.

--- protected/views/attribute/update.php
+++ protected/views/attribute/update.php
@@ -1,4 +1,4 @@
-<a href="/funder/admin">Back</a>
-<h1>Update Funder <?php echo $model->id; ?></h1>
+<a href="/attribute/admin">Back</a>
+<h1>Update Attribute <?php echo $model->id; ?></h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
