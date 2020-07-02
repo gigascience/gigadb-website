4451e4e912113e3bf7bc4d70be69666d47f70fe3
Ken <kencho.gigascience@gmail.com>
2020-07-02 17:59:30 +0800

    Update tests with 3 different roles

--- protected/tests/functional_test/AdminSiteAccessTest.php
+++ protected/tests/functional_test/AdminSiteAccessTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Functional test for only logged in user can access /site/admin
+ * Functional test for only logged in admin user can access /site/admin.
  */
 
 class AdminSiteAccessTest extends FunctionalTesting
@@ -8,30 +8,35 @@ class AdminSiteAccessTest extends FunctionalTesting
     use BrowserSignInSteps;
     use BrowserPageSteps;
 
-    public function setUp()
-    {
-        parent::setUp();
-        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("admin@gigadb.org","gigadb","Admin");
-    }
-
-
     public function testItShouldBeDisplayedToUsersWithAdminRole ()
     {
-        $url = "http://gigadb.dev/site/login";
+        // Logged in as Admin, and can visit to /site/admin page.
+        // 2 assertions, 2 passes.
+        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("admin@gigadb.org","gigadb","Admin");
+        $url = "http://gigadb.dev/site/admin";
         $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull($url, "Administration Page");
     }
 
-
-
-    /*
-    public function testNonLoggedInCanOnlyVisitLogin ()
+    public function testItShouldNotBeDisplayedToUsersWithUserRole ()
     {
-        // Using incorrect email account
-        $this->nonLoggedInAndCannotVisitAdminThenAssert("abc@gigadb.org", "gigadb", "Admin");
-        $url = "http://gigadb.dev/site/login";
-        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull($url, "Lost Password");
+        // Logged in as User, but cannot visit to /site/admin page.
+        // 2 assertions, login pass, but visit to /site/admin fail.
+        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("user@gigadb.org","vagrant","User");
+        $url = "http://gigadb.dev/site/admin";
+        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull($url, "Administration Page");
+
     }
-    */
+
+    public function testItShouldNotBeDisplayedToOrdinaryUsers ()
+    {
+        // No login was tested. 1 assertion,
+        // Visit to /site/admin, but would be redirected to /site/login, confirmed by seeing "Login in" as assert true,
+        // and the current url was /site/login.
+        $url = "http://gigadb.dev/site/admin";
+        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull($url, "Login");
+        $out = $this->getCurrentUrl();
+        print($out);
+    }
 }
 
 ?>
