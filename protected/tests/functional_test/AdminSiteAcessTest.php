af871ba900196d044c0f380e8904bf03438c4195
Ken <kencho.gigascience@gmail.com>
2020-07-02 11:30:58 +0800

    Change the test name

---
+++ protected/tests/functional_test/AdminSiteAcessTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Functional test for only logged in user can access /site/admin
+ */
+
+class AdminSiteAcessTest extends FunctionalTesting
+{
+    use BrowserSignInSteps;
+    use BrowserPageSteps;
+
+    public function testLoggedInCanVisitAdmin ()
+    {
+        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("admin@gigadb.org", "gigadb", "Admin");
+        $url = "http://gigadb.dev/site/admin";
+        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull($url, "Administration Page");
+    }
+
+
+
+    public function testNonLoggedInCanOnlyVisitLogin ()
+    {
+        // Using incorrect email account
+        $this->nonLoggedInAndCannotVisitAdminThenAssert("abc@gigadb.org", "gigadb", "Admin");
+        $url = "http://gigadb.dev/site/admin";
+        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull($url, "Lost Password");
+    }
+
+}
+
+?>
