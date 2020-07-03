22c9318560f173da9e78ecde1c726877159b13bd
Rija Menage <rija@gigasciencejournal.com>
2020-07-03 18:42:55 +0200

    comment out reference to yii\gii\module
    attempt to idenfity that dependency on production

--- gigadb/app/worker/file-worker/config/console.php
+++ gigadb/app/worker/file-worker/config/console.php
@@ -46,12 +46,12 @@ $config = [
     */
 ];
 
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
+// if (YII_ENV_DEV) {
+//     // configuration adjustments for 'dev' environment
+//     $config['bootstrap'][] = 'gii';
+//     $config['modules']['gii'] = [
+//         'class' => 'yii\gii\Module',
+//     ];
+// }
 
 return $config;
