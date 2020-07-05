f32e17c942b399e639f3b453738054a12b71c35c
Rija Menage <rija@gigasciencejournal.com>
2020-07-05 18:20:16 +0200

    Add console command to set smoke test data for production
    we need a set of data with the right upload status on dataset so we can test
    the file upload wizard workflow on cloud deployment.
    we need to be able to remove the data once we're done.

---
+++ protected/commands/SmokeTestCommand.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Create and remove smoke test data to allow end to end non-destructive testing of FUW workflow on cloud environments
+ *
+ * @author Rija Menage <rija+git@cinecinetique.com>
+ * @license GPL-3.0
+ */
+class SmokeTestCommand extends CConsoleCommand {
+	
+	public function getHelp() {
+		$helpText = "Create and remove smoke test data to allow end to end non-destructive testing of FUW workflow on cloud environments".PHP_EOL;
+		$helpText .= "Usage: ./protected/yiic smoketest createdata".PHP_EOL;
+		$helpText .= "Usage: ./protected/yiic smoketest removedata".PHP_EOL;
+        return $helpText;
+    }
+
+    public function actionCreateData($args) {
+    	echo "Creating smoke test data...".PHP_EOL;
+    	// Create a test author
+    	$sql = "insert into gigadb_user(id, email, password, first_name, last_name, affiliation, role, is_activated, newsletter, previous_newsletter_state, facebook_id, twitter_id, linkedin_id, google_id, username, orcid_id, preferred_link) 
+values(9999,'joy_fox@gigadb.org','5a4f75053077a32e681f81daa8792f95','Joy','Fox','BGI','user','t','f','t',NULL,NULL,NULL,NULL,'joy_fox@gigadb.org',NULL,'EBI')";
+
+		Yii::app()->db->createCommand($sql)->execute();
+    	// Create a test dataset
+    	$sql = "insert into dataset(id, submitter_id, identifier, title, dataset_size, ftp_site, upload_status) values(9999,9999, '000007','smoke test',342564,'ftp://','AssigningFTPbox')";
+    	Yii::app()->db->createCommand($sql)->execute();
+
+    	return 0;
+	}
+
+	public function actionRemoveData($args) {
+		echo "Removing smoke test data...".PHP_EOL;
+		$sql = "delete from gigadb_user where id=9999";
+		Yii::app()->db->createCommand($sql)->execute();
+		$sql = "delete from dataset where id=9999";
+		Yii::app()->db->createCommand($sql)->execute();
+		return 0;
+	}
+}
+
+?>
