51754926287a5a3758f00e1a892b30218e9ce87c
Peter Li <peter@gigasciencejournal.com>
2020-07-11 16:53:16 +0800

    Update select query to export attribute.csv
    The attribute table is linked to dataset_attributes, exp_attributes,
    file_attributes and sample_attributes tables.

--- data/scripts/exportcsv.sh
+++ data/scripts/exportcsv.sh
@@ -46,7 +46,7 @@ verbosity=$dbg_lvl
 # Set up and sanity check
 #########################
 
-# Initialise dataset ids from command line arguments as an array
+# Initialise dataset ids FROM command line arguments as an array
 DATASET_IDS=("$@")
 
 # Set working directory to this script directory
@@ -109,221 +109,221 @@ edebug "SQL sub-string for SELECT query: ${out_dataset_ids}"
 
 einfo "Creating: dataset.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from dataset where $out_ids) To '${OUTPUT_DIR}dataset.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM dataset WHERE $out_ids) To '${OUTPUT_DIR}dataset.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: publisher.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from publisher where id in (select publisher_id from dataset where $out_ids)) To '${OUTPUT_DIR}publisher.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM publisher WHERE id IN (SELECT publisher_id FROM dataset WHERE $out_ids)) To '${OUTPUT_DIR}publisher.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: manuscript.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from manuscript where $out_dataset_ids) To '${OUTPUT_DIR}manuscript.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM manuscript WHERE $out_dataset_ids) To '${OUTPUT_DIR}manuscript.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: dataset_type.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from dataset_type where $out_dataset_ids) To '${OUTPUT_DIR}dataset_type.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM dataset_type WHERE $out_dataset_ids) To '${OUTPUT_DIR}dataset_type.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: type.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from "type" where id in (select type_id from dataset_type where $out_dataset_ids)) To '${OUTPUT_DIR}type.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM "type" WHERE id IN (SELECT type_id FROM dataset_type WHERE $out_dataset_ids)) To '${OUTPUT_DIR}type.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: external_link.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from external_link where $out_dataset_ids) To '${OUTPUT_DIR}external_link.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM external_link WHERE $out_dataset_ids) To '${OUTPUT_DIR}external_link.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: external_link_type.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from "external_link_type" where id in (select external_link_type_id from external_link where $out_dataset_ids)) To '${OUTPUT_DIR}external_link_type.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM "external_link_type" WHERE id IN (SELECT external_link_type_id FROM external_link WHERE $out_dataset_ids)) To '${OUTPUT_DIR}external_link_type.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: dataset_funder.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from dataset_funder where $out_dataset_ids) To '${OUTPUT_DIR}dataset_funder.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM dataset_funder WHERE $out_dataset_ids) To '${OUTPUT_DIR}dataset_funder.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: funder_name.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from funder_name where id in (select funder_id from dataset_funder where $out_dataset_ids)) To '${OUTPUT_DIR}funder_name.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM funder_name WHERE id IN (SELECT funder_id FROM dataset_funder WHERE $out_dataset_ids)) To '${OUTPUT_DIR}funder_name.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: link.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from link where $out_dataset_ids) To '${OUTPUT_DIR}link.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM link WHERE $out_dataset_ids) To '${OUTPUT_DIR}link.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: dataset_log.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from dataset_log where $out_dataset_ids) To '${OUTPUT_DIR}dataset_log.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM dataset_log WHERE $out_dataset_ids) To '${OUTPUT_DIR}dataset_log.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: curation_log.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from curation_log where $out_dataset_ids) To '${OUTPUT_DIR}curation_log.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM curation_log WHERE $out_dataset_ids) To '${OUTPUT_DIR}curation_log.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: dataset_project.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from dataset_project where $out_dataset_ids) To '${OUTPUT_DIR}dataset_project.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM dataset_project WHERE $out_dataset_ids) To '${OUTPUT_DIR}dataset_project.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: project.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from project where id in (select project_id from dataset_project where $out_dataset_ids)) To '${OUTPUT_DIR}project.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM project WHERE id IN (SELECT project_id FROM dataset_project WHERE $out_dataset_ids)) To '${OUTPUT_DIR}project.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: image.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from image where id in (select image_id from dataset where $out_ids)) To '${OUTPUT_DIR}image.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM image WHERE id IN (SELECT image_id FROM dataset WHERE $out_ids)) To '${OUTPUT_DIR}image.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: dataset_attributes.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from dataset_attributes where $out_dataset_ids) To '${OUTPUT_DIR}dataset_attributes.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM dataset_attributes WHERE $out_dataset_ids) To '${OUTPUT_DIR}dataset_attributes.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: relation.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from relation where $out_dataset_ids) To '${OUTPUT_DIR}relation.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM relation WHERE $out_dataset_ids) To '${OUTPUT_DIR}relation.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: relationship.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from relationship where id in (select relationship_id from relation where $out_dataset_ids)) To '${OUTPUT_DIR}relationship.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM relationship WHERE id IN (SELECT relationship_id FROM relation WHERE $out_dataset_ids)) To '${OUTPUT_DIR}relationship.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: sample_rel.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from sample_rel where relationship_id in (select relationship_id from relation where $out_dataset_ids)) To '${OUTPUT_DIR}sample_rel.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM sample_rel WHERE relationship_id IN (SELECT relationship_id FROM relation WHERE $out_dataset_ids)) To '${OUTPUT_DIR}sample_rel.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: dataset_author.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from dataset_author where $out_dataset_ids) To '${OUTPUT_DIR}dataset_author.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM dataset_author WHERE $out_dataset_ids) To '${OUTPUT_DIR}dataset_author.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: gigadb_user.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from gigadb_user where id in (select submitter_id from dataset where $out_ids) or id in (select curator_id from dataset where $out_ids)) To '${OUTPUT_DIR}gigadb_user.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM gigadb_user WHERE id IN (SELECT submitter_id FROM dataset WHERE $out_ids) or id IN (SELECT curator_id FROM dataset WHERE $out_ids)) To '${OUTPUT_DIR}gigadb_user.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: author.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from author where id in (select author_id from dataset_author where $out_dataset_ids)) To '${OUTPUT_DIR}author.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM author WHERE id IN (SELECT author_id FROM dataset_author WHERE $out_dataset_ids)) To '${OUTPUT_DIR}author.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: search.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from "search" where user_id in (select submitter_id from dataset where $out_ids) or user_id in (select curator_id from dataset where $out_ids)) To '${OUTPUT_DIR}search.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM "search" WHERE user_id IN (SELECT submitter_id FROM dataset WHERE $out_ids) or user_id IN (SELECT curator_id FROM dataset WHERE $out_ids)) To '${OUTPUT_DIR}search.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: dataset_sample.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from dataset_sample where $out_dataset_ids) To '${OUTPUT_DIR}dataset_sample.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM dataset_sample WHERE $out_dataset_ids) To '${OUTPUT_DIR}dataset_sample.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: sample.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from sample where id in (select sample_id from dataset_sample where $out_dataset_ids)) To '${OUTPUT_DIR}sample.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM sample WHERE id IN (SELECT sample_id FROM dataset_sample WHERE $out_dataset_ids)) To '${OUTPUT_DIR}sample.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: species.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from species where id in (select species_id from sample where id in (select sample_id from dataset_sample where $out_dataset_ids))) To '${OUTPUT_DIR}species.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM species WHERE id IN (SELECT species_id FROM sample WHERE id IN (SELECT sample_id FROM dataset_sample WHERE $out_dataset_ids))) To '${OUTPUT_DIR}species.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: alternative_identifiers.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from alternative_identifiers where sample_id in (select sample_id from dataset_sample where $out_dataset_ids)) To '${OUTPUT_DIR}alternative_identifiers.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM alternative_identifiers WHERE sample_id IN (SELECT sample_id FROM dataset_sample WHERE $out_dataset_ids)) To '${OUTPUT_DIR}alternative_identifiers.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: extdb.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from extdb where id in (select extdb_id from alternative_identifiers where sample_id in (select sample_id from dataset_sample where $out_dataset_ids))) To '${OUTPUT_DIR}extdb.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM extdb WHERE id IN (SELECT extdb_id FROM alternative_identifiers WHERE sample_id IN (SELECT sample_id FROM dataset_sample WHERE $out_dataset_ids))) To '${OUTPUT_DIR}extdb.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: experiment.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from experiment where $out_dataset_ids) To '${OUTPUT_DIR}experiment.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM experiment WHERE $out_dataset_ids) To '${OUTPUT_DIR}experiment.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: sample_experiment.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from sample_experiment where experiment_id in (select id from experiment where $out_dataset_ids)) To '${OUTPUT_DIR}sample_experiment.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM sample_experiment WHERE experiment_id IN (SELECT id FROM experiment WHERE $out_dataset_ids)) To '${OUTPUT_DIR}sample_experiment.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: sample_attribute.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from sample_attribute where sample_id in (select sample_id from dataset_sample where $out_dataset_ids)) To '${OUTPUT_DIR}sample_attribute.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM sample_attribute WHERE sample_id IN (SELECT sample_id FROM dataset_sample WHERE $out_dataset_ids)) To '${OUTPUT_DIR}sample_attribute.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: attribute.csv"
 # Attribute id 497 is urltoredirect attribute which is essential for dataset page display
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from attribute where id=497 or id=(select attribute_id from sample_attribute where sample_id in (select sample_id from dataset_sample where $out_dataset_ids)) or id=(select distinct attribute_id from file_attributes where file_id in (select id from file where $out_dataset_ids))) To '${OUTPUT_DIR}attribute.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM attribute WHERE id IN (497) OR id IN (SELECT DISTINCT attribute_id FROM dataset_attributes WHERE $out_dataset_ids) OR id IN (SELECT DISTINCT attribute_id FROM exp_attributes WHERE exp_id IN (SELECT DISTINCT id FROM experiment WHERE $out_dataset_ids)) OR id IN (SELECT DISTINCT attribute_id FROM file_attributes WHERE file_id IN (SELECT DISTINCT id FROM file WHERE $out_dataset_ids)) OR id IN (SELECT DISTINCT attribute_id FROM sample_attribute WHERE sample_id IN (SELECT DISTINCT sample_id FROM dataset_sample WHERE $out_dataset_ids)) ORDER BY id ASC) To '${OUTPUT_DIR}attribute.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: file.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from file where $out_dataset_ids) To '${OUTPUT_DIR}file.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM file WHERE $out_dataset_ids) To '${OUTPUT_DIR}file.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: file_format.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from file_format where id in (select format_id from file where $out_dataset_ids)) To '${OUTPUT_DIR}file_format.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM file_format WHERE id IN (SELECT format_id FROM file WHERE $out_dataset_ids)) To '${OUTPUT_DIR}file_format.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: file_type.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from file_type where id in (select type_id from file where $out_dataset_ids)) To '${OUTPUT_DIR}file_type.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM file_type WHERE id IN (SELECT type_id FROM file WHERE $out_dataset_ids)) To '${OUTPUT_DIR}file_type.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: file_attributes.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from file_attributes where file_id in (select id from file where $out_dataset_ids)) To '${OUTPUT_DIR}file_attributes.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM file_attributes WHERE file_id IN (SELECT id FROM file WHERE $out_dataset_ids)) To '${OUTPUT_DIR}file_attributes.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: file_relationship.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from file_relationship where file_id in (select id from file where $out_dataset_ids)) To '${OUTPUT_DIR}file_relationship.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM file_relationship WHERE file_id IN (SELECT id FROM file WHERE $out_dataset_ids)) To '${OUTPUT_DIR}file_relationship.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: file_sample.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from file_sample where file_id in (select id from file where $out_dataset_ids)) To '${OUTPUT_DIR}file_sample.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM file_sample WHERE file_id IN (SELECT id FROM file WHERE $out_dataset_ids)) To '${OUTPUT_DIR}file_sample.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: file_experiment.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from file_experiment where file_id in (select id from file where $out_dataset_ids)) To '${OUTPUT_DIR}file_experiment.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM file_experiment WHERE file_id IN (SELECT id FROM file WHERE $out_dataset_ids)) To '${OUTPUT_DIR}file_experiment.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: exp_attributes.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from exp_attributes where exp_id in (select id from experiment where $out_dataset_ids)) To '${OUTPUT_DIR}exp_attributes.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM exp_attributes WHERE exp_id IN (SELECT id FROM experiment WHERE $out_dataset_ids)) To '${OUTPUT_DIR}exp_attributes.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: unit.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from unit where id in (select units_id from exp_attributes where exp_id in (select id from experiment where $out_dataset_ids))) To '${OUTPUT_DIR}unit.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM unit WHERE id IN (SELECT units_id FROM exp_attributes WHERE exp_id IN (SELECT id FROM experiment WHERE $out_dataset_ids))) To '${OUTPUT_DIR}unit.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: news.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from news) To '${OUTPUT_DIR}news.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM news) To '${OUTPUT_DIR}news.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: prefix.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from prefix) To '${OUTPUT_DIR}prefix.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM prefix) To '${OUTPUT_DIR}prefix.csv' With (FORMAT CSV, HEADER)
 EOF
 
 einfo "Creating: rss_message.csv"
 PGPASSWORD=vagrant psql -h localhost -p 54321 -U gigadb gigadbv3_20200210 <<EOF
-  \copy (select * from rss_message) To '${OUTPUT_DIR}rss_message.csv' With (FORMAT CSV, HEADER)
+  \copy (SELECT * FROM rss_message) To '${OUTPUT_DIR}rss_message.csv' With (FORMAT CSV, HEADER)
 EOF
