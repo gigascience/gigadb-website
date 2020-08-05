b80fbf83ad7c02e677715e116c23d992216c9cc3
Ken <kencho.gigascience@gmail.com>
2020-08-05 14:39:10 +0800

    Create new class FilesPagination

---
+++ protected/components/FilesPagination.php
@@ -0,0 +1,17 @@
+<?php
+
+Yii::import("system.web.CPagination");
+
+class FilesPagination extends CPagination {
+    public function createPageUrl($controller,$page)
+    {
+        $params=$this->params===null ? $_GET : $this->params;
+//        if($page>0) // page 0 is the default
+            $params[$this->pageVar]=$page+1;
+//        else
+//            unset($params[$this->pageVar]);
+        return $controller->createUrl($this->route,$params);
+    }
+}
+
+?>
