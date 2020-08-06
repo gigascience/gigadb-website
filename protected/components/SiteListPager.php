75bffc5a9a4c758ea536d4661a6a2547cbfac151
Ken <kencho.gigascience@gmail.com>
2020-08-06 12:08:33 +0800

    Create Go to page buttons and text input field

--- protected/components/SiteListPager.php
+++ protected/components/SiteListPager.php
@@ -1,7 +1,13 @@
 <?php
+Yii::import("system.web.widgets.pagers.CListPager");
 
-
-class SiteListPager
+class SiteListPager extends CListPager
 {
-
+    public function init()
+    {
+        parent::init();
+        $this->header = Yii::t('app', '');
+    }
 }
+
+
