fb329f4848211071404decaba4fbb7551675aaeb
Ken <kencho.gigascience@gmail.com>
2020-09-17 15:16:26 +0800

    Create adminfilecontext.php

--- features/bootstrap/AdminFileContext.php
+++ features/bootstrap/AdminFileContext.php
@@ -1,7 +1,17 @@
 <?php
+use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 
+/**
+ *
+ * @uses GigadbWebsiteContext For loading production like data
+ * @uses GigadbWebsiteContext::iSignInAsAnAdmin For signing in as a admin
+ * @uses GigadbWebsiteContext::iSignInAsAUser For signing in as a user
+ * @uses \Behat\MinkExtension\Context\MinkContext For controlling the web browser
+ * @uses \PHPUnit_Framework_Assert
+ */
 
-class AdminFileContext
+class AdminFileContext implements Context
 {
 
 }
