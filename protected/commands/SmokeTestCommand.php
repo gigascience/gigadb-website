d53854b044cfd84d5b530e8147c66456f874591f
Rija Menage <rija@gigasciencejournal.com>
2020-09-26 19:00:57 +0200

    Fix smoke test data
    make sure the dataset has the default image associated to it.
    Remove the directories created during smoke testing

--- protected/commands/SmokeTestCommand.php
+++ protected/commands/SmokeTestCommand.php
@@ -2,6 +2,8 @@
 /**
  * Create and remove smoke test data to allow end to end non-destructive testing of FUW workflow on cloud environments
  *
+ * TODO: make the test id a constant
+ *
  * @author Rija Menage <rija+git@cinecinetique.com>
  * @license GPL-3.0
  */
@@ -21,8 +23,13 @@ class SmokeTestCommand extends CConsoleCommand {
 values(9999,'gigadb-smoke-test-user@rijam.sent.as','5a4f75053077a32e681f81daa8792f95','Joy','Fox','BGI','user','t','f','t',NULL,NULL,NULL,NULL,'gigadb-smoke-test-user@rijam.sent.as',NULL,'EBI')";
 
 		Yii::app()->db->createCommand($sql)->execute();
+
+		// Create a default image record
+		$sql="insert into image(id,location, tag, url, license, photographer, source) values(9999,'no_image.png','no image icon', 'http://gigadb.org/images/data/cropped/no_image.png', 'Public domain','GigaDB','GigaDB')";
+		Yii::app()->db->createCommand($sql)->execute();
+
     	// Create a test dataset
-    	$sql = "insert into dataset(id, submitter_id, identifier, title, dataset_size, ftp_site, upload_status) values(9999,9999, '000007','smoke test',342564,'ftp://','AssigningFTPbox')";
+    	$sql = "insert into dataset(id, submitter_id, image_id, identifier, title, dataset_size, ftp_site, upload_status) values(9999,9999,9999, '000007','smoke test',342564,'ftp://','AssigningFTPbox')";
     	Yii::app()->db->createCommand($sql)->execute();
 
     	return 0;
@@ -34,6 +41,13 @@ values(9999,'gigadb-smoke-test-user@rijam.sent.as','5a4f75053077a32e681f81daa879
 		Yii::app()->db->createCommand($sql)->execute();
 		$sql = "delete from gigadb_user where id=9999";
 		Yii::app()->db->createCommand($sql)->execute();
+		$sql = "delete from image where id=9999";
+		Yii::app()->db->createCommand($sql)->execute();
+
+
+		system("rm -rf /home/uploader/000007");
+		system("rm -rf /home/downloader/000007");
+		system("rm -rf /home/credentials/000007");
 		return 0;
 	}
 }
