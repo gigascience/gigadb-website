42d183ecdb73c1a7f281d198a21aad015393b087
Ken <kencho.gigascience@gmail.com>
2020-09-29 10:38:57 +0800

    Separate deleteFileAttribute into different class

---
+++ protected/controllers/adminFile/DeleteFileAttributeAction.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This action will delete file attributes in admin file update page
+ */
+
+class DeleteFileAttributeAction extends CAction
+{
+    public function run()
+    {
+        if (!Yii::app()->request->isPostRequest)
+            throw new CHttpException(404, "The requested page does not exist.");
+
+        if (isset($_POST['id'])) {
+            $attribute = FileAttributes::model()->findByPk($_POST['id']);
+            if ($attribute) {
+                $attribute->delete();
+                Yii::app()->end();
+            }
+        }
+    }
+}
