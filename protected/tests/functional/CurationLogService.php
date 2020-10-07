731b6944dc3ca66250bf06557625058c2da910b2
Ken <kencho.gigascience@gmail.com>
2020-10-07 14:18:39 +0800

    Update guest and admin user log in

--- protected/tests/functional/CurationLogService.php
+++ protected/tests/functional/CurationLogService.php
@@ -6,5 +6,33 @@
 
 class CurationLogService extends FunctionalTesting
 {
+    use BrowserSignInSteps;
+    use BrowserPageSteps;
+
+    /**
+     * To test guest user cannot visit admin file update page.
+     *
+     * @uses \BrowserSignInSteps::loginToWebSiteWithSessionAndCredentialsThenAssert()
+     * @uses \BrowserPageSteps::visitPageWithSessionAndUrlThenAssertContentHasOrNull()
+     *
+     */
+    public function testItShouldNotBeDisplayedToGuestUsers()
+    {
+        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("user@gigadb.org", "gigadb", "John's GigaDB Page");
+        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull("http://gigadb.gigasciencejournal.com:9170/site/adminFile/", "Login");
+    }
+
+    /**
+     * To test admin user can login and access admin file update page.
+     *
+     * @uses \BrowserSignInSteps::loginToWebSiteWithSessionAndCredentialsThenAssert()
+     * @uses \BrowserPageSteps::visitPageWithSessionAndUrlThenAssertContentHasOrNull()
+     *
+     */
+    public function testItShouldBeDisplayedToAdminUser()
+    {
+        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("admin@gigadb.org", "gigadb", "Joe's GigaDB Page");
+        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull("http://gigadb.dev/site/adminFile/", "Files");
+    }
 
 }
