e772bcf5694ac159eb0f2a9e96db6c023091e501
Ken <kencho.gigascience@gmail.com>
2020-10-08 17:08:30 +0800

    Add dataset status

--- protected/components/CurationLogService.php
+++ protected/components/CurationLogService.php
@@ -21,17 +21,18 @@ class CurationLogService extends CApplicationComponent
      */
     public function createNewEntry($id, $creator, $message )
     {
-        print("Stuff from CurationLogService2");
-
-        $curationlog = new CurationLog;
-
-        $curationlog->creation_date = date("Y-m-d");
-        $curationlog->last_modified_date = null;
-        $curationlog->dataset_id = $id;
-        $curationlog->created_by = "System";
-        $curationlog->action = "Status changed to stuff";
-        if (!$curationlog->save())
-            return false;
+        $model = Dataset::model()->findByPk($id);
+        // Save log to curation_log when dataset status is Published
+        if ($model->upload_status !== "Published") {
+            $curationlog = new CurationLog;
+            $curationlog->creation_date = date("Y-m-d");
+            $curationlog->last_modified_date = null;
+            $curationlog->dataset_id = $id;
+            $curationlog->created_by = "System";
+            $curationlog->action = "Status changed to stuff";
+            if (!$curationlog->save())
+                return false;
+        }
     }
 }
 ?>
