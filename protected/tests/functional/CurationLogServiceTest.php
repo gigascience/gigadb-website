6b6708ca9a358f5ba15eda32ffaf074568d62d30
Ken <kencho.gigascience@gmail.com>
2020-10-08 15:30:36 +0800

    Update functional test

--- protected/tests/functional/CurationLogServiceTest.php
+++ protected/tests/functional/CurationLogServiceTest.php
@@ -6,47 +6,57 @@
 
 class CurationLogServiceTest extends FunctionalTesting
 {
-    use BrowserSignInSteps;
-    use BrowserPageSteps;
-
-    /**
-     * To test guest user cannot visit admin file update page.
-     *
-     * @uses \BrowserSignInSteps::loginToWebSiteWithSessionAndCredentialsThenAssert()
-     * @uses \BrowserPageSteps::visitPageWithSessionAndUrlThenAssertContentHasOrNull()
-     *
-     */
-    public function testItShouldDisplayToGuestUsers()
+    public function testItShouldCreateEntryInCurationLogTableUsingCurationLogService()
     {
-        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("user@gigadb.org", "gigadb", "John's GigaDB Page");
-        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull("http://gigadb.dev/adminFile/update1/id/211", "Error 403");
-    }
-
-    /**
-     * To test admin user can login and access admin file update page.
-     *
-     * @uses \BrowserSignInSteps::loginToWebSiteWithSessionAndCredentialsThenAssert()
-     * @uses \BrowserPageSteps::visitPageWithSessionAndUrlThenAssertContentHasOrNull()
-     *
-     */
-    public function testItShouldDisplayToAdminUser()
-    {
-        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("admin@gigadb.org", "gigadb", "Joe's GigaDB Page");
-        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull("http://gigadb.dev/adminFile/update1/id/211", "Update File");
-    }
-
-    /**
-     * To test admin user can see New Attribute, Edit and Delete buttons
-     *
-     * @uses \BrowserSignInSteps::loginToWebSiteWithSessionAndCredentialsThenAssert()
-     * @uses \BrowserPageSteps::getCurrentUrl()
-     */
-    public function testItShouldBeSeenByAdminUser()
-    {
-        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("admin@gigadb.org", "gigadb", "Joe's GigaDB Page");
-        $url = "http://gigadb.dev/adminFile/update1/id/211";
-        $this->session->visit($url);
-        $this->assertTrue($this->session->getPage()->hasContent("New Attribute"), "Admin cannot see New Attribute");
+        try {
+            $result = Yii::app()->curationLogService->createNewEntry("210", "20", "Stuff");
+            $this->assertTrue("true" === "true", "stuff");
+        } catch (Error $e) {
+            $this->fail("Exception thrown: " . $e->getMessage());
+        }
 
     }
+//    use BrowserSignInSteps;
+//    use BrowserPageSteps;
+//
+//    /**
+//     * To test guest user cannot visit admin file update page.
+//     *
+//     * @uses \BrowserSignInSteps::loginToWebSiteWithSessionAndCredentialsThenAssert()
+//     * @uses \BrowserPageSteps::visitPageWithSessionAndUrlThenAssertContentHasOrNull()
+//     *
+//     */
+//    public function testItShouldDisplayToGuestUsers()
+//    {
+//        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("user@gigadb.org", "gigadb", "John's GigaDB Page");
+//        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull("http://gigadb.dev/adminFile/update1/id/211", "Error 403");
+//    }
+//
+//    /**
+//     * To test admin user can login and access admin file update page.
+//     *
+//     * @uses \BrowserSignInSteps::loginToWebSiteWithSessionAndCredentialsThenAssert()
+//     * @uses \BrowserPageSteps::visitPageWithSessionAndUrlThenAssertContentHasOrNull()
+//     *
+//     */
+//    public function testItShouldDisplayToAdminUser()
+//    {
+//        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("admin@gigadb.org", "gigadb", "Joe's GigaDB Page");
+//        $this->visitPageWithSessionAndUrlThenAssertContentHasOrNull("http://gigadb.dev/adminFile/update1/id/211", "Update File");
+//    }
+//
+//    /**
+//     * To test admin user can see New Attribute, Edit and Delete buttons
+//     *
+//     * @uses \BrowserSignInSteps::loginToWebSiteWithSessionAndCredentialsThenAssert()
+//     * @uses \BrowserPageSteps::getCurrentUrl()
+//     */
+//    public function testItShouldBeSeenByAdminUser()
+//    {
+//        $this->loginToWebSiteWithSessionAndCredentialsThenAssert("admin@gigadb.org", "gigadb", "Joe's GigaDB Page");
+//        $url = "http://gigadb.dev/adminFile/update1/id/211";
+//        $this->session->visit($url);
+//        $this->assertTrue($this->session->getPage()->hasContent("New Attribute"), "Admin cannot see New Attribute");
+//
+//    }
 }
