aa9cf3af06fc3870625104226495c7136622e59b
Ken <kencho.gigascience@gmail.com>
2020-10-19 14:25:21 +0800

    Try to call the static function and output last_modified date

--- protected/tests/unit/CurationLogTest.php
+++ protected/tests/unit/CurationLogTest.php
@@ -5,6 +5,8 @@ class CurationLogTest extends CDbTestCase
     public function testNoLastModifiedDate()
     {
         $expectation = null;
-        $this->assertEquals($expectation, CurationLog::createCurationLogEntry(8)->last_modified_date, "Last modified date created");
+//        $this->assertEquals($expectation, CurationLog::createCurationLogEntry(8)->last_modified_date, "Last modified date created");
+        $out = CurationLog::createCurationLogEntry(8);
+        file_put_contents('test.log', var_export($out, true));
     }
 }
