1465fa7ec4c25acc62d482311690fa61b98423de
Ken <kencho.gigascience@gmail.com>
2020-10-22 17:52:26 +0800

    Create factory method for the createDatasetLogEntry functin

--- protected/tests/unit/DatasetLogTest.php
+++ protected/tests/unit/DatasetLogTest.php
@@ -3,23 +3,35 @@
 
 class DatasetLogTest extends CDbTestCase
 {
+    public function testCreateDatasetLogEntryFactory()
+    {
+        $datasetId = 8;
+        $fileName = "Pygoscelis_adeliae.scaf.fa.gz";
+        $fileModel = "FileAttribute";
+        $modelId = 11020;
+        $fileId = 664;
+        $datasetlog = DatasetLog::datasetLogEntryFactory($datasetId, $fileName, $fileModel, $modelId, $fileId);
+        $this->assertNotNull($datasetlog);
+        $this->assertTrue(is_a($datasetlog, DatasetLog::class));
+        $this->assertEquals($datasetId, $datasetlog->dataset_id);
+        $this->assertEquals("$fileName: file attribute deleted", $datasetlog->message);
+        $this->assertEquals($fileModel, $datasetlog->model);
+        $this->assertEquals($modelId, $datasetlog->model_id);
+        $this->assertEquals("./bin/adminFile/update/id/$fileId", $datasetlog->url);
+    }
+
     public function testCreateDatasetLogEntry()
     {
         $datasetId = 8;
         $fileName = "Pygoscelis_adeliae.scaf.fa.gz";
         $fileModel = "FileAttribute";
         $modelId = 11020;
-        $fileId = "/adminFile/update/id/664";
+        $fileId = 664;
         $datasetlog = new DatasetLog(); //Instantiate a new Object
         $this->assertNotNull($datasetlog);
         $this->assertTrue(is_a($datasetlog, DatasetLog::class));
         $result = $datasetlog->createDatasetLogEntry($datasetId, $fileName, $fileModel, $modelId, $fileId);
         $this->assertTrue(true===$result, "No new entry in dataset log table");
-////        $this->assertEquals($datasetId, $this->dataset_id);
-////        $this->assertEquals($fileName, $datasetlog->message);
-////        $this->assertEquals($fileModel, $datasetlog->model);
-////        $this->assertEquals($modelId, $datasetlog->model_id);
-////        $this->assertEquals($fileId, $datasetlog->url);
         $this->assertTrue($datasetlog->isNewRecord);
 
     }
