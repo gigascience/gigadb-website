7628db56d16ef72a1f5deb2aeb497fb209eff5a5
Ken <kencho.gigascience@gmail.com>
2020-10-22 12:21:09 +0800

    Update unit test

--- protected/tests/unit/DatasetLogTest.php
+++ protected/tests/unit/DatasetLogTest.php
@@ -6,19 +6,21 @@ class DatasetLogTest extends CDbTestCase
     public function testCreateDatasetLogEntry()
     {
         $datasetId = 8;
-        $fileName = "Pygoscelis_adeliae.scaf.fa.gz: file attribute deleted";
+        $fileName = "Pygoscelis_adeliae.scaf.fa.gz";
         $fileModel = "FileAttribute";
         $modelId = 11020;
         $fileId = "/adminFile/update/id/664";
         $datasetlog = new DatasetLog(); //Instantiate a new Object
-        $datasetlog->createDatasetLogEntry($datasetId, $fileName, $fileModel, $modelId, $fileId);
         $this->assertNotNull($datasetlog);
         $this->assertTrue(is_a($datasetlog, DatasetLog::class));
-//        $this->assertEquals($datasetId, $this->dataset_id);
-//        $this->assertEquals($fileName, $datasetlog->message);
-//        $this->assertEquals($fileModel, $datasetlog->model);
-//        $this->assertEquals($modelId, $datasetlog->model_id);
-//        $this->assertEquals($fileId, $datasetlog->url);
+        $result = $datasetlog->createDatasetLogEntry($datasetId, $fileName, $fileModel, $modelId, $fileId);
+        $this->assertTrue(true===$result, "No new entry in dataset log table");
+////        $this->assertEquals($datasetId, $this->dataset_id);
+////        $this->assertEquals($fileName, $datasetlog->message);
+////        $this->assertEquals($fileModel, $datasetlog->model);
+////        $this->assertEquals($modelId, $datasetlog->model_id);
+////        $this->assertEquals($fileId, $datasetlog->url);
         $this->assertTrue($datasetlog->isNewRecord);
+
     }
 }
