cb633b1703ff275b3ebf5c0eb59ef25aafc004d8
Peter Li <peter@gigasciencejournal.com>
2020-10-28 10:39:15 +0800

    Add missing semicolons

--- style-guide/js/sg-scripts.js
+++ style-guide/js/sg-scripts.js
@@ -23,15 +23,15 @@ var SGB = window.SGB || {};
     var queryAll = document.querySelectorAll.bind(document);
 
     Node.prototype.on = window.on = function (name, fn) {
-      this.addEventListener(name, fn)
+      this.addEventListener(name, fn);
     };
 
     NodeList.prototype.forEach = Array.prototype.forEach;
 
     NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
       this.forEach(function (elem, i) {
-        elem.on(name, fn)
-      })
+        elem.on(name, fn);
+      });
     };
 
      // Add functionality to toggle classes on elements
