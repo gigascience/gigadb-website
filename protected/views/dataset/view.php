b74d4bede35955d30567283336ef09fb74889727
Ken <kencho.gigascience@gmail.com>
2020-11-06 15:02:02 +0800

    Bring citation box to the front

--- protected/views/dataset/view.php
+++ protected/views/dataset/view.php
@@ -34,13 +34,12 @@ $this->pageTitle="GigaDB Dataset - DOI 10.5524/".$model->identifier." - ".$title
                             <div class="color-background color-background-block dataset-color-background-block">
                                 <p><?= $mainSection->getReleaseDetails()['authors'] ?> (<?=$mainSection->getReleaseDetails()['release_year']?>): <?= $mainSection->getReleaseDetails()['dataset_title'].' '.($mainSection->getReleaseDetails()['publisher'] ?? '<span class="label label-danger">NO PUBLISHER SET</span>').'. '; ?><a href="http://dx.doi.org/<?= $mainSection->getReleaseDetails()['full_doi']; ?>">http://dx.doi.org/<?= $mainSection->getReleaseDetails()['full_doi']; ?></a></p>
                                 <p><a class="doi-badge" href="#"><span class="badge">DOI</span><span class="badge"><?= $mainSection->getReleaseDetails()['full_doi']; ?></span></a></p>
-                                <div class="citation-dropdown-box">
-                                    <button onclick="showCitation()" class="drop-citation-btn" >Cite Dataset<span class="caret"></span></button>
-                                    <div id="citationDropdown" class="citation-content">
-                                        <a id="citeText" href='http://data.datacite.org/text/x-bibliography/<?php echo $mainSection->getReleaseDetails()['full_doi'];?>'>Text</a>
-                                        <a id="citeRis" href='http://data.datacite.org/application/x-research-info-systems/<?php echo $mainSection->getReleaseDetails()['full_doi'];?>' target="_self">RIS</a>
-                                        <a id="citeBibTeX" href='http://data.datacite.org/application/x-bibtex/<?php echo $mainSection->getReleaseDetails()['full_doi'];?>' target="_self">BibTeX</a>
-                                    </div>
+                                <div class="citation-dropdown-box"></div>
+                                <button onclick="showCitation()" class="drop-citation-btn" >Cite Dataset<span class="caret"></span></button>
+                                <div id="citationDropdown" class="citation-content">
+                                    <a id="citeText" href='http://data.datacite.org/text/x-bibliography/<?php echo $mainSection->getReleaseDetails()['full_doi'];?>'>Text</a>
+                                    <a id="citeRis" href='http://data.datacite.org/application/x-research-info-systems/<?php echo $mainSection->getReleaseDetails()['full_doi'];?>' target="_self">RIS</a>
+                                    <a id="citeBibTeX" href='http://data.datacite.org/application/x-bibtex/<?php echo $mainSection->getReleaseDetails()['full_doi'];?>' target="_self">BibTeX</a>
                                 </div>
                             </div>
                         </div>
