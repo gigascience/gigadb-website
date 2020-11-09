5bf603175ed872728566097c558e26c194c2dde5
Rija Menage <rija@gigasciencejournal.com>
2020-11-09 17:29:52 +0100

    Add script to setup test database and remove test binary dump
    It will create the database and use CSV-based migrations to populate it
    and then create a binary dump from that database.
    It allows us to remove the need of a binary dump in the repository for the
    test database and unit tests.
    
    In order to not have to alter environment variable for database settings,
    we configure a second application component for a CDbConnection class in Yii
    (``console.php.dist``).
    Then when running the migration, we just add a ``--connectionID`` parameter
    keyed on that secondary connection to
    the Yii migration command.

---
+++ ops/scripts/setup_testdb.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# bail out upon error
+set -e
+
+# bail out if an unset variable is used
+set -u
+
+# create database if not existing
+docker-compose run --rm test bash -c "psql -h database -U gigadb -c 'create database gigadb_test'" || true
+# set gigadb env variables to point to test database
+
+# generate migrations and run them
+docker-compose run --rm  application ./protected/yiic migrate to 300000_000000 --connectionID=testdb --migrationPath=application.migrations.admin --interactive=0
+docker-compose run --rm  application ./protected/yiic migrate mark 000000_000000 --connectionID=testdb --interactive=0
+docker-compose run --rm  application ./protected/yiic migrate --connectionID=testdb --migrationPath=application.migrations.schema --interactive=0
+docker-compose run --rm  application ./protected/yiic migrate --connectionID=testdb --migrationPath=application.migrations.data.test --interactive=0
+# export a binary dump
+docker-compose run --rm test bash -c "pg_dump --no-owner -U gigadb -h database -p 5432 -F custom -d gigadb_test -f /var/www/sql/gigadb_testdata.pgdmp"
