ea5413d3a2832528d72c7c49354aef19c88bf8b2
Ken <kencho.gigascience@gmail.com>
2020-11-20 09:25:52 +0800

    Add fixture to access the dataset table

--- protected/tests/unit/EndNoteHelperTest.php
+++ protected/tests/unit/EndNoteHelperTest.php
@@ -4,10 +4,15 @@
 
 class EndNoteHelperTest extends CDbTestCase
 {
+
+    protected $fixtures=array(
+        'dataset'=>'Dataset',
+    );
+
     public function testGetCorrectIdentifier()
     {
-        $full_doi = "10.5524/100002";
-        $identifier = "100002";
+        $full_doi = "10.5524/100243";
+        $identifier = "100243";
 
         $this->assertEquals($identifier, \EndNoteHelper::getRecords($full_doi));
 
