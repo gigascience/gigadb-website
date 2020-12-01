b3514cbb36d780c8b3ee363f3c3350f854cb5980
Rija Menage <rija@gigasciencejournal.com>
2020-12-01 16:50:29 +0100

    Add info panel with instructions for web-based file uploader

--- protected/views/authorisedDataset/filesUpload.php
+++ protected/views/authorisedDataset/filesUpload.php
@@ -11,9 +11,25 @@
                     <dataset-info identifier="<?= $identifier ?>" />
                 </div>
             </header>
-            <section>
-                <uploader identifier="<?= $identifier ?>" endpoint="<?php echo $tusd_path ?>" />
-            </section>
+            <div>
+                <section class="span6">
+                    <uploader identifier="<?= $identifier ?>" endpoint="<?php echo $tusd_path ?>" />
+                </section>
+                <aside class="span4">
+                    <div class="panel panel-success">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Step 1: Uploading the files</h3>
+                        </div>
+                      <div class="panel-body">
+                        <ul>
+                        <li>This is the first step for submitting files associated to the dataset.</li>
+                        <li>You can upload files from your computer by dragging them to the area on the left.</li>
+                        <li>Once uploads are marked as "Complete", a "Next" button will appear at the bottom. You can click it to proceed to the next stage (annotating the files).</li>
+                        </ul>
+                      </div>
+                    </div>
+                </aside>
+            </div>
             <footer>
                 <pager identifier="<?= $identifier ?>" <?php echo "uploads-exist=\"$uploadsCount\"" ?>/>
             </footer>
