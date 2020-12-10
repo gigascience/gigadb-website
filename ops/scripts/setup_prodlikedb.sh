63fec0bf80062ffa54d50f3f94327314951e9602
Rija Menage <rija@gigasciencejournal.com>
2020-12-10 17:22:32 +0100

    Enable DB setup script to run on AWS deployment
    docker-compose call need to have remote host passed to it as argument
    when deployment on the cloud.
    
    Also on the cloud, we would need to bootstrap the database with production-like
    data.

--- ops/scripts/setup_prodlikedb.sh
+++ ops/scripts/setup_prodlikedb.sh
@@ -23,13 +23,13 @@ $DOCKER_COMPOSE run --rm test bash -c "psql -h database -U gigadb -c 'create dat
 
 # generate migrations (by default "test" data is loaded in the dev db)
 # docker run -v `pwd`:/var/www node:14.9.0-buster bash -c "node /var/www/ops/scripts/csv_yii_migration.js test"
-$DOCKER_COMPOSE run --rm js bash -c "node /var/www/ops/scripts/csv_yii_migration.js test"
+$DOCKER_COMPOSE run --rm js bash -c "node /var/www/ops/scripts/csv_yii_migration.js prod_like"
 
 # and run them
 $DOCKER_COMPOSE run --rm  application ./protected/yiic migrate to 300000_000000 --connectionID=db --migrationPath=application.migrations.admin --interactive=0
 $DOCKER_COMPOSE run --rm  application ./protected/yiic migrate mark 000000_000000 --connectionID=db --interactive=0
 $DOCKER_COMPOSE run --rm  application ./protected/yiic migrate --connectionID=db --migrationPath=application.migrations.schema --interactive=0
-$DOCKER_COMPOSE run --rm  application ./protected/yiic migrate --connectionID=db --migrationPath=application.migrations.data.test --interactive=0
+$DOCKER_COMPOSE run --rm  application ./protected/yiic migrate --connectionID=db --migrationPath=application.migrations.data.prod_like --interactive=0
 
 # run migration for FUW database
 $DOCKER_COMPOSE exec -T console /app/yii migrate/fresh --interactive=0
