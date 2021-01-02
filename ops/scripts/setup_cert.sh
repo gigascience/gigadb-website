cee6b576e0d130ea9549cc91a192fde665d87e3b
Rija Menage <rija@gigasciencejournal.com>
2021-01-02 16:53:56 +0100

    Fix method for testing whether certificate exist
    remove set -e, as we need to read the exit code, not exiting the script

--- ops/scripts/setup_cert.sh
+++ ops/scripts/setup_cert.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# bail out upon error
-set -e
-
 # bail out if an unset variable is used
 set -u
 
@@ -19,7 +16,7 @@ else
 fi
 
 echo "Checking whether the certificate exists"
-$DOCKER_COMPOSE exec -T web ls -alrt /etc/letsencrypt/live/$REMOTE_HOSTNAME/fullchain.pem
+$DOCKER_COMPOSE exec -T web test -f /etc/letsencrypt/live/$REMOTE_HOSTNAME/fullchain.pem
 
 if [[ $? -eq 0 ]];then
 	echo "Renewing the certificate for $REMOTE_HOSTNAME"
