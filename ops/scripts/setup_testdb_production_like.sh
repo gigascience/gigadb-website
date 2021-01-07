26f3c9e4c1d04841cddb87706e8705f78d30b377
Peter Li <peter@gigasciencejournal.com>
2021-01-07 15:03:12 +0800

    Update shell script
    Script for creating production_like database now uses CSV files to
    load data into tables instead of migration scripts. Password for
    accessing database is obtained from .secrets instead of having to
    type it in when prompted on command line.

--- ops/scripts/setup_testdb_production_like.sh
+++ ops/scripts/setup_testdb_production_like.sh
@@ -1,16 +1,70 @@
 #!/usr/bin/env bash
 
+# bail out upon error
+set -e
+# bail out if an unset variable is used
+set -u
+
+# Use database variables in .secrets
+set -a
+source ./.secrets
+set +a
+
 # create test database if not existing
-docker-compose run --rm test bash -c "psql -h database -U gigadb -c 'create database production_like'" || true
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -h database -U gigadb -c 'create database production_like'" || true
 
-# generate migrations
-docker run -v `pwd`:/var/www node:14.9.0-buster bash -c "node /var/www/ops/scripts/csv_yii_migration.js production_like"
-
-# and run them
+# create schema for production_like database
 docker-compose run --rm  application ./protected/yiic migrate to 300000_000000 --connectionID=testdb_production_like --migrationPath=application.migrations.admin --interactive=0
 docker-compose run --rm  application ./protected/yiic migrate mark 000000_000000 --connectionID=testdb_production_like --interactive=0
 docker-compose run --rm  application ./protected/yiic migrate --connectionID=testdb_production_like --migrationPath=application.migrations.schema --interactive=0
-docker-compose run --rm  application ./protected/yiic migrate --connectionID=testdb_production_like --migrationPath=application.migrations.data.production_like --interactive=0
+
+# Migration scripts are not generated and used to create the production_like
+# database since the size of the scripts causes memory execution problems.
+# Tables in production_like database are loaded with data using CSV files
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy publisher FROM '/var/www/data/production_like/publisher.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy image FROM '/var/www/data/production_like/image.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy gigadb_user FROM '/var/www/data/production_like/gigadb_user.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy search FROM '/var/www/data/production_like/search.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy extdb FROM '/var/www/data/production_like/extdb.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy type FROM '/var/www/data/production_like/type.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy dataset FROM '/var/www/data/production_like/dataset.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy attribute FROM '/var/www/data/production_like/attribute.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy author FROM '/var/www/data/production_like/author.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy curation_log FROM '/var/www/data/production_like/curation_log.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy unit FROM '/var/www/data/production_like/unit.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy dataset_attributes FROM '/var/www/data/production_like/dataset_attributes.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy dataset_author FROM '/var/www/data/production_like/dataset_author.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy funder_name FROM '/var/www/data/production_like/funder_name.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy dataset_funder FROM '/var/www/data/production_like/dataset_funder.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy dataset_log FROM '/var/www/data/production_like/dataset_log.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy species FROM '/var/www/data/production_like/species.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy sample FROM '/var/www/data/production_like/sample.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy project FROM '/var/www/data/production_like/project.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy dataset_project FROM '/var/www/data/production_like/dataset_project.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy dataset_sample FROM '/var/www/data/production_like/dataset_sample.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy dataset_type FROM '/var/www/data/production_like/dataset_type.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy experiment FROM '/var/www/data/production_like/experiment.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy exp_attributes FROM '/var/www/data/production_like/exp_attributes.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy external_link_type FROM '/var/www/data/production_like/external_link_type.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy external_link FROM '/var/www/data/production_like/external_link.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy file_format FROM '/var/www/data/production_like/file_format.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy file_type FROM '/var/www/data/production_like/file_type.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy file FROM '/var/www/data/production_like/file.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy file_attributes FROM '/var/www/data/production_like/file_attributes.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy file_experiment FROM '/var/www/data/production_like/file_experiment.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy relationship FROM '/var/www/data/production_like/relationship.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy file_relationship FROM '/var/www/data/production_like/file_relationship.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy file_sample FROM '/var/www/data/production_like/file_sample.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy link FROM '/var/www/data/production_like/link.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy manuscript FROM '/var/www/data/production_like/manuscript.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy news FROM '/var/www/data/production_like/news.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy prefix FROM '/var/www/data/production_like/prefix.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy relation FROM '/var/www/data/production_like/relation.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy rss_message FROM '/var/www/data/production_like/rss_message.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy alternative_identifiers FROM '/var/www/data/production_like/alternative_identifiers.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy sample_attribute FROM '/var/www/data/production_like/sample_attribute.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy sample_experiment FROM '/var/www/data/production_like/sample_experiment.csv' delimiter ',' CSV HEADER\""
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD psql -U gigadb -h database -p 5432 -d production_like -c \"\copy sample_rel FROM '/var/www/data/production_like/sample_rel.csv' delimiter ',' CSV HEADER\""
 
 # export a binary dump
-docker-compose run --rm test bash -c "pg_dump --no-owner -U gigadb -h database -p 5432 -F custom -d production_like -f /var/www/sql/production_like.pgdmp"
+docker-compose run --rm test bash -c "PGPASSWORD=$GIGADB_PASSWORD pg_dump --no-owner -U gigadb -h database -p 5432 -F custom -d production_like -f /var/www/sql/production_like.pgdmp"
