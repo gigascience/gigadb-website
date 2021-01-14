b4311a373ff0953c20860bc366148266f7459350
Ken <kencho.gigascience@gmail.com>
2021-01-14 10:20:31 +0800

    Add rel attribute to prevent tabnabbing

--- protected/views/dataset/view.php
+++ protected/views/dataset/view.php
@@ -35,7 +35,7 @@ $this->pageTitle="GigaDB Dataset - DOI 10.5524/".$model->identifier." - ".$title
                                 <p><?= $mainSection->getReleaseDetails()['authors'] ?> (<?=$mainSection->getReleaseDetails()['release_year']?>): <?= $mainSection->getReleaseDetails()['dataset_title'].' '.($mainSection->getReleaseDetails()['publisher'] ?? '<span class="label label-danger">NO PUBLISHER SET</span>').'. '; ?><a href="https://doi.org/10.5524/<?php echo $model->identifier;?>">https://doi.org/10.5524/<?php echo $model->identifier;?></a></p>
                                 <p><a class="doi-badge" href="#"><span class="badge">DOI</span><span class="badge">10.5524/<?php echo $model->identifier;?></span></a><button onclick="showCitation()" class="drop-citation-btn" >Cite Dataset<span class="caret"></span></button></p>
                                 <div id="citationDropdown" class="citation-content">
-                                    <a id="citeText" href='https://data.datacite.org/text/x-bibliography/10.5524/<?php echo $model->identifier;?>' target="_blank">Formatted Text</a>
+                                    <a id="citeText" href='https://data.datacite.org/text/x-bibliography/10.5524/<?php echo $model->identifier;?>' target="_blank" rel="noopener noreferrer">Formatted Text</a>
                                     <a id="citeRis" href='https://data.datacite.org/application/x-research-info-systems/10.5524/<?php echo $model->identifier;?>' target="_self">RIS</a>
                                     <a id="citeBibTeX" href='https://data.datacite.org/application/x-bibtex/10.5524/<?php echo $model->identifier;?>' target="_self">BibTeX</a>
                                 </div>
