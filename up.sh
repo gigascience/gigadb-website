9053e791056dc875803276ab27b2c75632e7d879
Rija Menage <rija@gigasciencejournal.com>
2021-01-19 15:03:56 +0100

    Correct the up.sh startup script to include all setup and startup steps
    Upon code checkout, up.sh can now be run to cover all setup and startup steps.
    
    Updated the doc accordingly.
    
    Also removed the binary postgresql dump from repo, as it generated in deployment
    and already gitignored.
    
    Also ensure git is installed in Docker images used by FUW as one of the Composer dependency
    (Email/Parse) use git as a download method.

--- up.sh
+++ up.sh
@@ -5,6 +5,7 @@ set -e
 echo "Starting all services..."
 
 # Make the Docker API available on TCP port 2375 on mac (unnecessary on windows or linux)
+docker stop socat && docker rm socat
 docker run --name socat -d -v /var/run/docker.sock:/var/run/docker.sock -p 127.0.0.1:2375:2375 bobrik/socat TCP-LISTEN:2375,fork UNIX-CONNECT:/var/run/docker.sock || true
 
 # Configure the container services
@@ -12,13 +13,17 @@ docker-compose run --rm config
 docker-compose run --rm fuw-config
 
 # Launch the services required by GigaDB and FUW, and then start nginx (web server) 
-docker-compose up gigadb fuw && docker-compose up -d web 
+docker-compose up --build gigadb fuw && docker-compose up -d web
 
 # Compile the CSS files
 docker-compose run --rm less
 
-# Install the NPM dependencies for the Javascript application
+# Install the NPM dependencies for the Javascript application and the ops scripts
 docker-compose run --rm js bash -c "npm install"
+docker-compose run --rm js bash -c "cd /var/www/ops/scripts/ && npm install"
+
+# Build and deploy the Javascript application
+docker-compose run --rm js
 
 # Start Chome web driver container services for acceptance testing
 docker-compose up -d chrome
@@ -29,6 +34,8 @@ docker-compose exec console bash -c 'cd /gigadb-apps/worker/file-worker/ && comp
 # Start Beanstalkd workers
 docker-compose up -d fuw-worker gigadb-worker
 
-
+# Setup the main and test databases
+./ops/scripts/setup_devdb.sh
+./ops/scripts/setup_testdb.sh
 
 
