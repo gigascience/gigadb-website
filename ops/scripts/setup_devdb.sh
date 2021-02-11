b4c72e7d86417746bc78e8096f17a825d6186f31
Rija Menage <rija@gigasciencejournal.com>
2021-02-11 16:34:10 +0100

    Improve the database setup script to accept parameter
    that way we can tell the setup script whether we want the database bootstrapped
    with limited test data or with production like data.
    
    The acceptable values for the parameter should match directory name in the data
    directory at the root of the project.

--- ops/scripts/setup_devdb.sh
+++ ops/scripts/setup_devdb.sh
@@ -10,6 +10,9 @@ set -u
 # Load .env se we can access CSV_DIR
 source "./.env"
 
+# Accepting parameter and set a default value for it for selecting the kind of canned DB data to populate the database
+# the value should match directory name under the "data" directory at the root of the project
+dbSet=${1:-"test"}
 
 # docker-compose executable
 if [[ $GIGADB_ENV != "dev" && $GIGADB_ENV != "CI" ]];then
@@ -23,13 +26,13 @@ $DOCKER_COMPOSE run --rm test bash -c "psql -h database -U gigadb -c 'create dat
 
 # generate migrations (by default "test" data is loaded in the dev db)
 # docker run -v `pwd`:/var/www node:14.9.0-buster bash -c "node /var/www/ops/scripts/csv_yii_migration.js test"
-$DOCKER_COMPOSE run --rm js bash -c "node /var/www/ops/scripts/csv_yii_migration.js test"
+$DOCKER_COMPOSE run --rm js bash -c "node /var/www/ops/scripts/csv_yii_migration.js $dbSet"
 
 # and run them
 $DOCKER_COMPOSE run --rm  application ./protected/yiic migrate to 300000_000000 --connectionID=db --migrationPath=application.migrations.admin --interactive=0
 $DOCKER_COMPOSE run --rm  application ./protected/yiic migrate mark 000000_000000 --connectionID=db --interactive=0
 $DOCKER_COMPOSE run --rm  application ./protected/yiic migrate --connectionID=db --migrationPath=application.migrations.schema --interactive=0
-$DOCKER_COMPOSE run --rm  application ./protected/yiic migrate --connectionID=db --migrationPath=application.migrations.data.test --interactive=0
+$DOCKER_COMPOSE run --rm  application ./protected/yiic migrate --connectionID=db --migrationPath=application.migrations.data.$dbSet --interactive=0
 
 # run migration for FUW database
 $DOCKER_COMPOSE exec -T console /app/yii migrate/fresh --interactive=0
