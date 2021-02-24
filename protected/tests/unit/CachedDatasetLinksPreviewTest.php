867596f5137419f32013484c74b84836071cd501
Ken <kencho.gigascience@gmail.com>
2021-02-24 17:28:07 +0800

    Create test for cache preview data

--- protected/tests/unit/CachedDatasetLinksPreviewTest.php
+++ protected/tests/unit/CachedDatasetLinksPreviewTest.php
@@ -141,4 +141,46 @@ class CachedDatasetLinksPreviewTest extends CTestCase
         $imageUrlUnderTest = new CachedDatasetLinksPreview($cache, $cacheDependency, $storedDatasetLinksPreview);
         $this->assertEquals($expected_imageurl, $imageUrlUnderTest->getImageUrl());
     }
+
+    public function testGetPreviewDataForLinksCacheHit()
+    {
+        $dataset_id = 1;
+        $expected_previewData = array(
+            array(
+                'short_doi'=>'100243',
+                'url'=>'https://doi.org/10.5524/100243',
+                'title'=>'Supporting data for "Analyzing climate variations on multiple timescales can guide Zika virus response measures"',
+                'description'=>'The emergence of Zika virus (ZIKV) as a public health emergency in Latin America and the Caribbean (LAC) occurred during a period of severe drought and unusually high temperatures. Speculation in the literature exists that these climate conditions were associated with the 2015/2016 El Niño event and/or climate change but to date no quantitative ',
+                'image_url'=>'http://gigadb.org/images/data/cropped/100243.gif',
+            ),
+        );
+
+        $storedDatasetLinksPreview = $this->createMock(StoredDatasetLinksPreview::class);
+        $storedDatasetLinksPreview->method('getDatasetId')
+            ->willReturn(1);
+
+        $cache = $this->getMockBuilder(CApcCache::class)
+            ->setMethods(['get'])
+            ->getMock();
+
+        $cache->expects($this->once())
+            ->method('get')
+            ->willReturn(
+                array(
+                    array(
+                        'short_doi'=>'100243',
+                        'url'=>'https://doi.org/10.5524/100243',
+                        'title'=>'Supporting data for "Analyzing climate variations on multiple timescales can guide Zika virus response measures"',
+                        'description'=>'The emergence of Zika virus (ZIKV) as a public health emergency in Latin America and the Caribbean (LAC) occurred during a period of severe drought and unusually high temperatures. Speculation in the literature exists that these climate conditions were associated with the 2015/2016 El Niño event and/or climate change but to date no quantitative ',
+                        'image_url'=>'http://gigadb.org/images/data/cropped/100243.gif',
+                    ),
+                )
+            );
+
+        $cacheDependency = $this->createMock(CCacheDependency::class);
+
+        $previewDataUnderTest = new CachedDatasetLinksPreview($cache, $cacheDependency, $storedDatasetLinksPreview);
+        $this->assertEquals($expected_previewData, $previewDataUnderTest->getPreviewDataForLinks());
+    }
+
 }
