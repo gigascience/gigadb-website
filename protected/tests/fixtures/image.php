247c1f73e839ce252b21ffcde600c2d98cad35ca
Ken <kencho.gigascience@gmail.com>
2021-02-25 12:45:49 +0800

    Add id=2 to image fixture

--- protected/tests/fixtures/image.php
+++ protected/tests/fixtures/image.php
@@ -9,5 +9,14 @@ return array(
         'license'=>'CC0',
         'photographer'=>'Munoz et al 2016',
         'source'=>'Munoz et al 2016',
+    ),
+    array(
+        'id' => 2,
+        'location'=>'100249.gif',
+        'tag'=>'Image of an adult Tigriopus kingsejongensis specimen.',
+        'url'=>'http://gigadb.org/images/data/cropped/100249.jpg',
+        'license'=>'CC0',
+        'photographer'=>'Sanghee Kim',
+        'source'=>'Sanghee Kim',
     )
 );
