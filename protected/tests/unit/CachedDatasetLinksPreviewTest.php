6e1ec0e95cb9f4602c194a2fd1a63397620c0aba
Ken <kencho.gigascience@gmail.com>
2021-03-10 16:00:37 +0800

    Pass unit test for the caching

--- protected/tests/unit/CachedDatasetLinksPreviewTest.php
+++ protected/tests/unit/CachedDatasetLinksPreviewTest.php
@@ -52,20 +52,22 @@ class CachedDatasetLinksPreviewTest extends CTestCase
 
     public function testGetPreviewDataForLinksCacheHit()
     {
-        $dataset_id = 1;
+        $dataset_id = 2;
         $expected_previewData = array(
             array(
-                'short_doi'=>'100243',
-                'url'=>'https://doi.org/10.5524/100243',
-                'title'=>'Supporting data for "Analyzing climate variations on multiple timescales can guide Zika virus response measures"',
-                'description'=>'The emergence of Zika virus (ZIKV) as a public health emergency in Latin America and the Caribbean (LAC) occurred during a period of severe drought and unusually high temperatures. Speculation in the literature exists that these climate conditions were associated with the 2015/2016 El Niño event and/or climate change but to date no quantitative ',
-                'image_url'=>'http://gigadb.org/images/data/cropped/100243.gif',
-            ),
+                'short_doi'=>'100249',
+                'external_url'=>'http://foo6.com',
+                'type'=>'3D Models',
+                'external_title'=>'Exercise generates immune cells in bone',
+                'external_description'=>'Mechanosensing stem-cell niche promotes lymphocyte production.',
+                'external_imageUrl'=>'https://media.nature.com/lw1024/magazine-assets/d41586-021-00419-y/d41586-021-00419-y_18880568.png',
+            )
         );
 
+        //Assume the external website info was cached, so no need to mock GetPreviewDataForLinks
         $storedDatasetLinksPreview = $this->createMock(StoredDatasetLinksPreview::class);
         $storedDatasetLinksPreview->method('getDatasetId')
-            ->willReturn(1);
+            ->willReturn(2);
 
         $cache = $this->getMockBuilder(CApcCache::class)
             ->setMethods(['get'])
@@ -73,15 +75,18 @@ class CachedDatasetLinksPreviewTest extends CTestCase
 
         $cache->expects($this->once())
             ->method('get')
+            //mock the cached returned message
+            ->with($this->equalTo("dataset_${dataset_id}_CachedDatasetLinksPreview_getPreviewDataForLinks"))
             ->willReturn(
                 array(
                     array(
-                        'short_doi'=>'100243',
-                        'url'=>'https://doi.org/10.5524/100243',
-                        'title'=>'Supporting data for "Analyzing climate variations on multiple timescales can guide Zika virus response measures"',
-                        'description'=>'The emergence of Zika virus (ZIKV) as a public health emergency in Latin America and the Caribbean (LAC) occurred during a period of severe drought and unusually high temperatures. Speculation in the literature exists that these climate conditions were associated with the 2015/2016 El Niño event and/or climate change but to date no quantitative ',
-                        'image_url'=>'http://gigadb.org/images/data/cropped/100243.gif',
-                    ),
+                        'short_doi'=>'100249',
+                        'external_url'=>'http://foo6.com',
+                        'type'=>'3D Models',
+                        'external_title'=>'Exercise generates immune cells in bone',
+                        'external_description'=>'Mechanosensing stem-cell niche promotes lymphocyte production.',
+                        'external_imageUrl'=>'https://media.nature.com/lw1024/magazine-assets/d41586-021-00419-y/d41586-021-00419-y_18880568.png',
+                    )
                 )
             );
 
@@ -91,4 +96,79 @@ class CachedDatasetLinksPreviewTest extends CTestCase
         $this->assertEquals($expected_previewData, $previewDataUnderTest->getPreviewDataForLinks());
     }
 
+    public function testtestGetPreviewDataForLinksCacheMiss()
+    {
+        $dataset_id = 2;
+        $expected_previewData = array(
+            array(
+                'short_doi'=>'100249',
+                'external_url'=>'http://foo6.com',
+                'type'=>'3D Models',
+                'external_title'=>'Exercise generates immune cells in bone',
+                'external_description'=>'Mechanosensing stem-cell niche promotes lymphocyte production.',
+                'external_imageUrl'=>'https://media.nature.com/lw1024/magazine-assets/d41586-021-00419-y/d41586-021-00419-y_18880568.png',
+            )
+        );
+
+        //Assume external web site info was not cached, so need to mock GetPreviewDataForLinks
+        $storedDatasetLinksPreview = $this->getMockBuilder(StoredDatasetLinksPreview::class)
+            ->setMethods(['getDatasetId', 'getPreviewDataForLinks'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $storedDatasetLinksPreview->expects($this->exactly(2))
+            ->method('getDatasetId')
+            ->willReturn(2);
+
+        $storedDatasetLinksPreview->expects($this->once())
+            ->method('getPreviewDataForLinks')
+            ->willReturn(
+                array(
+                    array(
+                        'short_doi'=>'100249',
+                        'external_url'=>'http://foo6.com',
+                        'type'=>'3D Models',
+                        'external_title'=>'Exercise generates immune cells in bone',
+                        'external_description'=>'Mechanosensing stem-cell niche promotes lymphocyte production.',
+                        'external_imageUrl'=>'https://media.nature.com/lw1024/magazine-assets/d41586-021-00419-y/d41586-021-00419-y_18880568.png',
+                    )
+                )
+            );
+
+        $cacheDependency = $this->createMock(CCacheDependency::class);
+
+        $cache = $this->getMockBuilder(CApcCache::class)
+            ->setMethods(['get','set'])
+            ->getMock();
+
+        //set a Cache Miss
+        $cache->expects($this->once())
+            ->method('get')
+            ->with($this->equalTo("dataset_${dataset_id}_CachedDatasetLinksPreview_getPreviewDataForLinks"))
+            ->willReturn(
+                false
+            );
+
+        $cache->expects($this->once())
+            ->method('set')
+            ->with(
+                $this->equalTo("dataset_${dataset_id}_CachedDatasetLinksPreview_getPreviewDataForLinks"),
+                array(
+                    array(
+                        'short_doi'=>'100249',
+                        'external_url'=>'http://foo6.com',
+                        'type'=>'3D Models',
+                        'external_title'=>'Exercise generates immune cells in bone',
+                        'external_description'=>'Mechanosensing stem-cell niche promotes lymphocyte production.',
+                        'external_imageUrl'=>'https://media.nature.com/lw1024/magazine-assets/d41586-021-00419-y/d41586-021-00419-y_18880568.png',
+                    )
+                ),
+                Cacheable::defaultTTL*30,
+                $cacheDependency
+            )
+            ->willReturn(true);
+
+        $previewDataUnderTest = new CachedDatasetLinksPreview($cache, $cacheDependency, $storedDatasetLinksPreview);
+        $this->assertEquals($expected_previewData, $previewDataUnderTest->getPreviewDataForLinks());
+    }
 }
