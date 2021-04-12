7da3d41715cb2000d007d20420224752e4d61800
Ken <kencho.gigascience@gmail.com>
2021-04-12 15:56:53 +0800

    Remove extra step to check for empty value

--- features/bootstrap/DatasetViewContext.php
+++ features/bootstrap/DatasetViewContext.php
@@ -294,23 +294,12 @@ class DatasetViewContext implements Context
     public function iShouldSeeFieldWithValue($arg1, $arg2)
     {
         $row = $this->minkContext->getSession()->getPage()->find('css', sprintf('table tr:contains("%s")', $arg1));
-        preg_match('/<td>[\d*\s]*<\/td>/', $row->getHtml(), $matches);
+        preg_match('/<td>.+<\/td>/', $row->getHtml(), $matches);
         $search = array("<td>", "</td>");
-        $actual_attribute_id = str_replace($search,"", $matches[0]);
-        PHPUnit_Framework_Assert::assertEquals($arg2, $actual_attribute_id);
+        $actual_attribute_value = str_replace($search,"", $matches[0]);
+        PHPUnit_Framework_Assert::assertEquals($arg2, $actual_attribute_value);
     }
 
-    /**
-     * @Then I should see field :arg1 with empty value
-     */
-    public function iShouldSeeFieldIsEmpty($arg1)
-    {
-        $row = $this->minkContext->getSession()->getPage()->find('css', sprintf('table tr:contains("%s")', $arg1));
-        preg_match('/<td>[\d*\s]*<\/td>/', $row->getHtml(), $matches);
-        $search = array("<td>", "</td>");
-        $actual_attribute_id = str_replace($search,"", $matches[0]);
-        PHPUnit_Framework_Assert::assertEmpty($actual_attribute_id);
-    }
 
     /**
      * @Then I should see :arg1 tab with table
