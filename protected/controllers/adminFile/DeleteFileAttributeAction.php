dc1754e089cebfbfdc7c32f81181b82be2ecada7
Ken <kencho.gigascience@gmail.com>
2021-04-19 11:13:21 +0800

    Update variable name

--- protected/controllers/adminFile/DeleteFileAttributeAction.php
+++ protected/controllers/adminFile/DeleteFileAttributeAction.php
@@ -14,16 +14,16 @@ class DeleteFileAttributeAction extends CAction
             $attribute = FileAttributes::model()->findByPk($_POST['id']);
 
             if ($attribute) {
-                $out = $attribute->file->dataset_id;
+                $dataset_id = $attribute->file->dataset_id;
                 $fileName = $attribute->file->name;
                 $fileModel = get_class($attribute);
                 $fileId = $attribute->file->id;
                 $modelId = $attribute->id;
-                $model = Dataset::model()->findByPk($out);
+                $model = Dataset::model()->findByPk($dataset_id);
                 if ($model->upload_status === "Published") {
-                    DatasetLog::createDatasetLogEntry($out, $fileName, $fileModel, $modelId, $fileId);
+                    DatasetLog::createDatasetLogEntry($dataset_id, $fileName, $fileModel, $modelId, $fileId);
                 } else {
-                    CurationLog::createCurationLogEntry($out, $fileName); //Pass in dataset_id returned from File object.
+                    CurationLog::createCurationLogEntry($dataset_id, $fileName); //Pass in dataset_id returned from File object.
                 }
                 $attribute->delete();
                 Yii::app()->end();
