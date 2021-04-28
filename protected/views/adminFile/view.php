59a0ca313f97445154bae51aeca68e4082c1cd82
Ken <kencho.gigascience@gmail.com>
2021-04-28 17:36:25 +0800

    Add row using array_push

--- protected/views/adminFile/view.php
+++ protected/views/adminFile/view.php
@@ -8,58 +8,46 @@
 
 <?php
 $sample_id = FileSample::model()->find('file_id=:file_id', array(':file_id'=>$model->id));
-$attributes = FileAttributes::model()->findAll('file_id=:file_id', array(':file_id'=>$model->id));
+$fileAttributes = FileAttributes::model()->findAll('file_id=:file_id', array(':file_id'=>$model->id));
 
 if(isset($sample_id))
- {
- $sample_name= Sample::model()->find('id=:id',array(':id'=>$sample_id->sample_id));
- }
+{
+    $sample_name= Sample::model()->find('id=:id',array(':id'=>$sample_id->sample_id));
+}
 
- $name="Not Set";
+$name="Not Set";
  
- if(isset($sample_id)&&isset($sample_name))
- {
-     $name=$sample_name->name;
- }
+if(isset($sample_id)&&isset($sample_name))
+{
+    $name=$sample_name->name;
+}
 
+$attributes = array(
+    'id',
+    'dataset_id',
+    'name',
+    'location',
+    'extension',
+    'size',
+    'description',
+    'date_stamp',
+    'format_id',
+    'type_id',
+    array(
+        'name'=>'Sample',
+        'value'=> $name,
+    )
+);
 
-$attribute_array = array();
+if(!empty($fileAttributes))
+{
+    foreach ($fileAttributes as $fileAttribute)
+    {
+        array_push($attributes, array('name' => 'FileAttribute', 'value' => $fileAttribute->value));
+    }
+}
 
- if(!empty($attributes))
- {
-     $attribute_value = "";
-     foreach ($attributes as $attribute)
-     {
-         $attribute_value .= $attribute->value . ', ';
-     }
-     $attribute_value = rtrim($attribute_value, ', ');
-     $attribute_array['name'] = 'FileAttribute';
-     $attribute_array['value'] = $attribute_value;
-
- } else {
-     $attribute_array['name'] = '';
-     $attribute_array['value'] = '';
- }
-
-
-
- $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'dataset_id',
-		'name',
-		'location',
-		'extension',
-		'size',
-		'description',
-		'date_stamp',
-		'format_id',
-		'type_id',
-		  array(
-                    'name'=>'Sample',
-                    'value'=> $name,
-                ),
-        $attribute_array,
-	),
-)); ?>
+$this->widget('zii.widgets.CDetailView', array(
+        'data'=>$model,
+        'attributes'=> $attributes
+));
