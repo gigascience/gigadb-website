42d1b725e6c6ddd3ba22383ad518e1f08897b5b0
Ken <kencho.gigascience@gmail.com>
2021-05-19 16:06:47 +0800

    Bring the dropdown items to the front by changing position parameter

--- protected/views/dataset/view.php
+++ protected/views/dataset/view.php
@@ -39,25 +39,23 @@ $this->pageTitle="GigaDB Dataset - DOI 10.5524/".$model->identifier." - ".$title
                                     </div>
                                     <?php if ($model->upload_status == 'Published') { ?>
                                         <div id="dropdown-div">
-                                            <div class="dropdown">
+                                            <div class="dropdown-box">
                                                 <button id="CiteDataset" class="drop-citation-btn dropdown-toggle" type="button" data-toggle="dropdown">Cite Dataset<span class="caret"></span></button>
-<!--                                                <div id="citationDropdown" class="citation-content">-->
-                                                    <?php
-                                                    $text = file_get_contents('https://data.datacite.org/text/x-bibliography/10.5524/' . $model->identifier);
-                                                    $clean_text = strip_tags(preg_replace("/&#?[a-z0-9]+;/i", "", $text));
-                                                    ?>
-                                                    <script>
-                                                        function showText() {
-                                                            var textWindow = window.open();
-                                                            textWindow.document.write(`<?php echo $clean_text; ?>`);
-                                                        }
-                                                    </script>
-                                                    <ul class="dropdown-menu" aria-labelledby="CiteDataset">
-                                                        <li><a id="Text" onclick="showText()" target="_blank">Text</a></li>
-                                                        <li><a id="citeRis" href='https://data.datacite.org/application/x-research-info-systems/10.5524/<?php echo $model->identifier;?>' target="_self">RIS</a></li>
-                                                        <li><a id="citeBibTeX" href='https://data.datacite.org/application/x-bibtex/10.5524/<?php echo $model->identifier;?>' target="_self">BibTeX</a></li>
-                                                    </ul>
-<!--                                                </div>-->
+                                                <?php
+                                                $text = file_get_contents('https://data.datacite.org/text/x-bibliography/10.5524/' . $model->identifier);
+                                                $clean_text = strip_tags(preg_replace("/&#?[a-z0-9]+;/i", "", $text));
+                                                ?>
+                                                <script>
+                                                    function showText() {
+                                                        var textWindow = window.open();
+                                                        textWindow.document.write(`<?php echo $clean_text; ?>`);
+                                                    }
+                                                </script>
+                                                <ul class="dropdown-menu" aria-labelledby="CiteDataset">
+                                                    <li><a id="Text" onclick="showText()" target="_blank">Text</a></li>
+                                                    <li><a id="citeRis" href='https://data.datacite.org/application/x-research-info-systems/10.5524/<?php echo $model->identifier;?>' target="_self">RIS</a></li>
+                                                    <li><a id="citeBibTeX" href='https://data.datacite.org/application/x-bibtex/10.5524/<?php echo $model->identifier;?>' target="_self">BibTeX</a></li>
+                                                </ul>
                                             </div>
                                         </div>
                                     <?php } ?>
@@ -803,24 +801,4 @@ document.addEventListener("DOMContentLoaded", function(event) { //This event is 
         }
     }
 
-</script>
-<script>
-    /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
-    function showCitation() {
-        document.getElementById("citationDropdown").classList.toggle("show");
-    }
-
-    // Close the dropdown if the user clicks outside of it
-    window.onclick = function(event) {
-        if (!event.target.matches('.drop-citation-btn')) {
-            var dropdowns = document.getElementsByClassName("citation-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }
 </script>
