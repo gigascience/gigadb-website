5915b2e02f7fdabed17be1568cf9bf11e5078337
Peter <peter@gigasciencejournal.com>
2021-07-09 15:13:55 +0800

    Add verbose option to display out from coscmd tool

--- gigadb/app/tools/dataset-backup-tool/controllers/DatasetFilesController.php
+++ gigadb/app/tools/dataset-backup-tool/controllers/DatasetFilesController.php
@@ -20,7 +20,7 @@ class DatasetFilesController extends Controller
     /**
      * @var bool true to show the full audit of transformation
      */
-    public bool $verbose = false;
+    public bool $verbose = true;
 
     /**
      * @var string the directory path where dataset files are located
@@ -49,6 +49,17 @@ class DatasetFilesController extends Controller
     {
         $optSourcedir = $this->sourcedir;
         $optDestdir = $this->destdir;
+        $optVerbose = $this->verbose;
+
+        // Ask for confirmation to proceed
+        $this->stdout("\nWarning! ", Console::FG_RED);
+        switch($this->confirm("This command will upload files into bucket, are you sure you want to proceed?\n")) {
+            case false:
+                $this->stdout("Aborting.\n", Console::FG_BLUE);
+                return ExitCode::NOPERM;
+            default:
+                $this->stdout("Executing command...\n", Console::FG_BLUE);
+        }
 
         $this->stdout("\nUploading files to bucket\n", Console::BOLD);
         try {
@@ -59,173 +70,10 @@ class DatasetFilesController extends Controller
             Yii::error($e->getMessage());
             return ExitCode::OSERR;
         }
-        print_r($output);
+        if ($optVerbose) {
+            $this->stdout("\nOutput from coscmd:\n\n", Console::BOLD );
+            print_r($output);
+        }
         return ExitCode::OK;
     }
-
-//    /**
-//     * This command will create a Tencent COS bucket
-//     *
-//     * @throws \Throwable
-//     * @return int Exit code
-//     */
-//    public function actionCreateBucket()
-//    {
-//        $this->stdout("\nCreating Bucket for {$this->date}\n", Console::BOLD);
-//        try {
-//            $output = shell_exec("scripts/create_bucket.sh");
-//        }
-//        catch (Throwable $e) {
-//            $this->stdout($e->getMessage().PHP_EOL, Console::FG_RED);
-//            Yii::error($e->getMessage());
-//            return ExitCode::OSERR;
-//        }
-//        return $output;
-//    }
-//
-//    /**
-//     * This command will delete a Tencent COS bucket
-//     *
-//     * @throws \Throwable
-//     * @return int Exit code
-//     */
-//    public function actionDeleteBucket()
-//    {
-//        $this->stdout("\nDeleting Bucket for {$this->date}\n", Console::BOLD);
-//        try {
-//            $output = shell_exec("scripts/delete_bucket.sh");
-//        }
-//        catch (Throwable $e) {
-//            $this->stdout($e->getMessage().PHP_EOL, Console::FG_RED);
-//            Yii::error($e->getMessage());
-//            return ExitCode::OSERR;
-//        }
-//        return $output;
-//    }
-//
-//    /**
-//     * This command will delete directory
-//     *
-//     * @throws \Throwable
-//     * @return int Exit code
-//     */
-//    public function actionDeleteDirectory($directory)
-//    {
-//        $this->stdout("\nDeleting directory for {$this->date}\n", Console::BOLD);
-//
-//        try {
-//            // shell_exec returns complete output as string
-//            $output = shell_exec("coscmd -c ./scripts/.cos.conf delete -r -f ".$directory." 2>&1");
-//        }
-//        catch (Throwable $e) {
-//            $this->stdout($e->getMessage().PHP_EOL, Console::FG_RED);
-//            Yii::error($e->getMessage());
-//            return ExitCode::OSERR;
-//        }
-//        return $output;
-//    }
-//
-//    /**
-//     * This command will list the contents of a directory in a Tencent back up
-//     *
-//     * @throws \Throwable
-//     * @return int Exit code
-//     */
-//    public function actionListContents($directory)
-//    {
-//        $this->stdout("\nListing contents for {$this->date}\n", Console::BOLD);
-//
-//        try {
-//            // shell_exec returns complete output as string
-//            $output = shell_exec("coscmd -c ./scripts/.cos.conf list ".$directory." 2>&1");
-//        }
-//        catch (Throwable $e) {
-//            $this->stdout($e->getMessage().PHP_EOL, Console::FG_RED);
-//            Yii::error($e->getMessage());
-//            return ExitCode::OSERR;
-//        }
-//        return $output;
-//    }
-//
-//    /**
-//     * This command will backup a dataset directory to Tencent bucket
-//     *
-//     * @throws \Throwable
-//     * @return int Exit code
-//     */
-//    public function actionBackupDataset()
-//    {
-//        $this->stdout("\nBacking up dataset for {$this->date}\n", Console::BOLD);
-//        try {
-//            $output = shell_exec("coscmd --debug --config_path scripts/.cos.conf upload -H '{\"x-cos-storage-class\":\"DEEP_ARCHIVE\"}' -rs tests/_data/dataset1/ dataset/ 2>&1");
-//        }
-//        catch (Throwable $e) {
-//            $this->stdout($e->getMessage().PHP_EOL, Console::FG_RED);
-//            Yii::error($e->getMessage());
-//            return ExitCode::OSERR;
-//        }
-//        //        return ExitCode::OK;
-//        return $output;
-//    }
-//
-//    /**
-//     * This command will backup dataset2 to Tencent bucket
-//     *
-//     * @throws \Throwable
-//     * @return int Exit code
-//     */
-//    public function actionUpdateBackupWithChangedFile()
-//    {
-//        $this->stdout("\nUpdating backup with changed CSV for {$this->date}\n", Console::BOLD);
-//        try {
-//            $output = shell_exec("coscmd --debug --config_path scripts/.cos.conf upload -H '{\"x-cos-storage-class\":\"DEEP_ARCHIVE\"}' -rs tests/_data/dataset2/ dataset/ 2>&1");
-//        }
-//        catch (Throwable $e) {
-//            $this->stdout($e->getMessage().PHP_EOL, Console::FG_RED);
-//            Yii::error($e->getMessage());
-//            return ExitCode::OSERR;
-//        }
-//        return $output;
-//    }
-//
-//    /**
-//     * This command will backup dataset2 to Tencent bucket 2>&1
-//     *
-//     * @throws \Throwable
-//     * @return int Exit code
-//     */
-//    public function actionUpdateBackupWithDeletedFile()
-//    {
-//        $this->stdout("\nUpdating backup with deleted TSV file for {$this->date}\n", Console::BOLD);
-//        try {
-//            $output = shell_exec("coscmd --debug --config_path scripts/.cos.conf upload -H '{\"x-cos-storage-class\":\"DEEP_ARCHIVE\"}' -rsy --delete tests/_data/dataset3/ dataset/ 2>&1");
-//        }
-//        catch (Throwable $e) {
-//            $this->stdout($e->getMessage().PHP_EOL, Console::FG_RED);
-//            Yii::error($e->getMessage());
-//            return ExitCode::OSERR;
-//        }
-//        return $output;
-//    }
-//
-//    /**
-//     * Display for information for a given file $filepath
-//     *
-//     * @throws \Throwable
-//     * @return int Exit code
-//     */
-//    public function actionViewFileInfo($filepath)
-//    {
-//        $this->stdout("\nViewing file information for {$this->date}\n", Console::BOLD);
-//        try {
-//            $output = shell_exec("coscmd -c ./scripts/.cos.conf info ".$filepath." 2>&1");
-//        }
-//        catch (Throwable $e) {
-//            $this->stdout($e->getMessage().PHP_EOL, Console::FG_RED);
-//            Yii::error($e->getMessage());
-//            return ExitCode::OSERR;
-//        }
-//        //return ExitCode::OK;
-//        return $output;
-//    }
 }
