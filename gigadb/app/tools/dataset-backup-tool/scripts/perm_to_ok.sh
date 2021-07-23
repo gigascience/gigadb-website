499a82fff39a5866b29bf936cad56b7f54e1a927
Ken <kencho.gigascience@gmail.com>
2021-07-24 07:18:43 +0800

    Implement codeception _before and _after methods
    Also removed change permission scripts and file 
    permission helper class.

--- gigadb/app/tools/dataset-backup-tool/scripts/perm_to_ok.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-chmod 644 tests/_data/10.1234/100001_101009/100300/perm-not-ok.txt
--- gigadb/app/tools/dataset-backup-tool/tests/functional/FixPermissionCest.php
+++ gigadb/app/tools/dataset-backup-tool/tests/functional/FixPermissionCest.php
@@ -5,6 +5,20 @@ use yii\console\ExitCode;
 class FixPermissionCest
 {
     /**
+     * To make sure change the permission of the file to non globally readable before every test.
+     */
+    public function _before(\FunctionalTester $I) {
+        $I->runShellCommand("chmod 000 /app/tests/_data/10.1234/100001_101009/100300/perm-not-ok.txt");
+    }
+
+    /**
+     * To make sure change the permission of the file to globally readable after every test.
+     */
+    public function _after(\FunctionalTester $I) {
+        $I->runShellCommand("chmod 644 /app/tests/_data/10.1234/100001_101009/100300/perm-not-ok.txt");
+    }
+
+    /**
      * list file with permission Ok
      * dummy file created with permission -rw-r--r--
      */
