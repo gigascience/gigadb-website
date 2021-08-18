d6c823bf7fef674b842e93c8a78e2179bb4bbd2e
Peter Li <peter@gigasciencejournal.com>
2021-08-18 14:19:21 +0800

    Add original CNGB backup shell scripts
    These 2 shell scripts were used by CNGB to backup GigaDB dataset
    files onto the Tencent Cloud Object Storage.

---
+++ gigadb/app/tools/dataset-backup-tool/scripts/cngb/README.md
@@ -0,0 +1,4 @@
+# README.md
+
+This directory contains a copies of the original two shell scripts used by CNGB 
+to perform backups of GigaDB dataset files onto Tencent Cloud cold storage.
---
+++ gigadb/app/tools/dataset-backup-tool/scripts/cngb/giga_backup.sh
@@ -0,0 +1,3 @@
+export backup_date=`date "+%Y%m%d"`
+
+/usr/bin/coscmd -l /home/gigadb/giga_cosbackup.log$backup_date upload -H '{"x-cos-storage-class":"DEEP_ARCHIVE"}' -rs /data/gigadb/pub /cngbdb/giga/gigadb/
