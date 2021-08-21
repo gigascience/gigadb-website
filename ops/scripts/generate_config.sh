43019b64846e7d001732fa63b913341a81357526
Rija Ménagé <github@rijam.sent.as>
2021-08-21 14:22:41 +0200

    Select more variables to be available to generate_config script
    so the gitlab variables holding gigadb database connection strings for
    production (staging and live) environments are accessible

--- ops/scripts/generate_config.sh
+++ ops/scripts/generate_config.sh
@@ -58,10 +58,10 @@ export PHP_MEM=-1
 
 # If we are on staging environment override variable name with their remote environment counterpart
 if [[ $GIGADB_ENV != "dev" && $GIGADB_ENV != "CI" ]];then
-    GIGADB_HOST=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "$PROJECT_VARIABLES_URL/gigadb_db_host?filter%5benvironment_scope%5d=$GIGADB_ENV" | jq .value)
-    GIGADB_USER=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "$PROJECT_VARIABLES_URL/gigadb_db_user?filter%5benvironment_scope%5d=$GIGADB_ENV" | jq .value)
-    GIGADB_PASSWORD=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "$PROJECT_VARIABLES_URL/gigadb_db_password?filter%5benvironment_scope%5d=$GIGADB_ENV" | jq .value)
-    GIGADB_DB=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "$PROJECT_VARIABLES_URL/gigadb_db_database?filter%5benvironment_scope%5d=$GIGADB_ENV" | jq .value)
+    GIGADB_HOST=$gigadb_db_host
+    GIGADB_USER=$gigadb_db_user
+    GIGADB_PASSWORD=$gigadb_db_password
+    GIGADB_DB=$gigadb_db_database
     HOME_URL=$REMOTE_HOME_URL
     PUBLIC_HTTP_PORT=$REMOTE_PUBLIC_HTTP_PORT
     PUBLIC_HTTPS_PORT=$REMOTE_PUBLIC_HTTPS_PORT
