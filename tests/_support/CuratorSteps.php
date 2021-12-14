e8f3799a5538fb511bc0fc1dfb3f399e13b5277a
Rija Ménagé <11455+rija@users.noreply.github.com>
2021-12-14 19:27:40 +0100

    Add feature and acceptance scenario for dataset admin table filtering

--- tests/_support/CuratorSteps.php
+++ tests/_support/CuratorSteps.php
@@ -9,6 +9,13 @@
  */
 class CuratorSteps extends \Codeception\Actor
 {
+    protected $I;
+
+
+    public function __construct(AcceptanceTester $I)
+    {
+        $this->I = $I;
+    }
 
     /**
      * @Given there is no user with email :email
@@ -18,4 +25,19 @@ class CuratorSteps extends \Codeception\Actor
         $dbConfig = json_decode(file_get_contents(dirname(__FILE__).'/../../protected/config/db.json'), true);
         shell_exec("psql -h {$dbConfig['host']} -U {$dbConfig['user']} -d {$dbConfig['user']} -c \"DELETE FROM gigadb_user WHERE email='$email'\"");
     }
+
+    /**
+     * @Given I have signed in as admin
+     */
+    public function iHaveSignedInAsAdmin()
+    {
+        $this->I->amOnUrl('http://gigadb.test');
+        $this->I->amOnPage('/site/login');
+        $this->I->fillField(['name' => 'LoginForm[username]'], 'admin@gigadb.org');
+        $this->I->fillField(['name' => 'LoginForm[password]'], 'gigadb');
+        $this->I->click('Login');
+        $this->I->waitForText("Home",10);
+    }
+
+
 }
---
+++ tests/acceptance/AdminTablesFilter.feature
@@ -0,0 +1,19 @@
+Feature: filter tables on admin page
+  As a curator
+  I want to filter the tables on the admin pages
+  So that I can quickly access the rows I am interested in
+
+  Background:
+    Given I have signed in as admin
+
+  Scenario: Dataset table
+    Given I am on "/adminDataset/admin"
+    When I fill in the field of "name" "Dataset[identifier]" with "100005"
+    And I press return on the element "(//input)[2]"
+    And I wait "1" seconds
+    Then I should see "100005"
+    And I should not see "100039"
+    And I should not see "100002"
+    And I should not see "100003"
+    And I should not see "100004"
+
