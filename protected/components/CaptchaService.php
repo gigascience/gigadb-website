f10d5b0307bf147af5d801d839aec51ec212d13e
Rija Ménagé <11455+rija@users.noreply.github.com>
2021-12-21 17:15:03 +0100

    Configure a captcha service as a CApplicationComponent class

---
+++ protected/components/CaptchaService.php
@@ -0,0 +1,45 @@
+<?php
+
+use Gregwar\Captcha\CaptchaBuilder;
+/**
+ * Component service for handling captcha generation
+ *
+ * @author Rija Menage <rija+git@cinecinetique.com>
+ * @license GPL-3.0
+ */
+class CaptchaService extends CApplicationComponent
+{
+    /** @var Gregwar\Captcha\CaptchaBuilder */
+    protected $captchaBuilder;
+
+    /**
+     * Initialize this component
+     */
+    public function init()
+    {
+        $this->captchaBuilder = new CaptchaBuilder;
+        parent::init();
+    }
+
+    /**
+     * Generate text can save it to current session
+     */
+    public function generate(): void
+    {
+        $this->captchaBuilder->build();
+        $_SESSION["captcha"] = $this->captchaBuilder->getPhrase();
+    }
+
+    /**
+     * Generate raw image for the generated text
+     *
+     * @return string
+     */
+    public function output(): string
+    {
+        return $this->captchaBuilder->inline();
+    }
+}
+
+
+?>
