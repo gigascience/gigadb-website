55460ae392481e2d88f7f906881898d9caf427f0
Ken <kencho.gigascience@gmail.com>
2022-01-05 11:11:18 +0800

    Revert Update the icon image presentation

--- protected/views/dataset/view.php
+++ protected/views/dataset/view.php
@@ -84,19 +84,16 @@ $sampleDataProvider = $samples->getDataProvider() ;
                     ?>
                     </ul>
                      <?php } ?>
-                     <div class="pull-right">
-                        <p>
-                            <span class="citation-popup" data-content="View citations on Google Scholar">
-                                <a href="https://scholar.google.com/scholar?q=10.5524/<?php echo $model->identifier;?>" target="_blank"><img class="dataset-des-images" src="/images/google_scholar.png"/></a>
-                            </span>
-                            <span class="citation-popup" data-content="View citations on Europe PubMed Central">
-                                <a href="http://europepmc.org/search?scope=fulltext&query=(REF:'10.5524/<?php echo $model->identifier;?>')" target="_blank"><img class="dataset-des-images" src="/images/ePMC.jpg"/></a>
-                            </span>
-                            <span class="citation-popup" data-content="View citations on Dimensions">
-                                <a href="https://app.dimensions.ai/discover/publication?search_text=10.5524%2F<?php echo $model->identifier;?>" target="_blank"><img class="dataset-des-images" src="/images/dimensions.jpg"/></a>
-                            </span>
-                        </p>
-                    </div>
+                 <div class="pull-right">
+                    <p>
+                        <?php
+                            foreach ( array_values($mainSection->getCitationsLinks() ) as $citation) {
+                                echo $citation;
+                            }
+                        ?>
+
+                    </p>
+                </div>
                 </div>
                                 <div class="subsection">
                                      <span class="content-popup" <?= $email != '' ? '' : 'data-content="Please login to contact submitter"' ?> data-original-title="">
