33029451c1619a3a38e83ea582c53aaeea55daa3
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-01-05 11:50:42 +0100

    Add bootstrap code to Codeception acceptance tests to reset database
    So we we can manually run codeception acceptance tests suite anytime, even after running the functional tests suite.
    
    Also, allow us to remove the  call to setup_devdb.sh in the acceptance runner.

---
+++ tests/acceptance/initdb.php
@@ -0,0 +1,7 @@
+<?php
+
+shell_exec("./protected/yiic migrate to 300000_000000 --connectionID=db --migrationPath=application.migrations.admin --interactive=0");
+shell_exec("./protected/yiic migrate mark 000000_000000 --connectionID=db --interactive=0");
+shell_exec("./protected/yiic migrate --connectionID=db --migrationPath=application.migrations.schema --interactive=0");
+shell_exec("./protected/yiic migrate --connectionID=db --migrationPath=application.migrations.data.dev --interactive=0");
+shell_exec("./protected/yiic sequencefixer fixAll");
