5747a5e02fa381f4ab0eff7beb2fb771434206f4
Peter Li <peter@gigasciencejournal.com>
2022-01-11 22:26:23 +0800

    Add new test
    Currently only checks the submission of create user form but is
    meant to be the integration test for checking notification email
    is sent to curators.

---
+++ tests/functional/CreateNewUserCest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Class CreateNewUserCest
+ *
+ * generated with:
+ * docker-compose run --rm test ./vendor/codeception/codeception/codecept generate:cest functional CreateNewUserCest.php
+ *
+ * run with:
+ * docker-compose run --rm test ./vendor/codeception/codeception/codecept run functional CreateNewUserCest
+ */
+class CreateNewUserCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    /**
+     * Notification email should be sent to curator after user creation
+     * 
+     * @param FunctionalTester $I
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function trySendNotificationEmailUponCreateUser(FunctionalTester $I)
+    {
+        $targetUrl = "/user/create";
+
+        $I->amOnPage($targetUrl);
+        $I->fillField(['id' => 'User_email'], 'new@mailinator.com');
+        $I->fillField(['id' => 'User_first_name'], 'Duuncan');
+        $I->fillField(['id' => 'User_last_name'], 'Idaaho');
+        $I->fillField(['id' => 'User_password'], '123456787');
+        $I->fillField(['id' => 'User_password_repeat'], '123456787');
+        $I->fillField(['id' => 'User_affiliation'], 'GigaScience');
+        $I->selectOption('form select[id=User_preferred_link]', 'NCBI');
+        $I->checkOption('#User_newsletter');
+        $I->checkOption('#User_terms');
+        $I->fillField(['id' => 'User_verifyCode'], 'shazam');
+        $I->click('Register');
+        $I->see('Welcome!', 'h2');
+    }
+
+    /**
+     * @param FunctionalTester $I
+     * @throws \Codeception\Exception\ModuleException
+     */
+    public function tryLoadDistinctCaptchaOnContactForm(FunctionalTester $I)
+    {
+        $targetUrl = "/site/contact";
+
+        # load target url
+        $I->amOnPage($targetUrl);
+        # find captcha image
+        $I->seeElement("//div/img[@style='width:200px;']");
+        # Get the source url of the image
+        $imgSrc1 = $I->grabAttributeFrom("//div/img[@style='width:200px;']",'src');
+        # make sure it's a PNG image
+        $img_size = getimagesize("http://gigadb.test".$imgSrc1);
+        $I->assertEquals("image/png", $img_size['mime']);
+
+        # download content of captcha url
+        $I->amOnPage($imgSrc1);
+        $img1 = $I->checksumOfResponse();
+        # load the target url again
+        $I->amOnPage($targetUrl);
+        # download content of captcha url
+        $imgSrc2 = $I->grabAttributeFrom("//div/img[@style='width:200px;']",'src');
+        $I->amOnPage($imgSrc2);
+        $img2 = $I->checksumOfResponse();
+        # make sure both content are different
+        $I->assertNotEquals($img1, $img2);
+    }
+}
