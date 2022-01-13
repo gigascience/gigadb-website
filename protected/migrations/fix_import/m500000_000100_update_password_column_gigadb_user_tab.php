d208363a66ddc1f823b300e489ad0257a7ec4b58
Peter Li <peter@gigasciencejournal.com>
2022-01-13 11:29:44 +0800

    Make safeDown() an empty function

--- protected/migrations/fix_import/m500000_000100_update_password_column_gigadb_user_tab.php
+++ protected/migrations/fix_import/m500000_000100_update_password_column_gigadb_user_tab.php
@@ -10,7 +10,7 @@ class m500000_000100_update_password_column_gigadb_user_tab extends CDbMigration
 
     public function safeDown()
     {
-        $this->execute("ALTER TABLE gigadb_user
-            ALTER COLUMN password TYPE character varying(64);");
+        // This function is empty because reverting safeUp() will mean passwords
+        // after this migration will be too big for the old column size.
     }
 }
