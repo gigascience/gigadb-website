2e02994a5ea3e76e06bc00bd35db224d383d98f0
Ken <kencho.gigascience@gmail.com>
2022-02-08 10:41:41 +0800

    Change file name and update checking rule

--- tests/functional/check_template_files_downloaded.sh
+++ tests/functional/check_template_files_downloaded.sh
@@ -13,36 +13,39 @@ curl --url "http://gigadb.gigasciencejournal.com:9170/files/templates/GigaDBUplo
 
 # Check the existence of the files
 # Expected files and their path
-TemplateFile="tests/_data/upload/GigaDBUploadForm-forWebsite-v22Dec2021.xlsx"
-ExampleFile1="tests/_data/upload/GigaDBUpload-Example1-forWebsite-v22Dec2021.xlsx"
-ExampleFile2="tests/_data/upload/GigaDBUpload-Example2-forWebsite-v22Dec2021.xlsx"
+path="tests/_data/upload/"
+TemplateFile="GigaDBUploadForm-forWebsite-v22Dec2021.xlsx"
+ExampleFile1="GigaDBUpload-Example1-forWebsite-v22Dec2021.xlsx"
+ExampleFile2="GigaDBUpload-Example2-forWebsite-v22Dec2021.xlsx"
 
-fileCounts=0
+TemplateFileExists=0
+ExampleFile1Exists=0
+ExampleFile2Exists=0
 
-if [ -f "$TemplateFile" ]; then
-  ((fileCounts+=1))
+if [ -f "${path}$TemplateFile" ]; then
+  TemplateFileExists=1
   echo "$TemplateFile is downloaded!"
 else
   echo "$TemplateFile is not found"
 fi
 
-if [ -f "$ExampleFile1" ]; then
-  ((fileCounts+=1))
+if [ -f "${path}$ExampleFile1" ]; then
+  ExampleFile1Exists=1
   echo "$ExampleFile1 is downloaded!"
 else
   echo "$ExampleFile1 is not found"
 fi
 
-if [ -f "$ExampleFile2" ]; then
-  ((fileCounts+=1))
+if [ -f "${path}$ExampleFile2" ]; then
+  ExampleFile2Exists=1
   echo "$ExampleFile2 is downloaded!"
 else
   echo "$ExampleFile2 is not found"
 fi
 
-if [ "$fileCounts" -eq 3 ]; then
-  echo "All files are downloadable, the test download folder will be deleted!"
-  rm -rf "tests/_data/upload"
+if [ $TemplateFileExists -eq 1 ] && [ $ExampleFile1Exists -eq 1 ] && [ $ExampleFile2Exists -eq 1 ]; then
+    echo "All files are downloadable, the test download folder will be deleted!"
+    rm -rf $path
 else
-  echo "Not all files are downloaded, please check again!!!!!!"
+    echo "Not all files are downloaded, please check again!!!!!!"
 fi
