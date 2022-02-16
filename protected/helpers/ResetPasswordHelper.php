ea6825b37d230f11485cc918ed16e298e7320e26
Peter Li <peter@gigasciencejournal.com>
2022-02-16 22:26:54 +0800

    Add verifyToken function

--- protected/helpers/ResetPasswordHelper.php
+++ protected/helpers/ResetPasswordHelper.php
@@ -1,18 +1,61 @@
 <?php
-
+/**
+ * Class ResetPasswordHelper
+ * 
+ * Helper functions for verifying token from URL, creating tokens and random
+ * strings
+ */
 class ResetPasswordHelper
 {
     /**
+     * Verify a password reset token
+     *
+     * @param string $token The token to verify which consists of selector and verifier
+     * @return bool True if the re-calculated hash of the verifier matches the hash from the database.
+     * @see https://paragonie.com/book/pecl-libsodium/read/07-password-hashing.md
+     * 
+     * @param $token
+     * @return bool
+     */
+    public static function verifyToken($token)
+    {
+        $signingKey = Yii::app()->params['signing_key'];
+        
+        $selectorFromURL = substr($token, 0, 20);
+        $resetPasswordRequest = ResetPasswordRequest::findResetPasswordRequestBySelector($selectorFromURL);
+        if($resetPasswordRequest == null)
+        {
+            Yii::log("[INFO] [".__CLASS__.".php] ".__FUNCTION__." No password request record found using URL token", 'info');
+            return false;
+        }
+        $selectorFromDatabase = $resetPasswordRequest->selector;
+
+        // Re-calculate hash from verifier obtained from URL
+        $verifierFromUrl = substr($token, 20, 20);
+        $hashTokenFromUrlVerifier = ResetPasswordHelper::getHashedToken($signingKey, $verifierFromUrl);
+        $hashTokenFromDatabase = $resetPasswordRequest->hashed_token;
+        // Perform verification by comparing selectors and hashes
+        if($selectorFromDatabase == $selectorFromURL && $hashTokenFromUrlVerifier == $hashTokenFromDatabase) {
+            return true;
+        }
+        else {
+            // Delete token and show login page
+            Yii::log("[INFO] [".__CLASS__.".php] ".__FUNCTION__." Password reset token failed validation", 'info');
+            return false;
+        }
+    }
+
+    /**
      * Returns the hash of a token. Used to generate a hash for the verifier.
-     * @param signingKey Unique, random, cryptographically secure string
-     * @param data Token to be hashed
+     * 
+     * @param string $signingKey Unique, random, cryptographically secure string
+     * @param string $data Token to be hashed
+     * @return string
      */
     public static function getHashedToken(string $signingKey, string $data)
     {
-        $out = base64_encode(hash_hmac('sha256', $data, $signingKey, true));
-        Yii::log("[INFO] [".__CLASS__.".php] ".__FUNCTION__.": base64_hash ".$out, 'info');
-        return $out;
-//        return base64_encode(hash_hmac('sha256', $data, $signingKey, true));
+        $hash = base64_encode(hash_hmac('sha256', $data, $signingKey, true));
+        return $hash;
     }
 
     /**
