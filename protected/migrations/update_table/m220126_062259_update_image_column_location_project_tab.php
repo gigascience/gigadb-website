d077226813db73d7541e1a3fc7f898a4795d61f1
Ken <kencho.gigascience@gmail.com>
2022-02-16 15:04:25 +0800

    Create temp up method

--- protected/migrations/update_table/m220126_062259_update_image_column_location_project_tab.php
+++ protected/migrations/update_table/m220126_062259_update_image_column_location_project_tab.php
@@ -4,11 +4,17 @@ class m220126_062259_update_image_column_location_project_tab extends CDbMigrati
 {
 	public function safeUp()
 	{
-        $this->execute("update project set image_location = replace(image_location,'http://gigadb.org','');");
+        Yii::app()->db->createCommand("update project set name = lower(name);")->execute();
+        Yii::app()->db->createCommand("update project set name = replace( name, ' ', '_' );")->execute();
+        Yii::app()->db->createCommand("update project set name = 'carmen' where name = 'https://www.carmen.org.uk';")->execute();
+        Yii::app()->db->createCommand("update project set image_location = replace(image_location,'http://gigadb.org/images/project','');")->execute();
+        Yii::app()->db->createCommand("ALTER TABLE project ALTER COLUMN image_location TYPE text;")->execute();
+        Yii::app()->db->createCommand("update project set image_location = concat('/images/projects/', name, image_location);")->execute();
+//        $this->execute("update project set image_location = replace(image_location,'http://gigadb.org','');");
 	}
 
 	public function safeDown()
 	{
-        $this->execute("update project set image_location = concat('http://gigadb.org', image_location);");
+//        $this->execute("update project set image_location = concat('http://gigadb.org', image_location);");
 	}
 }
