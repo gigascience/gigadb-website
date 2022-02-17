7072b5ed6618e739e6a0f940330206f4c02240a9
Peter Li <peter@gigasciencejournal.com>
2022-02-17 14:55:52 +0800

    Create PasswordResetTokenUserIdentity class
    This is now used for authenticating a user's password reset token.

---
+++ protected/components/PasswordResetTokenUserIdentity.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Class to handle the identity of user and how they authenticate
+ *
+ * @see https://www.yiiframework.com/doc/guide/1.1/en/topics.auth
+ */
+class PasswordResetTokenUserIdentity extends CUserIdentity {
+
+    public $_id;
+    const ERROR_SELECTOR_NOT_ASSOCIATED_WITH_A_USER = 4;
+    const ERROR_RECALCULATED_HASH_OF_VERIFIER_DOES_NOT_MATCH_HASH_IN_DATABASE = 5;
+
+    /**
+     * @var string Token from URL sent by email
+     */
+    public $urlToken;
+
+    /**
+     * Constructor is needed as we don't need ($username, $password), because
+     * Oauth is the authentication instead we need to feed in the provider and
+     * uid returned form Oauth process
+     */
+    public function __construct($urlToken)
+    {
+        $this->urlToken = $urlToken;
+    }
+
+    /**
+     * Authenticates a user using a password reset token that consists of two
+     * parts: a selector and verifier.
+     *
+     * @return boolean whether authentication succeeds. True if successful, False otherwise
+     */
+    public function authenticate() 
+    {
+        // Find user associated with selector part in URL
+        $selectorFromURL = substr($this->urlToken, 0, 20);
+        $resetPasswordRequest = ResetPasswordRequest::findResetPasswordRequestBySelector($selectorFromURL);
+        $user = User::model()->findByAttributes(array('id' => $resetPasswordRequest->gigadb_user_id));
+
+        if ($user === null)  // User not found
+        {
+            $this->errorCode = self::ERROR_SELECTOR_NOT_ASSOCIATED_WITH_A_USER;
+        } 
+        else  // User found
+        {
+            // Re-calculate hash from verifier and check if it matches with
+            // hash stored in reset_password_request database table
+            $signingKey = Yii::app()->params['signing_key'];
+            $verifierFromURL = substr($this->urlToken, 20, 20);
+            $hashedTokenFromURLVerifier = ResetPasswordHelper::getHashedToken($signingKey, $verifierFromURL);
+            if($hashedTokenFromURLVerifier == $resetPasswordRequest->hashed_token)
+            {
+                $this->_id = $user->id;
+                $this->errorCode = self::ERROR_NONE;
+            }
+            else
+            {
+                // TODO: Delete token and show login page
+                $this->errorCode = self::ERROR_RECALCULATED_HASH_OF_VERIFIER_DOES_NOT_MATCH_HASH_IN_DATABASE;
+            }
+        }
+        return !$this->errorCode;
+    }
+
+    public function getId()
+    {
+        return $this->_id;
+    }
+}
+
+?>
--- protected/helpers/ResetPasswordHelper.php
+++ protected/helpers/ResetPasswordHelper.php
@@ -8,44 +8,6 @@
 class ResetPasswordHelper
 {
     /**
-     * Verify a password reset token
-     *
-     * @param string $token The token to verify which consists of selector and verifier
-     * @return bool True if the re-calculated hash of the verifier matches the hash from the database.
-     * @see https://paragonie.com/book/pecl-libsodium/read/07-password-hashing.md
-     * 
-     * @param $token
-     * @return bool
-     */
-    public static function verifyToken($token)
-    {
-        $signingKey = Yii::app()->params['signing_key'];
-        
-        $selectorFromURL = substr($token, 0, 20);
-        $resetPasswordRequest = ResetPasswordRequest::findResetPasswordRequestBySelector($selectorFromURL);
-        if($resetPasswordRequest == null)
-        {
-            Yii::log("[INFO] [".__CLASS__.".php] ".__FUNCTION__." No password request record found using URL token", 'info');
-            return false;
-        }
-        $selectorFromDatabase = $resetPasswordRequest->selector;
-
-        // Re-calculate hash from verifier obtained from URL
-        $verifierFromUrl = substr($token, 20, 20);
-        $hashTokenFromUrlVerifier = ResetPasswordHelper::getHashedToken($signingKey, $verifierFromUrl);
-        $hashTokenFromDatabase = $resetPasswordRequest->hashed_token;
-        // Perform verification by comparing selectors and hashes
-        if($selectorFromDatabase == $selectorFromURL && $hashTokenFromUrlVerifier == $hashTokenFromDatabase) {
-            return true;
-        }
-        else {
-            // Delete token and show login page
-            Yii::log("[INFO] [".__CLASS__.".php] ".__FUNCTION__." Password reset token failed validation", 'info');
-            return false;
-        }
-    }
-
-    /**
      * Returns the hash of a token. Used to generate a hash for the verifier.
      * 
      * @param string $signingKey Unique, random, cryptographically secure string
