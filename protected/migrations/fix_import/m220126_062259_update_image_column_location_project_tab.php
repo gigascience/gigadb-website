9942bdf8b4a0058214df16d265a0af0fea36a534
Ken <kencho.gigascience@gmail.com>
2022-02-18 13:09:45 +0800

    Update step to remove special char

--- protected/migrations/fix_import/m220126_062259_update_image_column_location_project_tab.php
+++ protected/migrations/fix_import/m220126_062259_update_image_column_location_project_tab.php
@@ -5,7 +5,10 @@ class m220126_062259_update_image_column_location_project_tab extends CDbMigrati
 	public function safeUp()
 	{
         Yii::app()->db->createCommand("update project set name = lower(name);")->execute();
-        Yii::app()->db->createCommand("update project set name = replace( name, ' ', '_' );")->execute();
+        Yii::app()->db->createCommand("update project set name = 'ligea' where name = 'ligea: a comprehensive database of human gene fusion events';")->execute();
+        Yii::app()->db->createCommand("update project set name = replace( name, ',', '');")->execute();
+        Yii::app()->db->createCommand("update project set name = replace( name, ' - ', ' ');")->execute();
+        Yii::app()->db->createCommand("update project set name = replace( name, ' ', '_');")->execute();
         Yii::app()->db->createCommand("update project set name = 'carmen' where name = 'https://www.carmen.org.uk';")->execute();
         Yii::app()->db->createCommand("update project set image_location = replace(image_location,'http://gigadb.org/images/project','');")->execute();
         Yii::app()->db->createCommand("ALTER TABLE project ALTER COLUMN image_location TYPE text;")->execute();
