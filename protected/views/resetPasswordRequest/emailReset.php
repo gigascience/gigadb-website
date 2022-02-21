f2bc273d80a9a81fb4db97d7d10980c00a42e259
Peter Li <peter@gigasciencejournal.com>
2022-02-21 12:14:02 +0800

    Remove styling from HTML email message

--- protected/views/resetPasswordRequest/emailReset.php
+++ protected/views/resetPasswordRequest/emailReset.php
@@ -1,19 +1,19 @@
 <html>
-	<body style="margin: 0; padding: 0; background: #ebf6f0;">
-		<table width="100%" cellpadding="0" cellspacing="0" border="0">
-			<tr>
-				<td align="center" width="100%" bgcolor="#ebf6f0" style="margin: 0;">
-					<table cellpadding="0" cellspacing="0" border="0" align="center" width="600" style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif; color: #6f6f6f;">
-						<tr>
-							<td width="600" bgcolor="#ffffff" style="padding-left:40px;padding-right:40px;line-height:20px;padding-bottom:10px;">
-								<h2 style="font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;color:#626262; font-size:24px; font-weight:200;padding-top:10px;">Password reset</h2>
-								<p style="color:#626262; font-size:15px; font-weight:200;font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;">Thank you for requesting to reset your password.</b>.
-                                                                <br/>Please click <a href="<?= $url ?>" target="_blank" style="color:#006633;">*here*</a> to set a new password and login</p>
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-		</table>
-	</body>
+<body>
+<table>
+    <tr>
+        <td>
+            <table>
+                <tr>
+                    <td>
+                        <h2>Password reset</h2>
+                        <p>Thank you for requesting to reset your password.</p>
+                        <p>Please click <a href="<?= $url ?>" target="_blank">here</a> to set a new password and login.</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
 </html>
