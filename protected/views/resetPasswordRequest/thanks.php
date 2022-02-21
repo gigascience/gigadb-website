e371d3795ffa21b1de4beb747debba3559ee5263
Peter Li <peter@gigasciencejournal.com>
2022-02-21 12:42:00 +0800

    Fix styling

--- protected/views/resetPasswordRequest/thanks.php
+++ protected/views/resetPasswordRequest/thanks.php
@@ -1,10 +1,19 @@
-<?php $this->pageTitle=Yii::app()->name . ' - Password Reset' ?>
-
-<h2><?=Yii::t('app' , 'Password Reset')?></h2>
-<div class="clear"></div>
-<p><?=Yii::t('app' , 'For security reasons, we cannot tell you if the email you entered is valid or not.<br/>If it is valid, we will send new password.')?></p>
-    <p>If you do not receive the reset password email within a few minutes, please check your Junk/Spam E-mail folder</p>
-    <p><?= CHtml::link(Yii::t('app' , "Contact us"), "mailto:" .
-    Yii::app()->params['support_email']) ?> <?=Yii::t('app' , 'if you are having problems and we will get it sorted out.')?></p>
-    
+<div class="content">
+    <div class="container">
+        <section class="page-title-section">
+            <div class="page-title">
+                <ol class="breadcrumb pull-right">
+                    <li><a href="/">Home</a></li>
+                    <li class="active">Thanks</li>
+                </ol>
+                <h4>Password Reset</h4>
+            </div>
+        </section>
+        <div class="subsection" style="margin-bottom: 130px;">
+            <p><?=Yii::t('app' , 'For security reasons, we cannot tell you if the email you entered is valid or not.<br/>If it is valid, we will send new password.')?></p>
+            <p>If you do not receive the reset password email within a few minutes, please check your Junk/Spam E-mail folder</p>
+            <p><?= CHtml::link(Yii::t('app' , "Contact us"), "mailto:".Yii::app()->params['support_email']) ?> <?=Yii::t('app' , 'if you are having problems and we will get it sorted out.')?></p>
+        </div>
+    </div>
+</div>
 
