fdd2944de8ad5672ee78cf7cad93ee715d52f5ed
Ken <kencho.gigascience@gmail.com>
2022-02-21 16:00:17 +0800

    Replace non gigadb url

--- protected/migrations/fix_import/m220126_062259_update_image_column_location_project_tab.php
+++ protected/migrations/fix_import/m220126_062259_update_image_column_location_project_tab.php
@@ -11,6 +11,8 @@ class m220126_062259_update_image_column_location_project_tab extends CDbMigrati
         Yii::app()->db->createCommand("update project set name = replace( name, ' ', '_');")->execute();
         Yii::app()->db->createCommand("update project set name = 'carmen' where name = 'https://www.carmen.org.uk';")->execute();
         Yii::app()->db->createCommand("update project set image_location = replace(image_location,'http://gigadb.org/images/project','');")->execute();
+        Yii::app()->db->createCommand("update project set image_location = replace(image_location,'http://hpc-bioinformatics.cineca.it/fusion/imgs','');")->execute();
+        Yii::app()->db->createCommand("update project set image_location = replace(image_location,'https://bioinfotraining.bio.cam.ac.uk/images','');")->execute();
         Yii::app()->db->createCommand("ALTER TABLE project ALTER COLUMN image_location TYPE text;")->execute();
         Yii::app()->db->createCommand("update project set image_location = concat('https://assets.gigadb-cdn.net/images/projects/', name, image_location);")->execute();
 	}
