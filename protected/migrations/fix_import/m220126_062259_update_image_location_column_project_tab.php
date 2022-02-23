f67d56d2e8ea49794eec6b331b5cabfdea98876a
Ken <kencho.gigascience@gmail.com>
2022-02-23 10:50:11 +0800

    Keep name column unchange

--- protected/migrations/fix_import/m220126_062259_update_image_location_column_project_tab.php
+++ protected/migrations/fix_import/m220126_062259_update_image_location_column_project_tab.php
@@ -4,18 +4,14 @@ class m220126_062259_update_image_location_column_project_tab extends CDbMigrati
 {
 	public function safeUp()
 	{
-        Yii::app()->db->createCommand("update project set name = lower(name);")->execute();
-        Yii::app()->db->createCommand("update project set name = 'ligea' where name = 'ligea: a comprehensive database of human gene fusion events';")->execute();
-        Yii::app()->db->createCommand("update project set name = replace( name, ',', '');")->execute();
-        Yii::app()->db->createCommand("update project set name = replace( name, ' - ', ' ');")->execute();
-        Yii::app()->db->createCommand("update project set name = replace( name, ' ', '_');")->execute();
-        Yii::app()->db->createCommand("update project set name = 'carmen' where name = 'https://www.carmen.org.uk';")->execute();
         Yii::app()->db->createCommand("update project set image_location = replace(image_location,'http://gigadb.org/images/project','');")->execute();
         Yii::app()->db->createCommand("update project set image_location = replace(image_location,'http://hpc-bioinformatics.cineca.it/fusion/imgs','');")->execute();
         Yii::app()->db->createCommand("update project set image_location = replace(image_location,'https://bioinfotraining.bio.cam.ac.uk/images','');")->execute();
         Yii::app()->db->createCommand("update project set image_location = replace(image_location,'http://gigadb.org/images/data/cropped','');")->execute();
         Yii::app()->db->createCommand("ALTER TABLE project ALTER COLUMN image_location TYPE text;")->execute();
-        Yii::app()->db->createCommand("update project set image_location = concat('https://assets.gigadb-cdn.net/images/projects/', name, image_location) where image_location != '' and image_location not like 'https://assets.gigadb-cdn.net/images/projects/%';")->execute();
+        Yii::app()->db->createCommand("update project set image_location = concat('https://assets.gigadb-cdn.net/images/projects/', lower(replace(replace(replace(name, ' ','_'),'_-_','_'),',','')),  image_location) where image_location != '' and image_location not like 'https://assets.gigadb-cdn.net/images/projects/%' and name not like 'LiGeA%' and name != 'https://www.carmen.org.uk';")->execute();
+        Yii::app()->db->createCommand("update project set image_location = concat('https://assets.gigadb-cdn.net/images/projects/', replace(name, 'LiGeA: a comprehensive database of human gene fusion events', 'ligea'), image_location) where name like 'LiGeA%' and image_location not like 'https://assets.gigadb-cdn.net/images/projects/%';")->execute();
+        Yii::app()->db->createCommand("update project set image_location = concat('https://assets.gigadb-cdn.net/images/projects/', replace(name, 'https://www.carmen.org.uk', 'carmen'), image_location) where name = 'https://www.carmen.org.uk' and image_location not like 'https://assets.gigadb-cdn.net/images/projects/%';")->execute();
 	}
 
 	public function safeDown()
