7105c0accff45788a3e25cd7fe0f418a7b313c7c
Peter Li <peter@gigasciencejournal.com>
2022-02-28 23:57:03 +0800

    Update text on thanks page

--- protected/views/resetPasswordRequest/thanks.php
+++ protected/views/resetPasswordRequest/thanks.php
@@ -13,7 +13,7 @@ $this->pageTitle='Thanks';
             </div>
         </section>
         <div class="subsection" style="margin-bottom: 130px;">
-            <p><?=Yii::t('app' , 'For security reasons, we cannot tell you if the email you entered is valid or not.<br/>If it is valid, we will send new password.')?></p>
+            <p><?=Yii::t('app' , 'For security reasons, we cannot tell you if the email you entered is valid or not.<br/>If it is valid, we will send an email containing a link to where you can reset your password.')?></p>
             <p>If you do not receive the reset password email within a few minutes, please check your Junk/Spam E-mail folder</p>
             <p><?= CHtml::link(Yii::t('app' , "Contact us"), "mailto:".Yii::app()->params['support_email']) ?> <?=Yii::t('app' , 'if you are having problems and we will get it sorted out.')?></p>
         </div>
