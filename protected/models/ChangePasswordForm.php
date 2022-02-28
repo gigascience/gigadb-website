4e4df569ebc2e56bb2515ffc27ad13498edeaed5
Peter Li <peter@gigasciencejournal.com>
2022-02-28 19:37:17 +0800

    Revert ChangePasswordForm.php to current develop branch state

--- protected/models/ChangePasswordForm.php
+++ protected/models/ChangePasswordForm.php
@@ -5,6 +5,8 @@ class ChangePasswordForm extends CFormModel
 	public $password;
 	public $confirmPassword;
 	public $user_id;
+        public $terms;
+        public $newsletter;
 
 	/**
 	 * Declares the validation rules.
@@ -15,8 +17,9 @@ class ChangePasswordForm extends CFormModel
 	{
 		return array(
 			// username and password are required
-			array('password, confirmPassword', 'required'),
+			array('password, confirmPassword, user_id, terms', 'required'),
                         array('password', 'compare', 'compareAttribute'=>'confirmPassword'),
+                        array('terms','compare', 'compareValue' => TRUE,'message'=>'Tick here to confirm you have read and understood our Terms of use and Privacy policy.'),
                     
 		);
 	}
@@ -37,6 +40,7 @@ class ChangePasswordForm extends CFormModel
         if(isset($user)){
             $user->password = $this->password;
             $user->password_repeat = $this->confirmPassword;
+            $user->newsletter = $this->newsletter;
             $user->encryptPassword();
 
             if($user->save(false)) {
