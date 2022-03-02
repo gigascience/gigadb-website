525925a17b5fdcde90085f2919bbf62dd31c28db
Peter Li <peter@gigasciencejournal.com>
2022-03-02 20:06:11 +0800

    Create unit test for CryptoService component

---
+++ tests/unit/CryptoTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Class DatasetComponentsTest
+ */
+class CryptoTest extends \Codeception\Test\Unit
+{
+    /**
+     * Test that random alphanumeric string contains 20 characters
+     * Test that random string is alphanumeric
+     *
+     * @throws Exception
+     */
+    public function testGetRandomAlphaNumStr()
+    {
+        $randomStr = CryptoService::getRandomAlphaNumStr();
+        $this->assertTrue(strlen($randomStr) == 20, "Random string does not contain 20 characters");
+        $this->assertTrue(ctype_alnum($randomStr), "Random string is not alphanumeric");
+    }
+
+    /**
+     * Verifies hashing algorithm produces expected results
+     *
+     * @throws Exception
+     */
+    public function testGetHashedToken()
+    {
+        $hashValue = "PMzM5S0Q2z6Eg+rc7D/3SkB3c4RQ1PNJzRtQ/bN0/nU=";
+        $data = "hobbit";
+        $key = "1234567890";
+        $result = CryptoService::getHashedToken($key, $data);
+        // Verify hashing function
+        $this->assertEquals($hashValue, $result, "Unexpected hashing result");
+    }
+}
