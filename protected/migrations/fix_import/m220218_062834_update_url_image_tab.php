e8453ab1daa5bacf6a55d77d50316ef38be03a56
Ken <kencho.gigascience@gmail.com>
2022-03-06 11:00:36 +0800

    Replace like operator

--- protected/migrations/fix_import/m220218_062834_update_url_image_tab.php
+++ protected/migrations/fix_import/m220218_062834_update_url_image_tab.php
@@ -5,7 +5,7 @@ class m220218_062834_update_url_image_tab extends CDbMigration
 	public function safeUp()
 	{
 	    Yii::app()->db->createCommand("update image set url = replace(url, 'http://gigadb.org/','https://assets.gigadb-cdn.net/live/images/datasets/') where url like 'http://gigadb.org/%';")->execute();
-	    Yii::app()->db->createCommand("update image set url = 'https://assets.gigadb-cdn.net/live/images/datasets/no_image.png' where url like '' and location like 'no_image%';")->execute();
+	    Yii::app()->db->createCommand("update image set url = 'https://assets.gigadb-cdn.net/live/images/datasets/no_image.png' where url='' and location like 'no_image%';")->execute();
 	}
 
 	public function safeDown()
