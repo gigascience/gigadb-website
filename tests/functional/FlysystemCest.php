e400d6c400190d5913ce4eff47d9d4df6de7313e
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-03-08 15:47:50 +0100

    Add functional test to check AWS credentials are populated in config

--- tests/functional/FlysystemCest.php
+++ tests/functional/FlysystemCest.php
@@ -7,15 +7,26 @@
  */
 class FlysystemCest
 {
+    const TEST_IMAGE_FILE = "/tmp/my.png";
+
     public function _after(FunctionalTester $I)
     {
-        unlink("/tmp/my.png");
+
+        if( file_exists(self::TEST_IMAGE_FILE) )
+            unlink(self::TEST_IMAGE_FILE);
     }
 
     // tests
+    public function tryCredentials(FunctionalTester $I)
+    {
+        $config = require(__DIR__."/../../protected/config/yii2/web.php");
+        $I->assertNotEmpty($config['components']['cloudStore']['key']);
+        $I->assertNotEmpty($config['components']['cloudStore']['secret']);
+    }
+
     public function tryFlysystemEndToEnd(FunctionalTester $I)
     {
-        shell_exec("curl -sS -o /tmp/my.png http://gigadb.test/site/flysystem-status");
-        $I->assertEquals("image/png", mime_content_type("/tmp/my.png") );
+        shell_exec("curl -sS -o ".self::TEST_IMAGE_FILE." http://gigadb.test/site/flysystem-status");
+        $I->assertEquals("image/png", mime_content_type(self::TEST_IMAGE_FILE) );
     }
 }
