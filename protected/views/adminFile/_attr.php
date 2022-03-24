4ff98b37a261e1b4427e995e2bc6078ff4c90acc
Ken <kencho.gigascience@gmail.com>
2022-03-24 11:02:48 +0800

    Make table row fit directly in file

--- protected/views/adminFile/_attr.php
+++ protected/views/adminFile/_attr.php
@@ -1,12 +1,12 @@
 <td>
 	<?php echo CHtml::activeHiddenField($attribute, 'id') ?>
-	<?php echo CHtml::activeDropDownList($attribute, 'attribute_id',CHtml::listData(Attribute::model()->findAll(),'id','attribute_name'), array('class'=>'attr-form', 'empty'=>'Select name')); ?>
+	<?php echo CHtml::activeDropDownList($attribute, 'attribute_id',CHtml::listData(Attribute::model()->findAll(),'id','attribute_name'), array('class'=>'attr-form', 'style'=>'width: 100%;', 'empty'=>'Select name')); ?>
 </td>
 <td>
-	<?php echo CHtml::activeTextField($attribute, 'value',array('class'=>'attr-form'));?> 
+	<?php echo CHtml::activeTextField($attribute, 'value',array('class'=>'attr-form', 'style'=>'width: 100%;'));?>
 </td>
 <td>
-	<?php echo CHtml::activeDropDownList($attribute, 'unit_id',CHtml::listData(Unit::model()->findAll(),'id','name'), array('class'=>'attr-form', 'empty'=>'Select unit')); ?>
+	<?php echo CHtml::activeDropDownList($attribute, 'unit_id',CHtml::listData(Unit::model()->findAll(),'id','name'), array('class'=>'attr-form', 'style'=>'width: 100%;', 'empty'=>'Select unit')); ?>
 </td>
 <td>
     <div>
