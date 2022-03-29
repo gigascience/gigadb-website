78b2faf5b8634c47db7908f521af3055daee2613
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-03-29 12:31:57 +0200

    Add a play to load the latest DB backup at the end of bastion playbook

--- gigadb/app/tools/files-url-updater/reset_database.sh
+++ gigadb/app/tools/files-url-updater/reset_database.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-set +ex
+set +e
+set +x
 
 source ./.env
+
+export PATH=/usr/local/bin/:$PATH
+# preflight checks
+which docker-compose
 
 # instantiate a container for a PostgreSQL 9.3 instance
 
@@ -11,24 +16,23 @@ sleep 5
 docker-compose ps
 docker-compose logs pg9_3
 
-
-source ./.env
-
-
 # Run files-url-updater
-
 echo yes | docker-compose run --rm updater ./yii dataset-files/download-restore-backup --latest
 
 # Convert backup using legacy postgresql client
 
 latest=$(date --date="1 days ago" +"%Y%m%d")
 thedate=${1:-$latest}
-version=$(docker-compose run --rm updater psql --version | cut -d' ' -f 3 | tr -d '\n' )
+version=$(docker-compose run --rm updater psql --version | cut -d' ' -f 3 | tr -d '\n\r' )
 docker-compose run --rm updater pg_dump --host=pg9_3 -p 5432  --username=gigadb  --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=gigadbv3_${thedate}_v${version}.backup
 
 # shut down the PostgreSQL 9.3 instance
 
 docker-compose down -v
+
+# Terminate other connections to RDS instance
+
+export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c  "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='gigadb';"
 
 # Load the dump in RDS using native postgresql client
 
