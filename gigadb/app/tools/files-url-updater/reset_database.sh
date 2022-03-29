94c50679831f6a481e627f0f95c41fdf6b494bfd
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-03-29 18:15:50 +0200

    Tidy up code

--- gigadb/app/tools/files-url-updater/reset_database.sh
+++ gigadb/app/tools/files-url-updater/reset_database.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-set +e
-set +x
-
 source ./.env
 
 export PATH=/usr/local/bin/:$PATH
@@ -22,8 +19,8 @@ twoDaysAgo=$(date --date="2 days ago" +"%Y%m%d")
 threeDaysAgo=$(date --date="3 days ago" +"%Y%m%d")
 thedate=${1:-$latest}
 
-# Default converted backup and legacy postgres version
-default_db = "/home/centos/database_bootstrap.backup"
+# Default converted backup (from Ansible properties) and legacy postgres version
+defaultDB="/home/centos/database_bootstrap.backup"
 version=$(docker-compose run --rm updater psql --version | cut -d' ' -f 3 | tr -d '\n\r' )
 
 # Run files-url-updater
@@ -44,33 +41,28 @@ export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOS
 
 # Load the dump in RDS using native postgresql client
 if [[ $downloadRestoreStatus -eq 0 && $convertStatus -eq 0 ]];then
+  echo "Loading gigadbv3_${thedate}_v${version}.backup"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "drop database if exists $DB_PG_DATABASE"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "create database $DB_PG_DATABASE owner $DB_PG_USER"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < converted/gigadbv3_${thedate}_v${version}.backup
-  echo "Loaded gigadbv3_${thedate}_v${version}.backup"
+  loadStatus=$?
 elif [[ -f converted/gigadbv3_${twoDaysAgo}_v${version}.backup ]];then
+  echo "Loading gigadbv3_${twoDaysAgo}_v${version}.backup from two days ago"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "drop database if exists $DB_PG_DATABASE"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "create database $DB_PG_DATABASE owner $DB_PG_USER"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < converted/gigadbv3_${twoDaysAgo}_v${version}.backup
-  echo "Loaded gigadbv3_${twoDaysAgo}_v${version}.backup from two days ago"
+  loadStatus=$?
 elif [[ -f converted/gigadbv3_${threeDaysAgo}_v${version}.backup ]];then
+  echo "Loading gigadbv3_${threeDaysAgo}_v${version}.backup from three days ago"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "drop database if exists $DB_PG_DATABASE"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "create database $DB_PG_DATABASE owner $DB_PG_USER"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < converted/gigadbv3_${threeDaysAgo}_v${version}.backup
-  echo "Loaded gigadbv3_${threeDaysAgo}_v${version}.backup from three days ago"
+  loadStatus=$?
 else
+  echo "Loading default backup /home/centos/database_bootstrap.backup"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "drop database if exists $DB_PG_DATABASE"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "create database $DB_PG_DATABASE owner $DB_PG_USER"
-  export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < /home/centos/database_bootstrap.backup
-  echo "Loaded default backup /home/centos/database_bootstrap.backup"
+  export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < "$defaultDB"
+  loadStatus=$?
 fi
-
-#TODO:
-# * [ ] Ensure the files-url-updater config use correct DB credentials
-# * [ ] Ensure the DB instance running on dockerhost is reachable from bastion (open a port)
-# * [x] Install docker-compose using pip in Ansible playbook
-# * [x] Ensure the Dockerhost certificates are downloaded in the bastion server
-# * [ ] Get a bash script (this file) to:
-#     * [x] download latest DB backup,
-#     * [ ] convert it to new format using PosgreSQL 9.3 instance running on dockerhost
-#     * [ ] load it in AWS RDS
+exit $loadStatus
