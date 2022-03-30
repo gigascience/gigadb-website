4f7c053dd3ed5e63fb0852c83aa50d5d0d9b801a
Peter Li <peter@gigasciencejournal.com>
2022-03-30 13:23:09 +0800

    Update tf file to use latest Terraform AWS module version

--- ops/infrastructure/modules/rds-instance/rds-instance.tf
+++ ops/infrastructure/modules/rds-instance/rds-instance.tf
@@ -21,8 +21,41 @@ module "security_group" {
   ]
 }
 
-output "rds_security_group_vpc_id" {
-  value = var.vpc_id
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+  identifier = "rds-server-${var.deployment_target}-${var.owner}"
+
+  snapshot_identifier = var.snapshot_identifier
+  restore_to_point_in_time = var.restore_to_point_in_time
+
+  db_name                = var.gigadb_db_database
+  username               = var.gigadb_db_user
+  password               = var.gigadb_db_password
+  port                   = 5432
+
+  # Create this RDS instance in database subnet group in VPC
+  db_subnet_group_name   = var.vpc_database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  create_db_option_group    = false
+  create_db_parameter_group = false
+
+  parameter_group_name      = (var.deployment_target == "staging" ? "gigadb-db-param-group-${var.owner}" : null)
+  engine                    = "postgres"
+  engine_version            = "11.13"
+  family                    = "postgres11"  # DB parameter group
+  major_engine_version      = "11"          # DB option group
+  instance_class            = "db.t3.micro"
+  allocated_storage         = 8
+  deletion_protection       = false
+  maintenance_window        = "Mon:00:00-Mon:03:00"
+  backup_window             = "03:00-06:00"  # UTC time
+  backup_retention_period   = 5  # days
+  skip_final_snapshot       = false  # Create final snapshot
+  final_snapshot_identifier_prefix = "snapshot-final-${var.deployment_target}-${var.owner}-${local.tstamp}"
+  copy_tags_to_snapshot     = true
+  delete_automated_backups  = false  # Do not delete backups on RDS instance termination
+  apply_immediately         = true
 }
 
 resource "aws_db_parameter_group" "gigadb-db-param-group" {
@@ -44,43 +77,6 @@ resource "aws_db_parameter_group" "gigadb-db-param-group" {
   }
 }
 
-#module "db" {
-#  source = "terraform-aws-modules/rds/aws"
-#  identifier = "rds-server-${var.deployment_target}-${var.owner}"
-#
-#  snapshot_identifier = var.snapshot_identifier
-#  restore_to_point_in_time = var.restore_to_point_in_time
-#
-#  db_name                = var.gigadb_db_database
-#  username               = var.gigadb_db_user
-#  password               = var.gigadb_db_password
-#  port                   = 5432
-#
-#  #  subnet_ids             = var.rds_subnet_ids
-#  db_subnet_group_name   = var.vpc_database_subnet_group
-#  vpc_security_group_ids = [module.security_group.security_group_id]
-#
-#  create_db_option_group    = false
-#  create_db_parameter_group = false
-#
-#  parameter_group_name      = (var.deployment_target == "staging" ? "gigadb-db-param-group-${var.owner}" : null)
-#  engine                    = "postgres"
-#  engine_version            = "11.13"
-#  family                    = "postgres11"  # DB parameter group
-#  major_engine_version      = "11"          # DB option group
-#  instance_class            = "db.t3.micro"
-#  allocated_storage         = 8
-#  deletion_protection       = false
-#  maintenance_window        = "Mon:00:00-Mon:03:00"
-#  backup_window             = "03:00-06:00"  # UTC time
-#  backup_retention_period   = 5  # days
-#  skip_final_snapshot       = false  # Create final snapshot
-#  final_snapshot_identifier_prefix = "snapshot-final-${var.deployment_target}-${var.owner}-${local.tstamp}"
-#  copy_tags_to_snapshot     = true
-#  delete_automated_backups  = false  # Do not delete backups on RDS instance termination
-#  apply_immediately         = true
-#}
-
-#output "rds_instance_address" {
-#  value = module.db.db_instance_address
-#}
+output "rds_instance_address" {
+  value = module.db.db_instance_address
+}
