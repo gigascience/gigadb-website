024854d7d5864b9c519f782aed6fea23d173f0d2
Peter Li <peter@gigasciencejournal.com>
2022-03-31 13:56:23 +0800

    Fix Error reading tfstate file: 0.12 format
    Seems like we need to provide the path to the directory with the
    .tfstate file. Adding ./ does this.
    
    See https://github.com/adammck/terraform-inventory/issues/121

--- ops/infrastructure/inventories/terraform-inventory.sh
+++ ops/infrastructure/inventories/terraform-inventory.sh
@@ -3,4 +3,4 @@
 # bash shell wrapper for terraform-inventory.
 # How to install the https://github.com/adammck/terraform-inventory command:
 # brew install terraform-inventory
-terraform-inventory $@
+terraform-inventory $@ ./
