2c9eb0a2c49697feb68036487cb7a6534753934b
Peter Li <peter@gigasciencejournal.com>
2022-03-31 14:01:58 +0800

    Fix random password used to access RDS instance

--- ops/infrastructure/modules/rds-instance/rds-instance.tf
+++ ops/infrastructure/modules/rds-instance/rds-instance.tf
@@ -30,6 +30,7 @@ module "db" {
 
   db_name                = var.gigadb_db_database
   username               = var.gigadb_db_user
+  create_random_password = false
   password               = var.gigadb_db_password
   port                   = 5432
 
