fdc6b52e3a096c963411a7c6248893bccb194650
Ken <kencho.gigascience@gmail.com>
2022-04-11 16:09:52 +0800

    Break the loop and allow empty input as latest

--- gigadb/app/tools/files-url-updater/reset_database.sh
+++ gigadb/app/tools/files-url-updater/reset_database.sh
@@ -25,7 +25,7 @@ defaultDB="/home/centos/database_bootstrap.backup"
 version=$(docker-compose run --rm updater psql --version | cut -d' ' -f 3 | tr -d '\n\r' )
 
 # Run files-url-updater
-if [[ $backupDate -eq "latest" ]];then
+if [[ -z $backupDate || $backupDate -eq "latest" ]];then
   echo yes | docker-compose run --rm updater ./yii dataset-files/download-restore-backup --latest
   downloadLatestRestoreStatus=$?
 else
@@ -35,7 +35,7 @@ fi
 
 # Convert backup using legacy postgresql client
 if [[ $downloadLatestRestoreStatus -eq 0 ]];then
-  docker-compose run --rm updater pg_dump --host=pg9_3 -p 5432  --username=gigadb  --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=converted/gigadbv3_${thedate}_v${version}.backup
+  docker-compose run --rm updater pg_dump --host=pg9_3 -p 5432  --username=gigadb  --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=converted/gigadbv3_${latest}_v${version}.backup
   convertStatus=$?
 fi
 
@@ -52,10 +52,10 @@ export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOS
 
 # Load the latest dump in RDS using native postgresql client
 if [[ $downloadLatestRestoreStatus -eq 0 && $convertStatus -eq 0 ]];then
-  echo "Loading gigadbv3_${thedate}_v${version}.backup"
+  echo "Loading gigadbv3_${latest}_v${version}.backup"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "drop database if exists $DB_PG_DATABASE"
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "create database $DB_PG_DATABASE owner $DB_PG_USER"
-  export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < converted/gigadbv3_${thedate}_v${version}.backup
+  export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < converted/gigadbv3_${latest}_v${version}.backup
   loadStatus=$?
 elif [[ -f converted/gigadbv3_${twoDaysAgo}_v${version}.backup ]];then
   echo "Loading gigadbv3_${twoDaysAgo}_v${version}.backup from two days ago"
@@ -76,6 +76,7 @@ else
   export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < "$defaultDB"
   loadStatus=$?
 fi
+exit $loadStatus
 
 # Load the specific dump in RDS using native postgresql client
 if [[ $downloadSpecificRestoreStatus -eq 0 && $convertStatus -eq 0 ]];then
