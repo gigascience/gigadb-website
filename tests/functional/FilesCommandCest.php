9b7ffa84dc43314448cfa7dbf0353dd0bc0ef9ae
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-05-03 10:33:29 +0200

    Add missing functional test scenario for checking ftp file urls

--- tests/functional/FilesCommandCest.php
+++ tests/functional/FilesCommandCest.php
@@ -14,7 +14,7 @@ class FilesCommandCest
         $I->assertNull($output);
     }
 
-    public function tryToOutputNonResolvableLinks(FunctionalTester $I)
+    public function tryToOutputNonResolvableHTTPLinks(FunctionalTester $I)
     {
 
         $I->haveInDatabase("file", [
@@ -29,5 +29,23 @@ class FilesCommandCest
 
         $output = shell_exec("./protected/yiic files checkUrls --doi=100020");
         $I->assertContains("https://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100020/bogus.txt", $output);
+    }
+
+
+    public function tryToOutputNonResolvableFTPLinks(FunctionalTester $I)
+    {
+
+        $I->haveInDatabase("file", [
+            "dataset_id" => 22,
+            "name" => "bogus.txt",
+            "location" => "ftp://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100020/bogus.txt",
+            "extension" => "txt",
+            "size" => "999",
+            "format_id" => 1,
+            "type_id" => 1,
+        ]);
+
+        $output = shell_exec("./protected/yiic files checkUrls --doi=100020");
+        $I->assertContains("ftp://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100020/bogus.txt", $output);
     }
 }
