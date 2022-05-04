4981adfaf809b9365c7bcd626463c76428d806da
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-05-04 17:04:31 +0200

    Add failing tests for detecting if file url is directory

--- tests/functional/FilesCommandCest.php
+++ tests/functional/FilesCommandCest.php
@@ -48,4 +48,45 @@ class FilesCommandCest
         $output = shell_exec("./protected/yiic files checkUrls --doi=100020");
         $I->assertContains("ftp://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100020/bogus.txt", $output);
     }
+
+
+    public function tryToOutputNonResolvableDirectoryLinks(FunctionalTester $I)
+    {
+
+        $I->haveInDatabase("file", [
+            "dataset_id" => 22,
+            "name" => "some_stuff",
+            "location" => "https://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100020",
+            "extension" => "txt",
+            "size" => "999",
+            "format_id" => 1,
+            "type_id" => 1,
+        ]);
+
+        $I->haveInDatabase("file", [
+            "dataset_id" => 22,
+            "name" => "another_stuff",
+            "location" => "https://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100020/",
+            "extension" => "txt",
+            "size" => "999",
+            "format_id" => 1,
+            "type_id" => 1,
+        ]);
+
+        $I->haveInDatabase("file", [
+            "dataset_id" => 8,
+            "name" => "another_stuff",
+            "location" => "https://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100006/phylogeny_study_update",
+            "extension" => "txt",
+            "size" => "999",
+            "format_id" => 1,
+            "type_id" => 1,
+        ]);
+
+
+        $output = shell_exec("./protected/yiic files checkUrls --doi=100020");
+        $I->assertContains("https://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100020", $output);
+        $I->assertContains("https://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100020/", $output);
+        $I->assertContains("https://ftp.cngb.org/pub/gigadb/pub/10.5524/100001_101000/100006/phylogeny_study_update", $output);
+    }
 }
