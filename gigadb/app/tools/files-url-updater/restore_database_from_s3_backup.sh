09dd2f658e0d0e942f737e5109d8cae7944c2d7b
Peter Li <peter@gigasciencejournal.com>
2022-05-05 15:08:31 +0800

    Update script

--- gigadb/app/tools/files-url-updater/restore_database_from_s3_backup.sh
+++ gigadb/app/tools/files-url-updater/restore_database_from_s3_backup.sh
@@ -8,23 +8,23 @@ export PATH=/usr/local/bin/:$PATH
 latest=$(date --date="1 days ago" +"%Y%m%d")
 backupDate="$1"
 
-# Download database dump from S3
+echo "Downloading database dump from S3"
 if [[ -z $backupDate || $backupDate -eq "latest" ]];then
-  clone copy s3_remote:gigadb_${latest}.backup restore/
+  rclone copy s3_remote:gigadb-database-backups/gigadb_${latest}.backup restore/
 else
-  clone copy s3_remote:gigadb_${backupDate}.backup restore/
+  rclone copy s3_remote:gigadb-database-backups/gigadb_${backupDate}.backup restore/
 fi
 
-# Terminate other connections to RDS instance
-export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c  "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='gigadb';"
+echo "Terminating other connections to RDS instance"
+export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='gigadb';"
 export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "drop database if exists $DB_PG_DATABASE"
 export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "create database $DB_PG_DATABASE owner $DB_PG_USER"
 
-# Restore dump onto RDS
+echo "Restoring dump onto RDS"
 if [[ -z $backupDate || $backupDate -eq "latest" ]];then
   echo "Loading gigadb_${latest}.backup"
-  export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < converted/gigadb_${latest}.backup
+  export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < restore/gigadb_${latest}.backup
 else
   echo "Loading gigadb_${backupDate}.backup"
-  export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < converted/gigadb_${backupDate}.backup
+  export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < restore/gigadb_${backupDate}.backup
 fi
