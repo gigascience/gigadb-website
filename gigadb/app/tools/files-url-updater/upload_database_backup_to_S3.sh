35c8e85bcfeb720b6163f9b5d764b1210e06b610
Peter Li <peter@gigasciencejournal.com>
2022-05-13 13:52:44 +0800

    Pull variables from .env in files-url-updater directory
    These variables are required for naming the backup file.

--- gigadb/app/tools/files-url-updater/upload_database_backup_to_S3.sh
+++ gigadb/app/tools/files-url-updater/upload_database_backup_to_S3.sh
@@ -9,11 +9,14 @@ which rclone
 # Calculate dates
 latest=$(date +"%Y%m%d")
 
+gitlab_environment=$GIGADB_ENVIRONMENT
+gitlab_project=$GITLAB_PROJECT_STRING
+
 # Terminate other connections to RDS instance
 export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c  "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='gigadb';"
 
 # Create pgdmp database dump file
-export PGPASSWORD=$DB_PG_PASSWORD; pg_dump --host=$DB_PG_HOST -p 5432 --username=$DB_PG_USER --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=backups/gigadb_${GITLAB_PROJECT_STRING}_${GIGADB_ENVIRONMENT}_${latest}.backup
+export PGPASSWORD=$DB_PG_PASSWORD; pg_dump --host=$DB_PG_HOST -p 5432 --username=$DB_PG_USER --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=backups/gigadb_${gitlab_project}_${gitlab_environment}_${latest}.backup
 
 # Upload dump files in backups folder to S3 using rclone - skips identical files
 rclone copy backups/ s3_remote:gigadb-database-backups
