0d136fdf3794b3e41bf543cdfccc595da3709258
Peter Li <peter@gigasciencejournal.com>
2022-05-13 11:45:31 +0800

    Fix filename for backup file
    Backup file name now includes Gitlab project name, environment and
    correct date when backup was created.

--- gigadb/app/tools/files-url-updater/upload_database_backup_to_S3.sh
+++ gigadb/app/tools/files-url-updater/upload_database_backup_to_S3.sh
@@ -13,7 +13,7 @@ latest=$(date +"%Y%m%d")
 export PGPASSWORD=$DB_PG_PASSWORD; psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c  "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='gigadb';"
 
 # Create pgdmp database dump file
-export PGPASSWORD=$DB_PG_PASSWORD; pg_dump --host=$DB_PG_HOST -p 5432 --username=$DB_PG_USER --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=backups/gigadb_${latest}.backup
+export PGPASSWORD=$DB_PG_PASSWORD; pg_dump --host=$DB_PG_HOST -p 5432 --username=$DB_PG_USER --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=backups/gigadb_${GITLAB_PROJECT_STRING}_${GIGADB_ENVIRONMENT}_${latest}.backup
 
 # Upload dump files in backups folder to S3 using rclone - skips identical files
 rclone copy backups/ s3_remote:gigadb-database-backups
