2431fc2fca5dc1c9a8ded414d6f50d0469c4545e
Ken <kencho.gigascience@gmail.com>
2022-07-04 15:35:31 +0800

    Update unit test

--- gigareview/common/tests/unit/ManuscriptTest.php
+++ gigareview/common/tests/unit/ManuscriptTest.php
@@ -19,6 +19,19 @@ class ManuscriptTest extends \Codeception\Test\Unit
     }
 
     // tests
+    public function testCanCreateTimestamp()
+    {
+        $manuscript = new Manuscript();
+        $this->assertNotNull($manuscript);
+        $this->assertNull($manuscript->created_at);
+        $this->assertNull($manuscript->updated_at);
+        $manuscript->save();
+        $this->assertNotNull($manuscript->created_at);
+        $manuscript->manuscript_number = "Test-GIGA-D-22-12345";
+        sleep(1);
+        $manuscript->save();
+        $this->assertGreaterThan($manuscript->created_at, $manuscript->updated_at);
+    }
     public function testCanInsertValuesToTable()
     {
         $manuscript = new Manuscript();
@@ -26,19 +39,12 @@ class ManuscriptTest extends \Codeception\Test\Unit
         $this->assertNull($manuscript->manuscript_number);
         $this->assertNull($manuscript->article_title);
         $this->assertNull($manuscript->revision_number);
-        $this->assertNull($manuscript->created_at);
-        $this->assertNull($manuscript->updated_at);
-        $manuscript->save();
-        $this->assertNotNull($manuscript->created_at);
-        $manuscript->manuscript_number = "Test-GIGA-D-22-00060";
+        $manuscript->manuscript_number = "Test-GIGA-D-22-12345";
         $manuscript->article_title = "Test-title";
         $manuscript->revision_number = "1";
-        sleep(1);
         $manuscript->save();
         $this->assertNotNull($manuscript->manuscript_number);
         $this->assertNotNull($manuscript->article_title);
         $this->assertNotNull($manuscript->revision_number);
-        $this->assertGreaterThan($manuscript->created_at, $manuscript->updated_at);
-
     }
 }
