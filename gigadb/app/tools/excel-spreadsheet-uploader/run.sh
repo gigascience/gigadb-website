e0329811296cb93281fc90e238f231020305c71e
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-07-12 18:53:47 +0200

    Put java tool logs in a directory so can be bind-mounted from host
    also simplify the log cleanup.

--- gigadb/app/tools/excel-spreadsheet-uploader/run.sh
+++ gigadb/app/tools/excel-spreadsheet-uploader/run.sh
@@ -5,19 +5,11 @@ export PROJECT_HOME="/tool"
 export LIB_DIR="$PROJECT_HOME/lib"
 export CLASSPATH=".:$LIB_DIR/*"
 
+JAVA_LOG="logs/java.log"
+JAVAC_LOG="logs/javac.log"
 # Clean up previous bash script runs
-declare -a files=("./java.log" "./javac.log")
-for i in "${files[@]}"
-do
-   echo "$i"
-   if [[ -f "$i" ]]
-   then
-       echo "Deleting $i"
-       rm "$i"
-   else
-       echo "$i not found"
-   fi
-done
+rm -f $JAVA_LOG
+rm -f $JAVAC_LOG
 
 # Compile Java source files
 javac -d $PROJECT_HOME/bin \
@@ -25,7 +17,7 @@ javac -d $PROJECT_HOME/bin \
   $PROJECT_HOME/src/Log/*.java \
   $PROJECT_HOME/src/Exception/*.java \
   $PROJECT_HOME/src/Test/*.java \
-  &> javac.log
+  &> $JAVAC_LOG
 
 # Execute ExceltoGigaDB tool
-java -cp $CLASSPATH:$PROJECT_HOME/configuration:$PROJECT_HOME/bin Main &> java.log
+java -cp $CLASSPATH:$PROJECT_HOME/configuration:$PROJECT_HOME/bin Main &> $JAVA_LOG
