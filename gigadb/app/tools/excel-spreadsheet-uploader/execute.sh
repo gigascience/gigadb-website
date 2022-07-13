3fac8789512331d4bb9c15917033328926fcddd1
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-07-13 16:05:14 +0200

    Ensure dataset uploader shell script works on staging and live

--- gigadb/app/tools/excel-spreadsheet-uploader/execute.sh
+++ gigadb/app/tools/excel-spreadsheet-uploader/execute.sh
@@ -4,15 +4,16 @@ PATH=/usr/local/bin:$PATH
 export PATH
 
 if [[ $(uname -n) =~ compute ]];then
-  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:staging -c 'drop trigger if exists file_finder_trigger on file RESTRICT'
-  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:staging -c 'drop trigger if exists sample_finder_trigger on sample RESTRICT'
-  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:staging -c 'drop trigger if exists dataset_finder_trigger on dataset RESTRICT'
+  . /home/centos/.bash_profile
+  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:$GIGADB_ENV -c 'drop trigger if exists file_finder_trigger on file RESTRICT'
+  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:$GIGADB_ENV -c 'drop trigger if exists sample_finder_trigger on sample RESTRICT'
+  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:$GIGADB_ENV -c 'drop trigger if exists dataset_finder_trigger on dataset RESTRICT'
 
-  docker run --rm -v /home/centos/uploadDir:/tool/uploadDir -v /home/centos/uploadLogs:/tool/logs registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_xls_uploader:staging ./run.sh
+  docker run --rm -v /home/centos/uploadDir:/tool/uploadDir -v /home/centos/uploadLogs:/tool/logs registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_xls_uploader:$GIGADB_ENV ./run.sh
 
-  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:staging -c 'create trigger file_finder_trigger after insert or update or delete or truncate on file for each statement execute procedure refresh_file_finder()'
-  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:staging -c 'create trigger sample_finder_trigger after insert or update or delete or truncate on sample for each statement execute procedure refresh_sample_finder()'
-  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:staging -c 'create trigger dataset_finder_trigger after insert or update or delete or truncate on dataset for each statement execute procedure refresh_dataset_finder()'
+  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:$GIGADB_ENV -c 'create trigger file_finder_trigger after insert or update or delete or truncate on file for each statement execute procedure refresh_file_finder()'
+  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:$GIGADB_ENV -c 'create trigger sample_finder_trigger after insert or update or delete or truncate on sample for each statement execute procedure refresh_sample_finder()'
+  docker run --rm  --env-file ./db-env registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pgclient:$GIGADB_ENV -c 'create trigger dataset_finder_trigger after insert or update or delete or truncate on dataset for each statement execute procedure refresh_dataset_finder()'
 else
   mkdir -p logs
   docker-compose run --rm pg_client -c 'drop trigger if exists file_finder_trigger on file RESTRICT'
