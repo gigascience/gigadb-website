ae9289c3ddb3fd4acb9b261ba1b068d2ae667978
Ken <kencho.gigascience@gmail.com>
2022-07-20 22:27:12 +0800

    Create EMReportJob functional test

---
+++ gigareview/console/tests/functional/EMReportJobCest.php
@@ -0,0 +1,21 @@
+<?php
+namespace console\tests\functional;
+use console\tests\FunctionalTester;
+use yii\console\ExitCode;
+
+class EMReportJobCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToPushManuscriptQueneJobToTable(FunctionalTester $I)
+    {
+//        $I->runShellCommand("./yii_test fetch-reports/fetch", false);
+//        $I->runShellCommand("/usr/local/bin/php /app/yii manuscripts-q/run --verbose", false);
+        $I->amConnectedToDatabase('reviewdb_test');
+        $I->canSeeResultCodeIs(Exitcode::OK);
+//        $I->amConnectedToDatabase('reviewdb_test');
+    }
+}
