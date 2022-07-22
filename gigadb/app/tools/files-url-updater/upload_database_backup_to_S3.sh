e0512695f337f5818d3cf833c1d4de86b76f79a9
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-07-22 13:15:44 +0200

    Delete database backup after they are uploaded to S3

--- gigadb/app/tools/files-url-updater/upload_database_backup_to_S3.sh
+++ gigadb/app/tools/files-url-updater/upload_database_backup_to_S3.sh
@@ -20,6 +20,9 @@ export PGPASSWORD=$DB_PG_PASSWORD; pg_dump --host=$DB_PG_HOST -p 5432 --username
 
 # Upload dump files in backups folder to S3 using rclone - skips identical files
 rclone copy backups/ s3_remote:gigadb-database-backups
+cloneStatus=$?
 
-# Delete back up files older than 20 days
-find backups/* -mtime +20 -exec rm {} +
+# Housekeeping
+if [[ $cloneStatus -eq 0 ]];then
+  rm -f backups/gigadb_${gitlab_project}_${gitlab_environment}_${latest}.backup
+fi
