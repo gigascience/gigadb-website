4c18ee09510421755ad5acecb44c8660126e06ba
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-07-26 08:42:46 +0200

    comment jq parsing of response from gitlab variables for now

--- gigadb/app/tools/files-url-updater/generate_config.sh
+++ gigadb/app/tools/files-url-updater/generate_config.sh
@@ -47,7 +47,7 @@ fi
 
 if ! [ -s ./.secrets ];then
     echo "Retrieving variables from ${MISC_VARIABLES_URL}"
-    curl --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "${MISC_VARIABLES_URL}?per_page=100" | jq --arg ENVIRONMENT "$GIGADB_ENV" -r '.[] | select(.environment_scope == "*" or .environment_scope == $ENVIRONMENT ) | select(.key | test("_ftp_") ) | .key + "=" + .value' > .secrets
+    curl -s --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "${MISC_VARIABLES_URL}?per_page=100" | tee .secrets #| jq --arg ENVIRONMENT "$GIGADB_ENV" -r '.[] | select(.environment_scope == "*" or .environment_scope == $ENVIRONMENT ) | select(.key | test("_ftp_") ) | .key + "=" + .value' > .secrets
 fi
 echo "Sourcing secrets"
 source "./.secrets"
