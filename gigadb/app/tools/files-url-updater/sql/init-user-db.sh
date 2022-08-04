11de1f5737fd926de59f13145765a81053ac248f
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-08-04 11:52:54 +0200

    Bring to parity local dev and cloud re init of legacy database

--- gigadb/app/tools/files-url-updater/sql/init-user-db.sh
+++ gigadb/app/tools/files-url-updater/sql/init-user-db.sh
@@ -2,7 +2,8 @@
 set -exu
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER gigadb;
-	CREATE DATABASE gigadb;
-	GRANT ALL PRIVILEGES ON DATABASE gigadb TO gigadb;
+  create role gigadb NOSUPERUSER LOGIN;
+  create role gigadb_test NOSUPERUSER LOGIN;
+  create database gigadb owner gigadb;
+  create database gigadb_test owner gigadb_test;
 EOSQL
