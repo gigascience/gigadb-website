abfedad1cc27dfb1ad445402ea8234d7bff6f8ad
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-08-08 16:06:31 +0200

    Specify the docker host as db host for pg_dump

--- gigadb/app/tools/files-url-updater/exportLegacyToTextBackup.sh
+++ gigadb/app/tools/files-url-updater/exportLegacyToTextBackup.sh
@@ -3,4 +3,4 @@
 set -ex
 
 destFile=${1:-"/converted/gigadbv3.backup"}
-pg_dump --username=postgres --host=localhost  --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=$destFile
+pg_dump --username=postgres --host=host.docker.internal  --clean --create --schema=public --no-privileges --no-tablespaces --dbname=gigadb --file=$destFile
