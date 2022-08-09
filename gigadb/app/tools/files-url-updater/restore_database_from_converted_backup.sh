c47932fa4432c35aad4b9b1211db017d0f73312d
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-08-09 17:08:36 +0200

    Fix database target of the queries and remove query on nonexistent table

--- gigadb/app/tools/files-url-updater/restore_database_from_converted_backup.sh
+++ gigadb/app/tools/files-url-updater/restore_database_from_converted_backup.sh
@@ -9,6 +9,5 @@ psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c  "SELECT pg_terminate_
 psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "drop database if exists $DB_PG_DATABASE"
 psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "create database $DB_PG_DATABASE owner $DB_PG_USER"
 psql -v ON_ERROR_STOP=1 -U $DB_PG_USER -h $DB_PG_HOST -p 5432 < $backupFile
-psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "select count(*) from dataset"
-psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "select identifier, publication_date from dataset where publication_date is not null order by publication_date desc limit 1"
-psql -U $DB_PG_USER -d postgres -h $DB_PG_HOST -p 5432 -c "select * from tbl_migration"
+psql -U $DB_PG_USER -d $DB_PG_DATABASE -h $DB_PG_HOST -p 5432 -c "select count(*) from dataset"
+psql -U $DB_PG_USER -d $DB_PG_DATABASE -h $DB_PG_HOST -p 5432 -c "select identifier, publication_date from dataset where publication_date is not null order by publication_date desc limit 1"
