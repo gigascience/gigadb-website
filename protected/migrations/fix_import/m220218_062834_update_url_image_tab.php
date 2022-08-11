25fe7b09ae38e45d87c463b3c1c31ba65327084d
Peter Li <peter@gigasciencejournal.com>
2022-08-11 10:13:38 +0800

    Fix project image link bug
    In http://gigadb.gigasciencejournal.com:9170/dataset/100006, the
    project image was not being displayed because its link was
    https://assets.gigadb-cdn.net/live/images/projects/genome_10khttps://assets.gigadb-cdn.net/live/images/projects/genome_10k/G10Klogo.jpg.
    This was caused by m220126_062259_update_image_location_column_project_tab.php
    being executed twice, once by setup_devdb.sh and then by setup_testdb.sh
    in up.sh since the migration script uses Yii::app()->db->createCommand
    to always execute SQL commands on gigadb dev database. This bug was
    breaking an acceptance test in DatasetView.feature when run in dev.
    
    m220218_062834_update_url_image_tab.php has also been fixed to so
    it can be executed on gigadb and gigadb_testdata databases.

--- protected/migrations/fix_import/m220218_062834_update_url_image_tab.php
+++ protected/migrations/fix_import/m220218_062834_update_url_image_tab.php
@@ -2,13 +2,20 @@
 
 class m220218_062834_update_url_image_tab extends CDbMigration
 {
-	public function safeUp()
-	{
-	    Yii::app()->db->createCommand("update image set url = replace(url, 'http://gigadb.org/','https://assets.gigadb-cdn.net/live/images/datasets/') where url like 'http://gigadb.org/%';")->execute();
-	    Yii::app()->db->createCommand("update image set url = 'https://assets.gigadb-cdn.net/live/images/datasets/no_image.png' where url='' and location like 'no_image%';")->execute();
-	}
+    public function safeUp()
+    {
+        $this->execute("UPDATE image
+            SET url = REPLACE(url, 'http://gigadb.org/','https://assets.gigadb-cdn.net/live/images/datasets/')
+            WHERE url LIKE 'http://gigadb.org/%';");
 
-	public function safeDown()
-	{
-	}
+        $this->execute("UPDATE image
+            SET url = 'https://assets.gigadb-cdn.net/live/images/datasets/no_image.png'
+            WHERE url='' AND location LIKE 'no_image%';");
+    }
+
+    public function safeDown()
+    {
+        // This function is empty because reverting safeUp() will eventually
+        // result in dataset images not displayed when CNGB server is shutdown.
+    }
 }
