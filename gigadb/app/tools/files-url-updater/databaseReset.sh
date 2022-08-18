6d1d9f56e1abdccf19399c4cd9276bfebe02b6d2
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-08-18 16:50:28 +0200

    Fix hard-coded gitlab project problem in databaset reset script

--- gigadb/app/tools/files-url-updater/databaseReset.sh
+++ gigadb/app/tools/files-url-updater/databaseReset.sh
@@ -16,29 +16,29 @@ fi
 
 # Spin up legacy database (PosgresQL 9.3)
 # Make sure the port 5432 is accessible from host
-docker run --rm --detach --name pg9_3 -p 5432:5432 registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pg9_3:$GIGADB_ENVIRONMENT
+docker run --rm --detach --name pg9_3 -p 5432:5432 registry.gitlab.com/$GITLAB_PROJECT_NAME/production_pg9_3:$GIGADB_ENVIRONMENT
 
 # Download/restore legacy database on the legacy server
 # make sure to define --add-host, so the files-url-updater command can connect to the host on port 5432 using ``host.docker.internal``
-docker run --rm --add-host=host.docker.internal:host-gateway -v /home/centos:/logs -v /home/centos/downloads:/downloads registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_updater:$GIGADB_ENVIRONMENT ./yii dataset-files/download-restore-backup --date $backupDate --force
+docker run --rm --add-host=host.docker.internal:host-gateway -v /home/centos:/logs -v /home/centos/downloads:/downloads registry.gitlab.com/$GITLAB_PROJECT_NAME/production_updater:$GIGADB_ENVIRONMENT ./yii dataset-files/download-restore-backup --date $backupDate --force
 
 # Export legacy database as text in new format modern version of Pg can read
-docker run --rm --user=1000 --add-host=host.docker.internal:host-gateway -v /home/centos/converted:/converted registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_pg9_3:$GIGADB_ENVIRONMENT /exportLegacyToTextBackup.sh /converted/gigadbv3_${backupDate}.backup
+docker run --rm --user=1000 --add-host=host.docker.internal:host-gateway -v /home/centos/converted:/converted registry.gitlab.com/$GITLAB_PROJECT_NAME/production_pg9_3:$GIGADB_ENVIRONMENT /exportLegacyToTextBackup.sh /converted/gigadbv3_${backupDate}.backup
 
 # load the converted backup into RDS
-docker run --rm --env-file .env -v /home/centos/converted:/converted --entrypoint /restore_database_from_converted_backup.sh registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_s3backup:$GIGADB_ENVIRONMENT /converted/gigadbv3_${backupDate}.backup
+docker run --rm --env-file .env -v /home/centos/converted:/converted --entrypoint /restore_database_from_converted_backup.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT /converted/gigadbv3_${backupDate}.backup
 
 # Create the drop and add constraints queries that need to be run before and after schema migration respectively
-docker run --rm -e YII_PATH=/var/www/vendor/yiisoft/yii -v /home/centos:/var/www/protected/runtime registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_app:$GIGADB_ENVIRONMENT /var/www/protected/scripts/prepareConstraints.sh
+docker run --rm -e YII_PATH=/var/www/vendor/yiisoft/yii -v /home/centos:/var/www/protected/runtime registry.gitlab.com/$GITLAB_PROJECT_NAME/production_app:$GIGADB_ENVIRONMENT /var/www/protected/scripts/prepareConstraints.sh
 
 # drop constraints/indexes/triggers before running migrations
-docker run --rm --env-file .env -v /home/centos:/sql --entrypoint /dropConstraints.sh registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_s3backup:$GIGADB_ENVIRONMENT
+docker run --rm --env-file .env -v /home/centos:/sql --entrypoint /dropConstraints.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT
 
 # Run migrations
-docker run --rm -e YII_PATH=/var/www/vendor/yiisoft/yii -v /home/centos:/var/www/protected/runtime registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_app:$GIGADB_ENVIRONMENT /var/www/protected/scripts/updateDBSchema.sh
+docker run --rm -e YII_PATH=/var/www/vendor/yiisoft/yii -v /home/centos:/var/www/protected/runtime registry.gitlab.com/$GITLAB_PROJECT_NAME/production_app:$GIGADB_ENVIRONMENT /var/www/protected/scripts/updateDBSchema.sh
 
 #  Restore constraints/indexes/triggers
-docker run --rm --env-file .env -v /home/centos:/sql --entrypoint /addConstraints.sh registry.gitlab.com/gigascience/forks/rija-gigadb-website/production_s3backup:$GIGADB_ENVIRONMENT
+docker run --rm --env-file .env -v /home/centos:/sql --entrypoint /addConstraints.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT
 
 # Housekeeping
 
