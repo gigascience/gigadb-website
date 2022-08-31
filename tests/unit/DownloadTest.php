4b169109f4dbcd9eee44007df964a23b90c962d3
Peter Li <peter@gigasciencejournal.com>
2022-08-31 08:22:19 +0800

    Create unit test for DownloadService

---
+++ tests/unit/DownloadTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Tests DownloadService component
+ */
+class DownloadTest extends \Codeception\Test\Unit
+{
+    /**
+     * Test that a remote file can be downloaded
+     */
+    public function testDownloadFile()
+    {
+        // UniProt is a reliable database service
+        $url = "https://rest.uniprot.org/uniprotkb/A0A1A8NJ45.txt";
+        try {
+            $output = DownloadService::downloadFile($url);
+            $this->assertStringContainsString("Keratin", $output);
+        } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+            echo $e->getMessage().PHP_EOL;
+        }
+    }
+
+    /**
+     * Test that a remote file exists
+     */
+    public function testFileExists()
+    {
+        $url = "https://rest.uniprot.org/uniprotkb/A0A1A8NJ45.txt";
+        $file_exists = DownloadService::fileExists($url);
+        $this->assertTrue($file_exists);
+    }
+}
