e43d8a2abde49c9fe3311d58d7ad45ef1a36e241
Ken <kencho.gigascience@gmail.com>
2022-09-02 14:22:56 +0800

    Make reviewdb_test dump from reviewdb

--- gigareview/make_pgdmp_reviewdb_test.sh
+++ gigareview/make_pgdmp_reviewdb_test.sh
@@ -5,6 +5,5 @@ source .env
 source .secrets
 
 export PGPASSWORD=$REVIEW_DB_PASSWORD
-export testDB="${REVIEW_DB_DATABASE}_test"
 
-pg_dump --no-owner -U $REVIEW_DB_USERNAME -h $REVIEW_DB_HOST -p $REVIEW_DB_PORT -F custom -d $testDB -f sql/"$testDB".pgdmp
+pg_dump --no-owner -U $REVIEW_DB_USERNAME -h $REVIEW_DB_HOST -p $REVIEW_DB_PORT -F custom -d $REVIEW_DB_DATABASE -f sql/"${REVIEW_DB_DATABASE}_test".pgdmp
