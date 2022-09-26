11433eeb900a816ce10387f29dd3df5b37db5d1a
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-09-26 17:32:27 +0200

    Debug why POSTGRES_* variables are not in .secrets

--- gigareview/generate_config.sh
+++ gigareview/generate_config.sh
@@ -58,7 +58,7 @@ if ! [ -s ./.secrets ];then
     curl -s --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "${MISC_VARIABLES_URL}?per_page=100" | jq --arg ENVIRONMENT $REVIEW_ENV -r '.[] | select(.environment_scope == "*" or .environment_scope == $ENVIRONMENT ) | select(.key | test("sftp_") ) | .key + "=" + .value' > .misc_var
 
 
-    cat .group_var .fork_var .project_var .misc_var > .secrets && rm .group_var && rm .fork_var && rm .project_var && rm .misc_var && rm .project_var_raw1 && rm .project_var_raw2 && rm .project_vars.json
+    cat .group_var .fork_var .project_var .misc_var > .secrets #&& rm .group_var && rm .fork_var && rm .project_var && rm .misc_var && rm .project_var_raw1 && rm .project_var_raw2 && rm .project_vars.json
     echo "# Some help about this file in ops/configuration/variables/secrets-sample" >> .secrets
 fi
 echo "Sourcing secrets"
