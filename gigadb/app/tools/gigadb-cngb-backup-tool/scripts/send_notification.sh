989444d4dadc0da7f0e027bb91ef6702fcba176c
Ken <kencho.gigascience@gmail.com>
2022-10-25 14:31:32 +0800

    Remove extra variables

--- gigadb/app/tools/gigadb-cngb-backup-tool/scripts/send_notification.sh
+++ gigadb/app/tools/gigadb-cngb-backup-tool/scripts/send_notification.sh
@@ -2,12 +2,10 @@
 
 source .env
 
-token=$GITTER_API_TOKEN
-roomId=$GITTER_IT_NOTIFICATION_ROOM_ID
 serverName=$(uname -a | cut -f2 -d' ')
 date=$(date)
 
 curl -X POST -i -H "Content-Type: application/json" \
       -H "Accept: application/json" \
-      -H "Authorization: Bearer $token" "https://api.gitter.im/v1/rooms/$roomId/chatMessages" \
+      -H "Authorization: Bearer $GITTER_API_TOKEN" "https://api.gitter.im/v1/rooms/$GITTER_IT_NOTIFICATION_ROOM_ID/chatMessages" \
       -d '{"text":"Test Notification from '"$serverName"' at '"$date"':!!!!!!'"$1"'!!!!!!"}'
