01d7ef849e8ffc0c4bf49dbc868fbd85e21bac7a
Ken <kencho.gigascience@gmail.com>
2022-10-26 23:24:03 +0800

    Get the log after changing the source file name

--- gigadb/app/tools/gigadb-cngb-backup-tool/scripts/run_rclone_copy.sh
+++ gigadb/app/tools/gigadb-cngb-backup-tool/scripts/run_rclone_copy.sh
@@ -8,5 +8,5 @@
 
 # real run
 #time rclone --config=.rclone.conf copy --checksum --log-level DEBUG --log-file=tests/test-logs/test_rclone_copy_large_success_2nd.log s3genomics:1000genomes/data/NA12878/alignment/ gigaKen:test-transfer-interruption/NA12878
-time rclone --config=.rclone.conf copy --checksum --log-level DEBUG --log-file=tests/test-logs/test_rclone_copy_large_change_delete_source.log gigaKen:test-transfer-interruption/NA12878 wasabi:test-gigadb-datasets/test-ken-20221010/NA12878_delete_source
+time rclone --config=.rclone.conf copy --checksum --log-level DEBUG --s3-no-check-bucket --log-file=tests/test-logs/test_rclone_copy_large_change_file_name.log gigaKen:test-transfer-interruption/NA12878/NA12878.alt_bwamem_GRCh38DH.20150718.CEU.low_coverage.cram wasabi:test-gigadb-datasets/test-ken-20221010/NA12878_delete_source
 echo $? > tests/test-logs/test_rclone_status_"$?".log
