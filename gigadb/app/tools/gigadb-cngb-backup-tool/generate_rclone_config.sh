edbaeadee05be85a153f93228e7e84178764e8cb
Ken <kencho.gigascience@gmail.com>
2022-11-02 17:00:58 +0800

    Only add in wasabi parameters

--- gigadb/app/tools/gigadb-cngb-backup-tool/generate_rclone_config.sh
+++ gigadb/app/tools/gigadb-cngb-backup-tool/generate_rclone_config.sh
@@ -18,8 +18,8 @@ if ! [ -f  ./.rclone.conf ];then
   read -sp "To create rclone config, enter your wasabi secret_access_key: " secret
   read -p "To create rclone config, enter your wasabi region: " region
   cp config-source/rclone.config.template .rclone.conf
-  sed -i'.bak' "s/access_key_id =/access_key_id = $key/" .rclone.conf
-  sed -i'.bak' "s/secret_access_key =/secret_access_key = $secret/" .rclone.conf
-  sed -i'.bak' "s/region =/region = $region/" .rclone.conf
+  sed -i'.bak' "s/wasabi_access_key_id =/access_key_id = $key/" .rclone.conf
+  sed -i'.bak' "s/wasabi_secret_access_key =/secret_access_key = $secret/" .rclone.conf
+  sed -i'.bak' "s/wasabi_region =/region = $region/" .rclone.conf
   rm .rclone.conf.bak
 fi
