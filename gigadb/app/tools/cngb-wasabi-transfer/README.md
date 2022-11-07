389a8be8aef73afe5949bbed3c42a632f3f38c8f
Peter Li <peter@gigasciencejournal.com>
2022-11-07 17:54:16 +0800

    Fix bugs in bash script

--- gigadb/app/tools/cngb-wasabi-transfer/README.md
+++ gigadb/app/tools/cngb-wasabi-transfer/README.md
@@ -8,4 +8,12 @@ $ rclone ls wasabi:
 
 # Execute on host
 $ docker-compose run --rm rclone rclone ls wasabi:
+
+# Run rclone script
+$ docker-compose run --rm rclone /app/rclone_copy.sh --starting-doi 100002 --ending-doi 100304
+
+$ docker-compose run --rm rclone /app/rclone_copy.sh --starting-doi 101200 --ending-doi 101960
+
+$ docker-compose run --rm rclone /app/rclone_copy.sh --starting-doi 102100 --ending-doi 102990
+
 ```
--- gigadb/app/tools/cngb-wasabi-transfer/app/rclone_copy.sh
+++ gigadb/app/tools/cngb-wasabi-transfer/app/rclone_copy.sh
@@ -5,28 +5,30 @@ set -e
 
 # Allow all scripts to base include, log, etc. paths off the
 # directory where backup script is located
-PROJECT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+APP_HOME=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Setup logging
-LOGDIR="$PROJECT_HOME/logs"
+LOGDIR="$APP_HOME/logs"
 LOGFILE="$LOGDIR/transfer_$(date +'%Y%m%d_%H%M%S').log"
 mkdir -p $LOGDIR
 touch $LOGFILE
 
 # Include proxy settings to perform data transfer
-#source "$PROJECT_HOME/proxy_settings.sh" || exit 1
+#source "$APP_HOME/proxy_settings.sh" || exit 1
 
 # Parse command line parameters
-starting_doi=""
-ending_doi=""
+#starting_doi=""
+#ending_doi=""
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
     --starting-doi)
-            starting_doi=$1
+            has_starting_doi=true
+            starting_doi=$2
             shift
             ;;
     --ending-doi)
+            has_ending_doi=true
             ending_doi=$2
             shift
             ;;
@@ -41,41 +43,49 @@ done
 # Variables for creating directory paths
 DATASETS_PATH="/data/gigadb/pub/10.5524/"
 
+echo "$starting_doi"
+echo "$ending_doi"
+
 # Determine which DOI directory range to use
 dir_range=""
-if [ $1 -lt 101000 ]
+if [ "$starting_doi" -lt 101000 ];
 then
     dir_range="100001_101000"
-elif [ $1 -lt 102000 ] && [ $1 -gt 101001 ]; 
+elif [ "$starting_doi" -lt 102000 ] && [ "$starting_doi" -gt 101001 ]; 
 then
     dir_range="101001_102000"
-elif [ $1 -lt 103000 ] && [ $1 -gt 102001 ]; 
+elif [ "$starting_doi" -lt 103000 ] && [ "$starting_doi" -gt 102001 ]; 
 then
     dir_range="102001_103000"
 fi
 
 # Copy dataset files for all DOIs between starting and ending DOIs
-current_doi=starting_doi
+current_doi="$starting_doi"
 while [ "$current_doi" -lt "$ending_doi" ] || [ "$current_doi" -eq "$ending_doi" ]
 do
+#    echo "Current DOI: $current_doi"
+  
     # Create directory paths
-    source_path="${DATASETS_PATH}${dir_range}/${starting_doi}"
-    destination_path="/gigadb_datasets/${dir_range}/${starting_doi}"
+    source_path="${DATASETS_PATH}${dir_range}/${current_doi}"
+    destination_path="/gigadb_datasets/${dir_range}/${current_doi}"
     
     # Check directory for current DOI exists
-    
-    # Perform data transfer to Wasabi
-    rclone copy $source_path $destination_path \
-        --log-file=$LOGFILE \
-        --log-level INFO \
-        --stats-log-level DEBUG >> $LOGFILE;
-    
-    # Check exit code for rclone command
-    if [ $? -eq 0 ] 
-    then 
-      echo "Successfully executed rclone copy" 
-    else 
-      echo "Problem with copying files to Wasabi by rclone: " >&2 
+    if [ -d "$source_path" ]; then
+      echo "$source_path exists"
+      
+      # Perform data transfer to Wasabi
+      #    rclone copy $source_path $destination_path \
+      #        --log-file=$LOGFILE \
+      #        --log-level INFO \
+      #        --stats-log-level DEBUG >> $LOGFILE;
+      #    
+      #    # Check exit code for rclone command
+      #    if [ $? -eq 0 ] 
+      #    then 
+      #      echo "Successfully executed rclone copy" 
+      #    else 
+      #      echo "Problem with copying files to Wasabi by rclone: " >&2 
+      #    fi
     fi
     
     # Increment current DOI
