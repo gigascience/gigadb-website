da642610f035cbe3aed9a43a13b98dcbc2e44fd1
Ken <kencho.gigascience@gmail.com>
2022-11-16 22:15:32 +0800

    Create scripts for testing speed

---
+++ gigadb/app/tools/gigadb-cngb-backup-tool/scripts/download_from_tyo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# set up the appropriate network
+http_proxy=http://proxy.sz.cngb.org:3128
+https_proxy=http://proxy.sz.cngb.org:3128
+ftp_proxy=http://proxy.sz.cngb.org:3128
+no_proxy="127.0.0.1,localhost,*.cloud.am55.*,*.sz.cngb.org,*.genomics.cn"
+export http_proxy
+export https_proxy
+export ftp_proxy
+export no_proxy
+
+
+date=$(date +"%Y%m%d%H%M")
+
+rclone --config=/home/gigadb/ken/rclone.conf copy --checksum --log-level DEBUG --log-file=/home/gigadb/ken/logs/download_from_tyo_$date.log wasabiKenTyo:test-tyo-bucket/test-download/NA12878 /home/gigadb/ken/test-download-from-tyo/NA12878
+
+#rm -rf /home/gigadb/ken/test-download-from-tyo/NA12878
---
+++ gigadb/app/tools/gigadb-cngb-backup-tool/scripts/upload_to_tyo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# set up the appropriate network
+http_proxy=http://proxy.sz.cngb.org:3128
+https_proxy=http://proxy.sz.cngb.org:3128
+ftp_proxy=http://proxy.sz.cngb.org:3128
+no_proxy="127.0.0.1,localhost,*.cloud.am55.*,*.sz.cngb.org,*.genomics.cn"
+export http_proxy
+export https_proxy
+export ftp_proxy
+export no_proxy
+
+
+date=$(date +"%Y%m%d%H%M")
+
+rclone --config=/home/gigadb/ken/rclone.conf copy --checksum --log-level DEBUG --log-file=/home/gigadb/ken/logs/upload_to_tyo_$date.log /home/gigadb/ken/test-download-from-tyo/NA12878 wasabiKenTyo:test-tyo-bucket/test-upload/NA12878
+rclone --config=/home/gigadb/ken/rclone.conf purge --checksum --log-level DEBUG --log-file=/home/gigadb/ken/logs/upload_to_tyo_$date.log  wasabiKenTyo:test-tyo-bucket/test-upload/NA12878
