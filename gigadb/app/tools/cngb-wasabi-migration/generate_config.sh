28156d7711364ef6319dc014e52f19f7e383de2c
Peter Li <peter@gigasciencejournal.com>
2022-11-21 15:37:42 +0800

    Deal with upstream configuration
    Creating .secrets file using cat caused errors when running on
    upstream configuration since it will not a .fork_var file.

--- gigadb/app/tools/cngb-wasabi-migration/generate_config.sh
+++ gigadb/app/tools/cngb-wasabi-migration/generate_config.sh
@@ -1,37 +1,31 @@
 #!/usr/bin/env bash
 
-# bail out upon error
+# Bail out upon error
 set -e
 
-# bail out if an unset variable is used
+# Bail out if an unset variable is used
 set -u
 
-# display the lines of this script as they are executed for debugging
+# Display lines of this script as they are executed for debugging
 set -x
 
-# export all variables that need to be substituted in templates
+# Export all variables that need to be substituted in templates
 set -a
+
 # Setting up in-container application source variable (APP_SOURCE).
 # It's the counterpart of the host variable APPLICATION
 APP_SOURCE=/app
 
-
-# read env variables in same directory, from a file called .env.
-# They are shared by both this script and Docker compose files.
-# require gigadb's config to have run first
+# Read env variables in same directory from a file called .env.
 cd $APP_SOURCE
 echo "Current working directory: $PWD"
-
 if [ -f  ./.env ];then
     echo "An .env file is present, sourcing it"
     source "./.env"
 fi
 
 # Fetch and set environment variables from GitLab
-# Only necessary on DEV, as on CI (STG and PROD), the variables are exposed to build environment
-
 if ! [ -s ./.secrets ];then
-
     # deal with special case we are in Upstream
     if [[ $CI_PROJECT_URL == "https://gitlab.com/gigascience/upstream/gigadb-website" ]];then
       PROJECT_VARIABLES_URL="https://gitlab.com/api/v4/projects/gigascience%2Fupstream%2Fgigadb-website/variables"
@@ -51,20 +45,29 @@ if ! [ -s ./.secrets ];then
     jq -s 'add' .project_var_raw1 .project_var_raw2 > .project_vars.json
     cat .project_vars.json | jq --arg ENVIRONMENT $GIGADB_ENV -r '.[] | select(.environment_scope == "*" or .environment_scope == "dev" ) | select(.key | test("private_key|tlsauth|ca|pem|cert";"i") | not ) |.key + "=" + .value' > .project_var
 
-
     echo "Retrieving variables from ${MISC_VARIABLES_URL}"
     curl -s --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" "${MISC_VARIABLES_URL}?per_page=100" | jq --arg ENVIRONMENT $GIGADB_ENV -r '.[] | select(.environment_scope == "*" or .environment_scope == $ENVIRONMENT ) | select(.key | test("sftp_") ) | .key + "=" + .value' > .misc_var
 
-    cat .group_var .fork_var .project_var .misc_var > .secrets  && rm .group_var && rm .fork_var && rm .project_var && rm .misc_var && rm .project_var_raw1 && rm .project_var_raw2 && rm .project_vars.json
+    # Need to account for the fact that there is no .fork_var when dealing with
+    # upstream configuration. An error will be generated if we try to cat a
+    # non-existent file
+    if [ "$CI_PROJECT_URL" == "https://gitlab.com/gigascience/upstream/gigadb-website" ];
+    then
+      cat .group_var .project_var .misc_var > .secrets && rm .group_var && rm .project_var && rm .misc_var && rm .project_var_raw1 && rm .project_var_raw2 && rm .project_vars.json
+    else
+      # Fork configuration
+      cat .group_var .fork_var .project_var .misc_var > .secrets && rm .group_var && rm .fork_var && rm .project_var && rm .misc_var && rm .project_var_raw1 && rm .project_var_raw2 && rm .project_vars.json
+    fi
+
     echo "# Some help about this file in ops/configuration/variables/secrets-sample" >> .secrets
 fi
 echo "Sourcing secrets"
 source "./.secrets"
 
+# Restore default settings for variables
 set +a
 
-
-# Generate configuration for config files
+# Generate rclone configuration
 SOURCE=${APP_SOURCE}/rclone.conf.dist
 TARGET=${APP_SOURCE}/config/rclone.conf
 VARS='$WASABI_ACCESS_KEY_ID:$WASABI_SECRET_ACCESS_KEY'
