be8c85a136d6f083a2846488b2127203d24cb89a
Ken <kencho.gigascience@gmail.com>
2022-12-02 18:22:22 +0800

    Update the text structure

---
+++ gigadb/app/tools/wasabi-migration/send_notification.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source .secrets
+
+serverName=$(uname -a | cut -f2 -d' ')
+date=$(date)
+
+curl -X POST -i -H "Content-Type: application/json" \
+      -H "Accept: application/json" \
+      -H "Authorization: Bearer $GITTER_API_TOKEN" "https://api.gitter.im/v1/rooms/$GITTER_IT_NOTIFICATION_ROOM_ID/chatMessages" \
+      -d '{"text":"From server ***'"$serverName"'*** : '"$1"'"}'
