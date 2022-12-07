186cde0e1f8c7d406e1f1fafc3357e924446a04e
Ken <kencho.gigascience@gmail.com>
2022-12-07 15:18:12 +0800

    Make swatchdog only look for the last line of the latest log

--- gigadb/app/tools/wasabi-migration/spinup_swatchdog_daemon.sh
+++ gigadb/app/tools/wasabi-migration/spinup_swatchdog_daemon.sh
@@ -3,9 +3,6 @@
 # Get the name of the latest migration log file
 latestLog=$(ls -Art logs/ | tail -n 1)
 
-# Spin up swatch daemon
+# Spin up the swatchdog tool and make it watch the last line of the log
 SWATCHDOG_CONFIG="config/swatchdog.conf"
-swatchdog -c $SWATCHDOG_CONFIG -t logs/$latestLog --daemon
-
-# Shutdown swatchdog daemon
-#ps -ef | grep "migration" | grep -v "grep" | awk '{print $1}' | xargs -r kill -9
+swatchdog -c $SWATCHDOG_CONFIG  --tail-args "-1 -f" -t logs/$latestLog
