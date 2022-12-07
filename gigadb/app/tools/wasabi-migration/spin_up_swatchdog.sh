7dbdf19cf736af6c34a9af679cb96378c030b53c
Ken <kencho.gigascience@gmail.com>
2022-12-07 18:04:05 +0800

    Only look for the log file created in less than 2 minutes

--- gigadb/app/tools/wasabi-migration/spin_up_swatchdog.sh
+++ gigadb/app/tools/wasabi-migration/spin_up_swatchdog.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Get the name of the latest migration log file
-latestLog=$(ls -Art /app/logs/ | tail -n 1)
+# Get the name of the latest migration log file, that has been created less than 2 minutes
+latestLog=$(find /app/logs/ -type f -mmin -2)
 
 # Spin up the swatchdog tool and make it watch the last line of the log
 SWATCHDOG_CONFIG="/app/config/swatchdog.conf"
-swatchdog -c $SWATCHDOG_CONFIG  --tail-args "-1 -f" -t /app/logs/$latestLog
+swatchdog -c $SWATCHDOG_CONFIG  --tail-args "-1 -f" -t $latestLog
