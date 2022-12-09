011858f3a0d9251719958a74875369f58b3a83df
Ken <kencho.gigascience@gmail.com>
2022-12-09 15:08:15 +0800

    Source proxy setting if in the backup server

--- gigadb/app/tools/wasabi-migration/send_notification.sh
+++ gigadb/app/tools/wasabi-migration/send_notification.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
-source /app/.secrets
+source "/app/.secrets"
 
 serverName=$(uname -a | cut -f2 -d' ')
+
+# If we're on the backup server then source proxy settings for sending message to the gitter room
+if [ "$HOST_HOSTNAME" == "cngb-gigadb-bak" ];
+then
+    serverName="$HOST_HOSTNAME"
+    source "/app/proxy_settings.sh" || exit 1
+fi
+
 date=$(date)
 
 curl -X POST -i -H "Content-Type: application/json" \
