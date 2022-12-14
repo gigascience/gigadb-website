81f9e8eb654fa965ce0c2ee79e108591f681ebe7
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-12-14 20:13:23 +0100

    Feat: adapt upload script to be operated by user other than centos

--- gigadb/app/tools/excel-spreadsheet-uploader/execute.sh
+++ gigadb/app/tools/excel-spreadsheet-uploader/execute.sh
@@ -4,7 +4,14 @@ PATH=/usr/local/bin:$PATH
 export PATH
 
 if [[ $(uname -n) =~ compute ]];then
-  . /home/centos/.bash_profile
+  uploadDir=${1:-"/home/centos/uploadDir"}
+  cd /home/centos
+  . .bash_profile
+
+  if [[ $uploadDir != "/home/centos/uploadDir" ]];then
+    mv $uploadDir/* /home/centos/uploadDir/
+  fi
+
   docker run --rm  --env-file ./db-env registry.gitlab.com/$GITLAB_PROJECT/production_pgclient:$GIGADB_ENV -c 'drop trigger if exists file_finder_trigger on file RESTRICT'
   docker run --rm  --env-file ./db-env registry.gitlab.com/$GITLAB_PROJECT/production_pgclient:$GIGADB_ENV -c 'drop trigger if exists sample_finder_trigger on sample RESTRICT'
   docker run --rm  --env-file ./db-env registry.gitlab.com/$GITLAB_PROJECT/production_pgclient:$GIGADB_ENV -c 'drop trigger if exists dataset_finder_trigger on dataset RESTRICT'
@@ -14,6 +21,12 @@ if [[ $(uname -n) =~ compute ]];then
   docker run --rm  --env-file ./db-env registry.gitlab.com/$GITLAB_PROJECT/production_pgclient:$GIGADB_ENV -c 'create trigger file_finder_trigger after insert or update or delete or truncate on file for each statement execute procedure refresh_file_finder()'
   docker run --rm  --env-file ./db-env registry.gitlab.com/$GITLAB_PROJECT/production_pgclient:$GIGADB_ENV -c 'create trigger sample_finder_trigger after insert or update or delete or truncate on sample for each statement execute procedure refresh_sample_finder()'
   docker run --rm  --env-file ./db-env registry.gitlab.com/$GITLAB_PROJECT/production_pgclient:$GIGADB_ENV -c 'create trigger dataset_finder_trigger after insert or update or delete or truncate on dataset for each statement execute procedure refresh_dataset_finder()'
+
+   if [[ $uploadDir != "/home/centos/uploadDir" ]];then
+      mv /home/centos/uploadLogs/* $uploadDir/
+      mv /home/centos/uploadDir/* $uploadDir/ || true
+  fi
+  echo "Done."
 else
   mkdir -p logs
   docker-compose run --rm pg_client -c 'drop trigger if exists file_finder_trigger on file RESTRICT'
