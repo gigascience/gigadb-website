1c6140b0211883e06aea54d64017682c31a5b58e
Rija Ménagé <11455+rija@users.noreply.github.com>
2022-12-15 19:35:41 +0100

    Fix: change reference to path as file has been moved

--- tests/_support/Steps/CuratorSteps.php
+++ tests/_support/Steps/CuratorSteps.php
@@ -123,7 +123,7 @@ class CuratorSteps extends \Codeception\Actor
      */
     public function thereIsNoUserWithEmail($email)
     {
-        $dbConfig = json_decode(file_get_contents(dirname(__FILE__) . '/../../protected/config/db.json'), true);
+        $dbConfig = json_decode(file_get_contents(dirname(__FILE__) . '/../../../protected/config/db.json'), true);
         shell_exec("psql -h {$dbConfig['host']} -U {$dbConfig['user']} -d {$dbConfig['user']} -c \"DELETE FROM gigadb_user WHERE email='$email'\"");
     }
 
