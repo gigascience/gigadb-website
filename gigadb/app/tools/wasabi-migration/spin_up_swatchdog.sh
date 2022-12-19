032159a70e29aaee22b702e9041d4e63a27d91b0
Ken <kencho.gigascience@gmail.com>
2022-12-19 15:44:04 +0800

    Create swatchdog service separately

--- gigadb/app/tools/wasabi-migration/spin_up_swatchdog.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Assign the name of the latest migration log file
-logFile="/app/logs/rclone_latest.log"
-
-# Spin up the swatchdog tool and make it watch the last line of the log
-SWATCHDOG_CONFIG="/app/config/swatchdog.conf"
-swatchdog -c $SWATCHDOG_CONFIG  --tail-args "-1 -f" -t $logFile
