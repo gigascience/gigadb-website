1364de14455dcf757ca5d47f8f22b961b5e0d37b
Ken <kencho.gigascience@gmail.com>
2022-12-22 16:57:29 +0800

    Update the wrapper script using docker-compose and only stop swatchdog at the end

--- gigadb/app/tools/wasabi-migration/start_backup_process.sh
+++ gigadb/app/tools/wasabi-migration/start_backup_process.sh
@@ -11,12 +11,10 @@ maxBatchSize=$3
 useLiveData="false"
 
 devBackupStatus=""
-if [ "$serverName" != "cngb-gigadb-bak" ];
-then
-  if [ "$(docker ps -a | grep -c "wasabi-migration_swatchdog")" -gt 0 ];
-  then
-    # stop and remove all existing swatchdog service
-    docker stop "$(docker ps -aq)" && docker rm "$(docker ps -aq)"
+if [ "$serverName" != "cngb-gigadb-bak" ]; then
+  if [ "$(docker-compose ps | grep "wasabi-migration_swatchdog" | grep -c "Up")" -gt 0 ]; then
+    # stop existing swatchdog service, if any
+    docker-compose stop swatchdog || true
   fi
 
   # spin up a new swatchdog service
@@ -28,20 +26,17 @@ then
 fi
 
 liveBackupStatus=""
-if [ "$serverName" == "cngb-gigadb-bak" ];
-then
-  if [ "$(docker ps -a | grep -c "wasabi-migration_swatchdog_cngb")" -gt 0 ];
-    then
-      # stop and remove all existing swatchdog service
-      docker stop "$(docker ps -aq)" && docker rm "$(docker ps -aq)"
+if [ "$serverName" == "cngb-gigadb-bak" ]; then
+  if [ "$(docker-compose ps | grep "wasabi-migration_swatchdog_cngb" | grep -c "Up")" -gt 0 ]; then
+      # stop existing swatchdog service, if any
+      docker-compose stop swatchdog || true
     fi
 
   # spin up a new swatchdog service
   docker-compose up -d swatchdog_cngb
 
   # start the backup process
-  if [ "$useLiveData" == "true" ];
-  then
+  if [ "$useLiveData" == "true" ]; then
     docker-compose run --rm rclone_cngb /app/rclone_copy.sh --starting-doi "$startingDoi" --ending-doi "$endingDoi" --max-batch-size "$maxBatchSize" --use-live-data
     liveBackupStatus=$?
   else
@@ -54,8 +49,10 @@ fi
 # make sure the error message is sent, if any
 sleep 5
 
-# house keeping step, stop and remove all existing running services
-if [ "$devBackupStatus" -eq 0 ] || [ "$liveBackupStatus" -eq 0 ];
-then
-  docker stop "$(docker ps -aq)" && docker rm "$(docker ps -aq)"
+# house keeping step, stop swatchdog service
+# if the status in not empty and the backup process success
+if [ -n "$devBackupStatus" ] && [ "$devBackupStatus" -eq 0 ]; then
+  docker-compose stop swatchdog
+elif [ -n "$liveBackupStatus" ] && [ "$liveBackupStatus" -eq 0 ]; then
+  docker-compose stop swatchdog_cngb
 fi
