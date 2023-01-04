813319bfb365d55e60d576379bb61cd81bfe591a
Ken <kencho.gigascience@gmail.com>
2023-01-04 16:26:25 +0800

    Implement go to new tab step

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -323,6 +323,14 @@ class AcceptanceTester extends \Codeception\Actor
     }
 
     /**
+     * @When I go to the new tab
+     */
+    public function iGoToTheNewTab()
+    {
+        $this->switchToNextTab();
+    }
+
+    /**
      * @Then I should see an input button :button
      */
     public function iShouldSeeAnInputButton($button)
