6d69dd06ee074a044588ef7abd9838cf4095ef93
Peter Li <peter@gigasciencejournal.com>
2023-01-06 15:46:25 +0800

    Use test configuration when running functional test

---
+++ gigadb/app/tools/readme-generator/config/test.php
@@ -0,0 +1,31 @@
+<?php
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/test_db.php';
+
+/**
+ * Application configuration shared by all test types
+ */
+return [
+    'id' => 'basic-tests',
+    'basePath' => dirname(__DIR__),
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'language' => 'en-US',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+];
--- gigadb/app/tools/readme-generator/tests/functional/ReadmeCest.php
+++ gigadb/app/tools/readme-generator/tests/functional/ReadmeCest.php
@@ -1,8 +1,5 @@
 <?php
 
-use Yii;
-use yii\console\ExitCode;
-
 class ReadmeCest {
     public function _before() {
     }
@@ -16,7 +13,7 @@ class ReadmeCest {
      * @param FunctionalTester $I
      */
     public function tryCreate(\FunctionalTester $I) {
-        $I->runShellCommand("/app/yii readme/create --doi 100142 --outdir=/home/curators");
+        $I->runShellCommand("/app/yii_test readme/create --doi 100142 --outdir=/home/curators");
         $I->seeInShellOutput("[DOI] 10.5524/100142");
         $I->runShellCommand("ls /home/curators");
         $I->seeInShellOutput("readme_100142.txt");
