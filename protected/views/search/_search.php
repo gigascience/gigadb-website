b48fe41e5e396f4f0b3cf00bc8ff5722b6df5dfb
Ken <kencho.gigascience@gmail.com>
2023-01-09 15:29:25 +0800

    Add todo tag and fix a bit the code alignment

--- protected/views/search/_search.php
+++ protected/views/search/_search.php
@@ -20,11 +20,10 @@
              'value'=>$model->keyword
         ));
           ?>
-            <span class="input-group-btn">
-       <button class="btn background-btn" type="submit">
-                                                <i class="fa fa-search"></i> Search again
-                                            </button>
-    <?
+        <span class="input-group-btn">
+            <button class="btn background-btn" type="submit"><i class="fa fa-search"></i> Search again</button>
+<!--TODO: Will re-implement the save search function in ticket #1168-->
+//    <?
 //        if(!Yii::app()->user->isGuest) {
 //    ?>
 <!--            <input type="button" id="save-search-criteria" class="btn background-btn" value="Save current search criteria"/>-->
