3a2fd64c89ee0131eaeb247963c4929c4c42c70c
Peter Li <peter@gigasciencejournal.com>
2023-01-19 14:17:22 +0800

    Send drill message in manual local migration tests

--- gigadb/app/tools/wasabi-migration/send_notification.sh
+++ gigadb/app/tools/wasabi-migration/send_notification.sh
@@ -7,13 +7,17 @@ serverName=$(uname -a | cut -f2 -d' ')
 # If we're on the backup server then source proxy settings for sending message to the gitter room
 if [ "$HOST_HOSTNAME" == "cngb-gigadb-bak" ];
 then
-    serverName="$HOST_HOSTNAME"
-    source "/app/proxy_settings.sh" || exit 1
+  serverName="$HOST_HOSTNAME"
+  source "/app/proxy_settings.sh" || exit 1
+    
+  curl -X POST -i -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $GITTER_API_TOKEN" "https://api.gitter.im/v1/rooms/$GITTER_IT_NOTIFICATION_ROOM_ID/chatMessages" \
+    -d '{"text":"Server ***'"$serverName"'*** : '"$1"'"}'
+else
+  # Rclone Docker container wants to send Gitter notification message
+  curl -X POST -i -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer $GITTER_API_TOKEN" "https://api.gitter.im/v1/rooms/$GITTER_IT_NOTIFICATION_ROOM_ID/chatMessages" \
+    -d '{"text":"(Drill) Server ***rclone.docker*** : '"$1"'"}'
 fi
-
-date=$(date)
-
-curl -X POST -i -H "Content-Type: application/json" \
-      -H "Accept: application/json" \
-      -H "Authorization: Bearer $GITTER_API_TOKEN" "https://api.gitter.im/v1/rooms/$GITTER_IT_NOTIFICATION_ROOM_ID/chatMessages" \
-      -d '{"text":"From server ***'"$serverName"'*** : '"$1"'"}'
