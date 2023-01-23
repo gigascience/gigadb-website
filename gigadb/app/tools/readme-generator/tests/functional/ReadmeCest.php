43d89049b9330c8316cdfdfc08a9bf7dce38f5b7
Peter Li <peter@gigasciencejournal.com>
2023-01-23 23:21:47 +0800

    Add teardown code

--- gigadb/app/tools/readme-generator/tests/functional/ReadmeCest.php
+++ gigadb/app/tools/readme-generator/tests/functional/ReadmeCest.php
@@ -3,6 +3,20 @@
 class ReadmeCest
 {
     /**
+     * Teardown code that is run after each test
+     * 
+     * Currently just removes the readme file for dataset DOI 100005.
+     * 
+     * @return void
+     */
+    public function _after()
+    {
+        if (file_exists("/home/curators/readme_100005.txt")) {
+            unlink("/home/curators/readme_100005.txt");
+        }
+    }
+
+    /**
      * Test actionCreate function in ReadmeController
      *
      * @param FunctionalTester $I
