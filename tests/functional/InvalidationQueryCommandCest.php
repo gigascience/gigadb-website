0298b96eb84b5b2c72e83c5ef85480d645a411a6
Ken <kencho.gigascience@gmail.com>
2023-02-10 17:20:17 +0800

    Test query to select the latest from 2 tables

--- tests/functional/InvalidationQueryCommandCest.php
+++ tests/functional/InvalidationQueryCommandCest.php
@@ -49,4 +49,31 @@ class InvalidationQueryCommandCest
         codecept_debug($output);
         $I->assertContains($date, $output);
     }
+
+    public function tryToGetTheLatestCreatedAtFromDatasetlogAndCreationDateFromCurationlog(FunctionalTester $I)
+    {
+        $date = date('Y-m-d H:i:s');
+        $I->haveInDatabase("dataset_log", [
+            "dataset_id" => 8,
+            "message" => "File Pygoscelis_adeliae.RepeatMasker.out.gz updated",
+            "created_at" => $date,
+            "model" => "File",
+            "model_id" => 17679,
+            "url" => "/adminFile/update/id/17679"
+        ]);
+
+        $I->haveInDatabase("curation_log", [
+            "dataset_id" => 8,
+            "creation_date" => $date,
+            "created_by" => "John Smith",
+            "last_modified_date" => $date,
+            "last_modified_by" => "John Smith",
+            "action" => "Approved",
+            "comments" => "None",
+        ]);
+
+        $output = shell_exec(" ./protected/yiic invalidationquery getlatestcreateusingqueryfrommainconfigfile");
+        codecept_debug($output);
+        $I->assertContains($date, $output);
+    }
 }
