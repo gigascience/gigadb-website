5f338fcafcd26ed9c6127c78a2da0a9a512e785a
Rija Ménagé <11455+rija@users.noreply.github.com>
2023-02-18 20:51:20 +0100

    fix: add try/catch around less compilation to catch and print errors

--- protected/commands/LessCompilerCommand.php
+++ protected/commands/LessCompilerCommand.php
@@ -3,7 +3,13 @@ class LessCompilerCommand extends CConsoleCommand {
   public function run($args) {
     $less = new lessc;
     $basePath = Yii::app()->basePath;
-    $less->compileFile($basePath."/../less/site.less", $basePath."/../css/site.css");
-    $less->compileFile($basePath."/../less/current.less", $basePath."/../css/current.css");
+      try {
+          echo "Compiling site.less files at $basePath".PHP_EOL;
+          $less->compileFile($basePath."/../less/site.less", $basePath."/../css/site.css");
+          echo "Compiling current.less files at $basePath".PHP_EOL;
+          $less->compileFile($basePath."/../less/current.less", $basePath."/../css/current.css");
+      } catch (exception $e) {
+          echo "fatal error: " . $e->getMessage();
+      }
   }
 }
