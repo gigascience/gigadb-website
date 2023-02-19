38fa8b1fb02c02283a47fdcc5599bb82d0a61adf
Rija Ménagé <11455+rija@users.noreply.github.com>
2023-02-19 10:23:49 +0100

    fix: change how to refer to path to less and css file in less compiler

--- protected/commands/LessCompilerCommand.php
+++ protected/commands/LessCompilerCommand.php
@@ -2,12 +2,12 @@
 class LessCompilerCommand extends CConsoleCommand {
   public function run($args) {
     $less = new lessc;
-    $basePath = Yii::app()->basePath;
+    $basePath = "/var/www/";
       try {
           echo "Compiling site.less files at $basePath".PHP_EOL;
-          $less->compileFile($basePath."/../less/site.less", $basePath."/../css/site.css");
+          $less->compileFile($basePath."less/site.less", $basePath."css/site.css");
           echo "Compiling current.less files at $basePath".PHP_EOL;
-          $less->compileFile($basePath."/../less/current.less", $basePath."/../css/current.css");
+          $less->compileFile($basePath."less/current.less", $basePath."css/current.css");
       } catch (exception $e) {
           echo "fatal error: " . $e->getMessage();
       }
