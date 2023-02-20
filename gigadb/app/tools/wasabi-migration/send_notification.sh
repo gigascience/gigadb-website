9946cdf12bd5fa69dcbdf47d5978137e38f82a23
Ken <kencho.gigascience@gmail.com>
2023-02-20 15:30:29 +0800

    Use matrix tokens

--- gigadb/app/tools/wasabi-migration/send_notification.sh
+++ gigadb/app/tools/wasabi-migration/send_notification.sh
@@ -3,20 +3,22 @@
 source "/app/.secrets"
 
 serverName=$(uname -a | cut -f2 -d' ')
+transactionId=`date +%s`
+url="/_matrix/client/r0/rooms/$MATRIX_IT_NOTIFICATION_ROOM_ID/send/m.room.message/$transactionId"
 
 # If on backup server then source proxy settings and send notification
 if [ "$HOST_HOSTNAME" == "cngb-gigadb-bak" ];
 then
   source "/app/proxy_settings.sh" || exit 1
 
-  curl -X POST -i -H "Content-Type: application/json" \
+  curl -X PUT -i -H "Content-Type: application/json" \
     -H "Accept: application/json" \
-    -H "Authorization: Bearer $GITTER_API_TOKEN" "https://api.gitter.im/v1/rooms/$GITTER_IT_NOTIFICATION_ROOM_ID/chatMessages" \
-    -d '{"text":"***CNGB BACKUP SERVER*** : '"$1"'"}'
+    -H "Authorization: Bearer $MATRIX_TOKEN" \
+    -d '{"body":"***CNGB BACKUP SERVER*** : '"$1"'", "msgtype":"m.text"}' "${MATRIX_HOMESERVER}${url}"
 else
   # Rclone Docker container wants to send Gitter notification message
-  curl -X POST -i -H "Content-Type: application/json" \
+  curl -X PUT -i -H "Content-Type: application/json" \
     -H "Accept: application/json" \
-    -H "Authorization: Bearer $GITTER_API_TOKEN" "https://api.gitter.im/v1/rooms/$GITTER_IT_NOTIFICATION_ROOM_ID/chatMessages" \
-    -d '{"text":"(Drill) ***rclone.docker*** : '"$1"'"}'
+    -H "Authorization: Bearer $MATRIX_TOKEN" \
+    -d '{"body":"(Drill) ***rclone.docker*** : '"$1"'", "msgtype":"m.text"}' "${MATRIX_HOMESERVER}${url}"
 fi
