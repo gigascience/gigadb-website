c4d0f88d16966708cb5bd9c5f103abb63e11a7ec
Peter Li <peter@gigasciencejournal.com>
2023-02-21 14:19:05 +0800

    Add create readme file step

--- gigadb/app/tools/excel-spreadsheet-uploader/postUpload.sh
+++ gigadb/app/tools/excel-spreadsheet-uploader/postUpload.sh
@@ -11,9 +11,11 @@ if [[ $(uname -n) =~ compute ]];then
   . /home/centos/.bash_profile
   docker run -e YII_PATH=/var/www/vendor/yiisoft/yii registry.gitlab.com/$GITLAB_PROJECT/production_app:$GIGADB_ENV ./protected/yiic files checkUrls --doi=$DOI
   docker run -e YII_PATH=/var/www/vendor/yiisoft/yii registry.gitlab.com/$GITLAB_PROJECT/production_app:$GIGADB_ENV ./protected/yiic files updateMD5FileAttributes --doi=$DOI
+  docker run -it registry.gitlab.com/$GITLAB_PROJECT/production_tool:$GIGADB_ENV /app/yii readme/create --doi "$DOI" --outdir /home/curators
 else
   mkdir -p logs
   docker-compose run --rm  test ./protected/yiic files checkUrls --doi=$DOI
   docker-compose run --rm  test ./protected/yiic files updateMD5FileAttributes --doi=$DOI
+  docker-compose run --rm  tool /app/yii readme/create --doi "$DOI"
 fi
 
