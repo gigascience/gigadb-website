07ba78493c8d8f536e5cd330828f8cc90d3d8c1a
Peter Li <peter@gigasciencejournal.com>
2023-02-28 12:00:24 +0800

    Revert changes made to postUpload.sh script

--- gigadb/app/tools/excel-spreadsheet-uploader/postUpload.sh
+++ gigadb/app/tools/excel-spreadsheet-uploader/postUpload.sh
@@ -11,11 +11,9 @@ if [[ $(uname -n) =~ compute ]];then
   . /home/centos/.bash_profile
   docker run -e YII_PATH=/var/www/vendor/yiisoft/yii registry.gitlab.com/$GITLAB_PROJECT/production_app:$GIGADB_ENV ./protected/yiic files checkUrls --doi=$DOI
   docker run -e YII_PATH=/var/www/vendor/yiisoft/yii registry.gitlab.com/$GITLAB_PROJECT/production_app:$GIGADB_ENV ./protected/yiic files updateMD5FileAttributes --doi=$DOI
-  docker run --rm -v /home/centos/readmeFiles:/app/readmeFiles registry.gitlab.com/$GITLAB_PROJECT/production_tool:$GIGADB_ENV /app/yii readme/create --doi "$DOI" --outdir /app/readmeFiles
 else
   mkdir -p logs
-  docker-compose run --rm test ./protected/yiic files checkUrls --doi=$DOI
-  docker-compose run --rm test ./protected/yiic files updateMD5FileAttributes --doi=$DOI
-  docker-compose run --rm tool /app/yii_test readme/create --doi "$DOI" --outdir /home/curators
+  docker-compose run --rm  test ./protected/yiic files checkUrls --doi=$DOI
+  docker-compose run --rm  test ./protected/yiic files updateMD5FileAttributes --doi=$DOI
 fi
 
