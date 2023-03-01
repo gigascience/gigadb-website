ea95152e235e85ea9e5049f50f7938427f327881
Rija Ménagé <rm.dev.git.commits@eml.cc>
2023-03-01 18:32:46 +0100

    fix: make tests access database via docker,use docker to run tests on CI

--- gigadb/app/tools/files-metadata-console/config/test_db.php
+++ gigadb/app/tools/files-metadata-console/config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'pgsql:host=localhost;dbname=gigadb;port=54321';
+$db['dsn'] = getenv("DOCKER_RUNNING") ? 'pgsql:host=database;dbname=gigadb;port=5432' : 'pgsql:host=localhost;dbname=gigadb;port=54321';
 
 return $db;
