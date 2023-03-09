010409915089be60491a30d189b1388bbaba0b76
Ken <kencho.gigascience@gmail.com>
2023-03-09 10:43:28 +0800

    feat: functional test for non exist attribute

---
+++ tests/functional/AdminSampleCest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ *
+ * generated with:
+ * docker-compose run --rm test ./vendor/codeception/codeception/codecept generate:cest functional AdminSampleCest
+ *
+ *run with:
+ * docker-compose run --rm test ./vendor/codeception/codeception/codecept run functional AdminSampleCest
+ */
+class AdminSampleCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        $I->amOnPage("/site/login");
+        $I->submitForm('form.form-horizontal', [
+            'LoginForm[username]' => 'admin@gigadb.org',
+            'LoginForm[password]' => 'gigadb']);
+        $I->canSee("Admin");
+    }
+
+    // tests
+    public function tryUpdateAttributeListWithOneNonExistAttribute(FunctionalTester $I)
+    {
+        $I->amOnPage("/adminSample/update/id/432");
+        $I->canSee("Update Sample 432");
+        $I->fillField("Sample[attributesList]", "lat_lon=\"38.0,114.4\",animal=\"tiger\"");
+        $I->click("Save");
+        $I->canSee("Please fix the following input errors:");
+        $I->canSee("Attribute name animal does not exist in the database and will not be saved if continue!");
+        $I->click("Save");
+        $I->canSee("lat_lon=\"38.0,114.4\"");
+        $I->canSeeInDatabase("sample_attribute", [
+            "sample_id" => "432",
+            "attribute_id" => "269",
+            "value" => "38.0,114.4"
+        ]);
+        $I->dontSeeInDatabase("sample_attribute", [
+            "sample_id" => "432",
+            "value" => "tiger"
+        ]);
+    }
+
+//    public function tryUpdateAttributeListWithTwoNonExistAttribute(FunctionalTester $I)
+//    {
+//        $I->amOnPage("/adminSample/update/id/432");
+//        $I->canSee("Update Sample 432");
+//        $I->fillField("Sample[attributesList]", "animal=\"tiger\",plant=\"rose\"");
+//        $I->click("Save");
+//        $I->canSee("lease fix the following input errors:");
+//        $I->canSee("Attribute name animal does not exist in the database and will not be saved if continue!");
+//        $I->canSee("Attribute name plant does not exist in the database and will not be saved if continue!");
+//        $I->click("Save");
+//        $I->dontSeeInDatabase("sample_attribute", [
+//            "sample_id" => "432",
+//            "value" => "tiger"
+//        ]);
+//        $I->dontSeeInDatabase("sample_attribute", [
+//            "sample_id" => "432",
+//            "value" => "rose"
+//        ]);
+//    }
+}
