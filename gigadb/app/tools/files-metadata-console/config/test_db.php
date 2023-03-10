cb39af83635466486acd82db0f1a0515acc69d54
Rija Ménagé <rm.dev.git.commits@eml.cc>
2023-03-10 18:21:15 +0100

    feat: add configuration script and template for DB config

--- gigadb/app/tools/files-metadata-console/config/test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = require __DIR__ . '/db.php';
-// test database! Important not to run tests on production or development databases
-$db['dsn'] = getenv("DOCKER_RUNNING") ? 'pgsql:host=database;dbname=gigadb;port=5432' : 'pgsql:host=localhost;dbname=gigadb;port=54321';
-
-return $db;
