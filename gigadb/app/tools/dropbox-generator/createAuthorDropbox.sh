bd973473b757279258970ba240afe75895481fc9
Peter Li <peter@gigasciencejournal.com>
2023-05-20 09:45:02 +0800

    Add dry run mode to script

--- gigadb/app/tools/dropbox-generator/createAuthorDropbox.sh
+++ gigadb/app/tools/dropbox-generator/createAuthorDropbox.sh
@@ -6,12 +6,25 @@ set -e
 PATH=/usr/local/bin:$PATH
 export PATH
 
+manuscriptid=''
+
+# Assume this script will execute in dry run mode
+dryrun=true
+apply=false
+
 # Parse command line parameters
 while [[ $# -gt 0 ]]; do
     case "$1" in
     --manuscript-id)
         manuscriptid=$2
         shift
+        ;;
+    --dry-run)
+        dryrun=true
+        ;;
+    --apply)
+        dryrun=false
+        apply=true
         ;;
     *)
         echo "Invalid option: $1"
@@ -21,38 +34,54 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-# Check manuscript identifier contains only lowercase letters
+# If running in dry mode then just echo Wasabi API commands
+if [[ "$dryrun" = "true" ]] ; then
+    echo "Running script in dry run mode..."
+    cmd='echo'
+elif [[ "$apply" = "true" ]] ; then
+    cmd=''
+fi
+
+# Check manuscript identifier
 if [[ "$manuscriptid" =~ [[:upper:]] ]]; then
-  echo -e "Manuscript identifier: ${manuscriptid} cannot contain upper case letters.\nExiting..."
-  exit 1
+    echo -e "Manuscript identifier: ${manuscriptid} cannot contain upper case letters.\nExiting..."
+    exit 1
+elif [[ -z "${manuscriptid}" ]] ; then
+    echo -e "Manuscript identifier has not been provided.\nExiting..."
+    exit 1
+elif [[ "${manuscriptid}" == *['!'@#\$%^\&*()_+]* ]] ; then
+    echo -e "Manuscript identifier should contain special characters.\nExiting..."
+    exit 1
 fi
 
-if [[ $(uname -n) =~ compute ]];then
-  . /home/centos/.bash_profile
-  docker run --rm -v /home/centos/dropboxGenerator:/app/dropboxGenerator registry.gitlab.com/$GITLAB_PROJECT/production_tool:$GIGADB_ENV /app/createAuthorDropbox.sh --manuscript-id "${manuscriptid}"
+# Create Wasabi user account for author based on manuscript identifier
+authorUsername="author-${manuscriptid}"
+$cmd docker-compose run --rm tool /app/yii wasabi-user/create --username "${authorUsername}" > /dev/null 2>&1
+echo "Created Wasabi user account: ${authorUsername}"
+
+# Create access key for author 
+$cmd docker-compose run --rm tool /app/yii wasabi-user/create-access-key --username "${authorUsername}" > "${authorUsername}.txt"
+echo "Saved author's access key and secret in ${authorUsername}.txt"
+
+# Create bucket using bucket name
+bucketName="bucket-${manuscriptid}"
+$cmd docker-compose run --rm tool /app/yii wasabi-bucket/create --bucketName "${bucketName}" > /dev/null 2>&1
+echo "Created bucket: ${bucketName} for user: ${authorUsername}"
+
+# Create user policy for author
+policyArn=$($cmd docker-compose run --rm tool /app/yii wasabi-policy/create-author-policy --username "${authorUsername}")
+if [[ "$dryrun" = "true" ]] ; then
+    echo "Created policy: policy-${authorUsername}"
 else
-  # Create Wasabi user account for author based on manuscript identifier
-  authorUsername="author-${manuscriptid}"
-  docker-compose run --rm tool /app/yii wasabi-user/create --username "${authorUsername}" > /dev/null 2>&1
-  echo "Created Wasabi user account: ${authorUsername}"
-  
-  # Create access key for author 
-  docker-compose run --rm tool /app/yii wasabi-user/create-access-key --username "${authorUsername}" > "${authorUsername}.txt"
-  echo "Saved author's access key and secret in ${authorUsername}.txt"
-
-  # Create bucket using bucket name
-  bucketName="bucket-${manuscriptid}"
-  docker-compose run --rm tool /app/yii wasabi-bucket/create --bucketName "${bucketName}" > /dev/null 2>&1
-  echo "Created bucket: ${bucketName} for user: ${authorUsername}"
-  
-  # Create user policy for author
-  policyArn=$(docker-compose run --rm tool /app/yii wasabi-policy/create-author-policy --username "${authorUsername}")
-  echo "Created policy: ${policyArn}"
-  
-  # Attach policy to user
-  docker-compose run --rm tool /app/yii wasabi-policy/attach-to-user --username "${authorUsername}" --policy-arn "${policyArn}" > /dev/null 2>&1
-  echo "Attached policy: ${policyArn} to user: ${authorUsername}"
-
-  echo "Successfully finished create author dropbox workflow"
-  exit 1
+    echo "Created policy: ${policyArn}"
 fi
+
+# Attach policy to user
+$cmd docker-compose run --rm tool /app/yii wasabi-policy/attach-to-user --username "${authorUsername}" --policy-arn "${policyArn}" > /dev/null 2>&1
+if [[ "$dryrun" = "true" ]] ; then
+  policyArn="policy-${authorUsername}"
+fi
+echo "Attached policy: ${policyArn} to user: ${authorUsername}"
+
+echo "Successfully finished create author dropbox workflow"
+exit 1
