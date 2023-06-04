a2b747a45b595d476a14669fa877cd8f798a4b4c
Peter Li <peter@gigasciencejournal.com>
2023-06-04 19:16:14 +0800

    Check mandatory options are provided in command

--- gigadb/app/tools/files-metadata-console/controllers/UpdateController.php
+++ gigadb/app/tools/files-metadata-console/controllers/UpdateController.php
@@ -26,6 +26,11 @@ final class UpdateController extends Controller
     public array $excludedDois = [];
 
     /**
+     * @var int $next get list of next $next pending datasets
+     */
+    public int $next = 0;
+
+    /**
      * Console command for updating files' size for the given dataset
      *
      * ./yii update/file-size --doi=100142
@@ -45,12 +50,24 @@ final class UpdateController extends Controller
     /**
      * Console command for updating the URL for all files in a dataset 
      *
-     * ./yii update/urls --prefix=https://s3.ap-northeast-1.wasabisys.com/gigadb-datasets/live --separator=pub --exclude-dois=[1000,234324,43534534]
+     * docker-compose run --rm files-metadata-console  ./yii update/urls --doi=100142 --next <batch size> --prefix=https://s3.ap-northeast-1.wasabisys.com/gigadb-datasets/live  --separator=/pub/ --exclude-dois=[1000,234324,43534534]
      *
      * @return int
      */
     public function actionUrls(): int
     {
+        //Managing input
+        $optDoi = $this->doi;
+        $optPrefix = $this->prefix;
+        $optSeparator = $this->separator;
+        $optNext = $this->next;
+
+        //Return usage unless mandatory options are passed
+        if (!($optDoi) || !($optPrefix) || !($optSeparator)) {
+            $this->stdout("\nUsage:\n\t./yii update/urls --doi <dataset doi> --prefix <URL prefix> --separator <substring to separate current URL> [--next <batch size>][--exclude-dois <comma separated list of dois>][--dryrun][--verbose]\n\n");
+            return ExitCode::USAGE;
+        }
+
         $webClient = new Client([ 'allow_redirects' => false ]);
         $us = new URLsService();
         $dfu = new DatasetFilesUpdater([
