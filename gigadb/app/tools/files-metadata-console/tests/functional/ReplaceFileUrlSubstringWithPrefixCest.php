34ab4593b9e729aeaf1d652b71cc15a84020531a
Peter Li <peter@gigasciencejournal.com>
2023-06-07 23:19:40 +0800

    Add Db configuration for codeception

--- gigadb/app/tools/files-metadata-console/tests/functional/ReplaceFileUrlSubstringWithPrefixCest.php
+++ gigadb/app/tools/files-metadata-console/tests/functional/ReplaceFileUrlSubstringWithPrefixCest.php
@@ -8,25 +8,13 @@ use FunctionalTester;
 class ReplaceFileUrlSubstringWithPrefixCest
 {
     /**
-     * Teardown code that is run after each test
-     *
-     * @return void
-     */
-    public function _after()
-    {
-        # pgdmp file is used to restore the test database to original state
-        # because of problems getting Db module to work in functional.suite.yml
-        shell_exec("pg_restore -c -h database -p 5432 -U gigadb -d gigadb_testdata --no-owner /gigadb/app/tools/files-metadata-console/sql/gigadb_testdata.pgdmp");
-    }
-
-    /**
      * @param FunctionalTester $I
      */
     public function tryReplaceFileUrlSubstringWithPrefix(\FunctionalTester $I): void
     {
         $I->runShellCommand("echo yes | ./yii_test update/urls --prefix=https://s3.ap-northeast-1.wasabisys.com/gigadb-datasets/live --separator=/pub/ --doi=100002 --next=3 --excluded=['100020', '100039'] --apply");
-//        $I->canSeeInShellOutput("Downloading production backup for $dateStamp");
-//        $I->canSeeInShellOutput("Restoring the backup for $dateStamp");
-//        $I->seeResultCodeIs(Exitcode::OK);
+        $I->canSeeInShellOutput('Number of file changes: 7 on dataset DOI 100002');
+        $I->canSeeInShellOutput('Number of file changes: 6 on dataset DOI 100003');
+        $I->canSeeInShellOutput('Number of file changes: 2 on dataset DOI 100004');
     }
 }
