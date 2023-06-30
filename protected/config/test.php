0d3410f1b0adb4a609eefbae6aca273a34f0c8c7
kencho <kencho.gigascience@gmail.com>
2023-06-30 11:35:32 +0800

    feat: make unit tests connect with gigadb_testdata db

--- protected/config/test.php
+++ protected/config/test.php
@@ -1,7 +1,7 @@
 <?php
 
-// load database config
-//$testdb = json_decode(file_get_contents(dirname(__FILE__).'/db_test.json'), true);
+//load database config
+$testdb = json_decode(file_get_contents(dirname(__FILE__).'/db_test.json'), true);
 
 // enable multibyte unicode aware string functions. Only needed for PHP < 5.6. Requires php-mbstring module.
 
@@ -14,9 +14,14 @@ return CMap::mergeArray(
                 'class'=>'system.test.CDbFixtureManager',
             ),
             // uncomment the following to provide test database connection
-//            'db'=>array(
-//                'connectionString'=>"pgsql:dbname={$testdb['database']};host={$testdb['host']}",
-//            ),
+            'db' => [
+                'class' => 'system.db.CDbConnection',
+                'connectionString' => "pgsql:dbname={$testdb['database']};host={$testdb['host']}",
+                'username' => $testdb['user'],
+                'password' => $testdb['password'],
+                'charset' => 'utf8',
+                'persistent' => true
+            ],
         ),
         'params'=>array(
             // For fetching dataset files from @pli888's web server
