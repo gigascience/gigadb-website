01ff4914a4f17777e3125f249b18c41acf31f0e6
Peter Li <peter@gigasciencejournal.com>
2023-07-18 18:39:00 +0800

    Remove echo yes | from shell commands
    The shell commands used in the functional tests do not prompt for
    user input so echo yes | is not required.

--- gigadb/app/tools/files-metadata-console/tests/functional/ReplaceFileUrlSubstringWithPrefixCest.php
+++ gigadb/app/tools/files-metadata-console/tests/functional/ReplaceFileUrlSubstringWithPrefixCest.php
@@ -26,7 +26,7 @@ class ReplaceFileUrlSubstringWithPrefixCest
         $I->seeInDatabase('file', ['name' => 'Pygoscelis_adeliae.gff.gz', 'location' => "ftp://climb.genomics.cn/pub/10.5524/100001_101000/100006/phylogeny_study_update/Pygoscelis_adeliae.gff.gz"]);
 
         # Run tool to update file URLs for dataset 100002
-        $I->runShellCommand("echo yes | ./yii_test update/urls --separator=/pub/ --next=3 --exclude='100003,100004'");
+        $I->runShellCommand("./yii_test update/urls --separator=/pub/ --next=3 --exclude='100003,100004'");
 
         # Check output
         $I->canSeeInShellOutput("\tTransforming ftp_site for dataset 100002...\nDONE");
@@ -86,7 +86,7 @@ class ReplaceFileUrlSubstringWithPrefixCest
         $I->seeInDatabase('file', ['id' => '88266', 'location' => "ftp://climb.genomics.cn/pub/10.5524/100001_101000/100020/readme.txt"]);
 
         # Run tool to update file URLs for dataset 100002
-        $I->runShellCommand("echo yes | ./yii_test update/urls --separator=/pub/ --next=3 --apply");
+        $I->runShellCommand("./yii_test update/urls --separator=/pub/ --next=3 --apply");
 
         # Check output
         $I->canSeeInShellOutput("\tTransforming ftp_site for dataset 100002...\nDONE");
@@ -105,7 +105,7 @@ class ReplaceFileUrlSubstringWithPrefixCest
         $I->seeInDatabase('file', ['id' => '88266', 'location' => 'https://s3.ap-northeast-1.wasabisys.com/gigadb-datasets/' . Yii::$app->params['DEPLOYMENT_ENV'] . '/pub/10.5524/100001_101000/100020/readme.txt']);
 
         # Run tool again with the same arguments as before
-        $I->runShellCommand("echo yes | ./yii_test update/urls --separator=/pub/ --next=3 --exclude='100003,100004' --apply");
+        $I->runShellCommand("./yii_test update/urls --separator=/pub/ --next=3 --exclude='100003,100004' --apply");
 
         # Check output
         $I->canSeeInShellOutput("\tTransforming ftp_site for dataset 100005...\nDONE");
@@ -120,7 +120,7 @@ class ReplaceFileUrlSubstringWithPrefixCest
     public function tryStopDoi(\FunctionalTester $I): void
     {
         # Run tool to update file URLs for dataset 100002
-        $I->runShellCommand("echo yes | ./yii_test update/urls --separator=/pub/ --next=3 --stop=100004 --apply");
+        $I->runShellCommand("./yii_test update/urls --separator=/pub/ --next=3 --stop=100004 --apply");
 
         # Check output
         $I->canSeeInShellOutput("\tTransforming ftp_site for dataset 100002...\nDONE");
