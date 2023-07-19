a1753ad72cd98b9004d558d29809bac5f331d89d
kencho <kencho.gigascience@gmail.com>
2023-07-19 08:50:37 +0800

    Revert "fix: replace unsupported argument in db module"
    This reverts commit 874f3558d7d957018b03a21558681c9006af93f4.

--- ops/infrastructure/modules/rds-instance/rds-instance.tf
+++ ops/infrastructure/modules/rds-instance/rds-instance.tf
@@ -30,7 +30,7 @@ module "db" {
 
   db_name                = var.gigadb_db_database
   username               = var.gigadb_db_user
-  manage_master_user_password = false
+  create_random_password = false
   password               = var.gigadb_db_password
   port                   = 5432
 
