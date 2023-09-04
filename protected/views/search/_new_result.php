97674d63427eb1583a10395f5683d75ff18c32e9
luis-toptal <luis.martinez@toptal.com>
2023-09-04 14:43:12 +0200

    feat(search page): accessible result types

--- protected/views/search/_new_result.php
+++ protected/views/search/_new_result.php
@@ -15,8 +15,10 @@
         <?php $is_display = in_array('dataset', $display) || (in_array('file', $display) && $dfiles) || (in_array('sample', $display) && $dsamples); ?>
         <?php if($is_display) { ?>
         <div class="row1">
-            <div class="span1" style="margin-left: 40px;margin-top: 10px;height: 30px;width: 40px"><div class="text-icon text-icon-sm text-icon-blue" style="margin-right: 0px;">G
-            </div></div>
+            <div class="span1" style="margin-left: 40px;margin-top: 10px;height: 30px;width: 40px">
+                <div class="text-icon text-icon-sm text-icon-blue" style="margin-right: 0px;" aria-hidden="true">G</div>
+                <span class="sr-only">Dataset</span>
+            </div>
             <div class="span8 main-content" style="float:right">
                 <div class="nav nav-tabs nav-stacked result-cell">
                   <h3 class="result-cell-item"><a data-content="<?php echo CHtml::encode($dt['description']) ?>" class="result-main-link left content-popup" href="<?php echo $dt['shorturl'] ?>"><?php echo $dt['title'] ?></a></h3>
@@ -35,8 +37,10 @@
             foreach($dsamples as $sample) { ?>
             <!--Sample section-->
             <div class="row1">
-                <div class="span1" style="margin-left: 40px;margin-top: 10px;height: 30px;width: 40px"><div class="text-icon text-icon-sm text-icon-green" style="margin-right: 0px;">S
-            </div></div>
+                <div class="span1" style="margin-left: 40px;margin-top: 10px;height: 30px;width: 40px">
+                    <div class="text-icon text-icon-sm text-icon-green" style="margin-right: 0px;" aria-hidden="true">S</div>
+                    <span class="sr-only">Sample</span>
+                </div>
                 <div class="span8 main-content" style="float:right">
                     <ul class="nav nav-tabs nav-stacked result-cell">
                       <li><a class="result-main-link" href="<?= $dt['shorturl'] ?>"><?php echo $sample['name'] ?></a></li>
@@ -58,8 +62,10 @@
             <?php if(in_array('file', $display)) {
                 foreach($dfiles as $file) { ?>
             <div class="row1 file-container">
-                 <div class="span1" style="margin-left: 40px;margin-top: 10px;height: 30px;width: 40px"><div class="text-icon text-icon-sm text-icon-yellow" style="margin-right: 0px;">F
-            </div></div>
+                <div class="span1" style="margin-left: 40px;margin-top: 10px;height: 30px;width: 40px">
+                    <div class="text-icon text-icon-sm text-icon-yellow" style="margin-right: 0px;" aria-hidden="true">F</div>
+                    <span class="sr-only">File</span>
+                </div>
                 <div class="span3 file-name"><a href="<?php echo $file['location'] ?>"><?php echo strlen($file['name']) > 20 ? substr($file['name'], 0, 20). '...' : $file['name'] ?></a></div>
                 <div class="span2 file-type"><?php echo $file['file_type'] ?></div>
                 <div class="span2 file-size"><?php echo CHtml::encode(File::specifySizeUnits($file['size'],null, 2))?></div>
