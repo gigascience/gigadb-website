753ec358901b767ecba9626f8e056f950a333e77
luis-toptal <luis.martinez@toptal.com>
2023-09-11 14:02:27 +0200

    feat: display result card only if there is something to display

--- protected/views/search/_new_result.php
+++ protected/views/search/_new_result.php
@@ -9,77 +9,80 @@
         };
         $dsamples = array_filter($samples['data'], $filterOnDatasetId);
         $dfiles = array_filter($files['data'], $filterOnDatasetId);
+        $has_dataset = in_array('dataset', $display);
+        $is_display = $has_dataset || (in_array('file', $display) && $dfiles) || (in_array('sample', $display) && $dsamples);
         ?>
-        <div class="search-result-card">
-            <?php $is_display = in_array('dataset', $display) || (in_array('file', $display) && $dfiles) || (in_array('sample', $display) && $dsamples); ?>
-            <?php if ($is_display) { ?>
-                <!--Dataset section-->
-                <div class="search-result-row">
-                    <div class="search-result-icon-container">
-                        <div class="text-icon text-icon-sm text-icon-blue search-result-icon" aria-hidden="true">G</div>
-                        <span class="sr-only">Dataset</span>
+        <?php if ($is_display) { ?>
+            <div class="search-result-card">
+                <?php if ($has_dataset) { ?>
+                    <!--Dataset section-->
+                    <div class="search-result-row">
+                        <div class="search-result-icon-container">
+                            <div class="text-icon text-icon-sm text-icon-blue search-result-icon" aria-hidden="true">G</div>
+                            <span class="sr-only">Dataset</span>
+                        </div>
+                        <div class="search-result-content">
+                            <h3 class="search-result-title">
+                                <a data-content="<?php echo CHtml::encode($dt['description']) ?>" class="search-result-link content-popup" href="<?php echo $dt['shorturl'] ?>"><?php echo $dt['title'] ?></a>
+                            </h3>
+                            <?php if (!empty($dt['authornames'])) : ?>
+                                <!-- NOTE This should be a list of author links (ul>li>a) but the model directly returns a string sequence of <a>, so it's a bit more complicated to change -->
+                                <div class="search-result-subcontent">
+                                    <?php echo $dt['authornames']; ?>
+                                </div>
+                            <?php endif; ?>
+                            <div><?= Yii::t('app', 'DOI') ?>:<?php echo "10.5524/" . $dt['identifier'] ?></div>
+                        </div>
                     </div>
-                    <div class="search-result-content">
-                        <h3 class="search-result-title">
-                            <a data-content="<?php echo CHtml::encode($dt['description']) ?>" class="search-result-link content-popup" href="<?php echo $dt['shorturl'] ?>"><?php echo $dt['title'] ?></a>
-                        </h3>
-                        <?php if (!empty($dt['authornames'])) : ?>
-                            <!-- NOTE This should be a list of author links (ul>li>a) but the model directly returns a string sequence of <a>, so it's a bit more complicated to change -->
-                            <div class="search-result-subcontent">
-                                <?php echo $dt['authornames']; ?>
-                            </div>
-                        <?php endif; ?>
-                        <div><?= Yii::t('app', 'DOI') ?>:<?php echo "10.5524/" . $dt['identifier'] ?></div>
-                    </div>
-                </div>
-            <?php } ?>
+                <?php } ?>
 
-            <?php if (in_array('sample', $display)) { ?>
-                <ul class="search-result-list">
-                    <?php foreach ($dsamples as $sample) { ?>
-                        <!--Sample section-->
-                        <li class="search-result-row">
-                            <div class="search-result-icon-container">
-                                <div class="text-icon text-icon-sm text-icon-green search-result-icon" aria-hidden="true">S</div>
-                                <span class="sr-only">Sample</span>
-                            </div>
-                            <div class="search-result-content">
-                                <h4 class="search-result-title">
-                                    <a class="search-result-link" href="<?= $dt['shorturl'] ?>"><?php echo $sample['name'] ?></a>
-                                </h4>
-                                <div class="search-result-subcontent">
-                                    <?= $sample['species_common_name'] ?>
-                                    NCBI taxonomy :
-                                    <a class="search-result-link" target="_blank" href="http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=<?php echo $sample['species_tax_id'] ?>">
-                                        <?= $sample['species_tax_id'] ?>
-                                    </a>
+                <?php if (in_array('sample', $display)) { ?>
+                    <ul class="search-result-list">
+                        <?php foreach ($dsamples as $sample) { ?>
+                            <!--Sample section-->
+                            <li class="search-result-row">
+                                <div class="search-result-icon-container">
+                                    <div class="text-icon text-icon-sm text-icon-green search-result-icon" aria-hidden="true">S</div>
+                                    <span class="sr-only">Sample</span>
                                 </div>
-                                <div class="search-result-subcontent">
-                                    <a class="search-result-link" href="<?php echo $dt['shorturl'] ?>">DOI:10.5524/<?= $dt['identifier'] ?></a>
+                                <div class="search-result-content">
+                                    <h4 class="search-result-title">
+                                        <a class="search-result-link" href="<?= $dt['shorturl'] ?>"><?php echo $sample['name'] ?></a>
+                                    </h4>
+                                    <div class="search-result-subcontent">
+                                        <?= $sample['species_common_name'] ?>
+                                        NCBI taxonomy :
+                                        <a class="search-result-link" target="_blank" href="http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=<?php echo $sample['species_tax_id'] ?>">
+                                            <?= $sample['species_tax_id'] ?>
+                                        </a>
+                                    </div>
+                                    <div class="search-result-subcontent">
+                                        <a class="search-result-link" href="<?php echo $dt['shorturl'] ?>">DOI:10.5524/<?= $dt['identifier'] ?></a>
+                                    </div>
                                 </div>
-                            </div>
-                        </li>
-                    <?php } ?>
-                </ul>
-            <?php } ?>
-            <!--Download File list-->
-            <?php if (in_array('file', $display)) { ?>
-                <ul class="search-result-list">
-                    <?php foreach ($dfiles as $file) { ?>
-                        <li class="search-result-row">
-                            <div class="search-result-icon-container">
-                                <div class="text-icon text-icon-sm text-icon-yellow search-result-icon" aria-hidden="true">F</div>
-                                <span class="sr-only">File</span>
-                            </div>
-                            <div class="search-result-content file-content row">
-                                <div class="col-xs-5 file-name truncate-text"><a class="file-link" href="<?php echo $file['location'] ?>"><?php echo $file['name'] ?></a></div>
-                                <div class="col-xs-3 file-type"><?php echo $file['file_type'] ?></div>
-                                <div class="col-xs-3 file-size"><?php echo CHtml::encode(File::specifySizeUnits($file['size'], null, 2)) ?></div>
-                            </div>
-                        </li>
-                    <?php } ?>
-                </ul>
-            <?php } ?>
-        </div>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                <?php } ?>
+                <!--Download File list-->
+                <?php if (in_array('file', $display)) { ?>
+                    <ul class="search-result-list">
+                        <?php foreach ($dfiles as $file) { ?>
+                            <li class="search-result-row">
+                                <div class="search-result-icon-container">
+                                    <div class="text-icon text-icon-sm text-icon-yellow search-result-icon" aria-hidden="true">F</div>
+                                    <span class="sr-only">File</span>
+                                </div>
+                                <div class="search-result-content file-content row">
+                                    <div class="col-xs-5 file-name truncate-text"><a class="file-link" href="<?php echo $file['location'] ?>"><?php echo $file['name'] ?></a></div>
+                                    <div class="col-xs-3 file-type"><?php echo $file['file_type'] ?></div>
+                                    <div class="col-xs-3 file-size"><?php echo CHtml::encode(File::specifySizeUnits($file['size'], null, 2)) ?></div>
+                                </div>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                <?php } ?>
+            </div>
+        <?php } ?>
     <?php } ?>
 </div>
