621e3394ddb6ab3c58509b4e5c3513eba7a6abd0
luis-toptal <luis.martinez@toptal.com>
2023-09-12 16:48:48 +0200

    feat(table settings form): improve look and accessibility of form
    Rearranged layout, resized fieldset legend and add visible focus state to checkbox

--- protected/views/dataset/_files_setting.php
+++ protected/views/dataset/_files_setting.php
@@ -2,103 +2,88 @@
 ?>
 
 <div id="files_settings" class="modal fade" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Table settings</h4>
-        </div>
-        <div class="modal-body">
-
-            <div class="divForForm">
-                <form id="fileSettingsForm" name="myFilesSettingform" method="POST">
-                <input type='hidden' name='setting[]' value="name"/>
-
-                    <div class="attribute-setting-item">
-                        <label for="selectPageSizeFilesSetting"><strong>Items per page:</strong></label>
-                        <select name="pageSize" class="selectPageSize" id="selectPageSizeFilesSetting">
-                        <?php $options = [5,10,20,50,100,200];
-                            foreach ($options as $option) {
-                        ?>
-                                <option value="<?= $option ?>" <?= $option == $pageSize ? "selected":"" ?> ><?= $option ?></option>
-                         <?php   } ?>
-                        </select>
-                    </div>
-
-                    <div class="attribute-setting-content">
-                        <fieldset class="row">
-                            <legend>Check columns to display</legend>
-                            <div class="span4">
-                                <label class="row" for="description">
-                                    <div class="span3"><?= Yii::t('app','File Description') ?></div>
-                                    <div class="span1"><input id="description" type="checkbox" name="setting[]" value="description"
-                                        <?= (in_array("description", $setting))? "checked" : ""?> />
-                                    </div>
-                                </label>
-                                <label class="row" for="sample_id">
-                                    <div class="span3"><?= Yii::t('app','Sample ID') ?></div>
-                                    <div class="span1"><input id="sample_id" type="checkbox" name="setting[]" value="sample_id"
-                                        <?= (in_array("sample_id", $setting))? "checked" : ""?> />
-                                    </div>
-                                </label>
-                                <label class="row" for="type_id">
-                                    <div class="span3"><?= Yii::t('app','Data Type') ?></div>
-                                    <div class="span1"><input id="type_id" type="checkbox" name="setting[]" value="type_id"
-                                    <?= (in_array("type_id", $setting))? "checked" : ""?> />
-                                    </div>
-                                </label>
-                                <label class="row" for="format_id">
-                                    <div class="span3"><?= Yii::t('app','File Format') ?></div>
-                                    <div class="span1"><input id="format_id" type="checkbox" name="setting[]" value="format_id"
-                                    <?= (in_array("format_id", $setting))? "checked" : ""?> />
-                                    </div>
-                                </label>
-                            </div>
-
-                            <div class="span4">
-                                <label class="row" for="size">
-                                    <div class="span3"><?= Yii::t('app','Size') ?></div>
-                                    <div class="span1"><input id="size" type="checkbox" name="setting[]" value="size"
-                                    <?= (in_array("size", $setting))? "checked" : ""?> />
-                                    </div>
-                                </label>
-                                <label class="row" for="date_stamp">
-                                    <div class="span3"><?= Yii::t('app','Release Date') ?></div>
-                                    <div class="span1"><input id="date_stamp" type="checkbox" name="setting[]" value="date_stamp"
-                                    <?= (in_array("date_stamp", $setting))? "checked" : ""?> />
-                                    </div>
-                                </label>
-                                <label class="row" for="location">
-                                    <div class="span3"><?= Yii::t('app','Download Link') ?></div>
-                                    <div class="span1"><input id="location" type="checkbox" name="setting[]" value="location"
-                                    <?= (in_array("location", $setting))? "checked" : ""?> />
-                                    </div>
-                                </label>
-                                <label class="row" for="attribute">
-                                    <div class="span3"><?= Yii::t('app','File Attributes') ?></div>
-                                    <div class="span1"><input id="attribute" type="checkbox" name="setting[]" value="attribute"
-                                    <?= (in_array("attribute", $setting))? "checked" : ""?> />
-                                    </div>
-                                </label>
-                            </div>
-                        </fieldset>
-                    </div>
-                </form>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Table settings</h4>
             </div>
+            <div class="modal-body">
 
-        </div> <!-- /.modal-body -->
-        <div class="modal-footer">
-            <a class="btn btn-default" data-dismiss="modal" href="#" title="Close">Close</a>
-            <a id="save-files-settings" class="btn btn-primary" href="#" title="Save changes">Save changes</a>
-        </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
+                <div class="divForForm">
+                    <form id="fileSettingsForm" name="myFilesSettingform" method="POST" class="table-settings-form">
+                        <input type='hidden' name='setting[]' value="name" />
+
+                        <div class="attribute-setting-item form-group">
+                            <label for="selectPageSizeFilesSetting"><strong>Items per page:</strong></label>
+                            <select name="pageSize" class="selectPageSize" id="selectPageSizeFilesSetting">
+                                <?php $options = [5, 10, 20, 50, 100, 200];
+                                foreach ($options as $option) {
+                                ?>
+                                    <option value="<?= $option ?>" <?= $option == $pageSize ? "selected" : "" ?>><?= $option ?></option>
+                                <?php   } ?>
+                            </select>
+                        </div>
+
+                        <div class="attribute-setting-content">
+                            <fieldset class="form-group row">
+                                <legend class="table-settings-form-label col-xs-12">Check columns to display</legend>
+
+                                <div class="col-xs-6">
+                                    <div class="checkbox">
+                                        <input id="description" type="checkbox" name="setting[]" value="description" <?= (in_array("description", $setting)) ? "checked" : "" ?> />
+                                        <label for="description"><?= Yii::t('app', 'File Description') ?></label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <input id="sample_id" type="checkbox" name="setting[]" value="sample_id" <?= (in_array("sample_id", $setting)) ? "checked" : "" ?> />
+                                        <label for="sample_id"><?= Yii::t('app', 'Sample ID') ?></label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <input id="type_id" type="checkbox" name="setting[]" value="type_id" <?= (in_array("type_id", $setting)) ? "checked" : "" ?> />
+                                        <label for="type_id"><?= Yii::t('app', 'Data Type') ?></label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <input id="format_id" type="checkbox" name="setting[]" value="format_id" <?= (in_array("format_id", $setting)) ? "checked" : "" ?> />
+                                        <label for="format_id"><?= Yii::t('app', 'File Format') ?></label>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6">
+                                    <div class="checkbox">
+                                        <input id="size" type="checkbox" name="setting[]" value="size" <?= (in_array("size", $setting)) ? "checked" : "" ?> />
+                                        <label for="size"><?= Yii::t('app', 'Size') ?></label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <input id="date_stamp" type="checkbox" name="setting[]" value="date_stamp" <?= (in_array("date_stamp", $setting)) ? "checked" : "" ?> />
+                                        <label for="date_stamp"><?= Yii::t('app', 'Release Date') ?></label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <input id="location" type="checkbox" name="setting[]" value="location" <?= (in_array("location", $setting)) ? "checked" : "" ?> />
+                                        <label for="location"><?= Yii::t('app', 'Download Link') ?></label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <input id="attribute" type="checkbox" name="setting[]" value="attribute" <?= (in_array("attribute", $setting)) ? "checked" : "" ?> />
+                                        <label for="attribute"><?= Yii::t('app', 'File Attributes') ?></label>
+                                    </div>
+                                </div>
+
+                            </fieldset>
+                        </div>
+                    </form>
+                </div>
+
+            </div> <!-- /.modal-body -->
+            <div class="modal-footer">
+                <a class="btn btn-default" data-dismiss="modal" href="#" title="Close">Close</a>
+                <a id="save-files-settings" class="btn btn-primary" href="#" title="Save changes">Save changes</a>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
 
 <script>
     document.addEventListener("DOMContentLoaded", function(event) { //This event is fired after deferred scripts are loaded
-        $("#save-files-settings").click(function(){
+        $("#save-files-settings").click(function() {
             $("#fileSettingsForm").submit();
             return false;
         });
--- protected/views/dataset/_sample_setting.php
+++ protected/views/dataset/_sample_setting.php
@@ -2,88 +2,70 @@
 ?>
 
 <div id="samples_settings" class="modal fade" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Table settings</h4>
-        </div>
-        <div class="modal-body">
-
-            <div class="divForForm">
-                <form id="sampleSettingsForm" name="mySamplesSettingform" method="POST">
-                    <input type='hidden' name='columns[]' value="name"/>
-
-                    <div class="attribute-setting-item">
-                        <label for="selectPageSizeSampleSetting"><strong>Items per page:</strong></label>
-                        <select name="samplePageSize" class="selectPageSize" id="selectPageSizeSampleSetting">
-                        <?php $options = [5,10,20,50,100,200];
-                            foreach ($options as $option) {
-                        ?>
-                                <option value="<?= $option ?>" <?= $option == $pageSize ? "selected":"" ?> ><?= $option ?></option>
-                         <?php   } ?>
-                        </select>
-                    </div>
-
-                    <div class="modal-body" style="padding: 30px;">
-
-                        <fieldset class="form form-inline" style="margin-bottom: 60px;">
-                                <legend>Check columns to display</legend>
-
-
-                                    <div class="checkbox">
-                                        <input id="common_name" type="checkbox" name="columns[]" value="common_name"
-                                        <?= (in_array("common_name", $columns))? "checked" : ""?>/><label for="common_name"><?= Yii::t('app','Common Name') ?></label>
-                                    </div>
-
-
-
-                                    <div class="checkbox">
-                                        <input id="scientific_name" type="checkbox" name="columns[]" value="scientific_name"
-                                        <?= (in_array("scientific_name", $columns))? "checked" : ""?>/><label for="scientific_name"><?= Yii::t('app','Scientific Name') ?></label>
-                                    </div>
-
-
-
-                                    <div class="checkbox">
-                                        <input id="sample_attribute" type="checkbox" name="columns[]" value="attribute"
-                                        <?= (in_array("attribute", $columns))? "checked" : ""?>/><label for="sample_attribute"><?= Yii::t('app','Sample Attributes') ?></label>
-                                    </div>
-
-
-
-                                    <div class="checkbox">
-                                        <input id="taxonomic_id" type="checkbox" name="columns[]" value="taxonomic_id"
-                                        <?= (in_array("taxonomic_id", $columns))? "checked" : ""?>/><label for="taxonomic_id"><?= Yii::t('app','Taxonomic ID') ?></label>
-                                    </div>
-
-
-                                    <div class="checkbox">
-                                        <input id="genbank_name" type="checkbox" name="columns[]" value="genbank_name"
-                                        <?= (in_array("genbank_name", $columns))? "checked" : ""?>/><label for="genbank_name"><?= Yii::t('app','Genbank Name') ?></label>
-                                    </div>
-
-                            </fieldset>
-
-
-                        </div>
-
-
-                </form>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Table settings</h4>
             </div>
+            <div class="modal-body">
+                <div class="divForForm">
+                    <form id="sampleSettingsForm" name="mySamplesSettingform" method="POST" class="table-settings-form">
+                        <input type='hidden' name='columns[]' value="name" />
+                        <div class="attribute-setting-item form-group">
+                            <label for="selectPageSizeSampleSetting">
+                                <strong>Items per page:</strong>
+                            </label>
+                            <select name="samplePageSize" class="selectPageSize" id="selectPageSizeSampleSetting">
+                                <?php $options = [5, 10, 20, 50, 100, 200];
+                                foreach ($options as $option) {
+                                ?>
+                                    <option value="<?= $option ?>" <?= $option == $pageSize ? "selected" : "" ?>>
+                                        <?= $option ?>
+                                    </option>
+                                <?php   } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <fieldset class="form-group">
+                                <legend class="table-settings-form-label">Columns to display</legend>
 
-        </div> <!-- /.modal-body -->
-        <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <a id="save-samples-settings" href="#" class="btn btn-primary">Save changes</a>
-        </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
+                                <div class="checkbox">
+                                    <input id="common_name" type="checkbox" name="columns[]" value="common_name" <?= (in_array("common_name", $columns)) ? "checked" : "" ?> /><label for="common_name"><?= Yii::t('app', 'Common Name') ?></label>
+                                </div>
+
+                                <div class="checkbox">
+                                    <input id="scientific_name" type="checkbox" name="columns[]" value="scientific_name" <?= (in_array("scientific_name", $columns)) ? "checked" : "" ?> /><label for="scientific_name"><?= Yii::t('app', 'Scientific Name') ?></label>
+                                </div>
+
+                                <div class="checkbox">
+                                    <input id="sample_attribute" type="checkbox" name="columns[]" value="attribute" <?= (in_array("attribute", $columns)) ? "checked" : "" ?> /><label for="sample_attribute"><?= Yii::t('app', 'Sample Attributes') ?></label>
+                                </div>
+
+                                <div class="checkbox">
+                                    <input id="taxonomic_id" type="checkbox" name="columns[]" value="taxonomic_id" <?= (in_array("taxonomic_id", $columns)) ? "checked" : "" ?> /><label for="taxonomic_id"><?= Yii::t('app', 'Taxonomic ID') ?></label>
+                                </div>
+
+                                <div class="checkbox">
+                                    <input id="genbank_name" type="checkbox" name="columns[]" value="genbank_name" <?= (in_array("genbank_name", $columns)) ? "checked" : "" ?> /><label for="genbank_name"><?= Yii::t('app', 'Genbank Name') ?></label>
+                                </div>
+                            </fieldset>
+                        </div>
+                    </form>
+                </div>
+
+            </div> <!-- /.modal-body -->
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <a id="save-samples-settings" href="#" class="btn btn-primary">Save changes</a>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
 
 <script>
     document.addEventListener("DOMContentLoaded", function(event) { //This event is fired after deferred scripts are loaded
-        $("#save-samples-settings").click(function(){
+        $("#save-samples-settings").click(function() {
             $("#sampleSettingsForm").submit();
             return false;
         });
