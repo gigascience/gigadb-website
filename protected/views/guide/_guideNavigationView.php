dcd8b276a45eda842a35c01eaa50b0de0fb9896f
luis-toptal <luis.martinez@toptal.com>
2023-09-25 16:48:16 +0200

    refactor(guide navigation): replace echoed php by html

--- protected/views/guide/_guideNavigationView.php
+++ protected/views/guide/_guideNavigationView.php
@@ -1,24 +1,19 @@
-<?php
-echo CHtml::openTag('nav', ['style' => 'margin-bottom: 5px;', 'aria-label' => 'Submission Guidelines']);
-echo CHtml::openTag('div', ['style' => 'display:inline-block;']);
-echo CHtml::openTag('ul', ['class' => 'nav nav-tabs nav-border-tabs', 'style' => 'margin-top: 1px; margin-bottom: 1px']);
-
-echo CHtml::tag('li', ['class' => $isActiveGeneral ? 'active' : ''], CHtml::link('General Submission Guidelines', '/site/guide'));
-
-echo CHtml::openTag('li', ['class' => 'dropdown' . (!$isActiveGeneral ? ' active' : '')]);
-echo CHtml::link('Datasets Checklists&nbsp;' . CHtml::tag('i', ['class' => 'fa fa-angle-down'], ''), '#', [
-    'class' => 'dropdown-toggle',
-    'aria-haspopup' => 'true',
-    'aria-expanded' => 'false',
-    'aria-label' => 'Dataset Checklists',
-    'data-toggle' => 'dropdown',
-]);
-
-echo CHtml::openTag('ul', ['class' => 'dropdown-menu', 'style' => 'margin-top: 5px;']);
-echo $menuHtml;
-echo CHtml::closeTag('ul');
-
-echo CHtml::closeTag('li');
-echo CHtml::closeTag('ul');
-echo CHtml::closeTag('div');
-echo CHtml::closeTag('nav');
+<nav style="margin-bottom: 5px;" aria-label="Submission Guidelines">
+    <div style="display:inline-block;">
+        <ul class="nav nav-tabs nav-border-tabs" style="margin-top: 1px; margin-bottom: 1px">
+            <li class="<?= $isActiveGeneral ? 'active' : '' ?>"><?= CHtml::link('General Submission Guidelines', '/site/guide') ?></li>
+            <li class="dropdown<?= !$isActiveGeneral ? ' active' : '' ?>">
+                <?= CHtml::link('Datasets Checklists&nbsp;' . CHtml::tag('i', ['class' => 'fa fa-angle-down'], ''), '#', [
+                    'class' => 'dropdown-toggle',
+                    'aria-haspopup' => 'true',
+                    'aria-expanded' => 'false',
+                    'aria-label' => 'Dataset Checklists',
+                    'data-toggle' => 'dropdown',
+                ]) ?>
+                <ul class="dropdown-menu" style="margin-top: 5px;">
+                    <?= $menuHtml ?>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
