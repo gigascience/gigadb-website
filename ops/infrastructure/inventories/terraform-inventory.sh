e45f18cc6ac7f601acf761b16071da67c46cd1f3
Rija Ménagé <rm.dev.git.commits@eml.cc>
2023-09-28 18:30:49 +0200

    fix: remove the Invalid characters ansible warning
    Parsed /Users/owner/code/gigadb-website/ops/infrastructure/inventories/hosts inventory source with ini plugin
    setting up inventory plugins
    host_list declined parsing /Users/owner/code/gigadb-website/ops/infrastructure/inventories/terraform-inventory.sh as it did not pass its verify_file() method
    Not replacing invalid character(s) "{'-'}" in group name (system_t3_micro-centos8)
    [WARNING]: Invalid characters were found in group names but not replaced, use -vvvv to see details

--- ops/infrastructure/inventories/terraform-inventory.sh
+++ ops/infrastructure/inventories/terraform-inventory.sh
@@ -3,4 +3,4 @@
 # bash shell wrapper for terraform-inventory.
 # How to install the https://github.com/adammck/terraform-inventory command:
 # brew install terraform-inventory
-terraform-inventory $@ ./
+terraform-inventory $@ ./ | jq 'with_entries(select([.key] | inside(["system_t3_micro-centos8"])|not))'  
