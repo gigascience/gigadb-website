a6745e4a125edaa7115256c44428f075631d52ab
luis-toptal <luis.martinez@toptal.com>
2023-10-03 15:32:38 +0200

    feat: accessible pagination labels

--- protected/views/search/new.php
+++ protected/views/search/new.php
@@ -3,28 +3,28 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-xs-10 col-xs-offset-1 text-center">
-                            <h1 class="home-search-bar-title">GIGADB DATASETS</h1>     
+                            <h1 class="home-search-bar-title">GIGADB DATASETS</h1>
                              <p class="home-search-bar-subtitle"></p>
                              <br>
                              <br>
                              <br>
-                             <? $this->renderPartial('_search', array('model' => $model))?>   
-                                
+                             <? $this->renderPartial('_search', array('model' => $model))?>
+
                         </div>
                     </div>
                 </div>
             </section>
             <div class="container">
                 <div class="row">
-                    
-                    <div class="col-xs-4 search-filter-sidebar">                  
+
+                    <div class="col-xs-4 search-filter-sidebar">
                         <h4 class="search-result-title">Search result for <span><i><?php echo $model->keyword ?></i></span></h4>
                           <p><?php $this->renderPartial('_range', array(
                                     'total_dataset'=>$datasets['total'],
                                     'page'=>$page,
                                     'limit'=>$limit
                               ));?> </p>
-                          
+
                            <div>
                                 <?php $this->renderPartial("_filter", array(
                                           'model' => $model,
@@ -32,7 +32,7 @@
                                 )) ?>
                             </div>
                              </div>
-                        
+
                     <div class="col-xs-8">
                         <div class="span9 result" id="result">
         <!--<span class='pull-right'><?= Yii::t('app', 'Selected all files') ?> <input type="checkbox" class="select-all"/></span> -->
@@ -45,17 +45,17 @@
                                  )) ?>
                          </div>
                         <div class="row1">
-                            <div class="span9 offset3" style="margin-left: 50px;height:72px;">
-                              <ul id="search-pg" class="pagination-sm" style="margin: auto;width: 100%"></ul>
-                            </div>
+                            <nav class="span9 offset3 nav-pagination" aria-label="Pagination">
+                                <ul id="search-pg" class="pagination-sm"></ul>
+                            </nav>
                         </div>
-                        
-                        
+
+
                     </div>
-             
+
             </div>
         </div>
-    
+
     <br>
     <br>
 
@@ -89,6 +89,7 @@ document.addEventListener("DOMContentLoaded", function(event) { //This event is 
 });
 </script>
 
+<!-- NOTE this type of pagination is used only here so there's no need to make it reusable -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twbs-pagination/1.4.2/jquery.twbsPagination.min.js" defer></script>
 <script>
 document.addEventListener("DOMContentLoaded", function(event) { //This event is fired after deferred scripts are loaded
@@ -97,14 +98,40 @@ document.addEventListener("DOMContentLoaded", function(event) { //This event is 
         visiblePages: 5,
         onPageClick: function (event, page) {
             url = document.URL;
-            $.post(url, {'page': page}, function(result) {                
+            $.post(url, {'page': page}, function(result) {
                 if(result.success) {
                     $('#filter').html(result.filter);
                     $('#result').html(result.result);
                     $('#range').html(result.range);
                 }
             }, 'json');
+
+            handlePaginationAccessibility()
+
+            function handlePaginationAccessibility() {
+                $('#search-pg li a').each(function(index, element) {
+                    const pageNum = $(element).text();
+                    const isCurrent = (pageNum === page.toString());
+
+
+                    $(element).attr('aria-label', getLabel());
+                    $(element).attr('aria-current', isCurrent ? 'page' : undefined);
+
+                    function getLabel() {
+                        const isPageNum = !!parseInt(pageNum);
+
+                        if (isCurrent) {
+                            return `Current Page, Page ${pageNum}`;
+                        } else if (isPageNum) {
+                            return `Go to Page ${pageNum}`;
+                        } else {
+                            return `Go to ${pageNum} Page`;
+                        }
+                    }
+                });
+            }
         }
     });
+
 });
 </script>
