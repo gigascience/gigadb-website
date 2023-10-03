172d5b3a398df83ebf565d572d43f3b51d371305
luis-toptal <luis.martinez@toptal.com>
2023-10-03 09:38:04 +0200

    feat: update table column widths for better layout

--- protected/views/adminDatasetAuthor/admin.php
+++ protected/views/adminDatasetAuthor/admin.php
@@ -16,12 +16,14 @@
 		'itemsCssClass'=>'table table-bordered',
 		'filter'=>$model,
 		'columns'=>array(
-			array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier' , 'sortable' => True ),
+			array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier' , 'sortable' => True, 'htmlOptions'=>array('width'=>'200')),
 			array('name'=> 'author_name_search', 'value'=>'$data->author->name'),
-			array('name'=> 'orcid_search', 'value'=>'$data->author->orcid'),
-			array('name'=> 'rank_search', 'value'=>'$data->rank'),
+			array('name'=> 'orcid_search', 'value'=>'$data->author->orcid', 'htmlOptions'=>array('width'=>'200')),
+			array('name'=> 'rank_search', 'value'=>'$data->rank', 'htmlOptions'=>array('width'=>'100')),
 			array(
+				'header'=>'Options',
 				'class'=>'CButtonColumn',
+				'htmlOptions'=>array('width'=>'75')
 			),
 		),
 	)); ?>
