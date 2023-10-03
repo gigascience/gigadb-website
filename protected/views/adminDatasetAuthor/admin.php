d02dc8bebc19e8ba42592d42c40b49189dcdda91
luis-toptal <luis.martinez@toptal.com>
2023-10-03 12:48:59 +0200

    feat: update author dataset table with custom pagination

--- protected/views/adminDatasetAuthor/admin.php
+++ protected/views/adminDatasetAuthor/admin.php
@@ -14,22 +14,22 @@
 		Column headers with links are sortable. Cells with a text input are used for filtering.
 	</div>
 
-	<?php $this->widget('zii.widgets.grid.CGridView', array(
+	<?php $this->widget('application.components.CustomGridView', array(
 		'id'=>'dataset-author-grid',
 		'dataProvider'=>$model->search(),
 		'itemsCssClass'=>'table table-bordered',
 		'filter'=>$model,
-		'columns'=>array(
-			array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier' , 'sortable' => True, 'htmlOptions'=>array('width'=>'200')),
-			array('name'=> 'author_name_search', 'value'=>'$data->author->name'),
-			array('name'=> 'orcid_search', 'value'=>'$data->author->orcid', 'htmlOptions'=>array('width'=>'200')),
-			array('name'=> 'rank_search', 'value'=>'$data->rank', 'htmlOptions'=>array('width'=>'100')),
-			array(
-				'header'=>'Actions',
-				'class'=>'CButtonColumn',
-				'htmlOptions'=>array('width'=>'75')
+			'columns'=>array(
+				array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier' , 'sortable' => True, 'htmlOptions'=>array('width'=>'200')),
+				array('name'=> 'author_name_search', 'value'=>'$data->author->name'),
+				array('name'=> 'orcid_search', 'value'=>'$data->author->orcid', 'htmlOptions'=>array('width'=>'200')),
+				array('name'=> 'rank_search', 'value'=>'$data->rank', 'htmlOptions'=>array('width'=>'100')),
+				array(
+					'header'=>'Actions',
+					'class'=>'CButtonColumn',
+					'htmlOptions'=>array('width'=>'75')
+				),
 			),
-		),
 	)); ?>
 
 </div>
