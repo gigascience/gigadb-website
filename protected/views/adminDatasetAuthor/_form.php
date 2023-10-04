3e372e377fa9758c4ae94d1938422415f54c794f
luis-toptal <luis.martinez@toptal.com>
2023-10-04 07:17:48 +0200

    feat: apply autoformatting to form file

--- protected/views/adminDatasetAuthor/_form.php
+++ protected/views/adminDatasetAuthor/_form.php
@@ -1,43 +1,43 @@
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'dataset-author-form',
-	'enableAjaxValidation'=>false,
-)); ?>
+	<?php $form = $this->beginWidget('CActiveForm', array(
+		'id' => 'dataset-author-form',
+		'enableAjaxValidation' => false,
+	)); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="control-group">
-		<?php echo $form->labelEx($model,'dataset_id',array('class'=>'control-label')); ?>
-				<div class="controls">
-        <?= CHtml::activeDropDownList($model,'dataset_id',CHtml::listData(Util::getDois(),'id','identifier')); ?>
-		<?php echo $form->error($model,'dataset_id'); ?>
-				</div>
+		<?php echo $form->labelEx($model, 'dataset_id', array('class' => 'control-label')); ?>
+		<div class="controls">
+			<?= CHtml::activeDropDownList($model, 'dataset_id', CHtml::listData(Util::getDois(), 'id', 'identifier')); ?>
+			<?php echo $form->error($model, 'dataset_id'); ?>
+		</div>
 	</div>
 
 	<div class="control-group">
-		<?php echo $form->labelEx($model,'author_id',array('class'=>'control-label')); ?>
-				<div class="controls">
-        <?= CHtml::activeDropDownList($model,'author_id',CHtml::listData(Author::model()->findAll(array('order' => 'surname') ),'id','fullAuthor')) ;?>
-		<?php echo $form->error($model,'author_id'); ?>
-				</div>
+		<?php echo $form->labelEx($model, 'author_id', array('class' => 'control-label')); ?>
+		<div class="controls">
+			<?= CHtml::activeDropDownList($model, 'author_id', CHtml::listData(Author::model()->findAll(array('order' => 'surname')), 'id', 'fullAuthor')); ?>
+			<?php echo $form->error($model, 'author_id'); ?>
+		</div>
 	</div>
 
-        <div class="control-group">
-          <?php echo $form->labelEx($model,'rank', array('class'=>'control-label')); ?>
-          <div class="controls">
-            <?= $form->textField($model,'rank') ?>
-            <?php echo $form->error($model,'author_id'); ?>
-          </div>
-        </div>
+	<div class="control-group">
+		<?php echo $form->labelEx($model, 'rank', array('class' => 'control-label')); ?>
+		<div class="controls">
+			<?= $form->textField($model, 'rank') ?>
+			<?php echo $form->error($model, 'author_id'); ?>
+		</div>
+	</div>
 
 	<div class="row buttons">
-        <a href="/adminDatasetAuthor/admin" class="btn">Cancel</a>
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save',array('class'=>'btn')); ?>
+		<a href="/adminDatasetAuthor/admin" class="btn background-btn-o">Cancel</a>
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
 	</div>
 
-<?php $this->endWidget(); ?>
+	<?php $this->endWidget(); ?>
 
 </div><!-- form -->
