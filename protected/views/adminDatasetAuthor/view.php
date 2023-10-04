27fabace94989e5658042a5a36e50583cfa7445e
luis-toptal <luis.martinez@toptal.com>
2023-10-04 15:47:55 +0200

    feat: use new layout

--- protected/views/adminDatasetAuthor/view.php
+++ protected/views/adminDatasetAuthor/view.php
@@ -1,25 +1,28 @@
-<?php
-$this->breadcrumbs=array(
-	'Dataset Authors'=>array('index'),
-	$model->id,
-);
+<div class="container">
+	<?php
+	$this->breadcrumbs = array(
+		'Dataset Authors' => array('index'),
+		$model->id,
+	);
 
-$this->menu=array(
-	array('label'=>'List DatasetAuthor', 'url'=>array('index')),
-	array('label'=>'Create DatasetAuthor', 'url'=>array('create')),
-	array('label'=>'Update DatasetAuthor', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete DatasetAuthor', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage DatasetAuthor', 'url'=>array('admin')),
-);
-?>
+	$this->menu = array(
+		array('label' => 'List DatasetAuthor', 'url' => array('index')),
+		array('label' => 'Create DatasetAuthor', 'url' => array('create')),
+		array('label' => 'Update DatasetAuthor', 'url' => array('update', 'id' => $model->id)),
+		array('label' => 'Delete DatasetAuthor', 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm' => 'Are you sure you want to delete this item?')),
+		array('label' => 'Manage DatasetAuthor', 'url' => array('admin')),
+	);
+	?>
 
-<h1>View DatasetAuthor #<?php echo $model->id; ?></h1>
+	<h1>View DatasetAuthor #<?php echo $model->id; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'dataset_id',
-		'author_id',
-	),
-)); ?>
+	<?php $this->widget('zii.widgets.CDetailView', array(
+		'data' => $model,
+		'attributes' => array(
+			'id',
+			'dataset_id',
+			'author_id',
+		),
+	)); ?>
+
+</div>
