1fa640f5bb4b7bc24515d32d14cafae8ccc2d9d3
luis-toptal <luis.martinez@toptal.com>
2023-10-04 07:11:49 +0200

    feat: use title breadcrumbs component

--- protected/views/adminDatasetAuthor/create.php
+++ protected/views/adminDatasetAuthor/create.php
@@ -9,9 +9,16 @@
 		array('label'=>'List DatasetAuthor', 'url'=>array('index')),
 		array('label'=>'Manage DatasetAuthor', 'url'=>array('admin')),
 	);
-	?>
 
-	<h1>Create DatasetAuthor</h1>
+	$this->widget('application.components.TitleBreadcrumb', [
+		'pageTitle' => 'Create DatasetAuthor',
+		'breadcrumbItems' => [
+			['label' => 'Dashboard', 'href' => '/site/admin'],
+			['label' => 'Dataset Authors', 'href' => '/adminDatasetAuthor/admin'],
+			['isActive' => true, 'label' => 'Create'],
+		]
+	]);
+	?>
 
 	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
 </div>
