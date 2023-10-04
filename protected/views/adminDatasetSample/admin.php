089ea2a52c9331b1e9bbe8e46d66385a6a8f38ba
luis-toptal <luis.martinez@toptal.com>
2023-10-04 12:02:42 +0200

    feat: wrap content in container

--- protected/views/adminDatasetSample/admin.php
+++ protected/views/adminDatasetSample/admin.php
@@ -1,34 +1,37 @@
-<h1>Manage Dataset - Samples</h1>
+<div class="container">
+	<h1>Manage Dataset - Samples</h1>
 
-<a href="/adminDatasetSample/create" class="btn">Add a Sample to a Dataset</a>
+	<a href="/adminDatasetSample/create" class="btn">Add a Sample to a Dataset</a>
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'dataset-sample-grid',
-	'ajaxUpdate' => false,
-	'dataProvider'=>$model->search(),
-	'itemsCssClass'=>'table table-bordered',
-	'filter'=>$model,
-	'columns'=>array(
-		array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier'),
-		'sample_id',
-		array('name'=> 'sample_name', 'value'=>'$data->sample->name'),
-		array('header'=> 'Sample Attributes','type'=>'raw' ,'value'=>'FormattedDatasetSamples::getDisplayAttr($data->sample->id,$data->sample->getSampleAttributeArrayMap())'),
-		array(
-			'class'=>'CButtonColumn',
+	<?php $this->widget('zii.widgets.grid.CGridView', array(
+		'id'=>'dataset-sample-grid',
+		'ajaxUpdate' => false,
+		'dataProvider'=>$model->search(),
+		'itemsCssClass'=>'table table-bordered',
+		'filter'=>$model,
+		'columns'=>array(
+			array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier'),
+			'sample_id',
+			array('name'=> 'sample_name', 'value'=>'$data->sample->name'),
+			array('header'=> 'Sample Attributes','type'=>'raw' ,'value'=>'FormattedDatasetSamples::getDisplayAttr($data->sample->id,$data->sample->getSampleAttributeArrayMap())'),
+			array(
+				'class'=>'CButtonColumn',
+			),
 		),
-	),
-)); ?>
+	)); ?>
 
-<?php
-$clientScript = Yii::app()->clientScript;
-$register_script = <<<EO_SCRIPT
-    jQuery(".js-desc").click(function(e) {
-        e.preventDefault();
-        id = $(this).attr('data');
-        jQuery(this).hide();
-        jQuery('.js-short-'+id).toggle();
-        jQuery('.js-long-'+id).toggle();
-    })
-EO_SCRIPT;
-$clientScript->registerScript('register_script', $register_script, CClientScript::POS_READY);
-?>
+	<?php
+	$clientScript = Yii::app()->clientScript;
+	$register_script = <<<EO_SCRIPT
+			jQuery(".js-desc").click(function(e) {
+					e.preventDefault();
+					id = $(this).attr('data');
+					jQuery(this).hide();
+					jQuery('.js-short-'+id).toggle();
+					jQuery('.js-long-'+id).toggle();
+			})
+	EO_SCRIPT;
+	$clientScript->registerScript('register_script', $register_script, CClientScript::POS_READY);
+	?>
+
+</div>
