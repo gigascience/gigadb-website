ea855d40731f4f7c7b17847f2ef9274dd637ae8c
luis-toptal <luis.martinez@toptal.com>
2023-10-06 16:50:54 +0200

    chore: undo autoformatting
    so that PR is more readable

--- protected/views/user/admin.php
+++ protected/views/user/admin.php
@@ -88,67 +88,67 @@ $this->endWidget('zii.widgets.jui.CJuiDialog');
 )); ?>
 
 <script>
-  function open_controls(user_id) {
+function open_controls(user_id) {
     $("#controls").data('user_id', user_id);
-    $("#controls").dialog("option", "title", "Manage User Id: " + user_id);
-    $("#controls").dialog("open");
+    $("#controls").dialog( "option", "title", "Manage User Id: " + user_id);
+    $("#controls").dialog("open"); 
     return false;
-  }
+}
 
-  function goto_userview() {
-    <?
-    echo 'var userview_url = "' . Yii::app()->urlManager->createUrl('user/view', array('id' => '')) . '";'
-    ?>
-    var user_id = $("#controls").data('user_id');
-    window.location = userview_url + "/" + user_id;
-  }
+function goto_userview() {
+<?
+  echo 'var userview_url = "'.Yii::app()->urlManager->createUrl('user/view',array('id'=>'')).'";'
+?>
+  var user_id = $("#controls").data('user_id');
+  window.location= userview_url + "/" + user_id; 
+}
 
-  function goto_userview() {
-    <?
-    echo 'var base_url = "' . Yii::app()->urlManager->createUrl('user/view', array('id' => '')) . '";'
-    ?>
-    var user_id = $("#controls").data('user_id');
-    window.location = userview_url + "/" + user_id;
-  }
+function goto_userview() {
+<?
+  echo 'var base_url = "'.Yii::app()->urlManager->createUrl('user/view',array('id'=>'')).'";'
+?>
+  var user_id = $("#controls").data('user_id');
+  window.location= userview_url + "/" + user_id; 
+}
 
-  function goto_userview() {
-    <?
-    echo 'var base_url = "' . Yii::app()->urlManager->createUrl('user/view', array('id' => '')) . '";'
-    ?>
-    var user_id = $("#controls").data('user_id');
-    window.location = base_url + "/" + user_id;
-  }
+function goto_userview() {
+<?
+  echo 'var base_url = "'.Yii::app()->urlManager->createUrl('user/view',array('id'=>'')).'";'
+?>
+  var user_id = $("#controls").data('user_id');
+  window.location= base_url + "/" + user_id; 
+}
 
-  function goto_userupdate() {
-    <?
-    echo 'var base_url = "' . Yii::app()->urlManager->createUrl('user/update', array('id' => '')) . '";'
-    ?>
-    var user_id = $("#controls").data('user_id');
-    window.location = base_url + "/" + user_id;
-  }
+function goto_userupdate() {
+<?
+  echo 'var base_url = "'.Yii::app()->urlManager->createUrl('user/update',array('id'=>'')).'";'
+?>
+  var user_id = $("#controls").data('user_id');
+  window.location= base_url + "/" + user_id; 
+}
 
-  function goto_userdelete() {
-    <?
-    echo 'var base_url = "' . Yii::app()->urlManager->createUrl('user/delete', array('id' => '')) . '";'
-    ?>
-    var user_id = $("#controls").data('user_id');
-    //window.location= base_url + "/" + user_id;
-    $.ajax({
-      url: base_url + "/" + user_id,
-      type: 'POST',
-      success: function(data) {
+function goto_userdelete() {
+<?
+  echo 'var base_url = "'.Yii::app()->urlManager->createUrl('user/delete',array('id'=>'')).'";'
+?>
+  var user_id = $("#controls").data('user_id');
+  //window.location= base_url + "/" + user_id; 
+  $.ajax({
+    url:base_url + "/" + user_id,
+    type:'POST',
+    success:function(data){
         $("#status").addClass("alert alert-success")
           .append("user successfully deactivated.")
           .append("Refresh the page to see the changes.");
-      },
-    });
-  }
+    },
+  });
+}
 
-  function goto_userlinkauthor() {
-    <?
-    echo 'var base_url = "' . Yii::app()->urlManager->createUrl('adminAuthor/prepareUserLink', array('user_id' => '')) . '";'
-    ?>
-    var user_id = $("#controls").data('user_id');
-    window.location = base_url + "/" + user_id;
-  }
+function goto_userlinkauthor() {
+<?
+  echo 'var base_url = "'.Yii::app()->urlManager->createUrl('adminAuthor/prepareUserLink',array('user_id'=>'')).'";'
+?>
+  var user_id = $("#controls").data('user_id');
+  window.location= base_url + "/" + user_id; 
+}
 </script>
