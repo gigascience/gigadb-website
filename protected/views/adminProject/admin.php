a73c41411669526d2904a3dde1e6bf54ff5def43
luis-toptal <luis.martinez@toptal.com>
2023-10-13 15:17:58 +0200

    feat: update styles of adminProject/admin and add default action buttons to customgridview

--- protected/views/adminProject/admin.php
+++ protected/views/adminProject/admin.php
@@ -1,19 +1,31 @@
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'Manage Files',
+		'breadcrumbItems' => [
+			['label' => 'Datasets', 'href' => '/site/admin'],
+			['isActive' => true, 'label' => 'Manage Projects'],
+		]
+	]);
+	?>
+	<div class="mb-10">
+		<a href="/adminProject/create" class="btn background-btn">Create New Project</a>
+	</div>
+	<p>
+		Column headers with links are sortable. Cells with a text input are used for filtering.
+	</p>
 
-<h1>Manage Projects</h1>
-
-<a href="/adminProject/create" class="btn">Create New Project</a>
-
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'project-grid',
-	'dataProvider'=>$model->search(),
-	'itemsCssClass'=>'table table-bordered',
-	'filter'=>$model,
-	'columns'=>array(
-		'url',
-		'name',
-		'image_location',
-		array(
-			'class'=>'CButtonColumn',
+	<?php $this->widget('CustomGridView', array(
+		'id' => 'project-grid',
+		'dataProvider' => $model->search(),
+		'itemsCssClass' => 'table table-bordered',
+		'filter' => $model,
+		'columns' => array(
+			'url',
+			'name',
+			'image_location',
+			CustomGridView::getDefaultActionButtonsConfig()
 		),
-	),
-)); ?>
+	)); ?>
+
+</div>
