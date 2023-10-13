9762498bd453ad3311de8866288252b302c57a06
luis-toptal <luis.martinez@toptal.com>
2023-10-13 13:18:28 +0200

    feat: update adminFile/admin page

--- protected/views/adminFile/admin.php
+++ protected/views/adminFile/admin.php
@@ -1,31 +1,80 @@
+<div class="container">
+	<?php
+	$this->widget('application.components.TitleBreadcrumb', [
+		'pageTitle' => 'Manage Files',
+		'breadcrumbItems' => [
+			['label' => 'Datasets', 'href' => '/site/admin'],
+			['isActive' => true, 'label' => 'Manage'],
+		]
+	]);
+	?>
 
-<h1>Manage Files</h1>
+	<div class="mb-10">
+		<a href="/adminFile/create" class="btn background-btn">Create New File</a>&nbsp;&nbsp;<a href="/adminFile/linkFolder" class="btn background-btn">Link Temp File Folder</a>
+	</div>
+	<p>
+		Column headers with links are sortable. Cells with a text input are used for filtering.
+	</p>
 
-<a href="/adminFile/create" class="btn">Create New File</a>&nbsp;&nbsp;<a href="/adminFile/linkFolder" class="btn">Link Temp File Folder</a>
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'file-grid',
-	'dataProvider'=>$model->search(),
-	'itemsCssClass'=>'table table-bordered',
-	'filter'=>$model,
-	'columns'=>array(
-		array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier'),
-		'code',
-		array(
-            'name' => 'name',
-            'value'=>'$data->name',
-            'htmlOptions' => array('style' => 'width:20px;'),
-        ),
-		//'location',
-		//'extension',
+	<?php $this->widget('CustomGridView', array(
+		'id' => 'file-grid',
+		'dataProvider' => $model->search(),
+		'itemsCssClass' => 'table table-bordered',
+		'filter' => $model,
+		'columns' => array(
+			array('name' => 'doi_search', 'value' => '$data->dataset->identifier'),
+			'code',
+			array(
+				'name' => 'name',
+				'value' => '$data->name',
+				'htmlOptions' => array('style' => 'width:20px;'),
+			),
+			//'location',
+			//'extension',
 
-		'date_stamp',
-		array('name'=> 'format_search', 'value'=>'$data->format->name'),
-		array('name'=> 'type_search', 'value'=>'$data->type->name'),
-		array('name'=> 'download_count', 'value'=>'$data->download_count'),
+			'date_stamp',
+			array('name' => 'format_search', 'value' => '$data->format->name'),
+			array('name' => 'type_search', 'value' => '$data->type->name'),
+			array('name' => 'download_count', 'value' => '$data->download_count', 'headerHtmlOptions' => array('style' => 'width: 70px')),
 
-		array(
-			'class'=>'CButtonColumn',
+			array(
+				'class' => 'CButtonColumn',
+				'header' => "Actions",
+				'headerHtmlOptions' => array('style' => 'width: 120px'),
+				'template' => '{view}{update}{delete}',
+				'buttons' => array(
+					'view' => array(
+						'imageUrl' => false,
+						'label' => '',
+						'options' => array(
+							"title" => "View",
+							"class" => "fa fa-eye fa-lg icon icon-view",
+							"aria-label" => "View"
+						),
+					),
+					'update' => array(
+						'imageUrl' => false,
+						'label' => '',
+						'options' => array(
+							"title" => "Update",
+							"class" => "fa fa-pencil fa-lg icon icon-update",
+							"aria-label" => "Update"
+						),
+					),
+
+					'delete' => array(
+						'imageUrl' => false,
+						'label' => '',
+						'options' => array(
+							"title" => "Delete",
+							"class" => "fa fa-trash fa-lg icon icon-delete",
+							"aria-label" => "Delete"
+						),
+					),
+				),
+			),
 		),
-	),
-)); ?>
+	)); ?>
+
+</div>
