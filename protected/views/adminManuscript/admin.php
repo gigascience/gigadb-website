327002a84b47e54718a81b8f27c6ec838477e42c
luis-toptal <luis.martinez@toptal.com>
2023-10-13 13:50:17 +0200

    feat: update adminManuscript/admin styles

--- protected/views/adminManuscript/admin.php
+++ protected/views/adminManuscript/admin.php
@@ -1,19 +1,66 @@
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'Manage Files',
+		'breadcrumbItems' => [
+			['label' => 'Datasets', 'href' => '/site/admin'],
+			['isActive' => true, 'label' => 'Manage Manuscripts'],
+		]
+	]);
+	?>
 
-<h1>Manage Manuscripts</h1>
+	<div class="mb-10">
+		<a href="/adminManuscript/create" class="btn background-btn">Create A New Manuscript</a>
+	</div>
+	<p>
+		Column headers with links are sortable. Cells with a text input are used for filtering.
+	</p>
 
-<a href="/adminManuscript/create" class="btn">Create A New Manuscript</a>
-
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'manuscript-grid',
-	'dataProvider'=>$model->search(),
-	'itemsCssClass'=>'table table-bordered',
-	'filter'=>$model,
-	'columns'=>array(
-		'identifier',
-		'pmid',
-		array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier'),
-		array(
-			'class'=>'CButtonColumn',
+	<?php $this->widget('CustomGridView', array(
+		'id' => 'manuscript-grid',
+		'dataProvider' => $model->search(),
+		'itemsCssClass' => 'table table-bordered',
+		'filter' => $model,
+		'columns' => array(
+			'identifier',
+			'pmid',
+			array('name' => 'doi_search', 'value' => '$data->dataset->identifier', 'headerHtmlOptions' => array('style' => 'width: 120px')),
+			array(
+				'class' => 'CButtonColumn',
+				'header' => "Actions",
+				'headerHtmlOptions' => array('style' => 'width: 120px'),
+				'template' => '{view}{update}{delete}',
+				'buttons' => array(
+					'view' => array(
+						'imageUrl' => false,
+						'label' => '',
+						'options' => array(
+							"title" => "View",
+							"class" => "fa fa-eye fa-lg icon icon-view",
+							"aria-label" => "View"
+						),
+					),
+					'update' => array(
+						'imageUrl' => false,
+						'label' => '',
+						'options' => array(
+							"title" => "Update",
+							"class" => "fa fa-pencil fa-lg icon icon-update",
+							"aria-label" => "Update"
+						),
+					),
+					'delete' => array(
+						'imageUrl' => false,
+						'label' => '',
+						'options' => array(
+							"title" => "Delete",
+							"class" => "fa fa-trash fa-lg icon icon-delete",
+							"aria-label" => "Delete"
+						),
+					),
+				),
+			),
 		),
-	),
-)); ?>
+	)); ?>
+
+</div>
