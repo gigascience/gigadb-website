532a3d94d50849c69cead5eaacc8215eb83f6f3d
luis-toptal <luis.martinez@toptal.com>
2023-10-13 15:21:12 +0200

    feat: update adminExternalLink/admin styles

--- protected/views/adminExternalLink/admin.php
+++ protected/views/adminExternalLink/admin.php
@@ -1,19 +1,32 @@
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'Manage External Links',
+		'breadcrumbItems' => [
+			['label' => 'Datasets', 'href' => '/site/admin'],
+			['isActive' => true, 'label' => 'Manage External Links'],
+		]
+	]);
+	?>
 
-<h1>Manage External Links</h1>
+	<div class="mb-10">
+		<a href="/adminExternalLink/create" class="btn background-btn">Create New External Link</a>
+	</div>
+	<p>
+		Column headers with links are sortable. Cells with a text input are used for filtering.
+	</p>
 
-<a href="/adminExternalLink/create" class="btn">Create New External Link</a>
-
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'external-link-grid',
-	'dataProvider'=>$model->search(),
-	'itemsCssClass'=>'table table-bordered',
-	'filter'=>$model,
-	'columns'=>array(
-		array('name'=> 'doi_search', 'value'=>'$data->dataset->identifier'),
-		array('name'=> 'external_link_type_search', 'value'=>'$data->external_link_type->name'),
-		'url',
-		array(
-			'class'=>'CButtonColumn',
+	<?php $this->widget('CustomGridView', array(
+		'id' => 'external-link-grid',
+		'dataProvider' => $model->search(),
+		'itemsCssClass' => 'table table-bordered',
+		'filter' => $model,
+		'columns' => array(
+			array('name' => 'doi_search', 'value' => '$data->dataset->identifier', 'headerHtmlOptions' => array('style' => 'width: 120px')),
+			array('name' => 'external_link_type_search', 'value' => '$data->external_link_type->name'),
+			'url',
+			CustomGridView::getDefaultActionButtonsConfig()
 		),
-	),
-)); ?>
+	)); ?>
+
+</div>
