ca2b107c0d95b7370464b1f5d1fbcb84391f0205
luis-toptal <luis.martinez@toptal.com>
2023-10-16 12:58:33 +0200

    chore: textField full example usage

--- protected/components/controls/TextField.php
+++ protected/components/controls/TextField.php
@@ -1,4 +1,23 @@
 <?php
+
+/**
+ * Example usage:
+ *
+ * $this->widget('application.components.controls.TextField', array(
+ *  'form' => $form,
+ *  'model' => $model,
+ *  'attributeName' => 'username',
+ *  // Optional attributes
+ *  'description' => 'Enter your username.',
+ *  'inputOptions' => array(
+ *      'placeholder' => 'Username',
+ *  ),
+ *  'labelOptions' => array(
+ *      'class' => 'custom-label-class', // Additional class
+ *  ),
+ * ));
+ */
+
 Yii::import('application.components.controls.BaseInput');
 
 class TextField extends BaseInput
