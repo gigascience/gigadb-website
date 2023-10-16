642303069002af7356cfd7168a43fccddbff98bf
luis-toptal <luis.martinez@toptal.com>
2023-10-16 11:58:45 +0200

    feat: update dataset author breadcrumbs for consistency

--- protected/views/adminDatasetAuthor/create.php
+++ protected/views/adminDatasetAuthor/create.php
@@ -1,24 +1,24 @@
 <div class="container">
 	<?php
-	$this->breadcrumbs=array(
-		'Dataset Authors'=>array('index'),
+	$this->breadcrumbs = array(
+		'Dataset Authors' => array('index'),
 		'Create',
 	);
 
-	$this->menu=array(
-		array('label'=>'List DatasetAuthor', 'url'=>array('index')),
-		array('label'=>'Manage DatasetAuthor', 'url'=>array('admin')),
+	$this->menu = array(
+		array('label' => 'List DatasetAuthor', 'url' => array('index')),
+		array('label' => 'Manage DatasetAuthor', 'url' => array('admin')),
 	);
 
 	$this->widget('application.components.TitleBreadcrumb', [
 		'pageTitle' => 'Create DatasetAuthor',
 		'breadcrumbItems' => [
-			['label' => 'Dashboard', 'href' => '/site/admin'],
-			['label' => 'Dataset Authors', 'href' => '/adminDatasetAuthor/admin'],
+			['label' => 'Admin', 'href' => '/site/admin'],
+			['label' => 'Manage', 'href' => '/adminDatasetAuthor/admin'],
 			['isActive' => true, 'label' => 'Create'],
 		]
 	]);
 	?>
 
-	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+	<?php echo $this->renderPartial('_form', array('model' => $model)); ?>
 </div>
