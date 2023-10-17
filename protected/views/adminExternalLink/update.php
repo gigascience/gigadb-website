52c86eb443d3afa1cc691648ffdab37170411b9f
luis-toptal <luis.martinez@toptal.com>
2023-10-17 15:53:20 +0200

    feat: update adminExternalLink create and update styles

--- protected/views/adminExternalLink/update.php
+++ protected/views/adminExternalLink/update.php
@@ -1,18 +1,14 @@
-<?php
-$this->breadcrumbs=array(
-	'External Links'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
-	'Update',
-);
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'Update ExternalLink ' . $model->id,
+		'breadcrumbItems' => [
+			['label' => 'Admin', 'href' => '/site/admin'],
+			['label' => 'Manage', 'href' => '/adminExternalLink/admin'],
+			['isActive' => true, 'label' => 'Update'],
+		]
+	]);
+	?>
 
-$this->menu=array(
-	array('label'=>'List ExternalLink', 'url'=>array('index')),
-	array('label'=>'Create ExternalLink', 'url'=>array('create')),
-	array('label'=>'View ExternalLink', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage ExternalLink', 'url'=>array('admin')),
-);
-?>
-
-<h1>Update ExternalLink <?php echo $model->id; ?></h1>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+	<?php echo $this->renderPartial('_form', array('model' => $model)); ?>
+</div>
