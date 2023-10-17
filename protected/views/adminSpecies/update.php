f19ab94bcdc4501f9e10b9b44679c00e30c42684
luis-toptal <luis.martinez@toptal.com>
2023-10-17 15:40:30 +0200

    feat: update adminSpecies create and update styles

--- protected/views/adminSpecies/update.php
+++ protected/views/adminSpecies/update.php
@@ -1,18 +1,14 @@
-<?php
-$this->breadcrumbs=array(
-	'Species'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
-	'Update',
-);
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'Update Species ' . $model->id,
+		'breadcrumbItems' => [
+			['label' => 'Admin', 'href' => '/site/admin'],
+			['label' => 'Manage', 'href' => '/adminSpecies/admin'],
+			['isActive' => true, 'label' => 'Update'],
+		]
+	]);
+	?>
 
-$this->menu=array(
-	array('label'=>'List Species', 'url'=>array('index')),
-	array('label'=>'Create Species', 'url'=>array('create')),
-	array('label'=>'View Species', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Species', 'url'=>array('admin')),
-);
-?>
-
-<h1>Update Species <?php echo $model->id; ?></h1>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+	<?php echo $this->renderPartial('_form', array('model' => $model)); ?>
+</div>
