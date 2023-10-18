28a7484560bede7abed5d4664c71fbf9a9a3b602
luis-toptal <luis.martinez@toptal.com>
2023-10-18 10:42:08 +0200

    feat: update adminDatasetType create and update styles

--- protected/views/adminDatasetType/update.php
+++ protected/views/adminDatasetType/update.php
@@ -1,18 +1,14 @@
-<?php
-$this->breadcrumbs=array(
-	'Types'=>array('index'),
-	$model->name=>array('view','id'=>$model->id),
-	'Update',
-);
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'Update Type ' . $model->id,
+		'breadcrumbItems' => [
+			['label' => 'Admin', 'href' => '/site/admin'],
+			['label' => 'Manage', 'href' => '/adminDatasetType/admin'],
+			['isActive' => true, 'label' => 'Update'],
+		]
+	]);
+	?>
 
-$this->menu=array(
-	array('label'=>'List Type', 'url'=>array('index')),
-	array('label'=>'Create Type', 'url'=>array('create')),
-	array('label'=>'View Type', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Type', 'url'=>array('admin')),
-);
-?>
-
-<h1>Update Type <?php echo $model->id; ?></h1>
-
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+	<?php echo $this->renderPartial('_form', array('model' => $model)); ?>
+</div>
