a076ce785ba53ddef4fa0e3cdd5813a292acec56
luis-toptal <luis.martinez@toptal.com>
2023-10-19 10:23:54 +0200

    feat: update adminExternalLink/view styles

--- protected/views/adminExternalLink/view.php
+++ protected/views/adminExternalLink/view.php
@@ -1,17 +1,25 @@
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'View ExternalLink #' . $model->id,
+		'breadcrumbItems' => [
+			['label' => 'Admin', 'href' => '/site/admin'],
+			['label' => 'Manage', 'href' => '/adminExternalLink/admin'],
+			['isActive' => true, 'label' => 'View'],
+		]
+	]);
 
-<h1>View ExternalLink #<?php echo $model->id; ?></h1>
-<? if (Yii::app()->user->checkAccess('admin')) { ?>
-<div class="actionBar">
-[<?= CHtml::link('Manage External Links', array('admin')) ?>]
+	$this->widget('zii.widgets.CDetailView', array(
+		'data' => $model,
+		'attributes' => array(
+			'id',
+			'dataset_id',
+			'url',
+			'external_link_type_id',
+		),
+		'htmlOptions' => array('class' => 'table table-striped table-bordered dataset-view-table'),
+		'itemCssClass' => array('odd', 'even'),
+		'itemTemplate' => '<tr class="{class}"><th scope="row">{label}</th><td>{value}</td></tr>'
+	)); ?>
+
 </div>
-<? } ?>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'dataset_id',
-		'url',
-		'external_link_type_id',
-	),
-)); ?>
