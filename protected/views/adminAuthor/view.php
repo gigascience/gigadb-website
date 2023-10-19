993058363b0a821aded11063da01f72bba008c6d
luis-toptal <luis.martinez@toptal.com>
2023-10-19 10:16:39 +0200

    feat: update adminAuthor/view styles

--- protected/views/adminAuthor/view.php
+++ protected/views/adminAuthor/view.php
@@ -1,82 +1,84 @@
-<?php
-    foreach(Yii::app()->user->getFlashes() as $key => $message) {
-        echo '<div class="flash-' . $key . '">' . $message . "</div>\n";
-    }
-?>
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'View Author #' . $model->id,
+		'breadcrumbItems' => [
+			['label' => 'Admin', 'href' => '/site/admin'],
+			['label' => 'Manage', 'href' => '/adminAuthor/admin'],
+			['isActive' => true, 'label' => 'View'],
+		]
+	]);
+	foreach (Yii::app()->user->getFlashes() as $key => $message) {
+		echo '<div class="flash-' . $key . '">' . $message . "</div>\n";
+	}
 
-<?php
-$this->breadcrumbs=array(
-	'Authors'=>array('index'),
-	$model->name,
-);
+	$user_command = UserCommand::model()->findByAttributes(array("actionable_id" => $model->id, "action_label" => "claim_author"));
 
-$this->menu=array(
-	array('label'=>'List Author', 'url'=>array('index')),
-	array('label'=>'Create Author', 'url'=>array('create')),
-	array('label'=>'Update Author', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Author', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Author', 'url'=>array('admin')),
-);
-?>
+	$this->widget('zii.widgets.CDetailView', array(
+		'data' => $model,
+		'attributes' => array(
+			'id',
+			'name',
+			'displayName',
+			'orcid',
+			'gigadb_user_id',
+			'rank',
+		),
+		'htmlOptions' => array('class' => 'table table-striped table-bordered dataset-view-table'),
+		'itemCssClass' => array('odd', 'even'),
+		'itemTemplate' => '<tr class="{class}"><th scope="row">{label}</th><td>{value}</td></tr>'
+	));
 
-<?php
-    $user_command = UserCommand::model()->findByAttributes(array("actionable_id" => $model->id, "action_label" => "claim_author"));
-?>
-
-<h1>View Author #<?php echo $model->id; ?></h1>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'name',
-		'displayName',
-		'orcid',
-		'gigadb_user_id',
-		'rank',
-	),
-)); ?>
-
-<div class="clear"></div>
-<?php
-      if ( null != $user_command ) {
-      	echo "<div class=\alert alert-info\">";
-      	echo "There is a pending claim on this author.";
-        echo CHtml::link('Edit user to validate/reject the claim', 
-                                    array('user/update/','id' => $user_command->requester_id),
-                                    array('class' => 'btn'));
-        echo "</div>";
-      }
-
-      if (null != $model->gigadb_user_id) {
-      	$user = User::model()->findByPk($model->gigadb_user_id);
-      	if (null != $user) {
-	      	echo "<div class=\"alert alert-info\">";
-	      	echo "this author is linked to user {$user->first_name} {$user->last_name} ({$model->gigadb_user_id})";
-	      	echo "</div>";
-      	}
-      }
-
-?>
-<div class="clear"></div>
-<div class="merge_info">
-<?php
-		$identical_authors = $model->getIdenticalAuthors() ;
-		if( !empty($identical_authors) ) {
-?>
-		<div class="alert alert-info">
-		this author is merged with author(s):
-		<ul class="unstyled">
-<?php
-			foreach ($identical_authors as $author_id) {
-				$author = Author::model()->findByPk($author_id);
-				echo "<li>".$author->getAuthorDetails()."</li>";
-			}
-?>
-		</ul>
-
+	if (null != $user_command) {
+	?>
+		<div class="alert alert-gigadb-info">There is a pending claim on this author
+			<div>
+				<?php
+				echo CHtml::link(
+					'Edit user to validate/reject the claim',
+					array('user/update/', 'id' => $user_command->requester_id),
+					array('class' => 'btn background-btn ml-10 mr-10')
+				);
+				?>
+			</div>
 		</div>
+		<?php
+	}
 
-<?php	} ?>
+	if (null != $model->gigadb_user_id) {
+		$user = User::model()->findByPk($model->gigadb_user_id);
+		if (null != $user) {
+		?>
+			<div class="alert alert-gigadb-info">
+				<?php
+				echo "this author is linked to user {$user->first_name} {$user->last_name} ({$model->gigadb_user_id})";
+				?>
+			</div>
+	<?php
+		}
+	}
+
+	?>
+	<div class="merge_info">
+		<?php
+		$identical_authors = $model->getIdenticalAuthors();
+		if (!empty($identical_authors)) {
+		?>
+			<div class="alert alert-info alert-gigadb-info">
+				this author is merged with author(s):
+				<ul class="list-unstyled mt-10">
+					<?php
+					foreach ($identical_authors as $author_id) {
+						$author = Author::model()->findByPk($author_id);
+						echo "<li>" . $author->getAuthorDetails() . "</li>";
+					}
+					?>
+				</ul>
+
+			</div>
+
+		<?php	} ?>
+
+	</div>
 
 </div>
