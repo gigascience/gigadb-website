d36806a32d637654215ebb880b975d0e9d361d11
luis-toptal <luis.martinez@toptal.com>
2023-10-19 08:57:07 +0200

    feat: update adminLink/view styles

--- protected/views/adminLink/view.php
+++ protected/views/adminLink/view.php
@@ -1,26 +1,26 @@
-<?php
-$this->breadcrumbs=array(
-	'Links'=>array('index'),
-	$model->id,
-);
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'View Link #' . $model->id,
+		'breadcrumbItems' => [
+			['label' => 'Admin', 'href' => '/site/admin'],
+			['label' => 'Manage', 'href' => '/adminLink/admin'],
+			['isActive' => true, 'label' => 'View'],
+		]
+	]);
+	?>
 
-$this->menu=array(
-	array('label'=>'List Link', 'url'=>array('index')),
-	array('label'=>'Create Link', 'url'=>array('create')),
-	array('label'=>'Update Link', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Link', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Link', 'url'=>array('admin')),
-);
-?>
+	<?php $this->widget('zii.widgets.CDetailView', array(
+		'data' => $model,
+		'attributes' => array(
+			'id',
+			'dataset_id',
+			'is_primary',
+			'link',
+		),
+		'htmlOptions' => array('class' => 'table table-striped table-bordered dataset-view-table'),
+		'itemCssClass' => array('odd', 'even'),
+		'itemTemplate' => '<tr class="{class}"><th scope="row">{label}</th><td>{value}</td></tr>'
+	)); ?>
 
-<h1>View Link #<?php echo $model->id; ?></h1>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'dataset_id',
-		'is_primary',
-		'link',
-	),
-)); ?>
+</div>
