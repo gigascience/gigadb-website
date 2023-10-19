5d67ad15e3096d3f6a88af77f96ca21574d7f4c6
luis-toptal <luis.martinez@toptal.com>
2023-10-19 09:02:09 +0200

    feat: update datasetFunder/view styles

--- protected/views/datasetFunder/view.php
+++ protected/views/datasetFunder/view.php
@@ -1,22 +1,33 @@
+<div class="container">
+	<?php
+	$this->widget('TitleBreadcrumb', [
+		'pageTitle' => 'View Dataset Funder #' . $model->id,
+		'breadcrumbItems' => [
+			['label' => 'Admin', 'href' => '/site/admin'],
+			['label' => 'Manage', 'href' => '/datasetFunder/admin'],
+			['isActive' => true, 'label' => 'View'],
+		]
+	]);
+	?>
 
-<a href="/datasetFunder/admin">Back</a>
-
-<h1>View Dataset Funder #<?php echo $model->id; ?></h1>
-
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		array(
-			'label' => 'Dataset',
-			'value' => $model->dataset->identifier,
+	<?php $this->widget('zii.widgets.CDetailView', array(
+		'data' => $model,
+		'attributes' => array(
+			'id',
+			array(
+				'label' => 'Dataset',
+				'value' => $model->dataset->identifier,
+			),
+			array(
+				'label' => 'Funder',
+				'value' => $model->funder->primary_name_display,
+			),
+			'grant_award',
+			'awardee',
+			'comments',
 		),
-		array(
-			'label' => 'Funder',
-			'value' => $model->funder->primary_name_display,
-		),
-		'grant_award',
-                'awardee',
-		'comments',
-	),
-)); ?>
+		'htmlOptions' => array('class' => 'table table-striped table-bordered dataset-view-table'),
+		'itemCssClass' => array('odd', 'even'),
+		'itemTemplate' => '<tr class="{class}"><th scope="row">{label}</th><td>{value}</td></tr>'
+	)); ?>
+</div>
