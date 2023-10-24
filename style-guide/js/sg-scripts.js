f83891e1afc690267b347e24250e3e1265cc9473
luis-toptal <luis.martinez@toptal.com>
2023-10-24 15:02:39 +0200

    chore: autoformat all styleguide files
    current format made files hard to read so prettifying all of them in one go

--- style-guide/js/sg-scripts.js
+++ style-guide/js/sg-scripts.js
@@ -5,18 +5,16 @@
 var SGB = window.SGB || {};
 
 (function (w, SGB, undefined) {
-
   var doc = w.document,
-      docEl = doc.documentElement;
+    docEl = doc.documentElement;
 
-    // Replace no-js class with js class
-    docEl.className = docEl.className.replace(/no-js/gi,'');
-    docEl.className+=' js';
+  // Replace no-js class with js class
+  docEl.className = docEl.className.replace(/no-js/gi, "");
+  docEl.className += " js";
 
   // Cut the mustard
   if ("querySelector" in doc && Array.prototype.forEach) {
-
-    docEl.className+=' sg-enhanced';
+    docEl.className += " sg-enhanced";
 
     // Syntactic sugar for querySelectorAll and event delegates courtesy
     // @paul_irish: https://gist.github.com/paulirish/12fb951a8b893a454b32
@@ -28,25 +26,28 @@ var SGB = window.SGB || {};
 
     NodeList.prototype.forEach = Array.prototype.forEach;
 
-    NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
+    NodeList.prototype.on = NodeList.prototype.addEventListener = function (
+      name,
+      fn,
+    ) {
       this.forEach(function (elem, i) {
         elem.on(name, fn);
       });
     };
 
-     // Add functionality to toggle classes on elements
+    // Add functionality to toggle classes on elements
     function _hasClass(el, cl) {
-      var regex = new RegExp('(?:\\s|^)' + cl + '(?:\\s|$)');
+      var regex = new RegExp("(?:\\s|^)" + cl + "(?:\\s|$)");
       return !!el.className.match(regex);
     }
 
     function _addClass(el, cl) {
-      el.className += ' ' + cl;
+      el.className += " " + cl;
     }
 
     function _removeClass(el, cl) {
-      var regex = new RegExp('(?:\\s|^)' + cl + '(?:\\s|$)');
-      el.className = el.className.replace(regex, ' ');
+      var regex = new RegExp("(?:\\s|^)" + cl + "(?:\\s|$)");
+      el.className = el.className.replace(regex, " ");
     }
 
     function _toggleClass(el, cl) {
@@ -54,22 +55,21 @@ var SGB = window.SGB || {};
     }
 
     // Public methods
-    SGB.toggleNav = function() {
-      _toggleClass(docEl, 'nav-is-active');
+    SGB.toggleNav = function () {
+      _toggleClass(docEl, "nav-is-active");
     };
 
-    SGB.hideNav = function() {
-      _removeClass(docEl, 'nav-is-active');
+    SGB.hideNav = function () {
+      _removeClass(docEl, "nav-is-active");
     };
 
-    SGB.toggleSourceCode = function() {
+    SGB.toggleSourceCode = function () {
       var sourceCode = this.parentNode.nextElementSibling;
-      _toggleClass(sourceCode, 'sg-source-active');
+      _toggleClass(sourceCode, "sg-source-active");
     };
 
-    SGB.selectSourceCode = function() {
-      var range,
-          selection;
+    SGB.selectSourceCode = function () {
+      var range, selection;
 
       if (doc.body.createTextRange) {
         range = doc.body.createTextRange();
@@ -83,12 +83,12 @@ var SGB = window.SGB || {};
         selection.addRange(range);
       }
 
-       _toggleClass(this, 'sg-btn--select-active');
+      _toggleClass(this, "sg-btn--select-active");
     };
 
-    queryAll('.sg-nav-toggle').on('click', SGB.toggleNav);
-    queryAll('.sg-nav-group a').on('click', SGB.hideNav);
-    queryAll('.sg-btn--source').on('click', SGB.toggleSourceCode);
-    queryAll('.sg-btn--select').on('click', SGB.selectSourceCode);
+    queryAll(".sg-nav-toggle").on("click", SGB.toggleNav);
+    queryAll(".sg-nav-group a").on("click", SGB.hideNav);
+    queryAll(".sg-btn--source").on("click", SGB.toggleSourceCode);
+    queryAll(".sg-btn--select").on("click", SGB.selectSourceCode);
   }
-}(this, SGB));
+})(this, SGB);
