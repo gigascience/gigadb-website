875f447d7757b4dafc4d53a5b1849047f543606e
luis-toptal <luis.martinez@toptal.com>
2023-11-01 18:14:16 +0100

    feat: improve layout of forgot password form

--- protected/views/resetPasswordRequest/forgot.php
+++ protected/views/resetPasswordRequest/forgot.php
@@ -1,42 +1,45 @@
 <?php
-$this->pageTitle='Forgotten password';
+$this->pageTitle = 'Forgotten password';
 ?>
 <div class="content">
     <div class="container">
       <?php
-        $this->widget('TitleBreadcrumb', [
-          'pageTitle' => 'Forgotten password',
-          'breadcrumbItems' => [
-            ['label' => 'Home', 'href' => '/'],
-            ['isActive' => true, 'label' => 'Forgot'],
-          ]
-        ]);
-        ?>
-    <div class="subsection row" style="margin-bottom: 130px;">
+      $this->widget('TitleBreadcrumb', [
+        'pageTitle' => 'Forgotten password',
+        'breadcrumbItems' => [
+          ['label' => 'Home', 'href' => '/'],
+          ['isActive' => true, 'label' => 'Forgot'],
+        ]
+      ]);
+      ?>
+    <div class="subsection row">
         <div class="col-xs-12">
-            <?php if(Yii::app()->user->hasFlash('fail-reset-password')): ?>
-                <div class="alert alert-warning">
-                    <?php echo Yii::app()->user->getFlash('fail-reset-password'); ?>
-                </div>
+            <?php if (Yii::app()->user->hasFlash('fail-reset-password')): ?>
+                        <div class="alert alert-warning">
+                            <?php echo Yii::app()->user->getFlash('fail-reset-password'); ?>
+                        </div>
             <?php endif; ?>
         </div>
-            <div class="reset-message-div">
-                <p>Please enter your email. A link to reset your password will be sent to you.</p>
-            </div>
-            <div class="create-div">
-                <? $form=$this->beginWidget('CActiveForm', array(
-                    'id'=>'forgot-password-form',
-                    'enableAjaxValidation'=>false,
-                    'htmlOptions'=>array('class'=>'form-horizontal forgot-password-form')
-                )) ?>
-                <div class="form-group forgot-password-group">
-                    <?php echo $form->label($model, 'email', array('class' => 'col-xs-2 control-label forgot-password-label')); ?>
-                    <div class="col-xs-8 forgot-password-input">
+        <div class="col-xs-6 col-xs-offset-3 well">
+          <div class="reset-message-div">
+            <p class="mb-20">Please enter your email. A link to reset your password will be sent to you.</p>
+          </div>
+                <? $form = $this->beginWidget(
+                  'CActiveForm',
+                  array(
+                    'id' => 'forgot-password-form',
+                    'enableAjaxValidation' => false,
+                    'htmlOptions' => array('class' => 'form-horizontal forgot-password-form')
+                  )
+                ) ?>
+                <div class="form-group">
+                    <?php echo $form->label($model, 'email', array('class' => 'col-xs-2 control-label')); ?>
+                    <div class="col-xs-10">
                         <?php echo $form->emailField($model, 'email', array('class' => 'form-control', 'required' => 'true')); ?>
                     </div>
-                    <div class="col-xs-2">
-                        <?= CHtml::submitButton(Yii::t('app' , 'Reset Password'), array('class'=>'btn background-btn forgot-password-btn')) ?>
-                    </div>
+                </div>
+                <div class="pull-right">
+                    <?= CHtml::submitButton(Yii::t('app', 'Reset Password'), array('class' => 'btn background-btn forgot-password-btn')) ?>
                 </div>
                 <? $this->endWidget() ?>
             </div>
