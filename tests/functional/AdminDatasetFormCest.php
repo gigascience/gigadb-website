7d76af2e8e0c7ebafa4ce4dbc231a2e86917f0b6
kencho <kencho.gigascience@gmail.com>
2023-11-22 13:46:28 +0800

    feat: create functional tests for saving form value of private page

---
+++ tests/functional/AdminDatasetFormCest.php
@@ -0,0 +1,71 @@
+<?php
+
+use Codeception\Util\Locator;
+
+/**
+ *
+ * generated with:
+ * docker-compose run --rm test ./vendor/codeception/codeception/codecept generate:cest functional AdminDatasetFormCest
+ *
+ * run with:
+ * docker-compose run --rm test ./vendor/codeception/codeception/codecept run functional AdminDatasetFormCest
+ *
+ */
+class AdminDatasetFormCest
+{
+    public function _before(FunctionalTester $I)
+    {
+        $I->amOnPage('/site/login');
+        $I->submitForm('form.form-horizontal', [
+            'LoginForm[username]' => 'admin@gigadb.org',
+            'LoginForm[password]' => 'gigadb']);
+        $I->canSee('Admin');
+    }
+
+    // tests
+    public function tryToSaveChangesForPrivatePage(FunctionalTester $I)
+    {
+        $I->amOnPage('/adminDataset/update/id/668');
+        $I->canSee('Private');
+        $I->canSee('Open Private URL');
+        $I->seeElement('input', ['name' => 'Dataset[dataset_size]', 'type' => 'text', 'value' => '32']);
+        $I->canSee('well now, how to describe nothing in particular?');
+        $I->fillField('Dataset[dataset_size]', '1024');
+        $I->fillField('Dataset[description]', 'Test description');
+        $I->click('Save');
+        $I->seeInCurrentUrl('/adminDataset/update/id/668');
+        $I->seeElement('input', ['name' => 'Dataset[dataset_size]', 'type' => 'text', 'value' => '1024']);
+        $I->cantSeeElement('input', ['name' => 'Dataset[dataset_size]', 'type' => 'text', 'value' => '32']);
+        $I->canSee('Test description');
+        $I->cantSee('well now, how to describe nothing in particular');
+        $I->canSeeInDatabase('dataset', [
+            'dataset_size' => 1024,
+            'description' => 'Test description'
+        ]);
+        $I->cantSeeInDatabase('dataset', [
+            'dataset_size' => 32,
+            'description' => 'well now, how to describe nothing in particular?'
+        ]);
+    }
+    public function tryToCancelChangesForPrivatePage(FunctionalTester $I)
+    {
+        $I->amOnPage('/adminDataset/update/id/668');
+        $I->canSee('Private');
+        $I->canSee('Open Private URL');
+        $I->seeElement('input', ['name' => 'Dataset[dataset_size]', 'type' => 'text', 'value' => '32']);
+        $I->canSee('well now, how to describe nothing in particular?');
+        $I->fillField('Dataset[dataset_size]', '1024');
+        $I->fillField('Dataset[description]', 'Test description');
+        $I->click(Locator::find('a', ['href' => '/adminDataset/admin', 'class' => 'btn']));
+        $I->seeInCurrentUrl('/adminDataset/admin');
+        $I->see("Manage Datasets");
+        $I->canSeeInDatabase('dataset', [
+            'dataset_size' => 32,
+            'description' => 'well now, how to describe nothing in particular?'
+        ]);
+        $I->cantSeeInDatabase('dataset', [
+            'dataset_size' => 1024,
+            'description' => 'Test description'
+        ]);
+    }
+}
