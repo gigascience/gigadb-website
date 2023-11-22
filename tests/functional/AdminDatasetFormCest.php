f8054daca084d2e08fd80b7aa0d0396f84641a86
kencho <kencho.gigascience@gmail.com>
2023-11-22 14:30:16 +0800

    refactor: make the tests more precise

--- tests/functional/AdminDatasetFormCest.php
+++ tests/functional/AdminDatasetFormCest.php
@@ -21,50 +21,50 @@ class AdminDatasetFormCest
     }
 
     // tests
-    public function tryToSaveChangesForPrivatePage(FunctionalTester $I)
+    public function tryToSaveChangesForPrivateDataset(FunctionalTester $I)
     {
         $I->amOnPage('/adminDataset/update/id/668');
         $I->canSee('Private');
         $I->canSee('Open Private URL');
         $I->seeElement('input', ['name' => 'Dataset[dataset_size]', 'type' => 'text', 'value' => '32']);
-        $I->canSee('well now, how to describe nothing in particular?');
+        $I->seeElement('input', ['name' => 'Dataset[title]', 'type' => 'text', 'value' => 'supporting data for nothing in particular']);
         $I->fillField('Dataset[dataset_size]', '1024');
-        $I->fillField('Dataset[description]', 'Test description');
+        $I->fillField('Dataset[title]', 'Test title');
         $I->click('Save');
         $I->seeInCurrentUrl('/adminDataset/update/id/668');
         $I->seeElement('input', ['name' => 'Dataset[dataset_size]', 'type' => 'text', 'value' => '1024']);
         $I->cantSeeElement('input', ['name' => 'Dataset[dataset_size]', 'type' => 'text', 'value' => '32']);
-        $I->canSee('Test description');
-        $I->cantSee('well now, how to describe nothing in particular');
+        $I->seeElement('input', ['name' => 'Dataset[title]', 'type' => 'text', 'value' => 'Test title']);
+        $I->cantSeeElement('input', ['name' => 'Dataset[title]', 'type' => 'text', 'value' => 'supporting data for nothing in particular']);
         $I->canSeeInDatabase('dataset', [
             'dataset_size' => 1024,
-            'description' => 'Test description'
+            'title' => 'Test title'
         ]);
         $I->cantSeeInDatabase('dataset', [
             'dataset_size' => 32,
-            'description' => 'well now, how to describe nothing in particular?'
+            'title' => 'supporting data for nothing in particular'
         ]);
     }
-    public function tryToCancelChangesForPrivatePage(FunctionalTester $I)
+    public function tryToCancelChangesForPrivateDataset(FunctionalTester $I)
     {
         $I->amOnPage('/adminDataset/update/id/668');
         $I->canSee('Private');
         $I->canSee('Open Private URL');
         $I->seeElement('input', ['name' => 'Dataset[dataset_size]', 'type' => 'text', 'value' => '32']);
-        $I->canSee('well now, how to describe nothing in particular?');
+        $I->seeElement('input', ['name' => 'Dataset[title]', 'type' => 'text', 'value' => 'supporting data for nothing in particular']);
         $I->fillField('Dataset[dataset_size]', '1024');
-        $I->fillField('Dataset[description]', 'Test description');
+        $I->fillField('Dataset[title]', 'Test title');
         # Use the CSS locator for the "Cancel" button
         $I->click('a.btn[href="/adminDataset/admin"]');
         $I->seeInCurrentUrl('/adminDataset/admin');
         $I->see('Manage Datasets');
         $I->canSeeInDatabase('dataset', [
             'dataset_size' => 32,
-            'description' => 'well now, how to describe nothing in particular?'
+            'title' => 'supporting data for nothing in particular'
         ]);
         $I->cantSeeInDatabase('dataset', [
             'dataset_size' => 1024,
-            'description' => 'Test description'
+            'title' => 'Test title'
         ]);
     }
 
