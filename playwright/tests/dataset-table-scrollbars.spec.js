c17fe6cb44f234fa708dc1aee8c47dc99886847c
Peter Li <peter@gigasciencejournal.com>
2024-01-15 22:15:31 +0800

    Add playwright test to check horizontal table scrolling

---
+++ playwright/tests/dataset-table-scrollbars.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@playwright/test');
+
+test('Can scroll horizontally in File table', async ({ page }) => {
+    await page.goto('http://gigadb.test/dataset/102484');
+
+    // Check File Name column name in File table is visible
+    const filename_column_header = page.locator('xpath=//*[@id="files_table"]/thead/tr/th[1]');
+    await filename_column_header.scrollIntoViewIfNeeded();
+    await expect(page.getByText('File Name')).toBeInViewport();
+    // Check Download column name in 9th column is visible
+    const download_column_header = page.locator('xpath=//*[@id="files_table"]/thead/tr/th[9]');
+    await download_column_header.scrollIntoViewIfNeeded();
+    await expect(page.locator('xpath=//*[@id="files_table"]/thead/tr/th[9]')).toBeInViewport();
+});
