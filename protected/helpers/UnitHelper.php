81d2d226d2146c9f28c57f4a11c8416764174140
kencho <kencho.gigascience@gmail.com>
2024-01-16 15:50:40 +0800

    refactor: move the specifySizeUnits to a helper class

---
+++ protected/helpers/UnitHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This is a utility component for parsing, formatting, converting and manipulating byte units in various formats.
+ * @uses gabrielelana/byte-units package
+ *
+ * It supports both Metric (1000-byte) system and Binary (1024-byte) system.
+ *
+ */
+class UnitHelper
+{
+    /**
+     * Return the human readable binary size of files with configurable formatting
+     *
+     * It's extracted out of getSizeWithFormat so the functionality can be used in other contexts as well.
+     *
+     * @param int $bytes size in bytes to format/convert
+     * @param string $unit unit to convert to kB, MB, GB, TB, B or null
+     * @param int $precision number of decimals after the dot
+     * @uses ByteUnits\Metric
+     * @return string formatted size
+     */
+    public static function specifySizeUnits(int $bytes, string $unit = null, int $precision = null): string
+    {
+        if ($bytes<0) {
+            return (string) $bytes;
+        }
+        if ( null == $precision ) {
+            $precision = 2;
+        }
+        $metric = new ByteUnits\Metric($bytes);
+        $formatted_size = $metric->format("$unit/$precision"," ");
+        return $formatted_size ;
+    }
+
+}
