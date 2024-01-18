005d1d3ada135ca3c0b5f269906175b587097a12
luis-toptal <luis.martinez@toptal.com>
2024-01-18 16:00:49 +0700

    chore: update hardcoded playwright url

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -42,7 +42,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://gigadb.gigasciencejournal.com:9170',
+    baseURL: 'http://gigadb.gigasciencejournal.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
