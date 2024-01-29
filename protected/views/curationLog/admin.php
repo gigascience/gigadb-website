3d0f445c8e77311a3e5d627621e1c8270eb95f5c
kencho <kencho.gigascience@gmail.com>
2024-01-29 14:20:11 +0800

    refactor: make curation log admin page using default action buttons

--- protected/views/curationLog/admin.php
+++ protected/views/curationLog/admin.php
@@ -1,39 +1,32 @@
-<?php
-$this->widget('TitleBreadcrumb', [
-    'pageTitle' => 'Manage Curation Log',
-    'breadcrumbItems' => [
-        ['label' => 'Admin', 'href' => '/site/admin'],
-        ['isActive' => true, 'label' => 'Manage'],
-    ]
-]);
-?>
-<br>
-<div class="clear"></div>
-<?php
-$this->widget(
-    'zii.widgets.grid.CGridView',
-    [
-        'id'            => 'dataset-grid',
-        'dataProvider'  => $model->search(),
-        'itemsCssClass' => 'table table-bordered',
-        'columns'       => [
-            'id',
-            'dataset_id',
-            array('name' => 'doi', 'value' => '$data->dataset->identifier'),
-            'creation_date',
-            'created_by',
-            'action',
-            'comments',
-            'last_modified_date',
-            'last_modified_by',
-            [
-                'class'   => 'CButtonColumn',
-                'buttons' => [
-                    'view'   => ['url' => 'Yii::app()->createUrl("curationlog/view", array("id" => $data->id))'],
-                    'update' => ['url' => 'Yii::app()->createUrl("curationlog/update" , array("id" => $data->id))'],
-                    'delete' => ['url' => 'Yii::app()->createUrl("curationlog/delete" , array("id" => $data->id))'],
-                ],
+<div class="container">
+    <?php
+    $this->widget('TitleBreadcrumb', [
+        'pageTitle' => 'Manage Curation Log',
+        'breadcrumbItems' => [
+            ['label' => 'Admin', 'href' => '/site/admin'],
+            ['isActive' => true, 'label' => 'Manage'],
+        ]
+    ]);
+
+    $this->widget(
+        'CustomGridView',
+        [
+            'id'            => 'dataset-grid',
+            'dataProvider'  => $model->search(),
+            'itemsCssClass' => 'table table-bordered',
+            'columns'       => [
+                'id',
+                'dataset_id',
+                array('name' => 'doi', 'value' => '$data->dataset->identifier'),
+                'creation_date',
+                'created_by',
+                'action',
+                'comments',
+                'last_modified_date',
+                'last_modified_by',
+                CustomGridView::getDefaultActionButtonsConfig()
             ],
-        ],
-    ]
-);
+        ]
+    );
+    ?>
+</div>
