f615f8f586fd6fa7621538559c65a1917116e6f1
kencho <kencho.gigascience@gmail.com>
2024-01-29 13:01:16 +0800

    refactor: show curationlog id and dataset id

--- protected/views/curationLog/admin.php
+++ protected/views/curationLog/admin.php
@@ -1,19 +1,17 @@
-<?php
-$dataset = Dataset::model()->find('id=:dataset_id', [':dataset_id' => $dataset_id]);
-?>
 
-<h2>Dataset <?php echo $dataset->identifier; ?> Curation Log</h2>
+<h2>Manage Curation Log</h2>
 <br>
-<a href="/curationLog/create/id/<?php echo $dataset_id; ?>" class="btn">Create New Log</a>
 <div class="clear"></div>
 <?php
 $this->widget(
     'zii.widgets.grid.CGridView',
     [
         'id'            => 'dataset-grid',
-        'dataProvider'  => $model,
+        'dataProvider'  => $model->search(),
         'itemsCssClass' => 'table table-bordered',
         'columns'       => [
+            'id',
+            'dataset_id',
             'creation_date',
             'created_by',
             'action',
