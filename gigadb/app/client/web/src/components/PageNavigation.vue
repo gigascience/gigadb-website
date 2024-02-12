d115a8654b2dc2810ae42e2de12c82edcc667426
luis-toptal <luis.martinez@toptal.com>
2024-02-12 16:41:33 +0700

    feat: clean up page nav component

--- gigadb/app/client/web/src/components/PageNavigation.vue
+++ gigadb/app/client/web/src/components/PageNavigation.vue
@@ -1,36 +1,54 @@
 <template>
   <nav class="text-right">
     <div class="button-div" v-if="stage === 'uploading' && (uploadsComplete === true || uploadsExist > 0)">
-      <a v-bind:href="annotationUrl" class="btn background-btn" style="margin:5px;width:30%">Next (Metadata Form)</a>
+      <a :href="annotationUrl" class="btn background-btn nav-link">Next (Metadata Form)</a>
     </div>
     <div v-if="stage === 'annotating' && metadataComplete === true">
-      <button class="btn background-btn complete" type="submit" style="margin:5px">Complete and return to Your Uploaded
+      <button class="btn background-btn complete complete-btn" type="submit">Complete and return to Your Uploaded
         Datasets page</button>
     </div>
-    <!--         <div v-else>
-        	stage: {{ stage }}
-        	metadataComplete: {{ metadataComplete }}
-        </div> -->
+    <!-- <div v-else>
+      stage: {{ stage }}
+      metadataComplete: {{ metadataComplete }}
+    </div> -->
   </nav>
 </template>
-<style></style>
-<script>
 
+<style scoped>
+.nav-link {
+  margin: 5px;
+  width: 30%
+}
+
+.complete-btn {
+  margin: 5px;
+}
+</style>
+
+<script>
 import { eventBus } from '../index.js'
 
 export default {
-  props: ["identifier", "uploadsExist"],
+  props: {
+    identifier: {
+      type: String,
+      required: true
+    },
+    uploadsExist: {
+      type: Number
+    }
+  },
   data: function () {
     return {
       stage: 'undetermined',
       uploadsComplete: false,
       metadataComplete: false,
-      annotationUrl: '/authorisedDataset/annotateFiles/id/' + this.identifier
+      annotationUrl: `/authorisedDataset/annotateFiles/id/${this.identifier}`
     }
   },
   mounted: function () {
     const vm = this //see https://stackoverflow.com/a/47148828/6518111
-    eventBus.$on('stage-changed', function (stage) {
+    eventBus.$on('stage-changed', (stage) => {
       vm.stage = stage
       // console.log('Pager, stage changed to ' + vm.stage)
     })
