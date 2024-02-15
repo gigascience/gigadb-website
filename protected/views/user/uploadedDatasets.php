036eb406b4177e719931f2cbced3c5fc3f83fdcb
luis-toptal <luis.martinez@toptal.com>
2024-02-15 11:41:25 +0700

    feat: turn delete into button so it receives keyboard focus

--- protected/views/user/uploadedDatasets.php
+++ protected/views/user/uploadedDatasets.php
@@ -44,7 +44,7 @@
                 <tr class="<?php echo $class; ?>" id="js-dataset-row-<?=$data[$i]->id?>">
                     <?
                 $upload_status = $data[$i]->upload_status;
-                
+
                 if ( $upload_status != 'Published' && $upload_status!='Private' ) { ?>
                         <td class="content-popup" data-content="<? echo CHtml::encode($data[$i]->description); ?>">
                             unknown
@@ -80,11 +80,11 @@
                                 <td>
                                     <? if ($data[$i]->upload_status !='Published' && $data[$i]->upload_status!='AuthorReview' && $data[$i]->upload_status!='Private'){ ?>
                                         <div>
-                                            <a class="update btn" title="Update" href=<? echo "/datasetSubmission/datasetManagement/id/" . $data[$i]->id ?> >Update</a>
-                                            <a class="js-delete-dataset btn" did="<?=$data[$i]->id?>" title="Delete">
-                                       Delete</a>
-                                            <?php if ($data[$i]->upload_status === "UserUploadingData" || $data[$i]->upload_status === "DataPending") { 
-                                            echo CHtml::link('Upload Files', array('/authorisedDataset/uploadFiles', 'id'=>$data[$i]->identifier), array('class' => 'upload btn'));
+                                            <a class="update btn btn-transparent" title="Update" href=<? echo "/datasetSubmission/datasetManagement/id/" . $data[$i]->id ?> >Update</a>
+                                            <button class="js-delete-dataset btn btn-transparent" did="<?=$data[$i]->id?>" title="Delete">
+                                       Delete</button>
+                                            <?php if ($data[$i]->upload_status === "UserUploadingData" || $data[$i]->upload_status === "DataPending") {
+                                            echo CHtml::link('Upload Files', array('/authorisedDataset/uploadFiles', 'id'=>$data[$i]->identifier), array('class' => 'upload btn btn-transparent'));
                                             } ?>
                                         </div>
                                     <? } ?>
