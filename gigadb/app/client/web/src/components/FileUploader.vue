3b5524c7d72bf4de4fb1fdbf4155fe35f0ad6f7b
luis-toptal <luis.martinez@toptal.com>
2024-02-23 14:25:57 +0700

    feat: use brand styles in uppy upload box

--- gigadb/app/client/web/src/components/FileUploader.vue
+++ gigadb/app/client/web/src/components/FileUploader.vue
@@ -8,13 +8,140 @@
 </template>
 
 <style lang="less" scoped>
+@color-true-white: #ffffff;
+@color-lighter-gray: #f8f8f8;
+@color-light-gray: #e5e5e5;
+@color-darker-gray: #656565;
+@color-true-black: #000000;
 @color-gigadb-green: #08893e;
+@color-gigadb-green-800: #0d6e36;
+@color-gigadb-green-900: #0d5a2e;
 
+// wrapper for uppy drag n drop widget
 .uppy-drag-drop-area::v-deep {
+  .uppy-Dashboard-inner {
+    background-color: @color-lighter-gray;
+  }
+
+  // "browse files" button
   .uppy-Dashboard-browse {
     color: @color-gigadb-green;
-    &:hover, &:focus {
-      border-bottom: 1px solid @color-gigadb-green;
+
+    &:hover {
+      border-bottom: 1px solid @color-gigadb-green-900;
+      color: @color-gigadb-green-900;
+    }
+
+    &:focus {
+      outline: 5px auto -webkit-focus-ring-color;
+    }
+  }
+
+  // "cancel", "+ add more" and "done" buttons
+  .uppy-StatusBar-actionBtn,
+  .uppy-DashboardContent-back,
+  .uppy-DashboardContent-save,
+  .uppy-DashboardContent-addMore {
+    color: @color-gigadb-green;
+
+    &:focus {
+      outline: solid 2px @color-gigadb-green;
+      background: transparent;
+    }
+
+    &:hover {
+      background: @color-light-gray;
+      color: @color-gigadb-green-800;
+    }
+  }
+
+  // "upload" button
+  .uppy-StatusBar.is-waiting .uppy-StatusBar-actionBtn--upload {
+    background-color: @color-gigadb-green;
+    color: @color-true-white;
+
+    &:hover {
+      color: @color-true-white;
+      background: @color-gigadb-green-800;
+    }
+
+    &:focus {
+      color: @color-true-white;
+      outline-offset: 2px;
+      outline: solid 2px @color-gigadb-green;
+      box-shadow: none;
+    }
+  }
+
+  // "completed" icon
+  .uppy-Dashboard-Item-progressIcon--circle>circle {
+    fill: @color-gigadb-green;
+  }
+
+  // success checkmark
+  .uppy-StatusBar.is-complete .uppy-StatusBar-statusIndicator {
+    color: @color-gigadb-green;
+  }
+
+  // edit button
+  .uppy-Dashboard-Item-action--edit {
+    color: @color-darker-gray;
+
+    &:hover {
+      color: @color-gigadb-green;
+    }
+  }
+
+  // edit and delete buttons
+  .uppy-size--md .uppy-Dashboard-Item-action--copyLink,
+  .uppy-size--md .uppy-Dashboard-Item-action--edit,
+  .uppy-Dashboard-Item-action {
+    &:focus {
+      box-shadow: 0 0 0 3px fade(@color-gigadb-green, 50%);
+    }
+  }
+
+
+  // file size text
+  .uppy-Dashboard-Item-status {
+    color: @color-darker-gray;
+  }
+
+  // "save changes" button on file edit
+  .uppy-c-btn-primary {
+    color: @color-true-white;
+    background: @color-gigadb-green;
+    border: 1px @color-gigadb-green solid;
+
+    &:focus {
+      color: @color-true-white;
+      outline-offset: 2px;
+      outline: solid 2px @color-gigadb-green;
+      box-shadow: none;
+    }
+
+    &:hover {
+      color: @color-true-white;
+      background: @color-gigadb-green-800;
+    }
+  }
+
+  // "cancel" button on file edit
+  .uppy-c-btn-link {
+    color: @color-gigadb-green;
+    background: @color-true-white;
+    border: 1px @color-gigadb-green solid;
+
+    &:focus {
+      color: @color-gigadb-green;
+      outline-offset: 2px;
+      outline: solid 2px @color-gigadb-green;
+      box-shadow: none;
+    }
+
+    &:hover {
+      color: @color-true-white;
+      background: @color-gigadb-green;
     }
   }
 }
