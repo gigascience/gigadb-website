86e903caddce33b5efc9bcc9484a64ff3a58d49c
luis-toptal <luis.martinez@toptal.com>
2024-03-06 13:09:55 +0700

    feat: remove google plus and add x and mastodon social links

--- protected/views/shared/_footer.php
+++ protected/views/shared/_footer.php
@@ -15,19 +15,44 @@
                 <p class="base-footer-email"><a href="/site/contact"><i class="fa fa-envelope"></i> database@gigasciencejournal.com</a></p>
                 <ul class="list-inline base-footer-social-bar">
                     <li>
-                        <a class="fa fa-facebook" style="text-decoration: none;" href="http://facebook.com/GigaScience" title="GigaScience on Facebook" aria-label="GigaScience on Facebook"></a>
+                        <a class="fa fa-facebook" href="http://facebook.com/GigaScience" title="GigaScience on Facebook" aria-label="GigaScience on Facebook"></a>
                     </li>
                     <li>
-                        <a class="fa fa-twitter" style="text-decoration: none;" href="http://twitter.com/GigaScience" title="GigaScience on Twitter" aria-label="GigaScience on Twitter"></a>
+                        <a class="fa fa-twitter" href="http://twitter.com/GigaScience" title="GigaScience on Twitter" aria-label="GigaScience on Twitter"></a>
+                    </li>
+                    <li class="icon-list-item">
+                        <a
+                          href="http://x.com/GigaScience"
+                          title="GigaScience on X"
+                          class="icon-list-item__link"
+                          aria-label="GigaScience on X"
+                        >
+                            <img
+                              class="icon-list-item__image"
+                              src="/images/icons/x-logo.svg"
+                              alt=""
+                            >
+                        </a>
                     </li>
                     <li>
-                        <a class="fa fa-weibo" style="text-decoration: none;" href="http://weibo.com/gigasciencejournal" title="Gigascience on Weibo" aria-label="GigaScience on Weibo"></a>
+                        <a class="fa fa-weibo" href="http://weibo.com/gigasciencejournal" title="Gigascience on Weibo" aria-label="GigaScience on Weibo"></a>
+                    </li>
+                    <li class="icon-list-item">
+                        <a
+                          href="https://mastodon.social/@GigaScience"
+                          title="GigaScience on Mastodon"
+                          class="icon-list-item__link"
+                          aria-label="GigaScience on Mastodon"
+                        >
+                            <img
+                              class="icon-list-item__image"
+                              src="/images/icons/mastodon-logo.svg"
+                              alt=""
+                            >
+                        </a>
                     </li>
                     <li>
-                        <a class="fa fa-google-plus" style="text-decoration: none;" href="https://plus.google.com/u/0/104409890199786402308" title="GigaScience on Google+" aria-label="GigaScience on Google+"></a>
-                    </li>
-                    <li>
-                        <a class="fa fa-rss" style="text-decoration: none;" href="http://gigasciencejournal.com/blog/" title="Gigascience Blog" aria-label="GigaScience Blog"></a>
+                        <a class="fa fa-rss" href="http://gigasciencejournal.com/blog/" title="Gigascience Blog" aria-label="GigaScience Blog"></a>
                     </li>
                 </ul>
             </div>
--- protected/views/shared/_header.php
+++ protected/views/shared/_header.php
@@ -52,18 +52,40 @@
                                 </button>
                         </form>
                     </div>
-                    <ul class="share-zone clearfix">
+                    <ul class="share-zone clearfix icon-list">
                         <li>
                             <a class="fa fa-facebook" style="text-decoration: none;" href="http://facebook.com/GigaScience" title="GigaScience on Facebook" aria-label="GigaScience on Facebook"></a>
                         </li>
-                        <li>
-                            <a class="fa fa-twitter" style="text-decoration: none;" href="http://twitter.com/GigaScience" title="GigaScience on Twitter" aria-label="GigaScience on Twitter"></a>
+                        <li class="icon-list-item">
+                            <a
+                              href="http://x.com/GigaScience"
+                              title="GigaScience on X"
+                              class="icon-list-item__link"
+                              aria-label="GigaScience on X"
+                            >
+                                <img
+                                  class="icon-list-item__image"
+                                  src="/images/icons/x-logo.svg"
+                                  alt=""
+                                >
+                            </a>
                         </li>
                         <li>
                             <a class="fa fa-weibo" style="text-decoration: none;" href="http://weibo.com/gigasciencejournal" title="Gigascience on Weibo" aria-label="GigaScience on Weibo"></a>
                         </li>
-                        <li>
-                            <a class="fa fa-google-plus" style="text-decoration: none;" href="https://plus.google.com/u/0/104409890199786402308" title="GigaScience on Google+" aria-label="GigaScience on Google+"></a>
+                        <li class="icon-list-item">
+                            <a
+                              href="https://mastodon.social/@GigaScience"
+                              title="GigaScience on Mastodon"
+                              class="icon-list-item__link"
+                              aria-label="GigaScience on Mastodon"
+                            >
+                                <img
+                                  class="icon-list-item__image"
+                                  src="/images/icons/mastodon-logo.svg"
+                                  alt=""
+                                >
+                            </a>
                         </li>
                         <li>
                             <a class="fa fa-rss" style="text-decoration: none;" href="http://gigasciencejournal.com/blog/" title="Gigascience Blog" aria-label="GigaScience Blog"></a>
