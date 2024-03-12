27351d3cca377ce9bd10512cd563a635e2b501ec
Peter Li <peter@gigasciencejournal.com>
2024-03-12 15:09:19 +0800

    Add step to upload files into FTP server

--- gigadb/app/tools/files-metadata-console/scripts/filesizes.sh
+++ gigadb/app/tools/files-metadata-console/scripts/filesizes.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# How to use: Place this script in the dataset root directory
-# Run ./filesizes.sh *
+# Place this script in dataset directory and run ./filesizes.sh
+
 pwd=$(pwd)
 filename=$(basename $pwd)
-for i in $(find .  -type f);
+
+# Remove any existing *.tsv file
+rm -f "$filename.tsv"
+
+for i in $(find .  -type f ! -name 'filesizes.sh');
 do
-  echo "$i"
-  stat -f '%z' "$i" | numfmt --to=iec --format=%.2f
-  echo -e "$(stat -f '%z' "$i" | numfmt --to=iec --format=%.2f)\t$i" >> "$filename.tsv"
+  # Create file containing dataset file sizes
+  echo -e "$(stat -c '%s' "$i" | numfmt --to=iec --format=%2f)\t$i" >> "$filename.tsv"
 done
+echo -e "Created $filename.tsv file"
+
+# For FTP server access from Tencent backup server
+if test -f ~/.ftpconfig; then
+  source ~/.ftpconfig
+  curl -T "$filename.tsv" -u $ftp_credentials "$ftp_server/datasets/"
+  echo -e "Uploaded $filename.tsv file to FTP server"
+fi
--- gigadb/app/tools/files-metadata-console/scripts/md5.sh
+++ gigadb/app/tools/files-metadata-console/scripts/md5.sh
@@ -5,3 +5,10 @@
 pwd=$(pwd)
 filename=$(basename $pwd)
 find .  -type f -exec md5sum {} \; > "$filename.md5"
+
+# For FTP server access from Tencent backup server
+if test -f ~/.ftpconfig; then
+  source ~/.ftpconfig
+  curl -T "$filename.md5" -u $ftp_credentials "$ftp_server/datasets/"
+  echo -e "Uploaded $filename.md5 file to FTP server"
+fi
