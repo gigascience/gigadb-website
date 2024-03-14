4ca7468fa3372e529fa8a2b67eddf7feffd8ad54
kencho <kencho.gigascience@gmail.com>
2024-03-14 15:04:09 +0800

    refactor: remove the hardcoded creator name in test

--- tests/unit/CurationLogTest.php
+++ tests/unit/CurationLogTest.php
@@ -10,8 +10,9 @@ class CurationLogTest extends CTestCase
 {
     public function testMakeNewInstance()
     {
-        $datasetId = 1;
-        $creator = "System";
+        $datasetId = 8;
+        $user = User::model()-> find('id=:id', array(':id' => $datasetId));
+        $creator = $user->first_name . " " . $user->last_name;
         $curationLog = CurationLog::makeNewInstanceForDatasetBy($datasetId, $creator);
         $this->assertNotNull($curationLog);
         $this->assertTrue(is_a($curationLog, CurationLog::class));
