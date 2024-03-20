008d25e06ee589ec1a8ae0a49677e95e60201b11
Peter Li <peter@gigasciencejournal.com>
2024-03-20 21:47:54 +0800

    Separate file size and file name with a tab

--- gigadb/app/tools/files-metadata-console/scripts/md5.sh
+++ gigadb/app/tools/files-metadata-console/scripts/md5.sh
@@ -7,8 +7,13 @@ set -e
 # Run ./md5.sh *
 pwd=$(pwd)
 filename=$(basename $pwd)
-find .  -type f -exec md5sum {} \; > "$filename.md5"
-echo -e "Created $filename.md5"
+
+##############################################################
+# Create doi.md5 file containing md5 checksum values for files
+##############################################################
+
+find .  -type f ! -name 'md5.sh' -exec md5sum {} \; > "$filename.md5"
+echo "Created $filename.md5"
 
 ############################################################
 # Create doi.filesizes file containing file size information
@@ -20,12 +25,12 @@ S3_BUCKET="s3://gigadb-datasets-metadata"
 # Remove any existing filesizes file in current directory
 rm -f "$FILESIZE_FILE"
 
-for i in $(find .  -type f ! -name 'filesizes.sh');
+for i in $(find .  -type f ! -name 'md5.sh');
 do
   # Create file containing dataset file sizes
-  echo "$(wc -c $i)" >> "$FILESIZE_FILE"
+  echo -e "$(wc -c < $i)\t$i" >> "$FILESIZE_FILE"
 done
-echo -e "Created $FILESIZE_FILE"
+echo "Created $FILESIZE_FILE"
 
 # Copy files into S3 bucket
 aws s3 cp "$FILESIZE_FILE" "$S3_BUCKET"
