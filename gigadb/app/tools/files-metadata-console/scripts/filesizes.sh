c4315cc344cc7ab204efc922b301459f0bdcf2e6
Peter Li <peter@gigasciencejournal.com>
2024-03-20 15:17:40 +0800

    Tidy up shel script

--- gigadb/app/tools/files-metadata-console/scripts/filesizes.sh
+++ gigadb/app/tools/files-metadata-console/scripts/filesizes.sh
@@ -5,15 +5,18 @@
 pwd=$(pwd)
 filename=$(basename $pwd)
 
-# Remove any existing *.tsv file
-rm -f "$filename.tsv"
+FILESIZE_FILE="$filename.filesizes"
+S3_BUCKET="s3://gigadb-datasets-metadata"
+
+# Remove any existing filesizes file for dataset
+rm -f "$FILESIZE_FILE"
 
 for i in $(find .  -type f ! -name 'filesizes.sh');
 do
   # Create file containing dataset file sizes
-  echo "$(wc -c $i)" >> "$filename.tsv"
+  echo "$(wc -c $i)" >> "$FILESIZE_FILE"
 done
-echo -e "Created $filename.tsv file"
+echo -e "Created $FILESIZE_FILE file"
 
 # Copy file into S3 bucket
-aws s3 cp "$filename.tsv" s3://gigadb-datasets-metadata
+aws s3 cp "$FILESIZE_FILE" "$S3_BUCKET"
