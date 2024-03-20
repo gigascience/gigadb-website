b64bf03e353c367bf196feb43c15850c55a1d3a1
Peter Li <peter@gigasciencejournal.com>
2024-03-20 15:48:55 +0800

    Add functionality to create filesizes file into md5.sh

--- gigadb/app/tools/files-metadata-console/scripts/md5.sh
+++ gigadb/app/tools/files-metadata-console/scripts/md5.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
+# Exit script on error
+set -e
+
 # How to use: Place this script in the dataset root directory
 # Run ./md5.sh *
 pwd=$(pwd)
 filename=$(basename $pwd)
 find .  -type f -exec md5sum {} \; > "$filename.md5"
+echo -e "Created $filename.md5"
 
-# For FTP server access from Tencent backup server
-if test -f ~/.ftpconfig; then
-  source ~/.ftpconfig
-  curl -T "$filename.md5" -u $ftp_credentials "$ftp_server/datasets/"
-  echo -e "Uploaded $filename.md5 file to FTP server"
-fi
+############################################################
+# Create doi.filesizes file containing file size information
+############################################################
+
+FILESIZE_FILE="$filename.filesizes"
+S3_BUCKET="s3://gigadb-datasets-metadata"
+
+# Remove any existing filesizes file in current directory
+rm -f "$FILESIZE_FILE"
+
+for i in $(find .  -type f ! -name 'filesizes.sh');
+do
+  # Create file containing dataset file sizes
+  echo "$(wc -c $i)" >> "$FILESIZE_FILE"
+done
+echo -e "Created $FILESIZE_FILE"
+
+# Copy files into S3 bucket
+aws s3 cp "$FILESIZE_FILE" "$S3_BUCKET"
+aws s3 cp "$filename.md5" "$S3_BUCKET"
