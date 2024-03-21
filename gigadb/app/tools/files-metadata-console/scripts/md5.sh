346f87d15ef8c131b757eb3d5c9ba22ea07585e5
Peter Li <peter@gigasciencejournal.com>
2024-03-21 11:22:56 +0800

    Use alternate location for AWS config files for curators

--- gigadb/app/tools/files-metadata-console/scripts/md5.sh
+++ gigadb/app/tools/files-metadata-console/scripts/md5.sh
@@ -8,30 +8,27 @@ set -e
 pwd=$(pwd)
 filename=$(basename $pwd)
 
-##############################################################
-# Create doi.md5 file containing md5 checksum values for files
-##############################################################
-
-find .  -type f ! -name 'md5.sh' ! -name '*.filesizes' -exec md5sum {} \; > "$filename.md5"
-echo "Created $filename.md5"
-
-############################################################
-# Create doi.filesizes file containing file size information
-############################################################
-
+MD5_FILE="$filename.md5"
 FILESIZE_FILE="$filename.filesizes"
 S3_BUCKET="s3://gigadb-datasets-metadata"
 
-# Remove any existing filesizes file in current directory
-rm -f "$FILESIZE_FILE"
+# Create doi.md5 file containing md5 checksum values for files
+find .  -type f ! -name "$MD5_FILE" ! -name "$FILESIZE_FILE" -exec md5sum {} \; > "$MD5_FILE"
+echo "Created $MD5_FILE"
 
-for i in $(find .  -type f ! -name 'md5.sh' ! -name '*.filesizes');
+# Create doi.filesizes file containing file size information
+for i in $(find .  -type f ! -name "$MD5_FILE" ! -name "$FILESIZE_FILE");
 do
-  # Create file containing dataset file sizes
   echo -e "$(wc -c < $i)\t$i" >> "$FILESIZE_FILE"
 done
 echo "Created $FILESIZE_FILE"
 
+# In case we are on GigaDB file server
+if [[ $(uname -n) =~ cngb-gigadb-ftp ]];then
+  export AWS_CONFIG_FILE=/etc/aws/config
+  export AWS_SHARED_CREDENTIALS_FILE=/etc/aws/credentials
+fi
+  
 # Copy files into S3 bucket
 aws s3 cp "$FILESIZE_FILE" "$S3_BUCKET"
-aws s3 cp "$filename.md5" "$S3_BUCKET"
+aws s3 cp "$MD5_FILE" "$S3_BUCKET"
