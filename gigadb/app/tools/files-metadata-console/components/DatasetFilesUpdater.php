65094452f4bb6c73bd42871f95bab2a836095f98
Peter Li <peter@gigasciencejournal.com>
2024-03-22 20:06:41 +0800

    Use filesizes file stored in S3 bucket

--- gigadb/app/tools/files-metadata-console/components/DatasetFilesUpdater.php
+++ gigadb/app/tools/files-metadata-console/components/DatasetFilesUpdater.php
@@ -8,9 +8,7 @@ use Exception;
 use GigaDB\models\Dataset;
 use GigaDB\models\File;
 use GigaDB\services\URLsService;
-use Yii;
 use yii\base\Component;
-use yii\db\Query;
 
 /**
  * DatasetFilesUpdater
@@ -33,7 +31,13 @@ final class DatasetFilesUpdater extends Component
     public \GuzzleHttp\Client $webClient;
 
     /**
-     * Updates sizes for all files listed in doi.tsv
+     * @var string URLs helper functions (here we interested in batch grab of specific response header)
+     */
+    public string $GIGADB_DATASETS_METADATA_BUCKET_URL='https://s3.ap-northeast-1.amazonaws.com/gigadb-datasets-metadata';
+
+    /**
+     * Updates sizes for all files listed in doi.filesizes file located in 
+     * gigadb-datasets-metadata S3 bucket.
      *
      * @return int returns the number of files that has been successfully updated
      * @throws Exception
@@ -42,23 +46,34 @@ final class DatasetFilesUpdater extends Component
     {
         $success = 0;
         $d = Dataset::find()->where(['identifier' => $this->doi])->one();
-        
-        $filesizes = Yii::getAlias('@app') . '/filesizes/' . $this->doi . ".filesizes";
-        if (!file_exists($filesizes)) {
-            throw new Exception('File containing file size information could not be found!');
+
+        $filesizesURL = $this->GIGADB_DATASETS_METADATA_BUCKET_URL . '/' . $this->doi . '.filesizes';
+        $array = get_headers($filesizesURL);
+        if(!strpos($array[0],'200 OK')) {
+            throw new Exception("$filesizesURL not found");
         }
 
-        $content = file_get_contents($filesizes);
+        $content = file_get_contents($filesizesURL);
         $lines = explode("\n", $content);
         foreach($lines as $line) {
-            $tokens = explode(" ", $line);
+            $tokens = explode("\t", $line);
             $size = (int)$tokens[0];
             $filename = ltrim($tokens[1], './');
-            # Update file size
-            $f = File::find()->where(['name' => $filename, 'dataset_id' => $d->id])->one();
-            $f->size = $size;
-            if ($f->save()) {
-                $success++;
+            $filename = $this->doi . '/' . $filename;
+            
+            # Find file id for file to be updated
+            $result = (new \yii\db\Query())
+                ->select('id')
+                ->from('file')
+                ->where(['like', 'location', $filename])
+                ->one();
+            if($result) {
+                # Update file size
+                $f = File::find()->where(['id' => $result['id'], 'dataset_id' => $d->id])->one();
+                $f->size = $size;
+                if ($f->save()) {
+                    $success++;
+                }
             }
         }
         return $success;
