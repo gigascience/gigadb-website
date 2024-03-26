a9469dec1965d36a922cdb0bae395302ca62ae86
Rija Ménagé <rm.dev.git.commits@eml.cc>
2024-03-26 17:47:29 +0100

    fix: resolve mismatched private subnets
    Fix the following error that happened for each subnet
    Error: reading EC2 Subnet (10.99.*.0/24): couldn't find resource

--- ops/infrastructure/modules/efs-filesystem/efs-filesystem.tf
+++ ops/infrastructure/modules/efs-filesystem/efs-filesystem.tf
@@ -1,8 +1,6 @@
 locals {
 
   azs = slice(data.aws_availability_zones.available.names, 0, 3)
-  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
-  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
 
 }
 
@@ -11,10 +9,6 @@ data "aws_availability_zones" "available" {
 }
 
 data "aws_caller_identity" "current" {}
-
-data "aws_vpc" "selected" {
-  id = var.vpc_id
-}
 
 data "aws_region" "current" {}
 
@@ -49,15 +43,15 @@ module "efs" {
   ]
 
   # Mount targets / security group
-  mount_targets              = { for k, v in zipmap(local.azs, local.private_subnets) : k => { subnet_id = v } }
+  mount_targets              = { for k, v in zipmap(local.azs, var.vpc.private_subnets) : k => { subnet_id = v } }
 
   security_group_description = "gigadb-efs EFS SG for ${data.aws_caller_identity.current.arn} on ${var.deployment_target}"
-  security_group_vpc_id      = data.aws_vpc.selected.id
+  security_group_vpc_id      = var.vpc.vpc_id
   security_group_rules = {
     vpc = {
       # relying on the defaults provided for EFS/NFS (2049/TCP + ingress)
       description = "NFS ingress from VPC private subnets"
-      cidr_blocks = local.private_subnets
+      cidr_blocks = var.vpc.private_subnets_cidr_blocks
     }
   }
 
