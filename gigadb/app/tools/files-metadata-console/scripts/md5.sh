cda06053d8cc3a8cc35b7d965d1e23f4a458f916
kencho <kencho.gigascience@gmail.com>
2024-05-03 16:19:40 +0800

    refactor: update existing script instead creating a new one

--- gigadb/app/tools/files-metadata-console/scripts/md5.sh
+++ gigadb/app/tools/files-metadata-console/scripts/md5.sh
@@ -3,12 +3,20 @@
 # Exit script on error
 set -e
 
-pwd=$(pwd)
-filename=$(basename $pwd)
+if [[ -z "$1" ]]; then
+    echo "Error: DOI is required!"
+    echo "Usage: calculate_checksum_sizes.sh <DOI>"
+    exit 1
+fi
 
-MD5_FILE="$filename.md5"
-FILESIZE_FILE="$filename.filesizes"
-S3_BUCKET="s3://gigadb-datasets-metadata"
+doi="$1"
+
+MD5_FILE="$doi.md5"
+FILESIZE_FILE="$doi.filesizes"
+S3_BUCKET="gigadb-datasets-metadata"
+
+echo $MD5_FILE
+echo $FILESIZE_FILE
 
 # Create doi.md5 file containing md5 checksum values for files
 find .  -type f ! -name "$MD5_FILE" ! -name "$FILESIZE_FILE" -exec md5sum {} \; > "$MD5_FILE"
@@ -21,12 +29,8 @@ do
 done
 echo "Created $FILESIZE_FILE"
 
-# In case we are on GigaDB file server
-if [[ $(uname -n) =~ cngb-gigadb-ftp ]];then
-  export AWS_CONFIG_FILE=/etc/aws/config
-  export AWS_SHARED_CREDENTIALS_FILE=/etc/aws/credentials
+# In case we are on the bastion
+if [[ $(uname -n) =~ compute ]];then
+  rclone copy -v "$FILESIZE_FILE" s3_metadata:"S3_BUCKET"
+  rclone copy -v "$MD5_FILE" s3_metadata:"S3_BUCKET"
 fi
-  
-# Copy files into S3 bucket
-aws s3 cp "$FILESIZE_FILE" "$S3_BUCKET"
-aws s3 cp "$MD5_FILE" "$S3_BUCKET"
