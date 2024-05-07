91888b9e87d21d270b5f6d37a851a480203d0526
kencho <kencho.gigascience@gmail.com>
2024-05-07 09:51:52 +0800

    refactor: export aws credentials as env var

--- gigadb/app/tools/files-metadata-console/scripts/md5.sh
+++ gigadb/app/tools/files-metadata-console/scripts/md5.sh
@@ -31,6 +31,7 @@ echo "Created $FILESIZE_FILE"
 
 # In case we are on the bastion
 if [[ $(uname -n) =~ compute ]];then
-  rclone copy -v "$FILESIZE_FILE" s3_metadata:"S3_BUCKET"
-  rclone copy -v "$MD5_FILE" s3_metadata:"S3_BUCKET"
+  export AWS_SHARED_CREDENTIALS_FILE=/etc/aws/credentials
+  rclone copy -v "$FILESIZE_FILE" aws_metadata:"S3_BUCKET"
+  rclone copy -v "$MD5_FILE" aws_metadata:"S3_BUCKET"
 fi
