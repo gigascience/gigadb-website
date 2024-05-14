e80db0c036ee21abfcd69539e82b7fefa16af666
kencho <kencho.gigascience@gmail.com>
2024-05-14 12:04:55 +0800

    refactor: make sure only update md5 value for exiting file

--- protected/commands/FilesCommand.php
+++ protected/commands/FilesCommand.php
@@ -71,7 +71,12 @@ class FilesCommand extends CConsoleCommand
                         'dataset_id' => $dataset->id,
                         'name' => $filename,
                     ));
-                    $file->updateMd5Checksum($md5_value);
+                    if ($file !== null)
+                    {
+                        $file->updateMd5Checksum($md5_value);
+                    } else {
+                        echo "File not found: $filename\n";
+                    }
                 }
             }
         }
