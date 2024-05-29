b53db43b40232bd09685f7c3f0f8038ef560b8ec
kencho <kencho.gigascience@gmail.com>
2024-05-29 11:00:45 +0800

    refactor: update the function name

--- protected/views/shared/_header.php
+++ protected/views/shared/_header.php
@@ -9,7 +9,7 @@
                             <li><a href="/site/login"><i class="fa fa-sign-in"></i> Login / Signup</a></li>
                             <? } else {
 
-                            $name = Yii::app()->user->getFirst_Name();
+                            $name = Yii::app()->user->getFirstName();
 
                             if (substr($name, -1) === 's') {
 
