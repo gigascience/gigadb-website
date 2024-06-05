7767427a2e00815f0dc08e7df8477f241bcbc716
kencho <kencho.gigascience@gmail.com>
2024-06-05 14:59:50 +0800

    refactor: link the dir restore to the container

--- gigadb/app/tools/files-url-updater/databaseReset.sh
+++ gigadb/app/tools/files-url-updater/databaseReset.sh
@@ -14,25 +14,20 @@ else
   backupDate=$1
 fi
 
-# load the upstream backup into RDS
-docker run --rm --env-file .env -v /home/centos/.config/rclone/rclone.conf:/root/.config/rclone/rclone.conf --entrypoint /restore_database_from_s3_backup.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT
+# Download the upstream backup and load it into RDS
+docker run --rm --env-file .env -v /home/centos/.config/rclone/rclone.conf:/root/.config/rclone/rclone.conf -v /home/centos/restore:/restore --entrypoint /restore_database_from_s3_backup.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT "$backupDate"
 
 # Create the drop and add constraints queries that need to be run before and after schema migration respectively
 docker run --rm -e YII_PATH=/var/www/vendor/yiisoft/yii -v /home/centos:/var/www/protected/runtime registry.gitlab.com/$GITLAB_PROJECT_NAME/production_app:$GIGADB_ENVIRONMENT /var/www/protected/scripts/prepareConstraints.sh
 
-# drop constraints/indexes/triggers before running migrations
+# Drop constraints/indexes/triggers before running migrations
 docker run --rm --env-file .env -v /home/centos:/sql --entrypoint /dropConstraints.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT
 
 # Run migrations
 docker run --rm -e YII_PATH=/var/www/vendor/yiisoft/yii -v /home/centos:/var/www/protected/runtime registry.gitlab.com/$GITLAB_PROJECT_NAME/production_app:$GIGADB_ENVIRONMENT /var/www/protected/scripts/updateDBSchema.sh
 
-#  Restore constraints/indexes/triggers
+# Restore constraints/indexes/triggers
 docker run --rm --env-file .env -v /home/centos:/sql --entrypoint /addConstraints.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT
 
 # Housekeeping
-
-#docker stop pg9_3
-
-rm -f /home/centos/converted/gigadbv3*.backup
-
-rm -f /home/centos/downloads/gigadbv3*.backup
+rm -f /home/centos/restore/gigadb*.backup
