eb9320fa2d0ba2fd3745953a979e7156e451c01e
kencho <kencho.gigascience@gmail.com>
2024-06-05 11:09:51 +0800

    feat: get the upstream backup and restore

--- gigadb/app/tools/files-url-updater/databaseReset.sh
+++ gigadb/app/tools/files-url-updater/databaseReset.sh
@@ -14,22 +14,8 @@ else
   backupDate=$1
 fi
 
-# Stop any existing legacy database server if it hasn't done so in previous run (e.g: due to errors)
-docker stop pg9_3 || true
-
-# Spin up legacy database (PosgresQL 9.3)
-# Make sure the port 5432 is accessible from host
-docker run --rm --detach --name pg9_3 -p 5432:5432 registry.gitlab.com/$GITLAB_PROJECT_NAME/production_pg9_3:$GIGADB_ENVIRONMENT
-
-# Download/restore legacy database on the legacy server
-# make sure to define --add-host, so the files-url-updater command can connect to the host on port 5432 using ``host.docker.internal``
-docker run --rm --add-host=host.docker.internal:host-gateway -v /home/centos:/logs -v /home/centos/downloads:/downloads registry.gitlab.com/$GITLAB_PROJECT_NAME/production_updater:$GIGADB_ENVIRONMENT ./yii dataset-files/download-restore-backup --date $backupDate --force
-
-# Export legacy database as text in new format modern version of Pg can read
-docker run --rm --user=1000 --add-host=host.docker.internal:host-gateway -v /home/centos/converted:/converted registry.gitlab.com/$GITLAB_PROJECT_NAME/production_pg9_3:$GIGADB_ENVIRONMENT /exportLegacyToTextBackup.sh /converted/gigadbv3_${backupDate}.backup
-
-# load the converted backup into RDS
-docker run --rm --env-file .env -v /home/centos/converted:/converted --entrypoint /restore_database_from_converted_backup.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT /converted/gigadbv3_${backupDate}.backup
+# load the upstream backup into RDS
+docker run --rm --env-file .env -v /home/centos/.config/rclone/rclone.conf:/root/.config/rclone/rclone.conf --entrypoint /restore_database_from_s3_backup.sh registry.gitlab.com/$GITLAB_PROJECT_NAME/production_s3backup:$GIGADB_ENVIRONMENT
 
 # Create the drop and add constraints queries that need to be run before and after schema migration respectively
 docker run --rm -e YII_PATH=/var/www/vendor/yiisoft/yii -v /home/centos:/var/www/protected/runtime registry.gitlab.com/$GITLAB_PROJECT_NAME/production_app:$GIGADB_ENVIRONMENT /var/www/protected/scripts/prepareConstraints.sh
@@ -45,7 +31,7 @@ docker run --rm --env-file .env -v /home/centos:/sql --entrypoint /addConstraint
 
 # Housekeeping
 
-docker stop pg9_3
+#docker stop pg9_3
 
 rm -f /home/centos/converted/gigadbv3*.backup
 
