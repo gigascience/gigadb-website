5c583cfb4216d6973e4e3e695bdbdd6e6c28f96b
Allison Guilhem <allison.guilhem@gmail.com>
2024-06-12 11:54:11 +0200

    fix: update projects url and name

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -15,7 +15,9 @@
  */
 class Project extends CActiveRecord
 {
-	/**
+    const SCENARIO_CREATE = 'create';
+
+    /**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
 	 * @return Project the static model class
@@ -42,11 +44,11 @@ class Project extends CActiveRecord
 		// will receive user inputs.
 		return array(
 			array('url', 'required'),
-                        array('url', 'url','message'=>'Please check the URL format'),
+            array('url', 'url','message'=>'Please check the URL format'),
 			array('url', 'length', 'max'=>128),
 			array('name', 'length', 'max'=>255),
 			array('image_location', 'length', 'max'=>100),
-                        array('url','check_duplicate'),
+            array('url','check_duplicate', 'on' => self::SCENARIO_CREATE),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
 			array('id, url, name, image_location', 'safe', 'on'=>'search'),
@@ -109,19 +111,16 @@ class Project extends CActiveRecord
     }
     
     function check_duplicate(){
-        
-        
         $db_url= Project::model()->findBySql("select name from project where url='$this->url'");
         
-        if($db_url !=null){
-        $this->addError('url','Duplicate URL');}
+        if ($db_url !=null) {
+            $this->addError('url','Duplicate URL');
+        }
         
         $db_name= Project::model()->findBySql("select url from project where name='$this->name'");
        
-        if($db_name !=null){
-        $this->addError('name','Duplicate Project Name');}
-        
-             
+        if ($db_name !=null) {
+            $this->addError('name','Duplicate Project Name');
+        }
     }
-
 }
