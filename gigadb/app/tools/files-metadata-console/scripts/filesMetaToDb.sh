ba3a2e6bf43f317a8164e1c6633ae09231733428
Peter Li <peter@gigasciencejournal.com>
2024-08-25 19:46:26 +0800

    Tidy up status messages

--- gigadb/app/tools/files-metadata-console/scripts/filesMetaToDb.sh
+++ gigadb/app/tools/files-metadata-console/scripts/filesMetaToDb.sh
@@ -7,14 +7,6 @@ export PATH
 
 DOI=$1
 
-if [[ $(uname -n) =~ compute ]];then
-  outputDir="/home/centos/uploadLogs"
-else
-  # For creating readme file on dev environment
-  # /home/curators is mapped to gigadb/app/tools/readme-generator/runtime/curators directory
-  outputDir="/home/curators"
-fi
-
 if [ -z "$DOI" ];then
   if [[ $(uname -n) =~ compute ]]; then
     echo -e "Usage: /usr/local/bin/filesMetaToDb <DOI>\n"
@@ -24,33 +16,19 @@ if [ -z "$DOI" ];then
   exit 1;
 fi
 
-updateMD5ChecksumStartMessage="\n* About to update files' MD5 Checksum as file attribute for $DOI"
-updateMD5ChecksumEndMessage="\nDone with updating files' MD5 Checksum as file attribute for $DOI. Process status is saved in file: $outputDir/updating-md5checksum-$DOI.txt"
-
-updateFileSizeStartMessage="\n* About to update files' size for $DOI"
-updateFileSizeEndMessage="\nDone with updating files' size for $DOI. Nb of successful changes saved in file: $outputDir/updating-file-size-$DOI.txt"
-
 if [[ $(uname -n) =~ compute ]];then
   . /home/centos/.bash_profile
 
-  echo -e "$updateMD5ChecksumStartMessage"
+  echo -e "Updating md5 checksum values as file attributes for $DOI"
   docker run --rm -v /var/share/gigadb/metadata:/var/share/gigadb/metadata "registry.gitlab.com/$GITLAB_PROJECT/production-files-metadata-console:$GIGADB_ENV" ./yii update/md5-values --doi="$DOI"
-  echo -e "$updateMD5ChecksumEndMessage"
-
-  echo -e "$updateFileSizeStartMessage"
+  echo -e "Updating file sizes for $DOI"
   docker run --rm -v /var/share/gigadb/metadata:/var/share/gigadb/metadata "registry.gitlab.com/$GITLAB_PROJECT/production-files-metadata-console:$GIGADB_ENV" ./yii update/file-sizes --doi="$DOI"
-  echo -e "$updateFileSizeEndMessage"
-
-  echo -e "\nUpdate files meta data to database done!"
 
 else
-
-  echo -e "$updateMD5ChecksumStartMessage"
+  echo -e "Updating md5 checksum values as file attributes for $DOI"
   docker-compose run --rm files-metadata-console ./yii update/md5-values --doi="$DOI"
-  echo -e "$updateMD5ChecksumEndMessage"
-
-  echo -e "$updateFileSizeStartMessage"
+  echo -e "Updating file sizes for $DOI"
   docker-compose run --rm files-metadata-console ./yii update/file-sizes --doi="$DOI"
-  echo -e "$updateFileSizeEndMessage"
 fi
 
+echo -e "Updated file metadata for $DOI in database"
