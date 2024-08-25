73c6f824f3018502805694aa9e9d665905015c6b
Peter Li <peter@gigasciencejournal.com>
2024-08-25 17:53:45 +0800

    Add steps missing from curator workflow

--- gigadb/app/tools/files-metadata-console/scripts/postUpload.sh
+++ gigadb/app/tools/files-metadata-console/scripts/postUpload.sh
@@ -1,43 +1,73 @@
 #!/usr/bin/env bash
+#
+# Perform post upload operations to:
+# 1. Create readme file
+# 2. Copy readme file to user dropbox
+# 3. Create doi.md5 and doi.filesizes files in user dropbox
+# 4. Update database with file md5 values and file sizes
 
+# Stop script upon error
 set -e
+
+# Allow all scripts to base themselves from directory where this postUpload.sh
+# script is located
+APP_SOURCE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 PATH=/usr/local/bin:$PATH
 export PATH
 
-DOI=$1
+############################################
+# Function to write error messages to STDERR
+############################################
+function err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
 
-currentPath=$(pwd)
-userOutputDir="$currentPath/uploadDir"
+#DOI=$1
+#currentPath=$(pwd)
+#userOutputDir="$currentPath/uploadDir"
 
-if [[ $(uname -n) =~ compute ]];then
-  outputDir="/home/centos/uploadLogs"
-else
-  # For creating readme file on dev environment
-  # /home/curators is mapped to gigadb/app/tools/readme-generator/runtime/curators directory
-  outputDir="/home/curators"
-fi
+# Parse command line parameters
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --doi)
+        doi=$2
+        shift
+        ;;
+    --dropbox)
+        dropbox=$2
+        shift
+        ;;
+    *)
+        echo "Invalid option: $1"
+        exit 1  ## Could be optional.
+        ;;
+    esac
+    shift
+done
 
-# Check if DOI is not set or empty
-if [[ -z "$DOI" ]]; then
+# Check if DOI, user dropbox is not set or empty
+if [ -z "$doi" ] || [ -z "$dropbox" ]; then
   if [[ $(uname -n) =~ compute ]]; then
-    echo -e "Usage: /usr/local/bin/postUpload <DOI>\n"
+    echo -e "Usage: /usr/local/bin/postUpload --doi <DOI> --dropbox <DROPBOX>\n"
   else
-    echo -e "Usage: ./postUpload <DOI>\n"
+    echo -e "Usage: ./postUpload --doi <DOI> --dropbox <DROPBOX>\n"
   fi
   exit 1
 fi
 
-updateFileMetaDataStartMessage="\n* About to update files' size and MD5 checksum for $DOI"
-updateFileMetaDataEndMessage="\nDone with updating files' size and MD5 checksum for $DOI."
+updateFileMetaDataStartMessage="Updating file sizes and MD5 checksums for $doi"
+updateFileMetaDataEndMessage="\nDone with updating files' size and MD5 checksum for $doi."
 
 #checkValidUrlsStartMessage="\n* About to check that file urls are valid for $DOI"
 #checkValidUrlsEndMessage="\nDone with checking that file urls are valid for $DOI. Invalid Urls (if any) are save in file: $outputDir/invalid-urls-$DOI.txt"
 
-createReadMeFileStartMessage="\n* About to create the README file for $DOI"
-createReadMeFileEndMessage="\nDone with creating the README file for $DOI."
+createReadMeFileStartMessage="Creating README file for $doi"
+createReadMeFileEndMessage="\nDone with creating the README file for $doi."
 
 if [[ $(uname -n) =~ compute ]];then
+  outputDir="/home/centos/uploadLogs"
+  
   . /home/centos/.bash_profile
 
 # Execute create readme script
@@ -65,18 +95,36 @@ if [[ $(uname -n) =~ compute ]];then
 
 else  # Running on dev environment
 
+  # Check user dropbox exists
+  if [ ! -d "$APP_SOURCE/../../files-metadata-console/tests/_data/dropbox/$dropbox" ]; then
+    err "User dropbox at gigadb-website/app/tools/files-metadata-console/tests/_data/dropbox/$dropbox does not exist"
+    exit 1
+  fi
+
+  # For creating readme file on dev environment
+  # /home/curators is mapped to gigadb/app/tools/readme-generator/runtime/curators directory
+  outputDir="/home/curators"
+
   # Execute readme tool first to create readme-generator/runtime/curators
   # directory which /home/curators is mapped to
-  echo -e "$createReadMeFileStartMessage"
-  cd ../../readme-generator
+  echo -e "Creating README file for $doi"
+  cd "$APP_SOURCE"/../../readme-generator
   # Create readme file and upload to Wasabi dev directory
-  ./createReadme.sh --doi "$DOI" --outdir "$outputDir" --wasabi --apply
-  echo -e "$createReadMeFileEndMessage"
+  ./createReadme.sh --doi "$doi" --outdir "$outputDir" --wasabi --apply
 
-  echo -e "$updateFileMetaDataStartMessage"
-  cd ../files-metadata-console/scripts
-  ./filesMetaToDb.sh "$DOI"
-  echo -e "$updateFileMetaDataEndMessage"
+  # Copy readme file to dropbox
+  if [ "$outputDir" == '/home/curators' ]; then
+    cp "$APP_SOURCE"/../../readme-generator/runtime/curators/readme_"$doi".txt "$APP_SOURCE/../../files-metadata-console/tests/_data/dropbox/$dropbox"
+  fi
+
+  # Create file sizes and md5 metadata files
+  echo -e "Creating dataset metadata files for $doi"
+  cd "${APP_SOURCE}/../../files-metadata-console/tests/_data/dropbox/$dropbox"
+  docker-compose run --rm -w /gigadb/app/tools/files-metadata-console/tests/_data/dropbox/"$dropbox" files-metadata-console ../../../../scripts/md5.sh "$doi"
+
+  echo -e "Updating file sizes and MD5 values for $doi"
+  cd "$APP_SOURCE"/../../files-metadata-console/scripts
+  ./filesMetaToDb.sh "$doi"
 
 #  Skip this because it requires dataset files to be in public directory
 #  echo -e "$checkValidUrlsStartMessage"
