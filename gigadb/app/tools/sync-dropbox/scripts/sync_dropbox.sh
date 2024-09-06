e14356de9a1719ceaa859983ee7bc76607ce21e1
kencho <kencho.gigascience@gmail.com>
2024-09-06 16:32:24 +0800

    refactor: make the script more versitle

--- gigadb/app/tools/sync-dropbox/scripts/sync_dropbox.sh
+++ gigadb/app/tools/sync-dropbox/scripts/sync_dropbox.sh
@@ -4,9 +4,11 @@
 set -e
 
 if [[ $(uname -n) =~ compute ]]; then
-  source "./files-env"
+  source "./.bash_profile"
+  RCLONE_CONF='/etc/sync_dropbox/rclone.conf'
 else
   source "./.env"
+  RCLONE_CONF='config/rclone.conf'
 fi
 
 usage_message="Usage: $0 <option> \n
@@ -27,13 +29,14 @@ fi
 # Setup logging
 function set_up_logging() {
   if [[ $(uname -n) =~ compute ]];then
-    LOGDIR="/var/logs/sync_dropbox/"
+    LOGDIR="/var/log/gigadb/sync_dropbox"
   else
     currentPath=$(pwd)
     LOGDIR="$currentPath/logs"
+    SHAREDATADIR="data/share/dropbox"
   fi
   LOGFILE="$LOGDIR/sync_dropbox_$(date +'%Y%m%d_%H%M%S').log"
-  mkdir -p "${LOGDIR}"
+  mkdir -p "${LOGDIR}" "${SHAREDATADIR}"
   touch "${LOGFILE}"
 }
 
@@ -57,34 +60,40 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-# Dev rclone config
-DEV_RCLONE_CONF_LOCATION='config/rclone.conf'
-
 function start_sync () {
-  echo -e "$(date +'%Y/%m/%d %H:%M:%S') INFO : Start sync dropbox from production to alt-production" >> "${LOGFILE}"
-  rclone_sync_cmd="rclone sync production-staging:/share/dropbox/ /share/dropbox"
-  
-  if [[ "${dry_run}" == true ]]; then
-      rclone_sync_cmd+=" --dry-run"
-  fi
+ # Determine the rclone sync command based on the environment
+  case "${GIGADB_ENV}" in
+    dev)
+      ENV=$(awk 'NR==1 {print $1}' ${RCLONE_CONF} | tr -d '[]')
+      echo -e "$(date +'%Y/%m/%d %H:%M:%S') INFO : Start sync dropbox from ${ENV} to alt ${GIGADB_ENV}" | tee -a "${LOGFILE}"
+      rclone_sync_cmd="rclone sync production-staging:/share/dropbox/ ${SHAREDATADIR}"
+      ;;
+    staging)
+      ENV=$(awk 'NR==1 {print $1}' ${RCLONE_CONF} | tr -d '[]')
+      echo -e "$(date +'%Y/%m/%d %H:%M:%S') INFO : Start sync dropbox from ${ENV} to alt ${GIGADB_ENV}" | tee -a "${LOGFILE}"
+      rclone_sync_cmd="rclone sync production-staging:/share/dropbox/ /share/dropbox"
+      ;;
+    live)
+      ENV=$(awk 'NR==10 {print $10}' ${RCLONE_CONF} | tr -d '[]')
+      echo -e "$(date +'%Y/%m/%d %H:%M:%S') INFO : Start sync dropbox from ${ENV} to alt ${GIGADB_ENV}" | tee -a "${LOGFILE}"
+      rclone_sync_cmd="rclone sync production-live:/share/dropbox/ /share/dropbox"
+      ;;
+  esac
 
-  if [[ "${GIGADB_ENV}" == dev ]];then
-    rclone_sync_cmd+=" --config ${DEV_RCLONE_CONF_LOCATION}"
-  fi
+  # Append options
+  [[ "${dry_run}" == true ]] && rclone_sync_cmd+=" --dry-run"
+  rclone_sync_cmd+=" --config ${RCLONE_CONF}"
+  rclone_sync_cmd+=" --log-file ${LOGFILE} --log-level INFO --stats-log-level DEBUG"
 
-  rclone_sync_cmd+=" --log-file ${LOGFILE}"
-  rclone_sync_cmd+=" --log-level INFO"
-  rclone_sync_cmd+=" --stats-log-level DEBUG"
-  rclone_sync_cmd+=" >> ${LOGFILE}"
   # Execute command
   eval "${rclone_sync_cmd}"
   rclone_sync_exit_code=$?
   
-  echo "$(date +'%Y/%m/%d %H:%M:%S') INFO  : Executed: ${rclone_sync_cmd}" >> "$LOGFILE"
+  echo "$(date +'%Y/%m/%d %H:%M:%S') INFO  : Executed: ${rclone_sync_cmd}" | tee -a "$LOGFILE"
   if [ ${rclone_sync_exit_code} -eq 0 ]; then
-    echo -e "$(date +'%Y/%m/%d %H:%M:%S') INFO  : Successfully sync dropbox from production to alt-production" >> "${LOGFILE}"
+    echo -e "$(date +'%Y/%m/%d %H:%M:%S') INFO  : Successfully sync dropbox from ${ENV} to alt ${GIGADB_ENV}" | tee -a "${LOGFILE}"
   else
-    echo -e "$(date +'%Y/%m/%d %H:%M:%S') ERROR  : Problem with the sync - rclone has exit code: ${rclone_sync_exit_code}" >> "${LOGFILE}"
+    echo -e "$(date +'%Y/%m/%d %H:%M:%S') ERROR  : Problem with the sync - rclone has exit code: ${rclone_sync_exit_code}" | tee -a "${LOGFILE}"
   fi
 }
 
