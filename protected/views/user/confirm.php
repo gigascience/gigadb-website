ae9c61f974b0b0530c051f7bf7f676070ec58ac8
luis-toptal <luis.martinez@toptal.com>
2024-11-06 17:31:38 +0700

    feat: update layout for forlorn pages

--- protected/views/user/confirm.php
+++ protected/views/user/confirm.php
@@ -1,12 +1,29 @@
-<? if ($user->is_activated) { ?>
-<h3>Your account has been activated</h3>
+<div class="container">
+  <? if ($user->is_activated) {
+    $this->widget('TitleBreadcrumb', [
+      'pageTitle' => 'Account activated',
+      'breadcrumbItems' => [
+        ['label' => 'Home', 'href' => '/'],
+        ['isActive' => true, 'label' => 'Account activated'],
+      ]
+    ]);
 
-<p><?= CHtml::link("Log in", array('site/login')) ?> to configure your account.</p>
-<? } else { ?>
-<h3>Account Pending</h3>
 
-You are now registered. We will contact you shortly. Feel free to
-or <?= CHtml::link("contact us", "mailto:" . Yii::app()->params['support_email']) ?>&nbsp;
-if you prefer.</p>
-<? } ?>
+    ?>
+    <p>Your account has been activated. <?= CHtml::link("Log in", array('site/login')) ?> to configure your account.</p>
+  <? } else {
+    $this->widget('TitleBreadcrumb', [
+      'pageTitle' => 'Account Pending',
+      'breadcrumbItems' => [
+        ['label' => 'Home', 'href' => '/'],
+        ['isActive' => true, 'label' => 'Account Pending'],
+      ]
+    ]);
 
+
+    ?>
+    <p>You are now registered. We will contact you shortly. Feel free to
+      or <?= CHtml::link("contact us", "mailto:" . Yii::app()->params['support_email']) ?>&nbsp;if you prefer.</p>
+  <? } ?>
+
+</div>
