2421999476c5699e97ce0454e68616f62ae01214
luis-toptal <luis.martinez@toptal.com>
2024-11-20 21:27:36 +0700

    feat: updates team roles

--- protected/views/site/team.php
+++ protected/views/site/team.php
@@ -14,26 +14,17 @@
                 <article class="team-card">
                     <img src="../images/team/chunts.jpg" alt="Chris Hunter's portrait">
                     <div class="team-content">
-                        <h2 class="h3">Chris Hunter<br />
-                            <small class="member-role">GigaDB Director</small>
+                        <h2 class="h3">Chris Hunter, PhD<br />
+                            <small class="member-role">Director of GigaDB</small>
                         </h2>
                         <p>With a background in Genetics/Genomics, non-coding RNA, Cancer genetics, metagenomics and a long career in BioCuration our lead biocurator heads up the team responsible for archiving the data and metadata in GigaDB.</p>
-                    </div>
-                </article>
-                <article class="team-card">
-                    <img src="../images/team/chrisa.jpg" alt="Chris Armit's portrait">
-                    <div class="team-content">
-                        <h2 class="h3">Chris Armit<br />
-                            <small class="member-role">Data Scientist</small>
-                        </h2>
-                        <p>I am a Developmental Biologist with a strong interest in image informatics and next-gen imaging</p>
                     </div>
                 </article>
                 <article class="team-card">
                     <img src="../images/team/marya.jpg" alt="Mary Ann Tuli's portrait">
                     <div class="team-content">
                         <h2 class="h3">Mary Ann Tuli<br />
-                            <small class="member-role">Data Editor</small>
+                            <small class="member-role">Senior Data Editor</small>
                         </h2>
                         <p>I have worked as a BioCurator for over 20 years, first with EMBL-Bank and then as the curator of genetic data for the model organism database, WormBase.</p>
                     </div>
@@ -42,7 +33,7 @@
                     <img src="../images/team/rija.jpg" alt="Rija Ménagé's portrait">
                     <div class="team-content">
                         <h2 class="h3">Rija Ménagé<br />
-                            <small class="member-role">Principal Software Engineer</small>
+                            <small class="member-role">Senior Software Engineer</small>
                         </h2>
                         <p>Crafting software since 1999.</p>
                     </div>
@@ -50,8 +41,8 @@
                 <article class="team-card">
                     <img src="../images/team/hongling.png" alt="HongLing Zhou's portrait">
                     <div class="team-content">
-                        <h2 class="h3">HongLing Zhou<br />
-                            <small class="member-role">Junior Editor</small>
+                        <h2 class="h3">Zhou Hongling<br />
+                            <small class="member-role">Publishing Deputy Director</small>
                         </h2>
                         <p>Aiding in assessing and coordinating submission of scientific manuscripts and data, and handling all of the GigaScience social media accounts in China.</p>
                     </div>
@@ -59,16 +50,16 @@
                 <article class="team-card">
                     <img src="../images/team/qi.png" alt="Qi Chen's portrait">
                     <div class="team-content">
-                        <h2 class="h3">Qi Chen<br />
-                            <small class="member-role">Senior secretary</small>
+                        <h2 class="h3">Chen Qi<br />
+                            <small class="member-role">Editorial Assistant</small>
                         </h2>
                     </div>
                 </article>
                 <article class="team-card">
                     <img src="../images/team/hans.png" alt="Hans Zauner's portrait">
                     <div class="team-content collapsed">
-                        <h2 class="h3">Hans Zauner<br />
-                            <small class="member-role">Assistant Editor</small>
+                        <h2 class="h3">Hans Zauner, PhD<br />
+                            <small class="member-role">Editor</small>
                         </h2>
                         <p>Hans is Assistant Editor at GigaScience and also writes for Laborjournal. Many years ago, Hans did research in evolutionary and developmental biology. He clipped fish fins and handled a laser beam to shoot at reproductive organs of worms. Now in safe distance from any lab bench, he works on various science communication projects.</p>
                     </div>
@@ -77,8 +68,8 @@
                 <article class="team-card">
                     <img src="../images/team/nicole.png" alt="Nicole Nogoy's portrait">
                     <div class="team-content collapsed">
-                        <h2 class="h3">Nicole Nogoy<br />
-                            <small class="member-role">Editor</small>
+                        <h2 class="h3">Nicole Nogoy, PhD<br />
+                            <small class="member-role">Executive Editor</small>
                         </h2>
                         <p>Nicole has a PhD in Natural Sciences (Cardiovascular Molecular Medicine) from the University of Goettingen, Germany and obtained her BSc Hons in Biochemistry & Molecular Biology and Physiology from Victoria University of Wellington, New Zealand. Nicole has over a decade of experience in the STM publishing industry – most notably having launched and been Managing Editor of Genome Medicine, as well as the Commissioning Editor of several review journals for the Future Science Group. Nicole is now Editor with GigaScience having joined the team in 2012. She is also an Open Science, Open Data and reproducible research advocate.</p>
                     </div>
@@ -87,8 +78,8 @@
                 <article class="team-card">
                     <img src="../images/team/peter.png" alt="Peter Li's portrait">
                     <div class="team-content">
-                        <h2 class="h3">Peter Li<br />
-                            <small class="member-role">Lead Data Manager</small>
+                        <h2 class="h3">Peter Li, PhD<br />
+                            <small class="member-role">Head of IT</small>
                         </h2>
                     </div>
                 </article>
@@ -104,8 +95,8 @@
                 <article class="team-card">
                     <img src="../images/team/scott.png" alt="Scott Edmunds's portrait">
                     <div class="team-content collapsed">
-                        <h2 class="h3">Scott Edmunds<br />
-                            <small class="member-role">Executive Editor</small>
+                        <h2 class="h3">Scott Edmunds, PhD<br />
+                            <small class="member-role">Editor-in-Chief</small>
                         </h2>
                         <p>Executive Editor for GigaScience journal. Having worked in Open Access publishing for over a decade, his before that worked as a cancer molecular biologist in London and Lyon. Based in the Hong Kong office, as a self-proclaimed data nerd, outside of his day job he is Executive Committee member for Open Data Hong Kong and runs citizen science projects such as Citizenscience.Asia and the Bauhinia Genome project. Also teaching Data Management at HKU.</p>
                     </div>
@@ -114,8 +105,8 @@
                 <article class="team-card">
                     <img src="../images/team/hongfang.png" alt="HongFang Zhang's portrait">
                     <div class="team-content">
-                        <h2 class="h3">HongFang Zhang<br />
-                            <small class="member-role">Junior Editor</small>
+                        <h2 class="h3">Zhang Hongfang<br />
+                            <small class="member-role">Assistant Editor</small>
                         </h2>
                         <p>Junior editor for GigaScience and GigaByte journals.</p>
                     </div>
@@ -125,7 +116,7 @@
                 <article class="team-card">
                     <img src="../images/team/yannan.jpeg" alt="Yannan Fan's portrait">
                     <div class="team-content">
-                        <h2 class="h3">Yannan Fan<br />
+                        <h2 class="h3">Fan Yannan<br />
                             <small class="member-role">Assistant Data Editor</small>
                         </h2>
                         <p>Background with web application development and plant genome evolutional analysis, and now she joins the team as a data editor.</p>
@@ -135,7 +126,7 @@
                     <img src="../images/team/image-coming-soon.jpg" alt="Nafisa Qazi's portrait">
                     <div class="team-content">
                         <h2 class="h3">Nafisa Qazi<br />
-                            <small class="member-role">Editorial Office Manager (GigaScience Press)</small>
+                            <small class="member-role">Office Manager</small>
                         </h2>
                         <p>Nafisa works on both GigaScience, and GigaByte Journals, overseeing the communications between journal editors and database curators, as well as managing the GigaByte accounts and production.</p>
                     </div>
@@ -143,10 +134,19 @@
                 <article class="team-card">
                     <img src="../images/team/laurie.png" alt="Laurie Goodman's portrait">
                     <div class="team-content collapsed">
-                        <h2 class="h3">Laurie Goodman<br />
-                            <small class="member-role">Editor-in-Chief</small>
+                        <h2 class="h3">Laurie Goodman, PhD<br />
+                            <small class="member-role">Publishing Consultant</small>
                         </h2>
                         <p>Laurie received a BSc and MSc from Stanford University in 1986, and a PhD in Biochemistry and Molecular Biology from the University of Chicago in 1991. During her graduate work, she published a novel, A Spell of Deceit, with Del Rey Books. She completed a postdoctoral fellowship at the University of Colorado at Boulder then left the bench in 1995 to work as Assistant Editor at Nature Genetics. In 1997, she moved to Cold Spring Harbor Laboratory to become the founding Editor of Genome Research and Managing Editor of Learning & Memory. In 2006, she served as the Director for the Broad Institute Museum during its incipient year. In addition to her GigaScience role, she carries out manuscript writing seminars and owns the USA-based company Goodman Writing & Editing, which provides high-level editing of scientific manuscripts and grants, with a specialty in editing manuscripts from non-native English speakers.</p>
+                    </div>
+                    <button class="btn toggle-more" aria-expanded="false">Show More</button>
+                </article>
+                 <article class="team-card">
+                    <img src="../images/team/image-coming-soon.jpg" alt="Laurie Goodman's portrait">
+                    <div class="team-content collapsed">
+                        <h2 class="h3">Bastien Molcrette, PhD<br />
+                            <small class="member-role">Data Editor</small>
+                        </h2>
                     </div>
                     <button class="btn toggle-more" aria-expanded="false">Show More</button>
                 </article>
