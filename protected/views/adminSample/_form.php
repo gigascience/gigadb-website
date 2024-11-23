6945bc1c6c6f95a5df24568547850af5fb20afa2
luis-toptal <luis.martinez@toptal.com>
2024-11-23 13:35:22 +0700

    feat: updates admin sample form

--- protected/views/adminSample/_form.php
+++ protected/views/adminSample/_form.php
@@ -1,11 +1,13 @@
-<div class="section form row">
+<div class="section form">
+    <?php $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'sample-form',
+        'enableAjaxValidation' => false,
+        'htmlOptions' => [
+            'class' => 'row'
+        ]
+    )); ?>
 
-    <div class="col-md-offset-3 col-md-6">
-        <?php $form = $this->beginWidget('CActiveForm', array(
-            'id' => 'sample-form',
-            'enableAjaxValidation' => false,
-        )); ?>
-
+    <div class="col-md-12">
         <p class="note">Fields with <span class="required">*</span> are required.</p>
 
         <?php if ($model->hasErrors()) : ?>
@@ -13,7 +15,9 @@
                 <?php echo $form->errorSummary($model); ?>
             </div>
         <?php endif; ?>
+    </div>
 
+    <div class="col-md-6">
         <div class="form-group">
             <?php echo $form->labelEx($model, 'species_id', array('class' => 'control-label')); ?>
             <?php
@@ -44,15 +48,10 @@
                 <?php echo $form->error($model, 'species_id'); ?>
             </div>
         </div>
+    </div>
 
+    <div class="col-md-6">
         <?php
-        $model->attributesList = $model->attributesList ? $model->attributesList :  $model->getAttributesList(true); // this sets the default text content of the textarea
-        $this->widget('application.components.controls.TextArea', [
-            'form' => $form,
-            'model' => $model,
-            'attributeName' => 'attributesList',
-        ]);
-
         $this->widget('application.components.controls.TextField', [
             'form' => $form,
             'model' => $model,
@@ -62,13 +61,29 @@
             ],
         ]);
         ?>
+    </div>
 
+    <div class="col-md-12">
+        <?php
+        $model->attributesList = $model->attributesList ? $model->attributesList :  $model->getAttributesList(true);
+        $this->widget('application.components.controls.TextArea', [
+            'form' => $form,
+            'model' => $model,
+            'attributeName' => 'attributesList',
+            'inputOptions' => [
+                'rows' => 6,
+                'cols' => 50
+            ],
+        ]);
+        ?>
+    </div>
+
+    <div class="col-md-12">
         <div class="pull-right btns-row">
             <a href="/adminSample/admin" class="btn background-btn-o">Cancel</a>
             <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
         </div>
-
-        <?php $this->endWidget(); ?>
     </div>
 
+    <?php $this->endWidget(); ?>
 </div>
