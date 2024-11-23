36fd6a7a3277bc858f9e510affe17787e95517d1
luis-toptal <luis.martinez@toptal.com>
2024-11-23 13:58:37 +0700

    feat: admin dataset project form

--- protected/views/adminDatasetProject/_form.php
+++ protected/views/adminDatasetProject/_form.php
@@ -1,55 +1,64 @@
-<div class="section form row">
+<div class="section form">
+	<?php $form = $this->beginWidget('CActiveForm', array(
+		'id' => 'dataset-project-form',
+		'enableAjaxValidation' => false,
+		'htmlOptions' => [
+			'class' => 'row'
+		]
+	)); ?>
 
-	<div class="col-md-offset-3 col-md-6">
-		<?php $form = $this->beginWidget('CActiveForm', array(
-			'id' => 'dataset-project-form',
-			'enableAjaxValidation' => false,
-		)); ?>
-
-		<?php if ($model->hasErrors()) : ?>
+	<?php if ($model->hasErrors()) : ?>
+		<div class="col-md-12">
 			<div class="alert alert-danger">
 				<?php echo $form->errorSummary($model); ?>
 			</div>
-		<?php endif; ?>
-
-		<?php
-		$this->widget('application.components.controls.DropdownField', [
-			'form' => $form,
-			'model' => $model,
-			'attributeName' => 'dataset_id',
-			'listDataOptions' => [
-				'data' => Util::getDois(),
-				'valueField' => 'id',
-				'textField' => 'identifier',
-			],
-			'inputOptions' => [
-				'required' => true,
-			],
-		]);
-		?>
-
-		<?php
-		$this->widget('application.components.controls.DropdownField', [
-			'form' => $form,
-			'model' => $model,
-			'attributeName' => 'project_id',
-			'listDataOptions' => [
-				'data' => Project::model()->findAll(),
-				'valueField' => 'id',
-				'textField' => 'name',
-			],
-			'inputOptions' => [
-				'required' => true,
-			],
-		]);
-		?>
-
-		<div class="pull-right btns-row">
-			<a href="/adminDatasetProject/admin" class="btn background-btn-o">Cancel</a>
-			<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
 		</div>
+	<?php endif; ?>
 
-		<?php $this->endWidget(); ?>
+	<?php
+	$this->widget('application.components.controls.DropdownField', [
+		'form' => $form,
+		'model' => $model,
+		'attributeName' => 'dataset_id',
+		'listDataOptions' => [
+			'data' => Util::getDois(),
+			'valueField' => 'id',
+			'textField' => 'identifier',
+		],
+		'inputOptions' => [
+			'required' => true,
+		],
+		'groupOptions' => [
+			'class' => 'col-md-6'
+		],
+	]);
+	?>
+
+	<?php
+	$this->widget('application.components.controls.DropdownField', [
+		'form' => $form,
+		'model' => $model,
+		'attributeName' => 'project_id',
+		'listDataOptions' => [
+			'data' => Project::model()->findAll(),
+			'valueField' => 'id',
+			'textField' => 'name',
+		],
+		'inputOptions' => [
+			'required' => true,
+		],
+		'groupOptions' => [
+			'class' => 'col-md-6'
+		],
+	]);
+	?>
+
+	<div class="col-md-12">
+		<div class="pull-right btns-row">
+			<a href="/adminDatasetProject/admin" class="btn background-btn-o btn-min-width">Cancel</a>
+			<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn btn-min-width')); ?>
+		</div>
 	</div>
 
+	<?php $this->endWidget(); ?>
 </div>
