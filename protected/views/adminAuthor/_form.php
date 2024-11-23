2cea6567ac745bd562e5d999800b56c3b5b6cf86
luis-toptal <luis.martinez@toptal.com>
2024-11-23 13:44:27 +0700

    feat: update admin author and admin sample forms

--- protected/views/adminAuthor/_form.php
+++ protected/views/adminAuthor/_form.php
@@ -1,18 +1,19 @@
-<div class="section form row">
+<div class="section form">
+	<?php $form = $this->beginWidget('CActiveForm', array(
+		'id' => 'author-form',
+		'enableAjaxValidation' => false,
+	)); ?>
 
-	<div class="col-md-offset-3 col-md-6">
-		<?php $form = $this->beginWidget('CActiveForm', array(
-			'id' => 'author-form',
-			'enableAjaxValidation' => false,
-		)); ?>
+	<div class="row">
+		<div class="col-md-12">
+			<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-		<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-		<?php if ($model->hasErrors()) : ?>
-			<div class="alert alert-danger">
-				<?php echo $form->errorSummary($model); ?>
-			</div>
-		<?php endif; ?>
+			<?php if ($model->hasErrors()) : ?>
+				<div class="alert alert-danger">
+					<?php echo $form->errorSummary($model); ?>
+				</div>
+			<?php endif; ?>
+		</div>
 
 		<?php
 		$this->widget('application.components.controls.TextField', [
@@ -23,6 +24,9 @@
 				'required' => true,
 				'maxlength' => 255
 			],
+			'groupOptions' => [
+				'class' => 'col-md-4'
+			],
 		]);
 
 		$this->widget('application.components.controls.TextField', [
@@ -32,7 +36,11 @@
 			'inputOptions' => [
 				'maxlength' => 255
 			],
+			'groupOptions' => [
+				'class' => 'col-md-4'
+			],
 		]);
+
 		$this->widget('application.components.controls.TextField', [
 			'form' => $form,
 			'model' => $model,
@@ -40,7 +48,11 @@
 			'inputOptions' => [
 				'maxlength' => 255
 			],
+			'groupOptions' => [
+				'class' => 'col-md-4'
+			],
 		]);
+
 		$this->widget('application.components.controls.TextField', [
 			'form' => $form,
 			'model' => $model,
@@ -48,7 +60,11 @@
 			'inputOptions' => [
 				'maxlength' => 255
 			],
+			'groupOptions' => [
+				'class' => 'col-md-12'
+			],
 		]);
+
 		$this->widget('application.components.controls.TextField', [
 			'form' => $form,
 			'model' => $model,
@@ -56,7 +72,11 @@
 			'inputOptions' => [
 				'maxlength' => 128
 			],
+			'groupOptions' => [
+				'class' => 'col-md-6'
+			],
 		]);
+
 		$this->widget('application.components.controls.TextField', [
 			'form' => $form,
 			'model' => $model,
@@ -64,55 +84,59 @@
 			'inputOptions' => [
 				'maxlength' => 128
 			],
+			'groupOptions' => [
+				'class' => 'col-md-6'
+			],
 		]);
 		?>
 
-		<div class="merge-author-info">
-			<?php
-			$identical_authors = $model->getIdenticalAuthors();
-			if (!empty($identical_authors)) {
-			?>
-				<div class="alert alert-gigadb-info">
-					this author is merged with author(s):
-					<ul class="list-unstyled">
-						<?php
-						foreach ($identical_authors as $author_id) {
-							$author = Author::model()->findByPk($author_id);
-							echo "<li>" . $author->getAuthorDetails() . "</li>";
-						}
-						?>
-					</ul>
+		<div class="col-md-12">
+			<div class="merge-author-info">
+				<?php
+				$identical_authors = $model->getIdenticalAuthors();
+				if (!empty($identical_authors)) {
+				?>
+					<div class="alert alert-gigadb-info">
+						this author is merged with author(s):
+						<ul class="list-unstyled">
+							<?php
+							foreach ($identical_authors as $author_id) {
+								$author = Author::model()->findByPk($author_id);
+								echo "<li>" . $author->getAuthorDetails() . "</li>";
+							}
+							?>
+						</ul>
+					</div>
+				<?php	} ?>
 
-				</div>
-
-			<?php	} ?>
-
-			<?php
-			if (!empty($identical_authors)) {
-				echo CHtml::link(
-					'Unmerge author from those authors',
-					array('adminAuthor/unmerge', 'id' => $model->id),
-					array('class' => 'btn btn-link')
-				);
-			}
-			?>
+				<?php
+				if (!empty($identical_authors)) {
+					echo CHtml::link(
+						'Unmerge author from those authors',
+						array('adminAuthor/unmerge', 'id' => $model->id),
+						array('class' => 'btn btn-link')
+					);
+				}
+				?>
+			</div>
 		</div>
 
-		<div class="pull-right btns-row">
-			<?php
-      if ($model->id) {
-        echo CHtml::link(
-          'Merge with an author',
-          array('adminAuthor/prepareAuthorMerge', 'origin_author_id' => $model->id),
-          array('class' => 'btn background-btn-o')
-        );
-      }
-			?>
-			<a href="/adminAuthor/admin" class="btn background-btn-o">Cancel</a>
-			<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
+		<div class="col-md-12">
+			<div class="pull-right btns-row">
+				<?php
+				if ($model->id) {
+					echo CHtml::link(
+						'Merge with an author',
+						array('adminAuthor/prepareAuthorMerge', 'origin_author_id' => $model->id),
+						array('class' => 'btn background-btn-o btn-min-width')
+					);
+				}
+				?>
+				<a href="/adminAuthor/admin" class="btn background-btn-o btn-min-width">Cancel</a>
+				<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn btn-min-width')); ?>
+			</div>
 		</div>
-
-		<?php $this->endWidget(); ?>
 	</div>
 
+	<?php $this->endWidget(); ?>
 </div>
--- protected/views/adminSample/_form.php
+++ protected/views/adminSample/_form.php
@@ -1,89 +1,91 @@
 <div class="section form">
-    <?php $form = $this->beginWidget('CActiveForm', array(
-        'id' => 'sample-form',
-        'enableAjaxValidation' => false,
-        'htmlOptions' => [
-            'class' => 'row'
-        ]
-    )); ?>
+  <?php $form = $this->beginWidget('CActiveForm', array(
+    'id' => 'sample-form',
+    'enableAjaxValidation' => false,
+    'htmlOptions' => [
+      'class' => 'row'
+    ]
+  )); ?>
 
-    <div class="col-md-12">
-        <p class="note">Fields with <span class="required">*</span> are required.</p>
+  <div class="col-md-12">
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-        <?php if ($model->hasErrors()) : ?>
-            <div class="alert alert-danger">
-                <?php echo $form->errorSummary($model); ?>
-            </div>
-        <?php endif; ?>
+    <?php if ($model->hasErrors()): ?>
+      <div class="alert alert-danger">
+        <?php echo $form->errorSummary($model); ?>
+      </div>
+    <?php endif; ?>
+  </div>
+
+  <div class="col-md-6">
+    <div class="form-group">
+      <?php echo $form->labelEx($model, 'species_id', array('class' => 'control-label')); ?>
+      <?php
+      $criteria = new CDbCriteria;
+      $criteria->select = 't.id, t.common_name';
+      $criteria->limit = 100;
+
+      $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
+        'name' => 'name',
+        'model' => $model,
+        'attribute' => 'species_id',
+        'source' => $this->createUrl('/adminDatasetSample/autocomplete'),
+        'options' => array(
+          'minLength' => '2',
+        ),
+        'htmlOptions' => array(
+          'aria-describedby' => $form->error($model, 'species_id') ? 'species_id-desc' : '',
+          'required' => true,
+          'aria-required' => 'true',
+          'class' => 'form-control',
+          'placeholder' => 'name',
+          'size' => 'auto',
+        ),
+      ));
+      ?>
+
+      <div role="alert" id="species_id-error">
+        <?php echo $form->error($model, 'species_id'); ?>
+      </div>
     </div>
+  </div>
 
-    <div class="col-md-6">
-        <div class="form-group">
-            <?php echo $form->labelEx($model, 'species_id', array('class' => 'control-label')); ?>
-            <?php
-            $criteria = new CDbCriteria;
-            $criteria->select = 't.id, t.common_name';
-            $criteria->limit = 100;
+  <?php
+  $this->widget('application.components.controls.TextField', [
+    'form' => $form,
+    'model' => $model,
+    'attributeName' => 'name',
+    'inputOptions' => [
+      'maxlength' => 50
+    ],
+    'groupOptions' => [
+      'class' => 'col-md-6'
+    ]
+  ]);
+  ?>
 
-            $this->widget('zii.widgets.jui.CJuiAutoComplete', array(
-                'name' => 'name',
-                'model' => $model,
-                'attribute' => 'species_id',
-                'source' => $this->createUrl('/adminDatasetSample/autocomplete'),
-                'options' => array(
-                    'minLength' => '2',
-                ),
-                'htmlOptions' => array(
-                    'aria-describedby' => $form->error($model, 'species_id') ? 'species_id-desc' : '',
-                    'required' => true,
-                    'aria-required' => 'true',
-                    'class' => 'form-control',
-                    'placeholder' => 'name',
-                    'size' => 'auto',
-                ),
-            ));
-            ?>
+  <?php
+  $model->attributesList = $model->attributesList ? $model->attributesList : $model->getAttributesList(true);
+  $this->widget('application.components.controls.TextArea', [
+    'form' => $form,
+    'model' => $model,
+    'attributeName' => 'attributesList',
+    'inputOptions' => [
+      'rows' => 6,
+      'cols' => 50
+    ],
+    'groupOptions' => [
+      'class' => 'col-md-12'
+    ]
+  ]);
+  ?>
 
-            <div role="alert" id="species_id-error">
-                <?php echo $form->error($model, 'species_id'); ?>
-            </div>
-        </div>
+  <div class="col-md-12">
+    <div class="pull-right btns-row">
+      <a href="/adminSample/admin" class="btn background-btn-o btn-min-width">Cancel</a>
+      <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn btn-min-width')); ?>
     </div>
+  </div>
 
-    <div class="col-md-6">
-        <?php
-        $this->widget('application.components.controls.TextField', [
-            'form' => $form,
-            'model' => $model,
-            'attributeName' => 'name',
-            'inputOptions' => [
-                'maxlength' => 50
-            ],
-        ]);
-        ?>
-    </div>
-
-    <div class="col-md-12">
-        <?php
-        $model->attributesList = $model->attributesList ? $model->attributesList :  $model->getAttributesList(true);
-        $this->widget('application.components.controls.TextArea', [
-            'form' => $form,
-            'model' => $model,
-            'attributeName' => 'attributesList',
-            'inputOptions' => [
-                'rows' => 6,
-                'cols' => 50
-            ],
-        ]);
-        ?>
-    </div>
-
-    <div class="col-md-12">
-        <div class="pull-right btns-row">
-            <a href="/adminSample/admin" class="btn background-btn-o">Cancel</a>
-            <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
-        </div>
-    </div>
-
-    <?php $this->endWidget(); ?>
+  <?php $this->endWidget(); ?>
 </div>
