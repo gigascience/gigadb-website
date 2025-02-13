64fa6ad729c81bc10f984b5774c3f2552091027c
luis-toptal <luis.martinez@toptal.com>
2025-02-13 16:30:34 +0700

    feat: add focus trap to mobile navbar

--- gigadb/app/client/js/mobile-navigation.js
+++ gigadb/app/client/js/mobile-navigation.js
@@ -1,3 +1,5 @@
+import { trapFocus } from './trap-focus.js';
+
 export function initMobileNavigation() {
   const $mobileNav = $("#mobileNavigation");
   const $toggleButton = $(".navbar-toggle");
@@ -12,6 +14,7 @@ export function initMobileNavigation() {
   $toggleButton.on("click", function () {
     $mobileNav.addClass("is-visible");
     $body.css("overflow", "hidden");
+    trapFocus($mobileNav);
   });
 
   $closeButton.on("click", function () {
