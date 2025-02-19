d2d2e9ed6979f373b70ba341a23271267ce5d856
luis-toptal <luis.martinez@toptal.com>
2025-02-19 22:12:06 +0700

    feat: responsive title bar

--- protected/views/shared/_titleBreadcrumb.php
+++ protected/views/shared/_titleBreadcrumb.php
@@ -1,10 +1,8 @@
-<div class="section page-title-section">
-    <div class="page-title">
-        <nav aria-label="breadcrumbs">
-          <ol class="breadcrumb pull-right">
-              <?= $breadcrumbHtml ?>
-          </ol>
-        </nav>
-        <?php echo CHtml::tag($pageTitleLevel, ['class' => 'h4', 'id' => $pageTitleId], $pageTitle); ?>
-    </div>
+<div class="title-bar">
+    <?php echo CHtml::tag($pageTitleLevel, ['class' => 'h4 title title-bar-title', 'id' => $pageTitleId], $pageTitle); ?>
+    <nav aria-label="title-bar-breadcrumb-nav">
+        <ol class="title-bar-breadcrumb-list">
+            <?= $breadcrumbHtml ?>
+        </ol>
+    </nav>
 </div>
