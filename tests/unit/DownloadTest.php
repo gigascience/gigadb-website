fc217b01b518f412e5d90a712e25bcfc8948f9d2
kencho <kencho.gigascience@gmail.com>
2025-02-20 15:10:59 +0800

    Revert "refactor: skip as uniprot api is not working"
    This reverts commit 6d08c666db14f2519ee3a21d31a7ee81a23a008f.

--- tests/unit/DownloadTest.php
+++ tests/unit/DownloadTest.php
@@ -33,8 +33,6 @@ class DownloadTest extends \Codeception\Test\Unit
 
     /**
      * Test that a remote file exists
-     *
-     * @skip as the uniprot service is unavailable now
      */
     public function testFileExists()
     {
