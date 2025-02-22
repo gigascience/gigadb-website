99d76c3971db31a5cf857f3710148581ea8c0f6c
luis-toptal <luis.martinez@toptal.com>
2025-02-22 17:45:15 +0700

    feat: responsive login page

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -17,7 +17,7 @@ $this->breadcrumbs = array(
         ]);
         ?>
 
-        <div class="subsection row" style="margin-bottom: 130px;">
+        <div class="subsection row">
             <div class="col-xs-12">
                 <?php if (Yii::app()->user->hasFlash('success-reset-password')) : ?>
                     <div class="alert alert-success">
@@ -25,15 +25,15 @@ $this->breadcrumbs = array(
                     </div>
                 <?php endif; ?>
             </div>
-            <div class="col-xs-6">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="subsection-login">
                     <p><?= Yii::t('app', 'Please fill out the following form with your login credentials:') ?></p>
                     <!-- inputs are already announced as required so screen readers do not need this message -->
                     <p aria-hidden="true"><?= Yii::t('app', 'Fields with <span class="symbol">*</span> are required.') ?></p>
                 </div>
 
-                <div class="login-div">
-                    <? $form = $this->beginWidget('CActiveForm', array('htmlOptions' => array('class' => 'form-horizontal'))) ?>
+                <div class="login-container">
+                    <? $form = $this->beginWidget('CActiveForm', array('htmlOptions' => array('class' => 'form-horizontal login-form'))) ?>
                     <div class="form-group">
                         <label class="col-xs-3 control-label error required" for="LoginForm_username">
                             Email Address<span class="required" aria-hidden="true">*</span>
@@ -63,9 +63,9 @@ $this->breadcrumbs = array(
                             <?= $form->label($model, 'rememberMe', array('disabled' => "disabled")) ?>
                         </div>
                     </div>
-                    <hr aria-hidden="true">
-                    <div class="button-div">
-                        <?= CHtml::submitButton(Yii::t('app', 'Login'), array('class' => 'btn background-btn', 'style' => 'width:236px;')) ?>
+                    <hr class="login-divider" aria-hidden="true">
+                    <div class="login-button-container">
+                        <?= CHtml::submitButton(Yii::t('app', 'Login'), array('class' => 'btn background-btn login-button m-0')) ?>
                     </div>
                     <? $this->endWidget() ?>
                     <div class="login-links">
