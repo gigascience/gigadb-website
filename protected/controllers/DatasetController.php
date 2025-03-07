b425da8ecc150ec06aa15d7445d25cc58d70be65
kencho <kencho.gigascience@gmail.com>
2025-03-07 14:31:04 +0800

    refactor: update the metaData contents

--- protected/controllers/DatasetController.php
+++ protected/controllers/DatasetController.php
@@ -107,17 +107,15 @@ class DatasetController extends Controller
                 ->setSearchForm();
 
             // Rendering section
-            $this->metaData['title'] = rtrim(strip_tags($assembly->getDataset()->title));
             $this->metaData['description'] = rtrim(strip_tags($assembly->getDataset()->description));
-            $this->metaData['doi'] = $assembly->getDataset()->identifier;
-            $this->metaData['doiUrl'] = 'http://dx.doi.org/10.5524/' . $assembly->getDataset()->identifier;
+            $this->metaData['doiUrl'] = 'https://doi.org/10.5524/' . $assembly->getDataset()->identifier;
             $this->metaData['imageUrl'] = $assembly->getDataset()->getImageUrl();
 
             $urlToRedirect = trim($assembly->getDataset()->getUrlToRedirectAttribute());
             $currentAbsoluteFullUrl = Yii::app()->request->getBaseUrl(true) . Yii::app()->request->url ;
 
             if ($urlToRedirect && $currentAbsoluteFullUrl == $urlToRedirect) {
-                $this->metaData['redirect'] = 'http://dx.doi.org/10.5524/' . $assembly->getDataset()->identifier ;
+                $this->metaData['redirect'] = 'https://doi.org/10.5524/' . $assembly->getDataset()->identifier ;
                 $this->render('interstitial', array(
                     'model' => $assembly->getDataset()
                 ));
