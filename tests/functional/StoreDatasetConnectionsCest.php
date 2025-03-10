bc76ed8d7f7bfff9cde66cfc9942a579d7e7c673
kencho <kencho.gigascience@gmail.com>
2025-03-10 15:30:42 +0800

    refactor: make the test assert h1 text only

--- tests/functional/StoreDatasetConnectionsCest.php
+++ tests/functional/StoreDatasetConnectionsCest.php
@@ -20,7 +20,7 @@ class StoreDatasetConnectionsCest
 
         # go to the dataset page
         $I->amOnPage("/dataset/view/id/100142");
-        $I->canSee("Supporting scripts and data for \"Investigation into the annotation of protocol sequencing steps in the Sequence Read Archive\".");
+        $I->assertContains("Supporting scripts and data for \"Investigation into the annotation of protocol sequencing steps in the Sequence Read Archive\".", $I->grabTextFrom('h1'));
         $I->dontSee("Read the peer-reviewed publication(s):");
     }
 }
