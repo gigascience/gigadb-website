c2b5355e7ff561c29d10a81147ad7a86ea3e935a
Rija Ménagé <rm.dev.git.commits@eml.cc>
2025-04-02 19:31:02 +0200

    fix: explicitly use AWS profile as a terraform variable for IAM username

--- ops/infrastructure/getIAMUserNameToJSON.sh
+++ ops/infrastructure/getIAMUserNameToJSON.sh
@@ -6,5 +6,6 @@
 # the output has to be valid JSON
 
 set -e
-userName=$(aws sts get-caller-identity --output text --query Arn | cut -d"/" -f2 | tr '[:upper:]' '[:lower:]')
+aws_profile=$1
+userName=$(aws --profile=$aws_profile sts get-caller-identity --output text --query Arn | cut -d"/" -f2 | tr '[:upper:]' '[:lower:]')
 jq -n --arg userName "$userName" '{"userName":$userName}'
