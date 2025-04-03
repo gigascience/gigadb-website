bd7c81f90fc9d3b5a6b9cac09f26f8db20ae7a46
Rija Ménagé <rm.dev.git.commits@eml.cc>
2025-04-03 16:49:14 +0200

    fix: specify version for hashicorp providers

--- ops/infrastructure/terraform.tf
+++ ops/infrastructure/terraform.tf
@@ -112,22 +112,22 @@ terraform {
   backend "http" {
   }
 
-  # required_providers {
-  #   random = {
-  #     source  = "hashicorp/random"
-  #     version = "3.5.1"
-  #   }
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
 
-  #   external = {
-  #     source  = "hashicorp/external"
-  #     version = "2.3.1"
-  #   }
-    
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = "5.5.0"
-  #   }
-  # }
+    external = {
+      source  = "hashicorp/external"
+      version = "2.3.1"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
 
   # required_version = ">= 1.1"
 }
