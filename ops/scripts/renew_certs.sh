580e8061834f166ffe9e7de9d692406626a83c7d
kencho <kencho.gigascience@gmail.com>
2025-04-22 22:19:49 +0800

    refactor: copy cert contents from symlinked files

--- ops/scripts/renew_certs.sh
+++ ops/scripts/renew_certs.sh
@@ -27,13 +27,13 @@ CHAIN_LINK=/etc/letsencrypt/live/$REMOTE_HOSTNAME/chain.pem
 # Definition of functions
 renew_cert() {
     echo "Read content of files"
-    fullchain=$($DOCKER cat $FULLCHAIN_PEM)
-    privkey=$($DOCKER cat $PRIVATE_PEM)
-    chain=$($DOCKER cat $CHAIN_PEM)
+    fullchain=$($DOCKER cat $FULLCHAIN_LINK)
+    privkey=$($DOCKER cat $PRIVATE_LINK)
+    chain=$($DOCKER cat $CHAIN_LINK)
 
   	echo "Renewing the certificate for $REMOTE_HOSTNAME"
   	docker run --rm -v ${REPO_NAME}_le_config:/etc/letsencrypt -v ${REPO_NAME}_le_webrootpath:/var/www/.le certbot/certbot certificates \
-  	&& docker run --rm -v ${REPO_NAME}_le_config:/etc/letsencrypt -v ${REPO_NAME}_le_webrootpath:/var/www/.le certbot/certbot renew --cert-name $REMOTE_HOSTNAME
+  	&& docker run --rm -v ${REPO_NAME}_le_config:/etc/letsencrypt -v ${REPO_NAME}_le_webrootpath:/var/www/.le certbot/certbot renew
   	echo "Backup the fullchain cert to gitlab variable"
   	if [ $fullchain_pem_remote_exists == "true" ];then
   	  echo "/usr/bin/curl --show-error --silent --request PUT --write-out 'HTTP Response code: %{http_code}' --url '$CI_API_V4_URL/projects/$encoded_gitlab_project/variables/tls_fullchain_pem?filter%5benvironment_scope%5d=$GIGADB_ENV' --header 'PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN' --form 'environment_scope=$GIGADB_ENV' --form 'value=\$fullchain'"
