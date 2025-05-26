20e4d1fd4d6c51d071bb9af1099d2f94bf8279d6
kencho <kencho.gigascience@gmail.com>
2025-05-26 10:20:45 +0800

    refactor: implement html encoding

--- protected/views/dataset/_meta_tags.php
+++ protected/views/dataset/_meta_tags.php
@@ -12,19 +12,19 @@ if ($isPrivate || $isNonLiveEnv) {
 ?>
 
 <!-- Primary Meta Tags -->
-<meta name="title" content="<?php echo rtrim($this->pageTitle); ?>"/>
-<meta name="description" content="<?php echo $this->metaData['description']; ?>"/>
+<meta name="title" content="<?php echo CHtml::encode(rtrim($this->pageTitle)); ?>"/>
+<meta name="description" content="<?php echo CHtml::encode($this->metaData['description']); ?>"/>
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website"/>
-<meta property="og:url" content="<?php echo $this->metaData['doiUrl']; ?>"/>
-<meta property="og:title" content="<?php echo rtrim($this->pageTitle); ?>"/>
-<meta property="og:description" content="<?php echo $this->metaData['description']; ?>"/>
-<meta property="og:image" content="<?php echo $this->metaData['imageUrl']; ?>"/>
+<meta property="og:url" content="<?php echo CHtml::encode($this->metaData['doiUrl']); ?>"/>
+<meta property="og:title" content="<?php echo CHtml::encode(rtrim($this->pageTitle)); ?>"/>
+<meta property="og:description" content="<?php echo CHtml::encode($this->metaData['description']); ?>"/>
+<meta property="og:image" content="<?php echo CHtml::encode($this->metaData['imageUrl']); ?>"/>
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image"/>
-<meta property="twitter:url" content="<?php echo $this->metaData['doiUrl']; ?>"/>
-<meta property="twitter:title" content="<?php echo rtrim($this->pageTitle) ?>"/>
-<meta property="twitter:description" content="<?php echo $this->metaData['description']; ?>"/>
-<meta property="twitter:image" content="<?php echo $this->metaData['imageUrl']; ?>"/>
+<meta property="twitter:url" content="<?php echo CHtml::encode($this->metaData['doiUrl']); ?>"/>
+<meta property="twitter:title" content="<?php echo CHtml::encode(rtrim($this->pageTitle)); ?>"/>
+<meta property="twitter:description" content="<?php echo CHtml::encode($this->metaData['description']); ?>"/>
+<meta property="twitter:image" content="<?php echo CHtml::encode($this->metaData['imageUrl']); ?>"/>
