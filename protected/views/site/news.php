c0d73231af95b825216c5eb8e610f3daa9d62721
luis-toptal <luis.martinez@toptal.com>
2025-05-27 12:18:43 +0700

    chore: reapply css based line clamp and yii purification to news widget

--- protected/views/site/news.php
+++ protected/views/site/news.php
@@ -1,20 +1,13 @@
 <?php
 $news_slides = array();
-$max_excerpt_length = 200;
 
 foreach ($news as $temp_news) {
-  // Prepare news excerpt
-  $news_body = htmlspecialchars($temp_news->body);
-  $news_excerpt = mb_strlen($news_body) > $max_excerpt_length
-    ? mb_substr($news_body, 0, $max_excerpt_length) . '...'
-    : $news_body;
-
   // Build news slide HTML
   $news_slides[] = sprintf(
     '<h3 class="news-title">%s</h3>
-        <p class="news-body">%s</p>%s',
-    htmlspecialchars($temp_news->title),
-    $news_excerpt,
+    <p class="news-body">%s</p>%s',
+    Yii::app()->controller->widget("CHtmlPurifier")->purify($temp_news->title),
+    Yii::app()->controller->widget("CHtmlPurifier")->purify($temp_news->body),
     CHtml::link(
       "Read More",
       array("news/view", 'id' => $temp_news->id),
