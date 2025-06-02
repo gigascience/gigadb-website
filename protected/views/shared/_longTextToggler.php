c3312d9b212411e8c101468f1644405c3e1829c5
luis-toptal <luis.martinez@toptal.com>
2025-06-02 12:24:00 +0700

    refactor: minimizes impact on performance

--- protected/views/shared/_longTextToggler.php
+++ protected/views/shared/_longTextToggler.php
@@ -1,61 +1,45 @@
 <?php
-    /**
-     * @param string $id Unique identifier
-     * @param string $text Content to display
-     * @param int $maxLines Number of lines to clamp
-     */
-    $height = 1.4 * $maxLines . 'em'; // 1.4 is the line-height
+/**
+ * @param string $id Unique identifier
+ * @param string $text Content to display
+ * @param int $maxLines Number of lines to clamp
+ */
 ?>
 <span class="long-text-toggler-container">
   <div
     id="long-text-<?php echo $id ?>"
     class="long-text-toggler"
-    style="-webkit-line-clamp: <?php echo $maxLines ?>; line-clamp:<?php echo $maxLines ?>; max-height:<?php echo $height ?>;"
+    style="--lines:<?php echo $maxLines ?>"
   >
     <?php echo $text ?>
-  </div><button
-    class="long-text-toggler__toggle btn btn-subtle"
+  </div>
+  <button
+    class="long-text-toggler__toggle btn btn-subtle hidden"
     type="button"
     aria-expanded="false"
     aria-controls="long-text-<?php echo $id ?>"
     aria-label="show more"
     data-target="long-text-<?php echo $id ?>"
-    style="display: none;"
   ><i class="fa fa-caret-down"></i></button>
 </span>
 
 <script>
-$(document).ready(function() {
-  let $text = $('#long-text-<?php echo $id ?>');
-  let $btn = $('.long-text-toggler__toggle[data-target="long-text-<?php echo $id ?>"]');
-  let $container = $text.closest('.long-text-toggler-container');
+// defer operations until fonts are loaded, to determine correct heights
+document.fonts.ready.then(function() {
+  const $text = $('#long-text-<?php echo $id ?>');
+  const $btn = $('.long-text-toggler__toggle[data-target="long-text-<?php echo $id ?>"]');
 
-  function getFullTextHeight() {
-    const originalStyle = $text.attr('style');
-    $text.css({
-        '-webkit-line-clamp': 'unset',
-        'line-clamp': 'unset',
-        'max-height': 'none'
-    });
-    const fullHeight = $text.outerHeight();
-    $text.attr('style', originalStyle);
+  const fullHeight = $text.get(0).scrollHeight;
+  const clampedHeight = $text.get(0).clientHeight;
 
-    return fullHeight;
-  }
-
-  const maxHeight = parseFloat('<?php echo $height ?>') * parseFloat($('body').css('font-size'));
-
-  if (getFullTextHeight() > maxHeight) {
-    $btn.show();
+  if (fullHeight > clampedHeight) {
+    $btn.removeClass('hidden');
 
     $btn.click(function(e) {
-      e.preventDefault();
-      $btn = $(this);
-      $text = $('#' + $btn.data('target'));
-      $container = $text.closest('.long-text-toggler-container');
+      const $container = $text.closest('.long-text-toggler-container');
       const expanded = $container.hasClass('is-expanded');
-
       $container.toggleClass('is-expanded');
+
       $btn.attr('aria-expanded', !expanded)
         .attr('aria-label', expanded ? 'show more' : 'show less')
         .find('i').removeClass('fa-caret-down fa-caret-up')
