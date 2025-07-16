99dd1558ca49d04f35781ccdfd63477a104113e3
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-16 11:53:58 +1000

    codesniffer: rssMessage

--- protected/views/rssMessage/index.php
+++ protected/views/rssMessage/index.php
@@ -1,17 +1,17 @@
 <?php
-$this->breadcrumbs=array(
-	'Rss Messages',
+$this->breadcrumbs = array(
+    'Rss Messages',
 );
 
-$this->menu=array(
-	array('label'=>'Create RssMessage', 'url'=>array('create')),
-	array('label'=>'Manage RssMessage', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'Create RssMessage', 'url' => array('create')),
+    array('label' => 'Manage RssMessage', 'url' => array('admin')),
 );
 ?>
 
 <h1>Rss Messages</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+    'dataProvider' => $dataProvider,
+    'itemView' => '_view',
 )); ?>
