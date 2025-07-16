229d92dad67c43a352a939b9fd56779047f46e9f
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-16 11:14:11 +1000

    codesniffer: AdminImages

--- protected/views/adminImages/admin.php
+++ protected/views/adminImages/admin.php
@@ -1,23 +1,24 @@
-
+<div class='container'>
 <h1>Manage Images</h1>
 <a href="/adminImages/create" class="btn">Create New Image</a>
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'images-grid',
-	'dataProvider'=>$model->search(),
-	'itemsCssClass'=>'table table-bordered',
-	'filter'=>$model,
-	'columns'=>array(
-		'id',
-		'location',
-		'tag',
-		'license',
-		'photographer',
-		/*
-		'source',
-		*/
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
+    'id' => 'images-grid',
+    'dataProvider' => $model->search(),
+    'itemsCssClass' => 'table table-bordered',
+    'filter' => $model,
+    'columns' => array(
+        'id',
+        'location',
+        'tag',
+        'license',
+        'photographer',
+        /*
+        'source',
+        */
+        array(
+            'class' => 'CButtonColumn',
+        ),
+    ),
 )); ?>
+</div>
