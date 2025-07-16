30a7e322c341f7f1ce37d61f95f28ed9462dfa8d
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-16 11:15:33 +1000

    codesniffer: AdminLinks

--- protected/views/adminLink/_form.php
+++ protected/views/adminLink/_form.php
@@ -1,55 +1,55 @@
 <div class="section form row">
 
-	<div class="col-md-offset-3 col-md-6">
-		<?php $form = $this->beginWidget('CActiveForm', array(
-			'id' => 'link-form',
-			'enableAjaxValidation' => false,
-		)); ?>
+    <div class="col-md-offset-3 col-md-6">
+        <?php $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'link-form',
+            'enableAjaxValidation' => false,
+        )); ?>
 
-		<p class="note">Fields with <span class="required">*</span> are required.</p>
+        <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-		<?php if ($model->hasErrors()) : ?>
-			<div class="alert alert-danger">
-				<?php echo $form->errorSummary($model); ?>
-			</div>
-		<?php endif; ?>
+        <?php if ($model->hasErrors()) : ?>
+            <div class="alert alert-danger">
+                <?php echo $form->errorSummary($model); ?>
+            </div>
+        <?php endif; ?>
 
-		<?php
-		$this->widget('application.components.controls.DropdownField', [
-			'form' => $form,
-			'model' => $model,
-			'attributeName' => 'dataset_id',
-			'listDataOptions' => [
-				'data' => Util::getDois(),
-				'valueField' => 'id',
-				'textField' => 'identifier',
-			],
-			'inputOptions' => [
-				'required' => true,
-			],
-		]);
-		$this->widget('application.components.controls.CheckBoxField', [
-			'form' => $form,
-			'model' => $model,
-			'attributeName' => 'is_primary',
-		]);
-		$this->widget('application.components.controls.TextField', [
-			'form' => $form,
-			'model' => $model,
-			'attributeName' => 'link',
-			'inputOptions' => [
-				'required' => true,
-				'maxlength' => 100
-			],
-		]);
-		?>
+        <?php
+        $this->widget('application.components.controls.DropdownField', [
+            'form' => $form,
+            'model' => $model,
+            'attributeName' => 'dataset_id',
+            'listDataOptions' => [
+                'data' => Util::getDois(),
+                'valueField' => 'id',
+                'textField' => 'identifier',
+            ],
+            'inputOptions' => [
+                'required' => true,
+            ],
+        ]);
+        $this->widget('application.components.controls.CheckBoxField', [
+            'form' => $form,
+            'model' => $model,
+            'attributeName' => 'is_primary',
+        ]);
+        $this->widget('application.components.controls.TextField', [
+            'form' => $form,
+            'model' => $model,
+            'attributeName' => 'link',
+            'inputOptions' => [
+                'required' => true,
+                'maxlength' => 100
+            ],
+        ]);
+        ?>
 
-		<div class="pull-right btns-row">
-			<a href="/adminLink/admin" class="btn background-btn-o">Cancel</a>
-			<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
-		</div>
+        <div class="pull-right btns-row">
+            <a href="/adminLink/admin" class="btn background-btn-o">Cancel</a>
+            <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
+        </div>
 
-		<?php $this->endWidget(); ?>
-	</div>
+        <?php $this->endWidget(); ?>
+    </div>
 
 </div>
--- protected/views/adminLink/index.php
+++ protected/views/adminLink/index.php
@@ -1,17 +1,17 @@
 <?php
-$this->breadcrumbs=array(
-	'Links',
+$this->breadcrumbs = array(
+    'Links',
 );
 
-$this->menu=array(
-	array('label'=>'Create Link', 'url'=>array('create')),
-	array('label'=>'Manage Link', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'Create Link', 'url' => array('create')),
+    array('label' => 'Manage Link', 'url' => array('admin')),
 );
 ?>
 
 <h1>Links</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+    'dataProvider' => $dataProvider,
+    'itemView' => '_view',
 )); ?>
