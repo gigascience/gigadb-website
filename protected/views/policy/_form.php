e2086f6482d69b2886efca36c036abfbf344a6a9
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-16 11:51:32 +1000

    codesniffer: policy

--- protected/views/policy/_form.php
+++ protected/views/policy/_form.php
@@ -1,30 +1,30 @@
 <div class="row">
     <div class="span10 offset1 form well">
-        <?php $form=$this->beginWidget('CActiveForm', array(
-		'id'=>'policy-form',
-		'enableAjaxValidation'=>false,
-		'htmlOptions'=>array('class'=>'form-horizontal',
-							 'enctype'=>'multipart/form-data')
-	)); ?>
+        <?php $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'policy-form',
+        'enableAjaxValidation' => false,
+        'htmlOptions' => array('class' => 'form-horizontal',
+                             'enctype' => 'multipart/form-data')
+    )); ?>
         <p class="note">Fields with <span class="required">*</span> are required.</p>
         <?php echo $form->errorSummary($model); ?>
         <div class="control-group">
-            <?php echo $form->labelEx($model,'dataset_id',array('class'=>'control-label')); ?>
+            <?php echo $form->labelEx($model, 'dataset_id', array('class' => 'control-label')); ?>
             <div class="controls">
-                <?= CHtml::activeDropDownList($model,'dataset_id',CHtml::listData(Dataset::model()->findAll(),'id','identifier')); ?>
-                    <?php echo $form->error($model,'dataset_id'); ?>
+                <?= CHtml::activeDropDownList($model, 'dataset_id', CHtml::listData(Dataset::model()->findAll(), 'id', 'identifier')); ?>
+                    <?php echo $form->error($model, 'dataset_id'); ?>
             </div>
         </div>
         <div class="control-group">
-            <?php echo $form->labelEx($model,'until_date',array('class'=>'control-label')); ?>
+            <?php echo $form->labelEx($model, 'until_date', array('class' => 'control-label')); ?>
             <div class="controls">
-                <?php echo $form->textField($model, 'until_date', array('class'=>'date')); ?>
-                <?php echo $form->error($model,'until_date'); ?>
+                <?php echo $form->textField($model, 'until_date', array('class' => 'date')); ?>
+                <?php echo $form->error($model, 'until_date'); ?>
             </div>
         </div>
         <div class="pull-right">
             <a href="/site/admin" class="btn">Cancel</a>
-            <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save',array('class'=>'btn')); ?>
+            <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn')); ?>
         </div>
         <?php $this->endWidget(); ?>
     </div>
--- protected/views/policy/create.php
+++ protected/views/policy/create.php
@@ -4,4 +4,4 @@
 [<?= CHtml::link('Fairly Use Policy', array('admin')) ?>]
 </div>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model, 'image'=>$image)); ?>
+<?php echo $this->renderPartial('_form', array('model' => $model, 'image' => $image)); ?>
