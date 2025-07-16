37abe555f725aff40d40ad9955eecd80b2711300
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-16 10:56:54 +1000

    codesniffer: AdminDatasetSample

--- protected/views/adminDatasetSample/admin.php
+++ protected/views/adminDatasetSample/admin.php
@@ -1,34 +1,34 @@
 <div id="adminDatasetContainer" class="container">
-	<?php
-	$this->widget('TitleBreadcrumb', [
-		'pageTitle' => 'Manage Dataset - Samples',
-		'breadcrumbItems' => [
-			['label' => 'Admin', 'href' => '/site/admin'],
-			['isActive' => true, 'label' => 'Manage'],
-		]
-	]);
-	?>
-	<div class="mb-10">
-	  <a href="/adminDatasetSample/create" class="btn background-btn">Add a Sample to a Dataset</a>
+    <?php
+    $this->widget('TitleBreadcrumb', [
+        'pageTitle' => 'Manage Dataset - Samples',
+        'breadcrumbItems' => [
+            ['label' => 'Admin', 'href' => '/site/admin'],
+            ['isActive' => true, 'label' => 'Manage'],
+        ]
+    ]);
+    ?>
+    <div class="mb-10">
+      <a href="/adminDatasetSample/create" class="btn background-btn">Add a Sample to a Dataset</a>
   </div>
 
-	<p>
-		Column headers with links are sortable. Cells with a text input are used for filtering.
-	</p>
+    <p>
+        Column headers with links are sortable. Cells with a text input are used for filtering.
+    </p>
 
-	<?php $this->widget('CustomGridView', array(
+    <?php $this->widget('CustomGridView', array(
     'id' => 'dataset-sample-grid',
-		'dataProvider' => $model->search(),
-		'itemsCssClass' => 'table table-bordered',
-		'filter' => $model,
-		'columns' => array(
-			array('name' => 'doi_search', 'value' => '$data->dataset->identifier'),
-			'sample_id',
-			array('name' => 'sample_name', 'value' => '$data->sample->name'),
-			array('header' => 'Sample Attributes', 'type' => 'raw', 'value' => 'FormattedDatasetSamples::getDisplayAttr($data->sample->id,$data->sample->getSampleAttributeArrayMap())'),
-			CustomGridView::getDefaultActionButtonsConfig()
-		),
-	)); ?>
+        'dataProvider' => $model->search(),
+        'itemsCssClass' => 'table table-bordered',
+        'filter' => $model,
+        'columns' => array(
+            array('name' => 'doi_search', 'value' => '$data->dataset->identifier'),
+            'sample_id',
+            array('name' => 'sample_name', 'value' => '$data->sample->name'),
+            array('header' => 'Sample Attributes', 'type' => 'raw', 'value' => 'FormattedDatasetSamples::getDisplayAttr($data->sample->id,$data->sample->getSampleAttributeArrayMap())'),
+            CustomGridView::getDefaultActionButtonsConfig()
+        ),
+    )); ?>
 </div>
 
 <script>
