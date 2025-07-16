e5e647b33f2530a8b5adaf0024c8628ead193766
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-16 11:28:42 +1000

    codesniffer: AdminProject

--- protected/views/adminProject/admin.php
+++ protected/views/adminProject/admin.php
@@ -1,31 +1,31 @@
 <div class="container">
-	<?php
-	$this->widget('TitleBreadcrumb', [
-		'pageTitle' => 'Manage Projects',
-		'breadcrumbItems' => [
-			['label' => 'Admin', 'href' => '/site/admin'],
-			['isActive' => true, 'label' => 'Manage'],
-		]
-	]);
-	?>
-	<div class="mb-10">
-		<a href="/adminProject/create" class="btn background-btn">Create New Project</a>
-	</div>
-	<p>
-		Column headers with links are sortable. Cells with a text input are used for filtering.
-	</p>
+    <?php
+    $this->widget('TitleBreadcrumb', [
+        'pageTitle' => 'Manage Projects',
+        'breadcrumbItems' => [
+            ['label' => 'Admin', 'href' => '/site/admin'],
+            ['isActive' => true, 'label' => 'Manage'],
+        ]
+    ]);
+    ?>
+    <div class="mb-10">
+        <a href="/adminProject/create" class="btn background-btn">Create New Project</a>
+    </div>
+    <p>
+        Column headers with links are sortable. Cells with a text input are used for filtering.
+    </p>
 
-	<?php $this->widget('CustomGridView', array(
-		'id' => 'project-grid',
-		'dataProvider' => $model->search(),
-		'itemsCssClass' => 'table table-bordered',
-		'filter' => $model,
-		'columns' => array(
-			'url',
-			'name',
-			'image_location',
-			CustomGridView::getDefaultActionButtonsConfig()
-		),
-	)); ?>
+    <?php $this->widget('CustomGridView', array(
+        'id' => 'project-grid',
+        'dataProvider' => $model->search(),
+        'itemsCssClass' => 'table table-bordered',
+        'filter' => $model,
+        'columns' => array(
+            'url',
+            'name',
+            'image_location',
+            CustomGridView::getDefaultActionButtonsConfig()
+        ),
+    )); ?>
 
 </div>
