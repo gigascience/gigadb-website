9f9bd15aadddf0ad258d5fe1a33a8d553b280d84
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-16 11:04:41 +1000

    codesniffer: AdminFileType

--- protected/views/adminFileType/admin.php
+++ protected/views/adminFileType/admin.php
@@ -1,34 +1,34 @@
 <div class="container">
-	<?php
-	$this->widget('TitleBreadcrumb', [
-		'pageTitle' => 'Manage Data Types',
-		'breadcrumbItems' => [
-			['label' => 'Admin', 'href' => '/site/admin'],
-			['isActive' => true, 'label' => 'Manage'],
-		]
-	]);
-	?>
-	<p>
-		To list certain news items that you are looking for, you may search via keyword or value. Type your keyword or value into their respective boxes under the column headers and press the enter key. You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-		or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-	</p>
-	<div class="mb-10">
-		<a href="/adminFileType/create" class="btn background-btn">Create A New Data Type</a>
-	</div>
-	<p>
-		Column headers with links are sortable. Cells with a text input are used for filtering.
-	</p>
+    <?php
+    $this->widget('TitleBreadcrumb', [
+        'pageTitle' => 'Manage Data Types',
+        'breadcrumbItems' => [
+            ['label' => 'Admin', 'href' => '/site/admin'],
+            ['isActive' => true, 'label' => 'Manage'],
+        ]
+    ]);
+    ?>
+    <p>
+        To list certain news items that you are looking for, you may search via keyword or value. Type your keyword or value into their respective boxes under the column headers and press the enter key. You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+        or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+    </p>
+    <div class="mb-10">
+        <a href="/adminFileType/create" class="btn background-btn">Create A New Data Type</a>
+    </div>
+    <p>
+        Column headers with links are sortable. Cells with a text input are used for filtering.
+    </p>
 
-	<?php $this->widget('CustomGridView', array(
-		'id' => 'file-type-grid',
-		'dataProvider' => $model->search(),
-		'filter' => $model,
-		'itemsCssClass' => 'table table-bordered',
-		'columns' => array(
-			'name',
-			'description',
-			CustomGridView::getDefaultActionButtonsConfig()
-		),
-	)); ?>
+    <?php $this->widget('CustomGridView', array(
+        'id' => 'file-type-grid',
+        'dataProvider' => $model->search(),
+        'filter' => $model,
+        'itemsCssClass' => 'table table-bordered',
+        'columns' => array(
+            'name',
+            'description',
+            CustomGridView::getDefaultActionButtonsConfig()
+        ),
+    )); ?>
 
 </div>
--- protected/views/adminFileType/index.php
+++ protected/views/adminFileType/index.php
@@ -1,17 +1,17 @@
 <?php
-$this->breadcrumbs=array(
-	'File Types',
+$this->breadcrumbs = array(
+    'File Types',
 );
 
-$this->menu=array(
-	array('label'=>'Create FileType', 'url'=>array('create')),
-	array('label'=>'Manage FileType', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'Create FileType', 'url' => array('create')),
+    array('label' => 'Manage FileType', 'url' => array('admin')),
 );
 ?>
 
 <h1>File Types</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+    'dataProvider' => $dataProvider,
+    'itemView' => '_view',
 )); ?>
