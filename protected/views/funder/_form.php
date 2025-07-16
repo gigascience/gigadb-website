cfdd80b9cb29fded82211daebc1dc07842fe4eef
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-16 11:49:52 +1000

    codesniffer: funder

--- protected/views/funder/_form.php
+++ protected/views/funder/_form.php
@@ -1,47 +1,47 @@
 <div class="section form row">
 
-	<div class="col-md-offset-3 col-md-6">
-		<?php $form = $this->beginWidget('CActiveForm', array(
-			'id' => 'funder-form',
-			'enableAjaxValidation' => false,
-		)); ?>
+    <div class="col-md-offset-3 col-md-6">
+        <?php $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'funder-form',
+            'enableAjaxValidation' => false,
+        )); ?>
 
-		<p class="note">Fields with <span class="required">*</span> are required.</p>
+        <p class="note">Fields with <span class="required">*</span> are required.</p>
 
-		<?php if ($model->hasErrors()) : ?>
-			<div class="alert alert-danger">
-				<?php echo $form->errorSummary($model); ?>
-			</div>
-		<?php endif; ?>
+        <?php if ($model->hasErrors()) : ?>
+            <div class="alert alert-danger">
+                <?php echo $form->errorSummary($model); ?>
+            </div>
+        <?php endif; ?>
 
-		<?php
-		$this->widget('application.components.controls.TextField', [
-			'form' => $form,
-			'model' => $model,
-			'attributeName' => 'uri',
-			'inputOptions' => [
-				'required' => true,
-			],
-		]);
-		$this->widget('application.components.controls.TextField', [
-			'form' => $form,
-			'model' => $model,
-			'attributeName' => 'primary_name_display',
+        <?php
+        $this->widget('application.components.controls.TextField', [
+            'form' => $form,
+            'model' => $model,
+            'attributeName' => 'uri',
+            'inputOptions' => [
+                'required' => true,
+            ],
+        ]);
+        $this->widget('application.components.controls.TextField', [
+            'form' => $form,
+            'model' => $model,
+            'attributeName' => 'primary_name_display',
 
-		]);
-		$this->widget('application.components.controls.TextField', [
-			'form' => $form,
-			'model' => $model,
-			'attributeName' => 'country',
-		]);
-		?>
+        ]);
+        $this->widget('application.components.controls.TextField', [
+            'form' => $form,
+            'model' => $model,
+            'attributeName' => 'country',
+        ]);
+        ?>
 
-		<div class="pull-right btns-row">
-			<a href="/funder/admin" class="btn background-btn-o">Cancel</a>
-			<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
-		</div>
+        <div class="pull-right btns-row">
+            <a href="/funder/admin" class="btn background-btn-o">Cancel</a>
+            <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array('class' => 'btn background-btn')); ?>
+        </div>
 
-		<?php $this->endWidget(); ?>
-	</div>
+        <?php $this->endWidget(); ?>
+    </div>
 
 </div>
--- protected/views/funder/index.php
+++ protected/views/funder/index.php
@@ -1,17 +1,17 @@
 <?php
-$this->breadcrumbs=array(
-	'Funders',
+$this->breadcrumbs = array(
+    'Funders',
 );
 
-$this->menu=array(
-	array('label'=>'Create Funder', 'url'=>array('create')),
-	array('label'=>'Manage Funder', 'url'=>array('admin')),
+$this->menu = array(
+    array('label' => 'Create Funder', 'url' => array('create')),
+    array('label' => 'Manage Funder', 'url' => array('admin')),
 );
 ?>
 
 <h1>Funders</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
-	'dataProvider'=>$dataProvider,
-	'itemView'=>'_view',
+    'dataProvider' => $dataProvider,
+    'itemView' => '_view',
 )); ?>
