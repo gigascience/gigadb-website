407a056a1dc519736975fc1ed8bfa8ccbb1353f7
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-21 10:53:13 +1000

    fix unit test: we are supposed to have all relationships in db

--- tests/functional/RelationDAOCest.php
+++ tests/functional/RelationDAOCest.php
@@ -32,32 +32,6 @@ class RelationDAOCest
         $I->canSee('View Relation');
     }
 
-    public function tryToCreateRelationWithReciprocalRelationAndFail(FunctionalTester $I)
-    {
-        $I->amOnPage('/site/login');
-        $I->submitForm('form.form-horizontal', [
-                'LoginForm[username]' => 'admin@gigadb.org',
-                'LoginForm[password]' => 'gigadb'
-            ]
-        );
-
-        $I->canSee('Admin');
-        $I->click('Admin');
-        $I->click('Dataset:Relations');
-        $I->click('Create A New Relation');
-        $I->canSee('Create Relation');
-        $I->selectOption('select#Relation_dataset_id', '5');
-        $I->selectOption('select#Relation_related_doi', '100006');
-        $I->selectOption('select#Relation_relationship_id', '13');
-        $I->click('Create');
-
-        $I->dontSeeInDatabase('relation',
-            ['dataset_id' => 5, 'related_doi' => '100006', 'relationship_id' => 13]
-        );
-
-        $I->canSee('Failed as it was unable to save the reciprocal relation');
-    }
-
     public function tryToCreateRelationWithReciprocalRelation(FunctionalTester $I)
     {
         $I->amOnPage('/site/login');
