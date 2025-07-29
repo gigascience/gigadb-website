a91e133617bcccf2ab99eb55e7b58665628b0715
Allison Guilhem <allison.guilhem@gmail.com>
2025-07-29 18:38:22 +0200

    move db migration into schema directory

--- protected/migrations/schema/m250506_041933_update_gigadb_user_token.php
+++ protected/migrations/schema/m250506_041933_update_gigadb_user_token.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 class m250506_041933_update_gigadb_user_token extends CDbMigration
 {
-    public function safeUp() {
+    public function safeUp()
+    {
         $this->addColumn('gigadb_user', 'activation_token', 'VARCHAR(250) UNIQUE');
     }
 
-    public function safeDown() {
+    public function safeDown()
+    {
         $this->dropColumn('gigadb_user', 'activation_token');
     }
 }
